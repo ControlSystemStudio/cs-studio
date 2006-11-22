95e2ed492a1f68a3d965656f9735ea62e2f58b90
shofer <shofer>
2006-11-22 17:08:38 +0000

    restructored platform.ui help contents

--- core/plugins/org.csstudio.platform.ui/src/org/csstudio/platform/ui/internal/logging/FileAppenderPreferencePage.java
+++ core/plugins/org.csstudio.platform.ui/src/org/csstudio/platform/ui/internal/logging/FileAppenderPreferencePage.java
@@ -24,7 +24,6 @@ package org.csstudio.platform.ui.internal.logging;
 import org.csstudio.platform.logging.CentralLogger;
 import org.csstudio.platform.ui.internal.localization.Messages;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
-import org.eclipse.jface.preference.FileFieldEditor;
 import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.jface.preference.RadioGroupFieldEditor;
 import org.eclipse.jface.preference.StringFieldEditor;
@@ -57,7 +56,7 @@ public class FileAppenderPreferencePage extends AbstractAppenderPreferencePage {
 		addField(new StringFieldEditor(CentralLogger.PROP_LOG4J_FILE_PATTERN,
 				Messages.getString("FileAppenderPreferencePage.PATTERN"), getFieldEditorParent())); //$NON-NLS-1$
 
-		addField(new FileFieldEditor(
+		addField(new StringFieldEditor(
 				CentralLogger.PROP_LOG4J_FILE_DESTINATION, Messages.getString("FileAppenderPreferencePage.LOG_FILE"), //$NON-NLS-1$
 				getFieldEditorParent()));
 
