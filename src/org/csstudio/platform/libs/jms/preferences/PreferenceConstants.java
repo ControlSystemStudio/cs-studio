a8e81fd621080abdbb4526cd1b39cbd209556de7
swende <swende>
2006-11-29 10:02:22 +0000

    Library integration.

---
+++ src/org/csstudio/platform/libs/jms/preferences/PreferenceConstants.java
@@ -0,0 +1,10 @@
+package org.csstudio.platform.libs.jms.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+	public static final String INITIAL_CONTEXT_FACTORY = "context factory";
+	public static final String URL = "provider url";
+	public static final String QUEUE = "message queue";
+}
