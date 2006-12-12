43305d0bbd1597b59e17c322fe5d3ebc9780c8ae
Kay Kasemir <kasemirk@ornl.gov>
2006-12-12 17:53:18 +0000

    Added the layout to the perspective.

--- applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/Perspective.java
+++ applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/Perspective.java
@@ -1,5 +1,10 @@
 package org.csstudio.trends.databrowser;
 
+import org.csstudio.trends.databrowser.archiveview.ArchiveView;
+import org.csstudio.trends.databrowser.configview.ConfigView;
+import org.csstudio.trends.databrowser.exportview.ExportView;
+import org.csstudio.trends.databrowser.sampleview.SampleView;
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -8,7 +13,26 @@ import org.eclipse.ui.IPerspectiveFactory;
  */
 public class Perspective implements IPerspectiveFactory
 {
+    @SuppressWarnings("nls")
     public void createInitialLayout(IPageLayout layout)
     {
+        // left | editor
+        //      |
+        //      |
+        //      +-------------
+        //      | bottom
+        String editor = layout.getEditorArea();
+        IFolderLayout left = layout.createFolder("left",
+                        IPageLayout.LEFT, 0.25f, editor);
+        IFolderLayout bottom = layout.createFolder("bottom",
+                        IPageLayout.BOTTOM, 0.66f, editor);
+        // Stuff for 'left'
+        left.addView(ArchiveView.ID);
+        left.addView(IPageLayout.ID_RES_NAV);
+        // Stuff for 'bottom'
+        bottom.addView(ConfigView.ID);
+        bottom.addPlaceholder(SampleView.ID);
+        bottom.addView(ExportView.ID);
+        bottom.addPlaceholder(IPageLayout.ID_PROGRESS_VIEW);
     }
 }
