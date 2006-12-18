cb621042e31a97851d8bffe70f16ba3877ecb063
Kay Kasemir <kasemirk@ornl.gov>
2006-12-18 22:09:20 +0000

    Base on CSS Plugin base, log JCA type

--- applications/plugins/org.csstudio.utility.pv/src/org/csstudio/utility/pv/Plugin.java
+++ applications/plugins/org.csstudio.utility.pv/src/org/csstudio/utility/pv/Plugin.java
@@ -1,21 +1,35 @@
 package org.csstudio.utility.pv;
 
+import org.csstudio.platform.AbstractCssPlugin;
 import org.csstudio.platform.libs.epics.EpicsPlugin;
 import org.csstudio.utility.pv.epics.EPICS_V3_PV;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.osgi.framework.BundleContext;
 
 /** Plugin-activator for the PV.
  *  @author Kay Kasemir
  */
-public class Plugin extends org.eclipse.core.runtime.Plugin
+public class Plugin extends AbstractCssPlugin
 {
-    /** Determine which EPICS CA context type to use from preferences.
-     *  @see org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
-     */
+	public final static String ID = "org.csstudio.utility.pv";
+	
+	public String getPluginId()
+	{   return ID; }
+
+    /** @see AbstractCssPlugin */
     @Override
-    public void start(BundleContext context) throws Exception
+    protected void doStart(BundleContext context) throws Exception
     {
-        super.start(context);
+        EPICS_V3_PV.use_pure_java = EpicsPlugin.getDefault().usePureJava();
+        final String message = EPICS_V3_PV.use_pure_java ?
+		        				"Using pure java CAJ" : "Using JCA with JNI";
+		getLog().log(new Status(IStatus.INFO, ID, IStatus.OK, message, null));
+    }
+    /** @see AbstractCssPlugin */
+    @Override
+    protected void doStop(BundleContext context) throws Exception
+    {
         EPICS_V3_PV.use_pure_java = EpicsPlugin.getDefault().usePureJava();
     }
 }
