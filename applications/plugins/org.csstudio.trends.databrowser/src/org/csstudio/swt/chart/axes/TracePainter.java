1f668651733719a2764ba1ccac5ed70eb1225655
Kay Kasemir <kasemirk@ornl.gov>
2007-02-12 18:33:22 +0000

    Trace painter uses 'steps' between samples. Comment on archive order.

--- applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/swt/chart/axes/TracePainter.java
+++ applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/swt/chart/axes/TracePainter.java
@@ -6,6 +6,11 @@ import org.csstudio.swt.chart.Trace;
 import org.eclipse.swt.graphics.GC;
 
 /** Paints the samples of one trace.
+ *  <p>
+ *  Specifically, it draws a 'staircase' type line
+ *  from one sample to the next:
+ *  The 'y' value of a previous sample is considered valid
+ *  until the next sample.
  *  @author Kay Kasemir
  */
 public class TracePainter
@@ -21,7 +26,7 @@ public class TracePainter
         gc.setForeground(trace.getColor());
         gc.setBackground(trace.getColor());
         gc.setLineWidth(trace.getLineWidth());
-        boolean need_x0y0 = true;
+        boolean new_line = true;
         YAxis yaxis = trace.getYAxis();
         ChartSampleSequence samples = trace.getSampleSequence();
         // ** Lock the samples, so they don't change on us! **
@@ -36,30 +41,31 @@ public class TracePainter
             {
                 ChartSample sample = samples.get(i);
                 double y = sample.getY();
-                boolean no_line = Double.isInfinite(y) || Double.isNaN(y);
-                if (need_x0y0 || no_line)
-                {   // Remember coordinates
+                boolean plottable = !Double.isInfinite(y) && !Double.isNaN(y);
+                if (new_line)
+                {   // Sample starts a new line. Don't have a previous
+                    // sample from which to draw a staircase connection.
+                    // Remember coordinates
                     x0 = xaxis.getScreenCoord(sample.getX());
                     y0 = yaxis.getScreenCoord(y);
+                    if (plottable)
+                        gc.drawPoint(x0, y0);
                     // If we skip a line to/from this point,
                     // we'll still need another x0/y0.
                     // Otherwise, we are now ready to draw a line to the
                     // next sample.
-                    need_x0y0 = no_line;
+                    new_line = ! plottable;
                 }
                 else
                 {   // line from last to current point
                     x1 = xaxis.getScreenCoord(sample.getX());
                     y1 = yaxis.getScreenCoord(y);
-                    if (false)
-                    { // Simple line to the next sample
-                        gc.drawLine(x0, y0, x1, y1);
-                    }
-                    else
-                    {   // Staircase to the next sample
-                        gc.drawLine(x0, y0, x1, y0);
+                    // Staircase: Line from the last sample...
+                    gc.drawLine(x0, y0, x1, y0);
+                    if (plottable) // to this one
                         gc.drawLine(x1, y0, x1, y1);
-                    }
+                    else
+                        new_line = true; // or end current line, start new one
                     x0 = x1;
                     y0 = y1;
                 }
