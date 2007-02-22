ef06a36f3b19906769ac6bff06de157615e515dd
avodovni <avodovni>
2007-02-22 16:27:56 +0000

    Added a startup services extension point listener, and the implementation to startup any of the services (while separating high and low priority).

---
+++ core/plugins/org.csstudio.startup/src/org/csstudio/startup/ServiceProxy.java
@@ -0,0 +1,78 @@
+package org.csstudio.startup;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+
+public class ServiceProxy implements IStartupServiceListener{
+
+	private static final String ATT_CLASS = "class";
+	private static final String ATT_ID = "id";
+	private static final String ATT_NAME = "name";
+	private static final String ATT_PRIORITY = "isHighPriority";
+	
+	private final IConfigurationElement _configElement;
+	private IStartupServiceListener _serviceListener;
+	private final String _id;
+	private final String _name;
+	private final boolean _isHighPriority;
+	
+	public ServiceProxy(IConfigurationElement configElement, int orindal) {
+		this._configElement = configElement;
+		// ensure that the attribute is there
+		getAttribute(configElement, ATT_CLASS, null);
+		_id = getAttribute(configElement, ATT_ID, null);
+		_name = getAttribute(configElement, ATT_NAME, _id);
+		_isHighPriority = Boolean.parseBoolean(getAttribute(configElement, ATT_PRIORITY, "false"));
+	}
+	
+	private String getAttribute(
+			IConfigurationElement configElem,
+			String name,
+			String defaultValue) {
+		// get the value from the configuration element
+		String value = configElem.getAttribute(name);
+		// is value not null
+		if(value != null)
+			// ok, return that value
+			return value;
+		// it was null, do we have a default value?
+		if(defaultValue != null)
+			// return the default value
+			return defaultValue;
+		// we don't have any possible values, throw an exception
+		throw new IllegalArgumentException("Missing " + name + " attribute!");
+	}
+	
+	public void run() {
+		if(_serviceListener == null)
+			getServiceListener();
+		// if there was an error getting the action,
+		// there will be a null pointer exception here
+		// TODO: think about how to avoid this exception
+		_serviceListener.run();
+	}
+
+	private void getServiceListener() {
+		try {
+			this._serviceListener =
+				(IStartupServiceListener) 
+				this._configElement.createExecutableExtension(ATT_CLASS);
+		} catch (CoreException e) {
+			e.printStackTrace();
+			_serviceListener = null;
+		}
+	}
+	
+	public String getId() {
+		return this._id;
+	}
+	
+	public String getName() {
+		return this._name;
+	}
+	
+	public boolean isHighPriority() {
+		return this._isHighPriority;
+	}
+
+}
--- core/plugins/org.csstudio.startup/src/org/csstudio/startup/applications/Application.java
+++ core/plugins/org.csstudio.startup/src/org/csstudio/startup/applications/Application.java
@@ -21,14 +21,14 @@
  */
 package org.csstudio.startup.applications;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.csstudio.platform.CSSPlatformPlugin;
 import org.csstudio.platform.LocaleService;
-import org.csstudio.platform.security.ExecutionService;
-import org.csstudio.platform.ui.dialogs.LoginDialog;
-import org.csstudio.platform.ui.workbench.CssWorkbenchAdvisor;
+import org.csstudio.startup.ServiceProxy;
+import org.csstudio.startup.StartupServiceEnumerator;
 import org.eclipse.core.runtime.IPlatformRunnable;
 import org.eclipse.core.runtime.preferences.InstanceScope;
-import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
@@ -55,32 +55,53 @@ public class Application implements IPlatformRunnable {
 		Display display = PlatformUI.createDisplay();
 		
 		try {
-			boolean canLogin = handleLogin(display, coreStore);
-
-			int returnCode = EXIT_OK;
-
-			if (canLogin) {
-				// create the workbench with this advisor and run it until it
-				// exits
-				// N.B. createWorkbench remembers the advisor, and also
-				// registers
-				// the workbench globally so that all UI plug-ins can find it
-				// using
-				// PlatformUI.getWorkbench() or AbstractUIPlugin.getWorkbench()
-				returnCode = PlatformUI.createAndRunWorkbench(display,
-						new CssWorkbenchAdvisor());
-
-				// the workbench doesn't support relaunch yet (bug 61809) so
-				// for now restart is used, and exit data properties are checked
-				// here to substitute in the relaunch return code if needed
-				if (returnCode != PlatformUI.RETURN_RESTART) {
-					return EXIT_OK;
-				}
-
-				// if the exit code property has been set to the relaunch code,
-				// then
-				// return that code now, otherwise this is a normal restart
-				return EXIT_RESTART;
+//			boolean canLogin = handleLogin(display, coreStore);
+//
+//			int returnCode = EXIT_OK;
+//
+//			if (canLogin) {
+//				// create the workbench with this advisor and run it until it
+//				// exits
+//				// N.B. createWorkbench remembers the advisor, and also
+//				// registers
+//				// the workbench globally so that all UI plug-ins can find it
+//				// using
+//				// PlatformUI.getWorkbench() or AbstractUIPlugin.getWorkbench()
+//				returnCode = PlatformUI.createAndRunWorkbench(display,
+//						new CssWorkbenchAdvisor());
+//
+//				// the workbench doesn't support relaunch yet (bug 61809) so
+//				// for now restart is used, and exit data properties are checked
+//				// here to substitute in the relaunch return code if needed
+//				if (returnCode != PlatformUI.RETURN_RESTART) {
+//					return EXIT_OK;
+//				}
+//
+//				// if the exit code property has been set to the relaunch code,
+//				// then
+//				// return that code now, otherwise this is a normal restart
+//				return EXIT_RESTART;
+//			}
+			
+			// we will run all the services
+			// TODO: implement checking if there is a service the user
+			// does not wish to run
+			ServiceProxy[] proxies = StartupServiceEnumerator.getServices();
+			List<ServiceProxy> lowPriorityProxy = new ArrayList<ServiceProxy>();
+			
+			
+			for(ServiceProxy proxy : proxies) {
+				if(proxy.isHighPriority())
+					proxy.run();
+				else
+					lowPriorityProxy.add(proxy);
+			}
+			
+			// TODO: implement this so that each low priority proxy
+			// is created and ran in a separate thread
+			for(ServiceProxy proxy : lowPriorityProxy) {
+				// TODO: add thread creation code here!
+				proxy.run();
 			}
 
 			return EXIT_OK;
@@ -89,34 +110,6 @@ public class Application implements IPlatformRunnable {
 				display.dispose();
 			}
 		}
-	}
-
-	/**
-	 * Open the platform's login dialog if the according system property is set.
-	 * 
-	 * @param display
-	 *            The standard display.
-	 * @param coreStore
-	 *            The core preference store.
-	 * @return true, if the authentication succeeded.
-	 */
-	private boolean handleLogin(final Display display,
-			final IPreferenceStore coreStore) {
-		boolean result = true;
-
-		boolean performAuthentication = coreStore
-				.getBoolean(ExecutionService.PROP_AUTH_LOGIN);
-
-		if (performAuthentication) {
-			LoginDialog d = new LoginDialog(display.getActiveShell());
-			int ret = d.open();
-
-			if (ret == Dialog.CANCEL) {
-				result = false;
-			}
-		}
-
-		return result;
 	}
 
 	/**
