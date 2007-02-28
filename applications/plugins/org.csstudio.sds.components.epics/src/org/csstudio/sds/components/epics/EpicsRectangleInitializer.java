2ef436f7c4f09bd140a3e5648400dd3637e8c8cc
shofer <shofer>
2007-02-28 12:13:48 +0000

    added dialog for alias value modification that is displayed when widgets are created

--- applications/plugins/org.csstudio.sds.components.epics/src/org/csstudio/sds/components/epics/EpicsRectangleInitializer.java
+++ applications/plugins/org.csstudio.sds.components.epics/src/org/csstudio/sds/components/epics/EpicsRectangleInitializer.java
@@ -31,6 +31,18 @@ public class EpicsRectangleInitializer implements IWidgetModelInitializer {
 		
 		AliasDescriptor aliasDescriptor = new AliasDescriptor("record", "", "Enter the record name into the 'value' column.");  //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
 		model.addAliasDescriptor(aliasDescriptor);
+
+		AliasDescriptor aliasDescriptor2 = new AliasDescriptor("record2", "", "Enter the record name into the 'value' column.");  //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
+		model.addAliasDescriptor(aliasDescriptor2);
+		
+		AliasDescriptor aliasDescriptor3 = new AliasDescriptor("record3", "", "Enter the record name into the 'value' column.");  //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
+		model.addAliasDescriptor(aliasDescriptor3);
+		
+		AliasDescriptor aliasDescriptor4 = new AliasDescriptor("record4", "", "Enter the record name into the 'value' column.");  //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
+		model.addAliasDescriptor(aliasDescriptor4);
+		
+		AliasDescriptor aliasDescriptor5 = new AliasDescriptor("record5", "", "Enter the record name into the 'value' column.");  //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
+		model.addAliasDescriptor(aliasDescriptor5);
 		
 		final DynamicsDescriptor dynamicsDescriptor = new DynamicsDescriptor(DirectConnectionRule.TYPE_ID);
 		dynamicsDescriptor.addInputChannel(new ParameterDescriptor("$record$_calc", Double.class)); //$NON-NLS-1$
