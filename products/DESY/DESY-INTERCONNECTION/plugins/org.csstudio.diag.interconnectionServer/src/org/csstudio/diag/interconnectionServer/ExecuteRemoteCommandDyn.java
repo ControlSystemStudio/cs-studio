e67ac2b01e172d5a81ba3b3ebadceb48508fd053
Jan Hatje <jan.hatje@desy.de>
2007-03-01 10:59:28 +0000

    add dcf action with dynamic parameters

---
+++ products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/ExecuteRemoteCommandDyn.java
@@ -0,0 +1,47 @@
+package org.csstudio.diag.interconnectionServer;
+
+import java.util.Map;
+
+import org.csstudio.diag.interconnectionServer.server.PreferenceProperties;
+import org.csstudio.platform.libs.dcf.actions.IAction;
+
+public class ExecuteRemoteCommandDyn implements IAction {
+
+	public Object run(Object param) {
+		if(!(param instanceof Map))
+			return null;
+		
+		Map m = (Map)param;
+		String command 	= m.get("Command").toString();
+		String client 	= m.get("IOC").toString();
+		System.out.println("received param: " + command + " " + client);
+		int commandId = -1;
+		
+		for ( int i=0; i < PreferenceProperties.COMMAND_LIST.length; i++) {
+			if ( PreferenceProperties.COMMAND_LIST[i].equalsIgnoreCase(command)) {
+				commandId = i;
+			}
+		}
+		
+		switch ( commandId) {
+		case PreferenceProperties.COMMAND_SEND_ALL_ALARMS_I:
+			// all records
+			
+			break;
+		case PreferenceProperties.COMMAND_SEND_ALARM_I:
+			// individual record
+			
+			break;
+			
+		case PreferenceProperties.COMMAND_DISCONNECT_I:
+		case PreferenceProperties.COMMAND_SEND_STATUS_I:
+		case PreferenceProperties.COMMAND_TAKE_OVER_I:
+			default:
+				
+		
+		}
+		
+		return command + ":" + client + " - success";
+	}
+	
+}
--- products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/GetListOfClients.java
+++ products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/GetListOfClients.java
@@ -7,7 +7,6 @@ import org.csstudio.platform.libs.dcf.actions.IAction;
 public class GetListOfClients implements IAction {
 
 	public Object run(Object param) {
-		// TODO Auto-generated method stub
 		return Statistic.getInstance().getNodeNameArray();
 		//return PreferenceProperties.COMMAND_LIST;  //commandos
 	}
