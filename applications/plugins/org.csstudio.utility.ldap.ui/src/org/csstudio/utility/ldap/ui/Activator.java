a5c644d0361401130c5079052c32dbe2368d0933
Helge Rickens <helge.rickens@desy.de>
2007-04-12 11:43:30 +0000

    changes for use LDAPReader headless

---
+++ applications/plugins/org.csstudio.utility.ldap.ui/src/org/csstudio/utility/ldap/ui/Activator.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2006 Stiftung Deutsches Elektronen-Synchrotron,
+ * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY.
+ *
+ * THIS SOFTWARE IS PROVIDED UNDER THIS LICENSE ON AN "../AS IS" BASIS.
+ * WITHOUT WARRANTY OF ANY KIND, EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR PARTICULAR PURPOSE AND
+ * NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE. SHOULD THE SOFTWARE PROVE DEFECTIVE
+ * IN ANY RESPECT, THE USER ASSUMES THE COST OF ANY NECESSARY SERVICING, REPAIR OR
+ * CORRECTION. THIS DISCLAIMER OF WARRANTY CONSTITUTES AN ESSENTIAL PART OF THIS LICENSE.
+ * NO USE OF ANY SOFTWARE IS AUTHORIZED HEREUNDER EXCEPT UNDER THIS DISCLAIMER.
+ * DESY HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS,
+ * OR MODIFICATIONS.
+ * THE FULL LICENSE SPECIFYING FOR THE SOFTWARE THE REDISTRIBUTION, MODIFICATION,
+ * USAGE AND OTHER RIGHTS AND OBLIGATIONS IS INCLUDED WITH THE DISTRIBUTION OF THIS
+ * PROJECT IN THE FILE LICENSE.HTML. IF THE LICENSE IS NOT INCLUDED YOU MAY FIND A COPY
+ * AT HTTP://WWW.DESY.DE/LEGAL/LICENSE.HTM
+ */
+package org.csstudio.utility.ldap.ui;
+
+import org.csstudio.platform.ui.AbstractCssUiPlugin;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractCssUiPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.csstudio.utility.ui.ldap"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void doStart(BundleContext context) throws Exception {
+//		super.start(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void doStop(BundleContext context) throws Exception {
+		plugin = null;
+//		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	@Override
+	public String getPluginId() {
+		return PLUGIN_ID;
+	}
+	/** Add informational message to the plugin log. */
+    public static void logInfo(String message)
+    {
+        getDefault().log(IStatus.INFO, message, null);
+    }
+
+    /** Add error message to the plugin log. */
+    public static void logError(String message)
+    {
+        getDefault().log(IStatus.ERROR, message, null);
+    }
+
+    /** Add an exception to the plugin log. */
+    public static void logException(String message, Exception e)
+    {
+        getDefault().log(IStatus.ERROR, message, e);
+    }
+
+    /** Add a message to the log.
+     * @param type
+     * @param message
+     */
+    private void log(int type, String message, Exception e)
+    {
+        getLog().log(new Status(type, PLUGIN_ID, IStatus.OK, message, e));
+    }
+}
