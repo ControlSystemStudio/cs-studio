26267d5d14fdab27ebdd6e7725c530aed9d3fff7
swende <swende>
2007-04-18 07:37:21 +0000

    Refactoring of the connection service API.

--- applications/plugins/org.csstudio.sds.components.epics/src/org/csstudio/sds/components/epics/EpicsSchema.java
+++ applications/plugins/org.csstudio.sds.components.epics/src/org/csstudio/sds/components/epics/EpicsSchema.java
@@ -8,14 +8,18 @@ import org.csstudio.sds.model.initializers.AbstractControlSystemSchema;
 import org.eclipse.swt.graphics.RGB;
 
 /**
- * This initialization schema holds the property values of the EPICS control system.
+ * This initialization schema holds the property values of the EPICS control
+ * system.
  * 
  * @author Stefan Hofer
  * @version $Revision$
- *
+ * 
  */
 final public class EpicsSchema extends AbstractControlSystemSchema {
 
+	/**
+	 * The schema ID.
+	 */
 	private static final String ID = "schema.epics"; //$NON-NLS-1$
 
 	/**
@@ -24,14 +28,19 @@ final public class EpicsSchema extends AbstractControlSystemSchema {
 	@Override
 	protected Map<String, Object> createPropertyMap() {
 		Map<String, Object> properties = new HashMap<String, Object>();
-		
-		properties.put(AbstractWidgetModel.PROP_COLOR_BACKGROUND, new RGB(255, 0, 0));
-		properties.put(AbstractWidgetModel.PROP_COLOR_FOREGROUND, new RGB(0, 255, 0));
-		//TODO add more properties
-		
+
+		properties.put(AbstractWidgetModel.PROP_COLOR_BACKGROUND, new RGB(255,
+				0, 0));
+		properties.put(AbstractWidgetModel.PROP_COLOR_FOREGROUND, new RGB(0,
+				255, 0));
+		// TODO add more properties
+
 		return properties;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getTypeId() {
 		return ID;
