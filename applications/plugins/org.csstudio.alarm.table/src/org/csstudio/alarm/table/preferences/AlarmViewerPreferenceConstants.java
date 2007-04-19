1db006471320cccf376f60664c3f51f7ab047c96
Helge Rickens <helge.rickens@desy.de>
2007-04-19 06:29:28 +0000

    Change Defaults

--- applications/plugins/org.csstudio.alarm.table/src/org/csstudio/alarm/table/preferences/AlarmViewerPreferenceConstants.java
+++ applications/plugins/org.csstudio.alarm.table/src/org/csstudio/alarm/table/preferences/AlarmViewerPreferenceConstants.java
@@ -5,14 +5,14 @@ package org.csstudio.alarm.table.preferences;
  */
 public class AlarmViewerPreferenceConstants {
 
-	public static final String P_STRINGAlarm = "column names alarm";
+	public static final String P_STRINGAlarm = "Alarm.column_names";//$NON-NLS-1$
 	public static final String MAX = Messages.getString("AlarmViewerPreferenceConstants.rows.max"); //$NON-NLS-1$
 	public static final String REMOVE = Messages.getString("AlarmViewerPreferenceConstants.rows.min"); //$NON-NLS-1$
-	public static final String INITIAL_PRIMARY_CONTEXT_FACTORY = "primary context factory"; //$NON-NLS-1$
-	public static final String PRIMARY_URL = "primary provider url"; //$NON-NLS-1$
-	public static final String INITIAL_SECONDARY_CONTEXT_FACTORY = "secondary context factory"; //$NON-NLS-1$
-	public static final String SECONDARY_URL = "secondary provider url"; //$NON-NLS-1$
-	public static final String QUEUE = "message queue"; //$NON-NLS-1$
+	public static final String INITIAL_PRIMARY_CONTEXT_FACTORY = "Alarm.context_factory.primary "; //$NON-NLS-1$
+	public static final String PRIMARY_URL = "Alarm.provider_url.primary "; //$NON-NLS-1$
+	public static final String INITIAL_SECONDARY_CONTEXT_FACTORY = "Alarm.context_factory.secondary"; //$NON-NLS-1$
+	public static final String SECONDARY_URL = "Alarm.provider_url.secondary"; //$NON-NLS-1$
+	public static final String QUEUE = "Alarm.message_queue"; //$NON-NLS-1$
 
 
 }
--- applications/plugins/org.csstudio.alarm.table/src/org/csstudio/alarm/table/preferences/LogViewerPreferenceConstants.java
+++ applications/plugins/org.csstudio.alarm.table/src/org/csstudio/alarm/table/preferences/LogViewerPreferenceConstants.java
@@ -5,16 +5,14 @@ package org.csstudio.alarm.table.preferences;
  */
 public class LogViewerPreferenceConstants {
 
-	public static final String P_STRING = "column names log";
+	public static final String P_STRING = "Log.column_names";//$NON-NLS-1$
 	public static final String MAX = Messages.getString("LogViewerPreferenceConstants.rows.max"); //$NON-NLS-1$
 	public static final String REMOVE = Messages.getString("LogViewerPreferenceConstants.rows.min"); //$NON-NLS-1$
-	public static final String INITIAL_PRIMARY_CONTEXT_FACTORY = "primary context factory"; //$NON-NLS-1$
-	public static final String PRIMARY_URL = "primary provider url"; //$NON-NLS-1$
-	public static final String INITIAL_SECONDARY_CONTEXT_FACTORY = "secondary context factory"; //$NON-NLS-1$
-	public static final String SECONDARY_URL = "secondary provider url"; //$NON-NLS-1$
-	public static final String QUEUE = "message queue"; //$NON-NLS-1$
-
-
+	public static final String INITIAL_PRIMARY_CONTEXT_FACTORY = "Log.context_factory.primary "; //$NON-NLS-1$
+	public static final String PRIMARY_URL = "Log.provider_url.primary "; //$NON-NLS-1$
+	public static final String INITIAL_SECONDARY_CONTEXT_FACTORY = "Log.context_factory.secondary"; //$NON-NLS-1$
+	public static final String SECONDARY_URL = "Log.provider_url.secondary"; //$NON-NLS-1$
+	public static final String QUEUE = "Log.message_queue"; //$NON-NLS-1$
 
 
 }
