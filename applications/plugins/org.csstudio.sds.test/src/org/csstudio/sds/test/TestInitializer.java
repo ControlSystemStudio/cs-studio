49291ca777dde3033b9962116537c6179561ab0c
swende <swende>
2007-04-22 21:43:48 +0000

    *** empty log message ***

--- applications/plugins/org.csstudio.sds.test/src/org/csstudio/sds/test/TestInitializer.java
+++ applications/plugins/org.csstudio.sds.test/src/org/csstudio/sds/test/TestInitializer.java
@@ -2,21 +2,31 @@ package org.csstudio.sds.test;
 
 import org.csstudio.sds.model.AbstractWidgetModel;
 import org.csstudio.sds.model.initializers.AbstractControlSystemSchema;
-import org.csstudio.sds.model.initializers.IWidgetModelInitializer;
+import org.csstudio.sds.model.initializers.AbstractWidgetModelInitializer;
 
 /**
  * @author Stefan Hofer
  * @version $Revision$
- *
+ * 
  */
-public class TestInitializer implements IWidgetModelInitializer {
+public final class TestInitializer extends AbstractWidgetModelInitializer {
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public void initialize(AbstractWidgetModel model,
-			AbstractControlSystemSchema schema) {
-		model.setPropertyValue(TestSchema.PROP_TEST, schema.getDoubleProperty(TestSchema.PROP_TEST));
+	public void initialize(final AbstractWidgetModel model,
+			final AbstractControlSystemSchema schema) {
+		model.setPropertyValue(TestSchema.PROP_TEST, schema
+				.getDoubleProperty(TestSchema.PROP_TEST));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void initialize(final AbstractControlSystemSchema schema) {
+		initializeStaticProperty(TestSchema.PROP_TEST, schema
+				.getDoubleProperty(TestSchema.PROP_TEST));
 	}
 
 }
--- applications/plugins/org.csstudio.sds.test/src/org/csstudio/sds/test/TestSchema.java
+++ applications/plugins/org.csstudio.sds.test/src/org/csstudio/sds/test/TestSchema.java
@@ -1,33 +1,43 @@
 package org.csstudio.sds.test;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.csstudio.sds.model.AbstractWidgetModel;
 import org.csstudio.sds.model.initializers.AbstractControlSystemSchema;
 
 /**
-* A initialization schema for unit tests. 
-* 
-* @author Stefan Hofer
-* @version $Revision$
-*
-*/
+ * A initialization schema for unit tests.
+ * 
+ * @author Stefan Hofer, Sven Wende
+ * @version $Revision$
+ * 
+ */
 public final class TestSchema extends AbstractControlSystemSchema {
-	
+
 	/**
 	 * ID for a test property.
 	 */
 	public static final String PROP_TEST = "PROP_TEST"; //$NON-NLS-1$
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	protected Map<String, Object> createPropertyMap() {
-		Map<String, Object> properties = new HashMap<String, Object>();
-		properties.put(PROP_TEST, 3.14);
-		return properties;
+	protected void initializeAliases(final AbstractWidgetModel widgetModel) {
+
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public String getTypeId() {
-		return "schema.test"; //$NON-NLS-1$
+	protected void initializeProperties() {
+		addGlobalProperty(PROP_TEST, 3.14);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void initializeWidget(final AbstractWidgetModel widgetModel) {
+
 	}
 }
