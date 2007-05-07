f587f98c76ae571b2ddf7ab1ae7b095d972ba386
Helge Rickens <helge.rickens@desy.de>
2007-05-07 08:50:40 +0000

    The NameSpaceBrowser (LDAP) in the past NameSpaceBrowser

---
+++ applications/plugins/org.csstudio.utility.ldap.nameSpaceBrowser/src/org/csstudio/utility/ldap/namespacebrowser/Activator.java
@@ -0,0 +1,83 @@
+package org.csstudio.utility.ldap.namespacebrowser;
+
+import org.csstudio.platform.ui.AbstractCssUiPlugin;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractCssUiPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.csstudio.utility.ldap.nameSpaceBrowser";
+
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void doStart(BundleContext context) throws Exception {
+//		super.start(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void doStop(BundleContext context) throws Exception {
+//		super.stop(context);
+		plugin = null;
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	@Override
+	public String getPluginId() {
+		return PLUGIN_ID;
+	}
+
+	/** Add informational message to the plugin log. */
+    public static void logInfo(String message)
+    {
+        getDefault().log(IStatus.INFO, message, null);
+    }
+
+    /** Add error message to the plugin log. */
+    public static void logError(String message)
+    {
+        getDefault().log(IStatus.ERROR, message, null);
+    }
+
+    /** Add an exception to the plugin log. */
+    public static void logException(String message, Exception e)
+    {
+        getDefault().log(IStatus.ERROR, message, e);
+    }
+
+    /** Add a message to the log.
+     * @param type
+     * @param message
+     */
+    private void log(int type, String message, Exception e)
+    {
+        getLog().log(new Status(type, PLUGIN_ID, IStatus.OK, message, e));
+    }
+}
