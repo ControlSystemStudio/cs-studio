ae32e2c44875bd82e1a5803456266472390c3632
Kay Kasemir <kasemirk@ornl.gov>
2007-06-14 21:17:33 +0000

    cleanup

--- applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/Messages.java
+++ applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/Messages.java
@@ -2,23 +2,16 @@ package org.csstudio.trends.databrowser;
 
 import org.eclipse.osgi.util.NLS;
 
-public class Messages extends NLS
+class Messages extends NLS
 {
     private static final String BUNDLE_NAME = "org.csstudio.trends.databrowser.messages"; //$NON-NLS-1$
 
     public static String DataBrowser;
-
     public static String AddPV;
-
     public static String AddPV_TT;
-
     public static String EnterPVName;
-
     public static String EnterNewPVName;
-
-    
     public static String ErrorMessageTitle;
-    
     public static String ErrorMessage;
     
     static
