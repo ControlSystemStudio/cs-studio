8988353b80e17baee7d961abe359bd6fdfd4d2c1
Kay Kasemir <kasemirk@ornl.gov>
2007-06-14 18:14:04 +0000

    Implement IPV-with-sample directly

--- applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/sampleview/TableItem.java
+++ applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/sampleview/TableItem.java
@@ -1,62 +1,62 @@
 package org.csstudio.trends.databrowser.sampleview;
 
 import org.csstudio.platform.data.IValue;
-//import org.csstudio.platform.model.IProcessVariableWithSample;
 import org.csstudio.platform.model.IProcessVariableWithSamples;
 import org.csstudio.trends.databrowser.model.ModelSample;
+import org.eclipse.core.runtime.PlatformObject;
 
 /** One item displayed in the table.
+ *  <p>
+ *  Also provides access to <u>all</u> items in the table
+ *  via the IProcessVariableWithSamples interface.
+ *  
  *  @author Kay Kasemir
  *  @author Helge Rickens
  */
-public final class TableItem implements IProcessVariableWithSamples
+public final class TableItem
+    extends PlatformObject implements IProcessVariableWithSamples
 {
-    /** The actual sample of this table row. */
-    final private ModelSample sample;
+    /** The table model for this item. */
+    final private TableModel model;
     
-    /** The whole data as an IProcessVariableWithSample.
-     *  <p>
-     *  The sample would suffice for the table display, but
-     *  we need to implement IProcessVariableWithSample for
-     *  the context menu.
-     */
-    final private IProcessVariableWithSamples ipv_with_samples;
+    /** The row in the table model that has this item's sample. */
+    final private int row;
     
-    TableItem(ModelSample sample, IProcessVariableWithSamples ipv_with_samples)
+    TableItem(final TableModel model, final int row)
     {
-        this.sample = sample;
-        this.ipv_with_samples = ipv_with_samples;
+        this.model = model;
+        this.row = row;
     }
     
-    /** @return The ModelSample. */
+    /** @see IProcessVariableWithSamples */
+    public String getTypeId()
+    {
+        return IProcessVariableWithSamples.TYPE_ID;
+    }
+
+    /** @see IProcessVariableWithSamples */
+    public String getName()
+    {
+        return model.getName();
+    }
+
+    /** Get model sample for the label provider
+     *  @return The ModelSample.
+     */
     ModelSample getSample()
     {
-        return sample;
+        return model.getSample(row);
     }
 
     /** @see IProcessVariableWithSamples */
-    public IValue getSample(int index) {
-        return ipv_with_samples.getSample(index);
+    public IValue getSample(final int row)
+    {
+        return model.getSample(row).getSample();
     }
 
     /** @see IProcessVariableWithSamples */
-    public int size() {
-        return ipv_with_samples.size();
-    }
-
-    /** @see IProcessVariableWithSamples */
-    public String getName() {
-        return ipv_with_samples.getName();
-    }
-
-    /** @see IProcessVariableWithSamples */
-    public String getTypeId() {
-        return ipv_with_samples.getTypeId();
-    }
-
-    /** @see IProcessVariableWithSamples */
-    @SuppressWarnings("unchecked")
-    public Object getAdapter(Class adapter) {
-        return ipv_with_samples.getAdapter(adapter);
+    public int size()
+    {
+        return model.size();
     }
 }
--- applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/sampleview/TableModel.java
+++ applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/sampleview/TableModel.java
@@ -1,101 +1,75 @@
 package org.csstudio.trends.databrowser.sampleview;
 
+import java.util.ArrayList;
+
 import org.csstudio.platform.data.ITimestamp;
-import org.csstudio.platform.data.IValue;
-import org.csstudio.platform.model.CentralItemFactory;
-import org.csstudio.platform.model.IProcessVariableWithSamples;
 import org.csstudio.swt.chart.ChartSampleSearch;
 import org.csstudio.trends.databrowser.model.IModelItem;
 import org.csstudio.trends.databrowser.model.IModelSamples;
 import org.csstudio.trends.databrowser.model.Model;
 import org.csstudio.trends.databrowser.model.ModelSample;
 
+/** Model of the sample table.
+ *  <p>
+ *  Keeps a copy of the <u>visible</u> samples in the data browser model.
+ *  <p>
+ *  Alternatively, it could keep a reference to the model and its
+ *  first/last visible sample index, then use a model listener to
+ *  re-evalutate the visible range on model changes, and even
+ *  update the table.
+ *  But table updates are surprisingly expensive, and keeping an array
+ *  of value references (not whole value copies) looked acceptable for now.
+ *  
+ *  @author Kay Kasemir
+ */
 public class TableModel
 {
-    /** All the model samples. */
-    final private IModelSamples samples;
-
-    /** Start index of visible samples.
-     *  <p>
-     *  Don't use if <code>num</code> is zero!
-     */
-    final private int start_index;
-
-    /** Number of visible samples. */
-    final private int num;
+    /** Name of the PV that provided the data. */
+    final private String name;
     
-    /** IPV-with-samples interface to the visible samples. */
-    final private IProcessVariableWithSamples ipv_with_samples;
+    /** References to all the visible model samples. */
+    final private ArrayList<ModelSample> samples =
+        new ArrayList<ModelSample>();
 
     /** Constructor */
     TableModel(Model model, IModelItem item)
     {
-        samples = item.getSamples();
-        synchronized (samples)
+        name = item.getName();
+        final IModelSamples all_samples = item.getSamples();
+        synchronized (all_samples)
         {
             final ITimestamp start = model.getStartTime();
             final ITimestamp end = model.getEndTime();
             
-            start_index =
-                ChartSampleSearch.findClosestSample(samples, start.toDouble());
+            final int start_index =
+                ChartSampleSearch.findClosestSample(all_samples, start.toDouble());
             final int end_index =
-                ChartSampleSearch.findClosestSample(samples, end.toDouble());
+                ChartSampleSearch.findClosestSample(all_samples, end.toDouble());
             if (start_index >= 0  &&  end_index >= 0)
-            {
-                num = end_index - start_index + 1;
-                ipv_with_samples = createIPVwithSamples(item.getName(), samples,
-                                                        start_index, num);
-            }
-            else
-            {
-                num = 0;
-                ipv_with_samples = null;
-            }
+                for (int i=start_index; i<=end_index; ++i)
+                    samples.add(all_samples.get(i));
         }
     }
+    
+    /** @return Name of the PV */
+    String getName()
+    {   return name; }
     
     /** @return The number of samples in the table. */
     int size()
-    {   return num;   }
-    
-    TableItem getTableItem(int row)
+    {   return samples.size();   }
+
+    /** @return Sample in given row. */
+    @SuppressWarnings("nls")
+    ModelSample getSample(final int row)
     {
-        if (row < num)
-        {
-            synchronized (samples)
-            {
-                final int i = start_index + row;
-                if (i >= 0  &&  i < samples.size())
-                    return new TableItem(samples.get(i), ipv_with_samples);
-            }
-        }
-        return null;
+        return samples.get(row);
     }
     
-    /** Create an IProcessVariableWithSample for the given item and
-     *  sample range.
-     *  @param name Name of the PV
-     *  @param samples The samples
-     *  @param start Index of first sample
-     *  @param num Sample count
-     *  @return IProcessVariableWithSample.
-     */
-    private static IProcessVariableWithSamples createIPVwithSamples(
-                                                    final String name,
-                                                    final IModelSamples samples,
-                                                    final int start,
-                                                    final int num)
+    /** @return TableItem for given row. */
+    @SuppressWarnings("nls")
+    TableItem getTableItem(int row)
     {
-        final IValue[] values = new IValue[num];
-        synchronized (samples)
-        {
-            for (int i=0; i<num; ++i)
-            {
-                final ModelSample sample = samples.get(start + i);
-                values[i] = sample.getSample();
-            }
-        }
-        return CentralItemFactory.createProcessVariableWithSamples(name,
-                                                                   values);
+        return new TableItem(this, row);
     }
 }
