d3fbfa513a357a0e51a543336fefb01cc339d75b
Jan Hatje <jan.hatje@desy.de>
2007-06-18 09:06:54 +0000

    Change jms to use Job for Connection. Default retry is 3 times.

--- src/org/csstudio/platform/libs/jms/MessageReceiver.java
+++ src/org/csstudio/platform/libs/jms/MessageReceiver.java
@@ -22,12 +22,16 @@
 
 package org.csstudio.platform.libs.jms;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Hashtable;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.jms.ConnectionFactory;
 import javax.jms.Connection;
+import javax.jms.JMSException;
 import javax.jms.Session;
 import javax.jms.MessageConsumer;
 import javax.jms.TextMessage;
@@ -36,96 +40,268 @@ import javax.jms.MessageListener;
 import javax.jms.Topic;
 
 import org.csstudio.platform.libs.jms.preferences.PreferenceConstants;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.IJobChangeListener;
+import org.eclipse.core.runtime.jobs.Job;
 /**
- * Class used to establish connection with jms server
+ * Class used to establish connection with jms server.
  */
-
 public class MessageReceiver {
+    /**
+     * Properties for the Connection.
+     */
+    private Hashtable<String, String>   _properties  = null;
+    /**
+     * Context.
+     */
+    private Context                     _context     = null;
+    /**
+     * The Connection Factory.
+     */
+    private ConnectionFactory           _factory     = null;
+    /**
+     * The Connetion.
+     */
+    private Connection                  _connection  = null;
+    /**
+     * The Sesstion.
+     */
+    private Session                     _session     = null;
+    /**
+     * A Container for the Messanges.
+     */
+    private MessageConsumer[]           _receiver    = null;
+    /**
+     * The Text Message.
+     */
+    private TextMessage                 _textMessage = null;
+    /**
+     * A Message.
+     */
+    private Message                     _message     = null;
+    /**
+     * A JMS Topic.
+     */
+    private Topic		                _destination = null;  // if ! topic: Destination
+    /**
+     * ???.
+     */
+	private String[] _queues;
+    /**
+     * The listener of the Messages.
+     */
+    private MessageListener _listener;
+    /**
+     * Used Failover.
+     */
+    private boolean _failover;
+    /**
+     * counts of Retrais.
+     */
+    private int _countsOfRetrais;
+	private ConnectionJob cj;
+	private IJobChangeListener jobChangeListener;
+    
+    
 
-    Hashtable<String, String>   properties  = null;
-    Context                     context     = null;
-    ConnectionFactory           factory     = null;
-    Connection                  connection  = null;
-    Session                     session     = null;
-    MessageConsumer[]           receiver    = null;
-    TextMessage                 textMessage = null;
-    Message                     message     = null;
-    Topic		                destination = null;  // if ! topic: Destination
-	private String[] queues;
+    /**
+     * 
+     * @author hrickens
+     * @author $Author$
+     * @version $Revision$
+     * @since 15.06.2007
+     */
+    class ConnectionJob extends Job{
 
+        /**
+         * @param name of the Job.
+         */
+        public ConnectionJob(final String name) {
+            super(name);
+        }
 
-    public MessageReceiver() throws NamingException{
-        properties = new Hashtable<String, String>();
-        properties.put(Context.INITIAL_CONTEXT_FACTORY,
-        		JmsPlugin.getDefault().getPluginPreferences().getString(PreferenceConstants.INITIAL_CONTEXT_FACTORY));
-        properties.put(Context.PROVIDER_URL,
-        		JmsPlugin.getDefault().getPluginPreferences().getString(PreferenceConstants.URL));
-        context = new InitialContext(properties);
-//        destination = (Topic) context.lookup(JmsPlugin.getDefault().getPluginPreferences().getString(PreferenceConstants.QUEUE).split(",")[0]);
-        queues = JmsPlugin.getDefault().getPluginPreferences().getString(PreferenceConstants.QUEUE).split(",");
-    }
+        /**
+         * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor)
+         * @param monitor The Progress Monitor. 
+         * @return the Prozess ending Status.
+         */
+        @Override
+        protected IStatus run(final IProgressMonitor monitor) {
+            try {
+                _factory = (ConnectionFactory) _context.lookup("ConnectionFactory");
+                _connection = _factory.createConnection();
+                _session = _connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                //
+                // here we can decide whether we will get any messages regardless whether we are connected or not
+                // for now we use here the only_when_online method
+                //
+                _receiver = new MessageConsumer[_queues.length];
+                for (int i=0;i<_queues.length;i++){
+                    if (monitor.isCanceled()){ return Status.CANCEL_STATUS;}
+                    /*
+                     * changed from OpenJMS to ActiveMQ
+                     * MCL 2007-05-23
+                     */
+                    //destination = (Topic)context.lookup(queues[i]);
+                    _destination = _session.createTopic(_queues[i]);
+                    _receiver[i] = _session.createConsumer(_destination);
+                    _receiver[i].setMessageListener(_listener);
+                }
+                    /*else {
+                // create permanent connection:
+                    receiver = session.createDurableSubscriber(destination, uniqueNameOfCssInstance);
+                }*/
+                _connection.start();
+            } catch (NamingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (JMSException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return Status.CANCEL_STATUS;
+            }
 
-    public MessageReceiver(String initialContextFactory, String providerURL, String[] queues)throws NamingException{
-        properties = new Hashtable<String, String>();
-        properties.put(Context.INITIAL_CONTEXT_FACTORY,initialContextFactory);
-        properties.put(Context.PROVIDER_URL,providerURL);
-        context = new InitialContext(properties);
-//        destination = (Topic)context.lookup(queues[0]);
-        this.queues = queues;
+//            receiver.setMessageListener(listener);
+
+            return Status.OK_STATUS;
+//            return Job.ASYNC_FINISH;
+        }
+        
     }
 
     /**
-     * Parameter is listener, the one to be notified
+     * 
+     * @throws NamingException Throws an JMS NamingException.
      */
-	public void startListener(MessageListener listener) throws Exception{
+    public MessageReceiver() throws NamingException{
+        _properties = new Hashtable<String, String>();
+        _properties.put(Context.INITIAL_CONTEXT_FACTORY,
+        		JmsPlugin.getDefault().getPluginPreferences().getString(PreferenceConstants.INITIAL_CONTEXT_FACTORY));
+        String url = JmsPlugin.getDefault().getPluginPreferences().getString(PreferenceConstants.URL);
+//        if(url.contains("failover:")){
+//            _failover=true;
+//            int index = url.indexOf("maxReconnectAttempts=");
+//            if(index<0){
+//                url = url.concat("?maxReconnectAttempts=3");
+//            }else{
+//                int index2 = url.indexOf("&", index);
+//                if(index2<0){
+//                    url = url.substring(0,index-1)+"maxReconnectAttempts=3"; 
+//                }else{
+//                    url = url.substring(0,index-1)+"maxReconnectAttempts=3"+url.substring(index2);     
+//                }
+//            }
+//            
+//        }else{_failover=false;}
+        
+        ActiveMQURL uri = new ActiveMQURL(url);
+        
+        if(uri.getPrefix()!=null){
+            _failover=true;
+            if(uri.getMaxReconnectAttempts()==null){
+                uri.setMaxReconnectAttempts("maxReconnectAttempts=3");
+            }
+        }else{_failover=false;}
+        
+//        System.out.println("URL1: "+uri.getURL());
+        _properties.put(Context.PROVIDER_URL,uri.getURL());
+        _context = new InitialContext(_properties);
+//        destination = (Topic) context.lookup(JmsPlugin.getDefault().getPluginPreferences().getString(PreferenceConstants.QUEUE).split(",")[0]);
+        _queues = JmsPlugin.getDefault().getPluginPreferences().getString(PreferenceConstants.QUEUE).split(",");
+    }
 
-        factory = (ConnectionFactory) context.lookup("ConnectionFactory");
-        connection = factory.createConnection();
-        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        //
-        // here we can decide whether we will get any messages regardless whether we are connected or not
-        // for now we use here the only_when_online method
-        //
-        receiver = new MessageConsumer[queues.length];
-        for (int i=0;i<queues.length;i++){
-        	/*
-        	 * changed from OpenJMS to ActiveMQ
-        	 * MCL 2007-05-23
-        	 */
-        	//destination = (Topic)context.lookup(queues[i]);
-        	destination = session.createTopic(queues[i]);
-        	receiver[i] = session.createConsumer(destination);
-        	receiver[i].setMessageListener(listener);
-        }
-        	/*else {
-        // create permanent connection:
-        	receiver = session.createDurableSubscriber(destination, uniqueNameOfCssInstance);
-        }*/
-        connection.start();
+    /**
+     * 
+     * @param initialContextFactory the Initial Context Factory. 
+     * @param providerURL The URL from Provider.
+     * @param queues A Queue
+     * @throws NamingException Throws an JMS NamingException.
+     */
+    public MessageReceiver(final String initialContextFactory, final String providerURL, final String[] queues)throws NamingException{
+        _properties = new Hashtable<String, String>();
+        _properties.put(Context.INITIAL_CONTEXT_FACTORY,initialContextFactory);
+        
+        ActiveMQURL uri = new ActiveMQURL(providerURL);
+       
+        if(uri.getPrefix()!=null){
+            _failover=true;
+            if(uri.getMaxReconnectAttempts()==null){
+                uri.setMaxReconnectAttempts("maxReconnectAttempts=3");
+            }
+        }else{_failover=false;}
+//        System.out.println("URL2: "+uri.getURL());
+        _properties.put(Context.PROVIDER_URL,uri.getURL());
+        _context = new InitialContext(_properties);
+//        destination = (Topic)context.lookup(queues[0]);
+        this._queues = queues;
+    }
 
-//        receiver.setMessageListener(listener);
+    /**
+     * Parameter is listener, the one to be notified.
+     * @param listener of Message.
+     * 
+     */
+	public final void startListener(final MessageListener listener) {
+	    _listener = listener;
+        cj = new ConnectionJob("JMS Connetion");
+        jobChangeListener = new IJobChangeListener(){
+            public void done(final IJobChangeEvent event) {
+                if(event.getResult().isOK()){
+                    System.out.println("JMS Connecting is done : \r\n"+_properties+"\r\n");
+                }else{
+                    System.out.println("JMS Server ("+_properties.get(Context.PROVIDER_URL)+") is not available!!!");
+                    ActiveMQURL aURL = new ActiveMQURL(_properties.get(Context.PROVIDER_URL));
+                    aURL.setMaxReconnectAttempts("maxReconnectAttempts=0");
+                    _properties.put(Context.PROVIDER_URL,aURL.getURL());
+                    System.out.println(" Retry with "+_properties.get(Context.PROVIDER_URL));
+                    startListener(listener);
+                }
+            }
+
+            public void running(final IJobChangeEvent event) {
+                System.out.println("JMS Connecting to \r\n"+_properties+"\r\n"); 
+            }
+            // do nothing
+            public void aboutToRun(final IJobChangeEvent event) {}
+            public void awake(final IJobChangeEvent event) {}
+            public void scheduled(final IJobChangeEvent event) {}
+            public void sleeping(final IJobChangeEvent event) {}
+            
+        };
+        cj.addJobChangeListener(jobChangeListener);
+        cj.setSystem(true);
+        cj.schedule();
+        
 	}
 
 	/**
-	 * Cleans up resources
+	 * Cleans up resources.
+     * @throws Exception ___. 
+     * 
 	 */
-	public void stopListening() throws Exception{
-		for (MessageConsumer r: receiver) {
+	public final void stopListening() throws Exception{
+		cj.removeJobChangeListener(jobChangeListener);
+		cj.cancel();
+		for (MessageConsumer r: _receiver) {
 			r.close();
 			r=null;
 		}
-        session.close();
-        connection.stop();
-        connection.close();
-        properties  = null;
-        context     = null;
-        factory     = null;
-        connection  = null;
-        session     = null;
-        receiver    = null;
-        textMessage = null;
-        message     = null;
-        destination = null;
+        _session.close();
+        _connection.stop();
+        _connection.close();
+        _properties  = null;
+        _context     = null;
+        _factory     = null;
+        _connection  = null;
+        _session     = null;
+        _receiver    = null;
+        _textMessage = null;
+        _message     = null;
+        _destination = null;
+        
 	}
 
 
