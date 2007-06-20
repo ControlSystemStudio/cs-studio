0e85cf77319c73e33e5ff46efd408e86698d98c8
Kay Kasemir <kasemirk@ornl.gov>
2007-06-20 18:52:42 +0000

    Hook into file/new as well as navigator context menu.

--- applications/plugins/org.csstudio.display.pvtable/src/org/csstudio/display/pvtable/CreateNewPVTableConfig.java
+++ applications/plugins/org.csstudio.display.pvtable/src/org/csstudio/display/pvtable/CreateNewPVTableConfig.java
@@ -1,14 +1,6 @@
 package org.csstudio.display.pvtable;
 
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchWindow;
+import util.eclipse.menu.NewFileWizardMenuAction;
 
 /** Action to run the NewPVTableWizard.
  *  <p>
@@ -18,50 +10,10 @@ import org.eclipse.ui.IWorkbenchWindow;
  *  @author Kay Kasemir
  */
 @SuppressWarnings("nls")
-public class CreateNewPVTableConfig extends Action implements IObjectActionDelegate
+public class CreateNewPVTableConfig extends NewFileWizardMenuAction
 {
-    /** The currently active workbench part */
-    private IWorkbenchPart part = null;
-
-    /** Selection for the container */
-    private IStructuredSelection selection = null;
-    
-    /** Keep track of active workbench part.
-     *  @see IObjectActionDelegate
-     */
-    public void setActivePart(IAction action, IWorkbenchPart targetPart)
+    public CreateNewPVTableConfig()
     {
-        this.part = targetPart;
+        super(NewPVTableWizard.class);
     }
-
-    /** Display the new chart editor wizard */
-    public void run(IAction action)
-    {
-        // Compare Plugin book p. 439 for how to run a wizard manually
-        final IWorkbenchWindow window =
-            part.getSite().getWorkbenchWindow();
-        NewPVTableWizard wizard = new NewPVTableWizard();
-        wizard.init(window.getWorkbench(), selection);
-        WizardDialog dialog = new WizardDialog(window.getShell(), wizard);
-        dialog.open();
-    }
-
-    /** Keep track of active selection.
-     *  @see IObjectActionDelegate
-     */
-    public void selectionChanged(IAction action, ISelection selection)
-    {
-        if (selection instanceof IStructuredSelection)
-        {
-            IStructuredSelection sel = (IStructuredSelection) selection;
-            Object item = sel.getFirstElement();
-            if (item instanceof IContainer)
-            {
-                this.selection = sel;
-                return;
-            }
-        }
-        // No container selected
-        this.selection = null;
-   }
 }
