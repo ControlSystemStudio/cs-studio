811ffa47032a911d7db73d9c5b770fc91e42683c
claus <claus>
2007-06-27 15:36:50 +0000

    Add finallly after try/catch

---
+++ products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/server/ClientWatchdog.java
@@ -0,0 +1,50 @@
+package org.csstudio.diag.interconnectionServer.server;
+
+import org.csstudio.platform.logging.CentralLogger;
+
+public class ClientWatchdog extends Thread{
+	private ClientRequest clientID = null;
+	private int	timeout	= 0;
+	
+	ClientWatchdog ( ClientRequest clientID, int timeout) {
+		this.clientID = clientID;
+		this.timeout = timeout;
+		
+		this.start();
+	}
+	
+	public void run() {
+		/*
+		 * wait
+		 */
+		
+		try {
+			Thread.sleep( this.timeout);
+		
+		/*
+		 * check whether ClientRequest is still running
+		 * if so -> stop it!
+		 */
+		if ( this.clientID.isAlive()) {
+			/*
+			 * dammed - still running -> kill it!
+			 */
+			this.clientID.stop();
+			InterconnectionServer.getInstance().getClientRequestTheadCollector().decrementValue();
+			/*
+			 * another time we had to stop a thread! - countdown
+			 */
+			InterconnectionServer.getInstance().setSuccessfullJmsSentCountdown(false);
+			CentralLogger.getInstance().debug(this, "InterconnectionServer: Hard STOP for ClientRequest");
+			System.out.println("#");
+		}
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+		}
+		finally {
+			//clean up
+		}
+		
+	}
+
+}
