65f7b405af903703924fab67653854e278d85be6
claus <claus>
2007-07-03 12:53:56 +0000

    Use separate user/ password

--- products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/LoginCallbackHandler1.java
+++ products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/LoginCallbackHandler1.java
@@ -7,7 +7,7 @@ import org.csstudio.platform.security.ILoginCallbackHandler;
 public class LoginCallbackHandler1 implements ILoginCallbackHandler {
 
 	public Credentials getCredentials() {
-		return new Credentials(PreferenceProperties.XMPP_USER_NAME, PreferenceProperties.XMPP_USER_NAME);
+		return new Credentials(PreferenceProperties.XMPP_USER_NAME, PreferenceProperties.XMPP_PASSWORD);
 	}
 
 }
