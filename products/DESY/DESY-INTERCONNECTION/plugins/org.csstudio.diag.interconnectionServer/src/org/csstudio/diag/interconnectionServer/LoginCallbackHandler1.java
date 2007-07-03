831e19a1ba579f0179598c5a133bf5822d6b80f3
claus <claus>
2007-07-03 08:34:59 +0000

    Get user name from PreferenceProperties

--- products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/LoginCallbackHandler1.java
+++ products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/LoginCallbackHandler1.java
@@ -1,12 +1,13 @@
 package org.csstudio.diag.interconnectionServer;
 
+import org.csstudio.diag.interconnectionServer.server.PreferenceProperties;
 import org.csstudio.platform.security.Credentials;
 import org.csstudio.platform.security.ILoginCallbackHandler;
 
 public class LoginCallbackHandler1 implements ILoginCallbackHandler {
 
 	public Credentials getCredentials() {
-		return new Credentials("icserver", "icserver");
+		return new Credentials(PreferenceProperties.XMPP_USER_NAME, PreferenceProperties.XMPP_USER_NAME);
 	}
 
 }
