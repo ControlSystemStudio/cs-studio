11bd4aa51be73c74a7a74def49bdcc2f9b4bbc11
claus <claus>
2007-07-03 13:21:53 +0000

    Wait for IC-Server to greacefully stop

--- products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/StopIcServer.java
+++ products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/StopIcServer.java
@@ -14,8 +14,17 @@ public class StopIcServer implements IAction {
 		 * stop IC-Server
 		 */
 		boolean result = InterconnectionServer.getInstance().stopIcServer();
+		try {
+			/*
+			 * wait for IC-Server to greacefully stop
+			 */
+			this.wait(5000); //wait 5 sec
+		} catch (Exception e) {
+			// TODO: handle exception
+			// nothing to do we want to stop anyhow
+		}		
 		/*
-		 * stop here - how??
+		 * stop here - brutally
 		 */
 		System.exit(0);
 		return "" + result;
