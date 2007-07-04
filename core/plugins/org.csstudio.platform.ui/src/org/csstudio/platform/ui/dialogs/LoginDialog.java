f2beec51e3293f87be5aa8938775cbb6d7bf4c92
jrathlev <jrathlev>
2007-07-04 15:15:03 +0000

    added "remember password" checkbox for later usage

--- core/plugins/org.csstudio.platform.ui/src/org/csstudio/platform/ui/dialogs/LoginDialog.java
+++ core/plugins/org.csstudio.platform.ui/src/org/csstudio/platform/ui/dialogs/LoginDialog.java
@@ -1,33 +1,14 @@
-/* 
- * Copyright (c) 2006 Stiftung Deutsches Elektronen-Synchroton, 
- * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY.
- *
- * THIS SOFTWARE IS PROVIDED UNDER THIS LICENSE ON AN "../AS IS" BASIS. 
- * WITHOUT WARRANTY OF ANY KIND, EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED 
- * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR PARTICULAR PURPOSE AND 
- * NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
- * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
- * THE USE OR OTHER DEALINGS IN THE SOFTWARE. SHOULD THE SOFTWARE PROVE DEFECTIVE 
- * IN ANY RESPECT, THE USER ASSUMES THE COST OF ANY NECESSARY SERVICING, REPAIR OR 
- * CORRECTION. THIS DISCLAIMER OF WARRANTY CONSTITUTES AN ESSENTIAL PART OF THIS LICENSE. 
- * NO USE OF ANY SOFTWARE IS AUTHORIZED HEREUNDER EXCEPT UNDER THIS DISCLAIMER.
- * DESY HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, 
- * OR MODIFICATIONS.
- * THE FULL LICENSE SPECIFYING FOR THE SOFTWARE THE REDISTRIBUTION, MODIFICATION, 
- * USAGE AND OTHER RIGHTS AND OBLIGATIONS IS INCLUDED WITH THE DISTRIBUTION OF THIS 
- * PROJECT IN THE FILE LICENSE.HTML. IF THE LICENSE IS NOT INCLUDED YOU MAY FIND A COPY 
- * AT HTTP://WWW.DESY.DE/LEGAL/LICENSE.HTM
- */
 package org.csstudio.platform.ui.dialogs;
+
 
 import org.csstudio.platform.security.Credentials;
 import org.csstudio.platform.security.ILoginCallbackHandler;
-import org.csstudio.platform.ui.internal.localization.Messages;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
@@ -38,96 +19,102 @@ import org.eclipse.swt.widgets.Text;
  * A simple login dialog.
  * 
  * @author Alexander Will, Jõrg Rathlev, Anže Vodovnik
- * 
  */
 public class LoginDialog extends TitleAreaDialog implements ILoginCallbackHandler  {
 
-    /**
-     * Text box that holds the user name.
-     */
-    private Text _username;
+	/**
+	 * Text box that holds the user name.
+	 */
+	private Text _username;
 
-    /**
-     * Text box that holds the password.
-     */
-    private Text _password;
+	/**
+	 * Text box that holds the password.
+	 */
+	private Text _password;
+	
+	/**
+	 * Checkbox for option to remember username and password.
+	 */
+	private Button _rememberLogin;
 
-    /**
-     * Stores the credentials object after OK has been pressed.
-     */
-    private Credentials _credentials;
+	/**
+	 * Stores the credentials object after OK has been pressed.
+	 */
+	private Credentials _credentials;
+	
+	/**
+	 * Creates a new login dialog.
+	 * @param parentShell the parent shell.
+	 */
+	public LoginDialog(final Shell parentShell) {
+		super(parentShell);
+	}
+	
+	/**
+	 * Creates the contents of this dialog.
+	 */
+	@Override
+	protected Control createDialogArea(final Composite parent) {
+		Composite parentComposite = (Composite) super.createDialogArea(parent);
 
-    /**
-     * Standard constructor.
-     * 
-     * @param parentShell
-     *            The parent shell.
-     */
-    public LoginDialog(final Shell parentShell) {
-        super(parentShell);
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected Control createDialogArea(final Composite parent) {
-        Control control = super.createDialogArea(parent);
+		getShell().setText("Login - Control System Studio");
+		setTitle("Login");
+		setMessage("Please enter your user name and password.");
 
-        getShell().setText(Messages.LoginDialog_WINDOW_TITLE);
-        setTitle(Messages.LoginDialog_DIALOG_TITLE);
-        setMessage(Messages.LoginDialog_DIALOG_DESCRIPTION);
+		// Create the layout
+		Composite contents = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout(2, false);
+        layout.marginHeight = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_MARGIN);
+        layout.marginWidth = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_MARGIN);
+        layout.verticalSpacing = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_SPACING);
+        layout.horizontalSpacing = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
+		contents.setLayout(layout);
+		contents.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, true));
+		contents.setFont(parent.getFont());
 
-        Composite composite = new Composite(parent, SWT.NONE);
+		// user name
+		Label label = new Label(contents, SWT.NONE);
+		label.setText("User name:");
+		_username = new Text(contents, SWT.BORDER | SWT.FLAT);
+		_username.setFocus();
+		_username.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
 
-        // Create the layout
-        GridLayout layout = new GridLayout(2, false);
+		// password
+		label = new Label(contents, SWT.NONE);
+		label.setText("Password:");
+		_password = new Text(contents, SWT.BORDER | SWT.FLAT | SWT.PASSWORD);
+		_password.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false));
+		
+		// remember password checkbox (invisible by default)
+		_rememberLogin = new Button(contents, SWT.CHECK);
+		_rememberLogin.setText("Remember my user name and password");
+		_rememberLogin.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false, 2, 1));
+		_rememberLogin.setVisible(false);
+		
+		return parentComposite;
+	}
 
-        // assign the layout to the panel
-        composite.setLayout(layout);
-        composite.setLayoutData(new GridData(GridData.FILL_BOTH));
-        composite.setFont(parent.getFont());
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected final void okPressed() {
+		_credentials = new Credentials(this._username.getText(), 
+										this._password.getText());
+		// reset the username & password field
+		_username = null;
+		_password = null;
+		super.okPressed();
+	}
 
-        // user name
-        Label label = new Label(composite, SWT.NONE);
-        label.setText(Messages.LoginDialog_USER_NAME);
-        _username = new Text(composite, SWT.BORDER | SWT.FLAT);
-        _username.setFocus();
-        _username.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL
-                | GridData.FILL_HORIZONTAL));
-
-        // password
-        label = new Label(composite, SWT.NONE);
-        label.setText(Messages.LoginDialog_PASSWORD);
-        _password = new Text(composite, SWT.BORDER | SWT.FLAT | SWT.PASSWORD);
-        _password.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL
-                | GridData.FILL_HORIZONTAL));
-        _password.setEchoChar('*');
-
-        return control;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected final void okPressed() {
-        _credentials = new Credentials(this._username.getText(),
-                                        this._password.getText());
-        // reset the username & password field
-        _username = null;
-        _password = null;
-        super.okPressed();
-    }
-
-    /**
-     * Opens the login window and queries the user
-     * for credentials which it returns.
-     */
-    public Credentials getCredentials() {
-        _credentials = null;
-        this.setBlockOnOpen(true);
-        this.open();
-        return _credentials;
-    }
+	/**
+	 * Opens the login window and queries the user
+	 * for credentials which it returns.
+	 */
+	public Credentials getCredentials() {
+		_credentials = null;
+		this.setBlockOnOpen(true);
+		this.open();
+		return _credentials;
+	}
 }
