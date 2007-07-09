bd2b43f0668051aa3962c5f3259b054d0827eee0
Kay Kasemir <kasemirk@ornl.gov>
2007-07-09 20:52:18 +0000

    imports

--- applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/configview/ConfigView.java
+++ applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/configview/ConfigView.java
@@ -49,7 +49,6 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.TabFolder;
