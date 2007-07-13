4fd01f1e813188e0ca4ed080cffb18e85606cb16
Helge Rickens <helge.rickens@desy.de>
2007-07-13 13:23:05 +0000

    add getCollectionAsXMLFile

--- core/plugins/org.csstudio.platform/src/org/csstudio/platform/statistic/CollectorSupervisor.java
+++ core/plugins/org.csstudio.platform/src/org/csstudio/platform/statistic/CollectorSupervisor.java
@@ -1,6 +1,13 @@
 package org.csstudio.platform.statistic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.Vector;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 
 public class CollectorSupervisor {
 	
@@ -52,7 +59,80 @@ public class CollectorSupervisor {
 		}
 		return result;
 	}
-
+    public File getCollectionAsXMLFile () {
+        final String[] _COLUMN_NAMES = new String[] {"Application","Descriptor","Counter","Actual value","Date","Count","Highest Value","Date","Count","Lowest Value","Date","Count","Mean Value abs","Mean Value rel.","Alarm Limit (abs)","Alarm Limit (rel)","Hard Limit"};
+        File xmlFile = new File("XMLStatisticFile.xml");
+        XMLOutputFactory factory = XMLOutputFactory.newInstance(); 
+        try {
+            XMLStreamWriter writer = factory.createXMLStreamWriter(new FileOutputStream(xmlFile));
+            writer.writeStartDocument("0.1");
+            writer.writeStartElement("Collection Supervisor");
+                writer.writeAttribute("size", collectorVector.size()+"");
+                for (Collector collector : collectorVector) {
+                    int i=0;
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                        writer.writeCharacters(collector.getApplication());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getDescriptor());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getCount().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getActualValue().getValue().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getActualValue().getTime().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getActualValue().getCount().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getHighestValue().getValue().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getHighestValue().getTime().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getHighestValue().getCount().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getLowestValue().getValue().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getLowestValue().getTime().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getLowestValue().getCount().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getMeanValueAbsolute().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getMeanValuerelative().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getAlarmHandler().getHighAbsoluteLimit().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getAlarmHandler().getHighRelativeLimit().toString());
+                    writer.writeEndElement();
+                    writer.writeStartElement(_COLUMN_NAMES[i++]);
+                    writer.writeCharacters(collector.getHardLimit().toString());
+                    writer.writeEndElement();
+                }
+                writer.writeEndElement();
+            writer.writeEndDocument();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (XMLStreamException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+        return xmlFile;
+    }
 	public Vector<Collector> getCollectorVector() {
 		return collectorVector;
 	}
