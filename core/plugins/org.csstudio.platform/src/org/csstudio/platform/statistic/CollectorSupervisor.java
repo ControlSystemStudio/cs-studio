95f9b86d035a051d3030fb4b111f5116dbaac938
Helge Rickens <helge.rickens@desy.de>
2007-07-18 07:43:21 +0000

    change getCollectionAsXMLFile to getCollectionAsXMLString and finshed it

--- core/plugins/org.csstudio.platform/src/org/csstudio/platform/statistic/CollectorSupervisor.java
+++ core/plugins/org.csstudio.platform/src/org/csstudio/platform/statistic/CollectorSupervisor.java
@@ -1,149 +1,168 @@
 package org.csstudio.platform.statistic;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.StringWriter;
 import java.util.Vector;
 
+import org.jdom.DocType;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
 
-public class CollectorSupervisor {
-	
-	private static CollectorSupervisor thisCollectorSupervisor= null;
-	
-	private Vector<Collector> collectorVector = null;
-	
-	public CollectorSupervisor () {
-		
-		collectorVector= new Vector<Collector>();
-	}
-	
-	public static CollectorSupervisor getInstance() {
-		//
-		// get an instance of our sigleton
-		//
-		if ( thisCollectorSupervisor == null) {
-			synchronized (CollectorSupervisor.class) {
-				if (thisCollectorSupervisor == null) {
-					thisCollectorSupervisor = new CollectorSupervisor();
-				}
-			}
-		}
-		return thisCollectorSupervisor;
-	}
-	
-	public void printCollection () {
-		/*
-		 * print all actuall collections
-		 */
-		System.out.println("======== Collection Supervisor - Printout overview  ================");
-		System.out.println("Vector-Size: " + collectorVector.size());
-		String singleStatus = null;
-		for ( int i = 0; i< collectorVector.size(); i++) {
-			singleStatus = collectorVector.elementAt(i).getCollectorStatus();
-			System.out.print(singleStatus);
-		}
-	}
-	
-	public String getCollectionAsString () {
-		/*
-		 * print all actuall collections
-		 */
-		String result = "======== Collection Supervisor - Printout overview  ================";
-		result += "Vector-Size: " + collectorVector.size();
-
-		for ( int i = 0; i< collectorVector.size(); i++) {
-			result += collectorVector.elementAt(i).getCollectorStatus();
-		}
-		return result;
-	}
+public final class CollectorSupervisor {
     
-    public File getCollectionAsXMLFile () {
-        /*
-        final String[] _COLUMN_NAMES = new String[] {"Application","Descriptor","Counter","Actual value","Date","Count","Highest Value","Date","Count","Lowest Value","Date","Count","Mean Value abs","Mean Value rel.","Alarm Limit (abs)","Alarm Limit (rel)","Hard Limit"};
-        File xmlFile = new File("XMLStatisticFile.xml");
-        XMLOutputFactory factory = XMLOutputFactory.newInstance(); 
-        try {
-            XMLStreamWriter writer = factory.createXMLStreamWriter(new FileOutputStream(xmlFile));
-            writer.writeStartDocument("0.1");
-            writer.writeStartElement("Collection Supervisor");
-                writer.writeAttribute("size", collectorVector.size()+"");
-                for (Collector collector : collectorVector) {
-                    int i=0;
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                        writer.writeCharacters(collector.getApplication());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getDescriptor());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getCount().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getActualValue().getValue().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getActualValue().getTime().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getActualValue().getCount().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getHighestValue().getValue().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getHighestValue().getTime().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getHighestValue().getCount().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getLowestValue().getValue().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getLowestValue().getTime().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getLowestValue().getCount().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getMeanValueAbsolute().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getMeanValuerelative().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getAlarmHandler().getHighAbsoluteLimit().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getAlarmHandler().getHighRelativeLimit().toString());
-                    writer.writeEndElement();
-                    writer.writeStartElement(_COLUMN_NAMES[i++]);
-                    writer.writeCharacters(collector.getHardLimit().toString());
-                    writer.writeEndElement();
-                }
-                writer.writeEndElement();
-            writer.writeEndDocument();
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (XMLStreamException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } 
-        return xmlFile;
-        */
-        return null;
+    private static CollectorSupervisor thisCollectorSupervisor= null;
+    
+    private Vector<Collector> collectorVector = null;
+    
+    public CollectorSupervisor () {
+        
+        collectorVector= new Vector<Collector>();
     }
-	public Vector<Collector> getCollectorVector() {
-		return collectorVector;
-	}
+    
+    public static CollectorSupervisor getInstance() {
+        //
+        // get an instance of our sigleton
+        //
+        if ( thisCollectorSupervisor == null) {
+            synchronized (CollectorSupervisor.class) {
+                if (thisCollectorSupervisor == null) {
+                    thisCollectorSupervisor = new CollectorSupervisor();
+                }
+            }
+        }
+        return thisCollectorSupervisor;
+    }
+    
+    public void printCollection () {
+        /*
+         * print all actuall collections
+         */
+        System.out.println("======== Collection Supervisor - Printout overview  ================");
+        System.out.println("Vector-Size: " + collectorVector.size());
+        String singleStatus = null;
+        for ( int i = 0; i< collectorVector.size(); i++) {
+            singleStatus = collectorVector.elementAt(i).getCollectorStatus();
+            System.out.print(singleStatus);
+        }
+    }
+    
+    public String getCollectionAsString () {
+        /*
+         * print all actuall collections
+         */
+        String result = "======== Collection Supervisor - Printout overview  ================";
+        result += "Vector-Size: " + collectorVector.size();
 
-	public void setCollectorVector(Vector<Collector> collectorVector) {
-		this.collectorVector = collectorVector;
-	}
-	
-	public void addCollector( Collector collector) {
-		collectorVector.add( collector);
-	}
+        for ( int i = 0; i< collectorVector.size(); i++) {
+            result += collectorVector.elementAt(i).getCollectorStatus();
+        }
+        return result;
+    }
+    
+    public final String getCollectionAsXMLString () {
+        final String[] _COLUMN_NAMES = new String[] {"Application","Descriptor","Counter","Actual value","Date","Count","Highest Value","Date","Count","Lowest Value","Date","Count","Mean Value abs","Mean Value rel.","Alarm Limit abs","Alarm Limit rel","Hard Limit"};
+//        File xmlFile = new File("c:\\tmp\\XMLStatisticFile.xml");
+        System.out.println("Make new Doc");
+        try{
+            DocType dt = new DocType("CSS-StatisticProtocol");
+            Element root = new Element("StatisticProtocol");
+            root.setAttribute("Version","0.1");
+            Document d = new Document(root,dt);
+            Element collectionSupervisor = new Element("CollectionSupervisor");
+            collectionSupervisor.setAttribute("size",collectorVector.size()+"");
+            root.addContent(collectionSupervisor);
+            for (Collector collector : collectorVector) {
+                int i=0;
+                Element collectorElement = new Element("Collector");
+                collectionSupervisor.addContent(collectorElement);
+                Element coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                coulmn.setText(collector.getApplication());
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                coulmn.setText(collector.getDescriptor());
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                coulmn.setText(collector.getCount().toString());
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                coulmn.setText(collector.getActualValue().getValue().toString());
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                coulmn.setText(collector.getActualValue().getTime().toString());
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                coulmn.setText(collector.getActualValue().getCount().toString());
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                coulmn.setText(collector.getHighestValue().getValue().toString());
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                coulmn.setText(collector.getHighestValue().getTime().toString());
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                coulmn.setText(collector.getHighestValue().getCount().toString());
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                if(collector.getLowestValue().getValue()!=null){
+                    coulmn.setText(collector.getLowestValue().getValue().toString());
+                }
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                if(collector.getLowestValue().getTime()!=null){
+                    coulmn.setText(collector.getLowestValue().getTime().toString());
+                }
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                if(collector.getLowestValue().getCount()!=null){
+                    coulmn.setText(collector.getLowestValue().getCount().toString());
+                }
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                if(collector.getMeanValueAbsolute()!=null){
+                    coulmn.setText(collector.getMeanValueAbsolute().toString());
+                }
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                if(collector.getMeanValuerelative()!=null){
+                    coulmn.setText(collector.getMeanValuerelative().toString());
+                }
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                if(collector.getAlarmHandler().getHighAbsoluteLimit()!=null){
+                    coulmn.setText(collector.getAlarmHandler().getHighAbsoluteLimit().toString());
+                }
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                if(collector.getAlarmHandler().getHighRelativeLimit()!=null){
+                    coulmn.setText(collector.getAlarmHandler().getHighRelativeLimit().toString());
+                }
+                collectorElement.addContent(coulmn);
+                coulmn = new Element(_COLUMN_NAMES[i++].replace(' ', '_'));
+                if(collector.getHardLimit()!=null){
+                    coulmn.setText(collector.getHardLimit().toString());
+                }
+                collectorElement.addContent(coulmn);
+            }
+            StringWriter sw = new StringWriter();
+            XMLOutputter xmlOutputter = new XMLOutputter();
+            xmlOutputter.output(d, sw);
+            return sw.toString();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+    public Vector<Collector> getCollectorVector() {
+        return collectorVector;
+    }
+
+    public void setCollectorVector(Vector<Collector> collectorVector) {
+        this.collectorVector = collectorVector;
+    }
+    
+    public void addCollector( Collector collector) {
+        collectorVector.add( collector);
+    }
 
 }
