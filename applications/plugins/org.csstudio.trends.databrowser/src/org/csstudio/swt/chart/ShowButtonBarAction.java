a4afb55da2550e9e21e623a6632b7d1410574aa3
Kay Kasemir <kasemirk@ornl.gov>
2007-07-19 19:14:37 +0000

    TODO handled by accepting JFace dependency

--- applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/swt/chart/ShowButtonBarAction.java
+++ applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/swt/chart/ShowButtonBarAction.java
@@ -8,11 +8,6 @@ import org.eclipse.jface.action.Action;
  *  Suggested use is in the context menu of an editor or view that
  *  uses the InteractiveChart.
  *  
- *  TODO packaging. Most users of the InteractiveChart will want to put this
- *       action in a toolbar or context menu, so this way we get consistency.
- *       On the other hand, this adds a JFace dependency to 
- *       org.csstudio.swt.chart...
- *  
  *  @author Kay Kasemir
  */
 public class ShowButtonBarAction extends Action
