cc25fa7594a58f3abd94838442fbb7e544ddd9ae
Kay Kasemir <kasemirk@ornl.gov>
2007-07-26 19:04:33 +0000

    import

---
+++ applications/plugins/org.csstudio.apputil/src/org/csstudio/util/xml/XMLHelper.java
@@ -0,0 +1,55 @@
+package org.csstudio.util.xml;
+
+/** Helper for XML output.
+ *  
+ *  @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class XMLHelper
+{
+    /** @return Returns string for intentation to given level. */
+    public static final void indent(StringBuffer buf, int level)
+    {
+        while (level > 0)
+        {
+            buf.append("    ");
+            --level;
+        }
+    }
+    
+    /** @return Returns text with less-than and ampersands replaced by XML escapes.
+     *  @param text
+     */
+    public static final String escapeXMLstring(String text)
+    {
+        StringBuffer b = new StringBuffer(text.length()+3);
+        int i;
+        for (i=0; i<text.length(); ++i)
+        {
+            char c = text.charAt(i);
+            // Escape '&' into '&amp;'.
+            if (c == '&')
+                b.append("&amp;");
+            // Escape '<' into '&lt;'.
+            else if (c == '<')
+                b.append("&lt;");
+            else if (c < 32 || c > 126)
+            {   // Other non-printable. Exact definition not clear.
+                b.append("&#");
+                b.append((int) c);
+                b.append(";");            
+            }
+            else
+                b.append(c);
+        }
+        return b.toString();
+    }
+
+    public static final void XML(StringBuffer buf, int level, String tag, String value)
+    {
+        indent(buf, level);
+        buf.append("<").append(tag).append(">");
+        buf.append(escapeXMLstring(value));
+        buf.append("</").append(tag).append(">\n");
+    }
+}
