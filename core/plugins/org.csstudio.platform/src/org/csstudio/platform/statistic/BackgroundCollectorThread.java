73546d2ca210e15810dabc4a74b5b032c6628cfc
claus <claus>
2007-07-31 15:08:49 +0000

    Add max Memory to statitic collector

--- core/plugins/org.csstudio.platform/src/org/csstudio/platform/statistic/BackgroundCollectorThread.java
+++ core/plugins/org.csstudio.platform/src/org/csstudio/platform/statistic/BackgroundCollectorThread.java
@@ -6,6 +6,7 @@ public class BackgroundCollectorThread extends Thread{
 	
 	private int	timeout	= 0;
 	private boolean runForever	= true;
+	final static double MB = 1024.0*1024.0;
 	
 	BackgroundCollectorThread (  int timeout) {
 		this.timeout = timeout;
@@ -17,13 +18,13 @@ public void run() {
 	
 	while (runForever) {
 		
-		BackgroundCollector.getInstance().getMemoryAvailableApplication().setValue( new Double(Runtime.getRuntime().totalMemory()/1000.0));
-		BackgroundCollector.getInstance().getMemoryUsedApplication().setValue( new Double(Runtime.getRuntime().maxMemory()/1000.0));
+		BackgroundCollector.getInstance().getMemoryAvailableApplication().setValue( new Double(Runtime.getRuntime().freeMemory()/MB));
+		BackgroundCollector.getInstance().getMemoryUsedApplication().setValue( new Double(Runtime.getRuntime().totalMemory()/MB));
+		BackgroundCollector.getInstance().getMemoryUsedSystem().setValue( new Double(Runtime.getRuntime().maxMemory()/MB));
 //		TODO: find out how to fill these!
 //		before uncommenting: enable instanciating in BackgroundCollector!!
 //		BackgroundCollector.getInstance().getCpuUsedApplication().setValue
 //		BackgroundCollector.getInstance().getCpuUsedSystem().setValue
-//		BackgroundCollector.getInstance().getMemoryUsedSystem().setValue
 		
 			try {
 				Thread.sleep(this.timeout);
