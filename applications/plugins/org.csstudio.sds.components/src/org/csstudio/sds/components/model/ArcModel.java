19d6d0f10842c418aedaabc8fcd87cb81e43d7e6
kmeyer <kmeyer>
2007-08-30 19:43:55 +0000

    Kommentare angepasst

--- applications/plugins/org.csstudio.sds.components/src/org/csstudio/sds/components/model/ArcModel.java
+++ applications/plugins/org.csstudio.sds.components/src/org/csstudio/sds/components/model/ArcModel.java
@@ -20,15 +20,33 @@ public final class ArcModel extends AbstractWidgetModel {
 	public static final String ID = "org.csstudio.sds.components.Arc";
 	
 	/**
-	 * The IDs of the properties.
+	 * The ID of the <i>transparent</i> property.
 	 */
 	public static final String PROP_TRANSPARENT = "transparent_background";
+	/**
+	 * The IDs of the <i>startangle</i> property.
+	 */
 	public static final String PROP_STARTANGLE = "start_angle";
+	/**
+	 * The IDs of the <i>angle</i> property.
+	 */
 	public static final String PROP_ANGLE = "angle";
+	/**
+	 * The IDs of the <i>linewidth</i> property.
+	 */
 	public static final String PROP_LINEWIDTH = "linewidth";
+	/**
+	 * The IDs of the <i>filled</i> property.
+	 */
 	public static final String PROP_FILLED = "filled";
+	/**
+	 * The IDs of the <i>fillcolor</i> property.
+	 */
 	public static final String PROP_FILLCOLOR = "color.fill";
 
+	/**
+	 * Constructor.
+	 */
 	public ArcModel () {
 		setWidth(50);
 		setHeight(50);
@@ -54,26 +72,50 @@ public final class ArcModel extends AbstractWidgetModel {
 		addProperty(PROP_FILLCOLOR, new ColorProperty("Fill Color",WidgetPropertyCategory.Display,new RGB(255,0,0)));
 	}
 
+	/**
+	 * Returns the transparent state of the background.
+	 * @return true, if the background is transparent, false otherwise
+	 */
 	public boolean getTransparent() {
 		return (Boolean) getProperty(PROP_TRANSPARENT).getPropertyValue();
 	}
 	
+	/**
+	 * Returns the value for the start angle. 
+	 * @return The value for the start angle
+	 */
 	public int getStartAngle() {
 		return (Integer) getProperty(PROP_STARTANGLE).getPropertyValue();
 	}
 	
+	/**
+	 * Returns the value for the angle.
+	 * @return The value for the angle
+	 */
 	public int getAngle() {
 		return (Integer) getProperty(PROP_ANGLE).getPropertyValue();
 	}
 	
+	/**
+	 * Returns the width of the arc.
+	 * @return The width of the arc
+	 */
 	public int getLineWidth() {
 		return (Integer) getProperty(PROP_LINEWIDTH).getPropertyValue();
 	}
 	
+	/**
+	 * Returns the fill state of the arc.
+	 * @return true, if the arc should be filled, false otherwise
+	 */
 	public boolean getFill() {
 		return (Boolean) getProperty(PROP_FILLED).getPropertyValue();
 	}
 	
+	/**
+	 * Returns the fill color of the arc.
+	 * @return The fill color of the arc
+	 */
 	public RGB getFillColor() {
 		return (RGB) getProperty(PROP_FILLCOLOR).getPropertyValue();
 	}
