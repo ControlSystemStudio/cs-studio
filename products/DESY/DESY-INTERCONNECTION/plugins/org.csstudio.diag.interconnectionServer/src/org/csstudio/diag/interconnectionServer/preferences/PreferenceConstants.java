624dc15fd7621759ff1651099fc8ccb6824ba38e
Jan Hatje <jan.hatje@desy.de>
2007-10-10 11:21:29 +0000

    using new preference deployment

--- products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/preferences/PreferenceConstants.java
+++ products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/preferences/PreferenceConstants.java
@@ -14,8 +14,8 @@ public class PreferenceConstants {
 	public static final String JMS_TIME_TO_LIVE_ALARMS = "jmsTimeToLiveAlarms";
 	public static final String JMS_TIME_TO_LIVE_LOGS = "jmsTimeToLiveLogs";
 	public static final String JMS_TIME_TO_LIVE_PUT_LOGS = "jmsTimeToLivePutLogs";
-	public static final String PRIMARY_JMS_URL = "failover:(tcp://krynfs.desy.de:62616,tcp://krykjmsb.desy.de:64616)?maxReconnectDelay=500,maxReconnectAttempts=50";
-	public static final String SECONDARY_JMS_URL = "failover:(tcp://krykjmsb.desy.de:64616,tcp://krynfs.desy.de:62616)?maxReconnectDelay=500,maxReconnectAttempts=50";
+	public static final String PRIMARY_JMS_URL = "primaryJmsUrl";
+	public static final String SECONDARY_JMS_URL = "secondaryJmsUrl";
 
 	
 }
