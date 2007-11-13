eb280de6e0e7659b5fead730737ea1acb888fa2d
kmeyer <kmeyer>
2007-11-13 16:13:54 +0000

    Neues Widget erstellt. Das Timer-Widget soll als Grundlage dienen, um Skripte nach einer bestimmten Zeit automatisch auszuf?hren.

---
+++ applications/plugins/org.csstudio.sds.components/src/org/csstudio/sds/components/model/TimerModel.java
@@ -0,0 +1,143 @@
+/* 
+ * Copyright (c) 2006 Stiftung Deutsches Elektronen-Synchroton, 
+ * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY.
+ *
+ * THIS SOFTWARE IS PROVIDED UNDER THIS LICENSE ON AN "../AS IS" BASIS. 
+ * WITHOUT WARRANTY OF ANY KIND, EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED 
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR PARTICULAR PURPOSE AND 
+ * NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE. SHOULD THE SOFTWARE PROVE DEFECTIVE 
+ * IN ANY RESPECT, THE USER ASSUMES THE COST OF ANY NECESSARY SERVICING, REPAIR OR 
+ * CORRECTION. THIS DISCLAIMER OF WARRANTY CONSTITUTES AN ESSENTIAL PART OF THIS LICENSE. 
+ * NO USE OF ANY SOFTWARE IS AUTHORIZED HEREUNDER EXCEPT UNDER THIS DISCLAIMER.
+ * DESY HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, 
+ * OR MODIFICATIONS.
+ * THE FULL LICENSE SPECIFYING FOR THE SOFTWARE THE REDISTRIBUTION, MODIFICATION, 
+ * USAGE AND OTHER RIGHTS AND OBLIGATIONS IS INCLUDED WITH THE DISTRIBUTION OF THIS 
+ * PROJECT IN THE FILE LICENSE.HTML. IF THE LICENSE IS NOT INCLUDED YOU MAY FIND A COPY 
+ * AT HTTP://WWW.DESY.DE/LEGAL/LICENSE.HTM
+ */
+package org.csstudio.sds.components.model;
+
+import org.csstudio.sds.model.AbstractWidgetModel;
+import org.csstudio.sds.model.WidgetPropertyCategory;
+import org.csstudio.sds.model.properties.IntegerProperty;
+import org.csstudio.sds.model.properties.StringProperty;
+
+/**
+ * This class defines a timer widget model.
+ * 
+ * @author Kai Meyer
+ * @version $Revision$
+ * 
+ */
+public final class TimerModel extends AbstractWidgetModel {
+	/**
+	 * The ID of the fill grade property.
+	 */
+	public static final String PROP_SCRIPT = "script"; //$NON-NLS-1$
+	
+	/**
+	 * The ID of the orientation property.
+	 */
+	public static final String PROP_DELAY = "delay"; //$NON-NLS-1$
+
+	/**
+	 * The ID of this widget model.
+	 */
+	public static final String ID = "org.csstudio.sds.components.Timer"; //$NON-NLS-1$
+
+	/**
+	 * The default value of the height property.
+	 */
+	
+	private static final int DEFAULT_HEIGHT = 16;
+
+	/**
+	 * The default value of the width property.
+	 */
+	private static final int DEFAULT_WIDTH = 16;
+
+	/**
+	 * The default value of the fill grade property.
+	 */
+	private static final int DEFAULT_DELAY = 1000;
+
+	/**
+	 * Standard constructor.
+	 */
+	public TimerModel() {
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getTypeID() {
+		return ID;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void configureProperties() {
+		addProperty(PROP_SCRIPT, new StringProperty("Script",
+				WidgetPropertyCategory.Behaviour, ""));
+		addProperty(PROP_DELAY, new IntegerProperty("Delay (in ms)", 
+				WidgetPropertyCategory.Behaviour, DEFAULT_DELAY, 0, Integer.MAX_VALUE));
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void markPropertiesAsInvisible() {
+		this.markPropertyAsInvisible(PROP_ACTIONDATA);
+		this.markPropertyAsInvisible(PROP_BORDER_COLOR);
+		this.markPropertyAsInvisible(PROP_BORDER_STYLE);
+		this.markPropertyAsInvisible(PROP_BORDER_WIDTH);
+		this.markPropertyAsInvisible(PROP_COLOR_BACKGROUND);
+		this.markPropertyAsInvisible(PROP_COLOR_FOREGROUND);
+		this.markPropertyAsInvisible(PROP_HEIGHT);
+		this.markPropertyAsInvisible(PROP_POS_X);
+		this.markPropertyAsInvisible(PROP_POS_Y);
+		this.markPropertyAsInvisible(PROP_VISIBILITY);
+		this.markPropertyAsInvisible(PROP_WIDTH);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected String getDefaultToolTip() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(createParameter(PROP_NAME)+"\n");
+		buffer.append("Script:\t");
+		buffer.append(createParameter(PROP_SCRIPT)+"\n");
+		buffer.append("Delay:\t");
+		buffer.append(createParameter(PROP_DELAY));
+		return buffer.toString();
+	}
+
+	/**
+	 * Gets the script.
+	 * 
+	 * @return the script
+	 */
+	public String getScript() {
+		return (String) getProperty(PROP_SCRIPT).getPropertyValue();
+	}
+	
+	/**
+	 * Gets the delay.
+	 * 
+	 * @return the delay.
+	 */
+	public int getDelay() {
+		return (Integer) getProperty(PROP_DELAY).getPropertyValue();
+	}
+}
