65dcfafd07154ef9b227a8bf0888928bc943f1db
claus <claus>
2008-01-15 09:07:13 +0000

    Get list including status

--- products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/GetListOfClients.java
+++ products/DESY/DESY-INTERCONNECTION/plugins/org.csstudio.diag.interconnectionServer/src/org/csstudio/diag/interconnectionServer/GetListOfClients.java
@@ -7,7 +7,7 @@ import org.csstudio.platform.libs.dcf.actions.IAction;
 public class GetListOfClients implements IAction {
 
 	public Object run(Object param) {
-		return Statistic.getInstance().getNodeNameArray();
+		return Statistic.getInstance().getNodeNameStatusArray();
 		//return PreferenceProperties.COMMAND_LIST;  //commandos
 	}
 
