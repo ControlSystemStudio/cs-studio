4b74c520f0e796d153878533502efca6e3b4f0d4
Kay Kasemir <kasemirk@ornl.gov>
2008-02-13 18:52:21 +0000

    printing!!

--- applications/plugins/org.csstudio.swt.chart/src/org/csstudio/swt/chart/actions/PrintImageAction.java
+++ applications/plugins/org.csstudio.swt.chart/src/org/csstudio/swt/chart/actions/PrintImageAction.java
@@ -6,12 +6,13 @@ import org.csstudio.swt.chart.Messages;
 import org.eclipse.jface.action.Action;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.printing.PrintDialog;
 import org.eclipse.swt.printing.Printer;
 import org.eclipse.swt.printing.PrinterData;
 
-/** An Action for saving the current image to a file.
+/** An Action for printing the current image.
  *  <p>
  *  Suggested use is in the context menu of an editor or view that
  *  uses the InteractiveChart.
@@ -21,6 +22,8 @@ import org.eclipse.swt.printing.PrinterData;
 public class PrintImageAction extends Action
 {
     private final Chart chart;
+    private Image snapshot;
+    private Printer printer;
 
     /** Constructor */
     public PrintImageAction(Chart chart)
@@ -35,47 +38,69 @@ public class PrintImageAction extends Action
     @Override
     public void run()
     {
-        final Image snapshot = chart.createSnapshot();
+        snapshot = chart.createSnapshot();
         if (snapshot == null)
+            return;
+        
+        // Printer GUI
+        PrintDialog dlg = new PrintDialog(chart.getShell());
+        PrinterData data = dlg.open();
+        if (data == null)
+            return;
+        // Get filename
+        if (data.printToFile == true)
+            data.fileName = ImageFileName.get(chart.getShell());
+        printer = new Printer(data);
+        // Print in background thread
+        final Thread print_thread = new Thread("Print Thread") //$NON-NLS-1$
+        {
+            @Override
+            public void run()
+            {
+                print();
+            }
+        };
+        print_thread.start();
+    }
+
+    private void print()
+    {
+        if (!printer.startJob("Data Browser")) //$NON-NLS-1$
             return;
         try
         {
-            // Printer GUI
-            PrintDialog dlg = new PrintDialog(chart.getShell());
-            PrinterData data = dlg.open();
-            if (data == null)
-                return;
-            // Get filename
-            if (data.printToFile == true)
-                data.fileName = ImageFileName.get(chart.getShell());
-            // Print
-            final Printer printer = new Printer(data);
-            // ... in background thread
-            final Thread print_thread = new Thread("Print Thread") //$NON-NLS-1$
-            {
-                @Override
-                public void run()
-                {
-                    if (!printer.startJob("Data Browser"))
-                        return;
-                    // TODO Compute layout
-                    final Rectangle area = printer.getClientArea();
-                    final Rectangle trim = printer.computeTrim(0, 0, 0, 0);
-                    final GC gc = new GC(printer);
-                    
-                    // Print one page
-                    printer.startPage();
-                    gc.drawImage(snapshot, 0, 0);
-                    printer.endPage();
-                    // Done
-                    printer.endJob();
-                }
-            };
-            print_thread.start();
+            // Printer page info
+            final Rectangle area = printer.getClientArea();
+            final Rectangle trim = printer.computeTrim(0, 0, 0, 0);
+            final Point dpi = printer.getDPI();
+            
+            // Compute layout
+            final Rectangle image_rect = snapshot.getBounds();
+            // Leave one inch on each border
+            final int left_right = dpi.x + trim.x;
+            final int top_bottom = dpi.y + trim.y;
+            final int printed_width = area.width - 2*left_right;
+            // Scale height to keep the on-screen aspect ratio
+            final int max_height = area.height - 2*top_bottom;
+            final int printed_height = Math.min(max_height,
+               image_rect.height * printed_width / image_rect.width);
+            
+            final GC gc = new GC(printer);
+            
+            // Print one page
+            printer.startPage();
+            gc.drawImage(snapshot, 0, 0, image_rect.width, image_rect.height,
+                            left_right, top_bottom, printed_width, printed_height);
+            
+            printer.endPage();
+            // Done
+            printer.endJob();
         }
         finally
         {
             snapshot.dispose();
         }
     }
+
 }
+
