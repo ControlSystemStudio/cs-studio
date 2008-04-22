76ac9d31a52a0c8944150d12fd43239346146f04
Kay Kasemir <kasemirk@ornl.gov>
2008-04-22 17:35:20 +0000

    Avoid endless loop when requesting start/end range that has no data, but there are many samples with same time and value just before start.

--- applications/plugins/org.csstudio.archive/src/org/csstudio/archive/crawl/BatchIterator.java
+++ applications/plugins/org.csstudio.archive/src/org/csstudio/archive/crawl/BatchIterator.java
@@ -86,10 +86,7 @@ public class BatchIterator
             return;
         // Empty samples?
         if (samples.length < 1)
-        {
             samples = null;
-            return;
-        }
     }
 
     /** @return Returns the current batch of samples or <code>null</code>
@@ -116,7 +113,7 @@ public class BatchIterator
         if (samples == null)
             return null;
         
-        // On many cases, this fetch should return the last_sample again:
+        // In most cases, this fetch should return the last_sample again:
         //   some_timestamp value A
         //   last_timestamp value B <-- last_sample of previous batch
         //   new_timestamp  value C
@@ -124,47 +121,37 @@ public class BatchIterator
         // we get the last_sample once more and need to skip it.
         //
         // But also consider the following situation, where the last batch ended
-        // in a range of data that had the same time stamp:
+        // in a range of data that had the same time stamp, and even some same
+        // values:
         //   some_timestamp value A
         //   last_timestamp value B
+        //   last_timestamp value C
+        //   last_timestamp value C
         //   last_timestamp value C <-- last_sample of previous batch
+        //   last_timestamp value C
+        //   last_timestamp value C
         //   last_timestamp value D
         //   last_timestamp value E
         //   new_timestamp  value F
         // Reasons for same timestamps: Stuck IOC clock,
-        // or sequences like .. Repeat N, next value, Disconnected, Arch. Off
+        // or sequences like .. Repeat N, next value, Disconnected, Arch. Off.
+        // Reason for the same value: General mess-up.
         //
         // When we request new data from 'last_sample.getTime()' on,
-        // i.e. from last_timestamp on, we get value E, since that's
-        // stamped at-or-before last_timestamp.
-        // So we never see value D. Probably OK, since how many values
-        // can you show for the same time stamp?
-        //
-        // But also handle the case where a different data server might
-        // start at value B, i.e. the first value with last_timestamp.
+        // i.e. from last_timestamp on, we could get any of the values B to E,
+        // since they're all stamped at-or-before last_timestamp.
+        // Which one exactly depends on optimization inside the data server.
         
-        int skip = 0;
-        if (samples[0].equals(last_sample))
-        {   // Got a new batch that starts with the last known sample. Skip it.
-            skip = 1;
-        }
-        else
-        {   // See if we find the last_sample again.
-            for (int i=0;  i<samples.length;  ++i)
-            {
-                final IValue skip_sample = samples[i];
-                // If we reach new samples, we're done
-                if (skip_sample.getTime().isGreaterThan(last_sample.getTime()))
-                    break;
-                // If we find the previous batch's last sample...
-                if (skip_sample.equals(last_sample))
-                {   // Skip all the samples up to and including it
-                    skip = i + 1;
-                    break;
-                }
+        // From the end of the new samples, go backward:
+        int skip = samples.length;
+        for (int i=skip-1;  i>=0;  --i)
+        {   // If we find the previous batch's last sample...
+            if (samples[i].equals(last_sample))
+            {   // Skip all the samples up to and including it
+                skip = i + 1;
+                break;
             }
-        }
-        
+        }        
         // Nothing to skip? Return as is.
         if (skip <= 0)
             return samples;
@@ -176,8 +163,9 @@ public class BatchIterator
         }
         // Reduce samples to those that are actually new
         IValue new_samples[] = new IValue[samples.length - skip];
-        for (int i=0;  i<new_samples.length;  ++i)
-            new_samples[i] = samples[skip + i];
+        System.arraycopy(samples, skip, new_samples, 0, new_samples.length);
+        //for (int i=0;  i<new_samples.length;  ++i)
+        //    new_samples[i] = samples[skip + i];
         samples = new_samples;
         return samples;
     }
