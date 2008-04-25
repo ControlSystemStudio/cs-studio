7a26f7451f206551561d0947f4294f7e73599cd1
mzeimer <mzeimer>
2008-04-25 16:31:28 +0000

    *** empty log message ***

---
+++ applications/plugins/org.csstudio.nams.service.messaging.impl.jms/src/org/csstudio/nams/service/messaging/impl/jms/ConsumerFactoryServiceFactoryImpl.java
@@ -0,0 +1,12 @@
+package org.csstudio.nams.service.messaging.impl.jms;
+
+import org.csstudio.nams.service.messaging.extensionPoint.ConsumerFactoryServiceFactory;
+
+public class ConsumerFactoryServiceFactoryImpl extends
+		ConsumerFactoryServiceFactory {
+
+	public ConsumerFactoryServiceFactoryImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ applications/plugins/org.csstudio.nams.service.messaging.impl.jms/src/org/csstudio/nams/service/messaging/impl/jms/ProducerFactoryServiceFactoryImpl.java
@@ -0,0 +1,12 @@
+package org.csstudio.nams.service.messaging.impl.jms;
+
+import org.csstudio.nams.service.messaging.extensionPoint.ProducerFactoryServiceFactory;
+
+public class ProducerFactoryServiceFactoryImpl extends
+		ProducerFactoryServiceFactory {
+
+	public ProducerFactoryServiceFactoryImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
