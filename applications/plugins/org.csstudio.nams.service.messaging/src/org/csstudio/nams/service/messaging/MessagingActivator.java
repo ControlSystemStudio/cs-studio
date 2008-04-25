cb12e464a1cd19867ae97b5f51524f936aa5c6e3
mzeimer <mzeimer>
2008-04-25 16:07:10 +0000

    *** empty log message ***

--- applications/plugins/org.csstudio.nams.service.messaging/src/org/csstudio/nams/service/messaging/MessagingActivator.java
+++ applications/plugins/org.csstudio.nams.service.messaging/src/org/csstudio/nams/service/messaging/MessagingActivator.java
@@ -1,5 +1,7 @@
 package org.csstudio.nams.service.messaging;
 
+import org.csstudio.nams.service.messaging.declaration.ConsumerFactoryService;
+import org.csstudio.nams.service.messaging.declaration.ProducerFactoryService;
 import org.csstudio.nams.service.messaging.extensionPoint.ConsumerFactoryServiceFactory;
 import org.csstudio.nams.service.messaging.extensionPoint.ProducerFactoryServiceFactory;
 import org.eclipse.core.runtime.IConfigurationElement;
@@ -12,9 +14,11 @@ import org.osgi.framework.BundleContext;
  */
 public class MessagingActivator extends Plugin {
 
+	private static final String NAME_OF_IMPLEMENTATION_ELEMENT_OF_EXTENSION_POINT = "implementation";
 	/** The plug-in ID */
 	public static final String PLUGIN_ID = "org.csstudio.nams.service.messaging";
-//	private ServiceTracker _ensureJustOneInstanceTracker;
+
+	// private ServiceTracker _ensureJustOneInstanceTracker;
 
 	/**
 	 * The constructor
@@ -29,27 +33,57 @@ public class MessagingActivator extends Plugin {
 		super.start(context);
 
 		// ConsumerFactoryServiceFactory
-		String extensionPointIdConsumerServiceFactory = ConsumerFactoryServiceFactory.class.getName();
-		IConfigurationElement[] elementsConsumerFactoryServiceFactory = Platform.getExtensionRegistry()
-				.getConfigurationElementsFor(extensionPointIdConsumerServiceFactory);
+		String extensionPointIdConsumerServiceFactory = ConsumerFactoryServiceFactory.class
+				.getName();
+		IConfigurationElement[] elementsConsumerFactoryServiceFactory = Platform
+				.getExtensionRegistry().getConfigurationElementsFor(
+						extensionPointIdConsumerServiceFactory);
 
 		if (elementsConsumerFactoryServiceFactory.length != 1) {
-			throw new RuntimeException("One and only one extension for extension point \""
-							+ extensionPointIdConsumerServiceFactory + "\" should be present in current runtime configuration!");
+			throw new RuntimeException(
+					"One and only one extension for extension point \""
+							+ extensionPointIdConsumerServiceFactory
+							+ "\" should be present in current runtime configuration!");
 		} else {
-			// TODO
+			Object executableExtension = elementsConsumerFactoryServiceFactory[0]
+					.createExecutableExtension(NAME_OF_IMPLEMENTATION_ELEMENT_OF_EXTENSION_POINT);
+			if (!(executableExtension instanceof ConsumerFactoryServiceFactory)) {
+				throw new RuntimeException("Only a extension of type "
+						+ ConsumerFactoryServiceFactory.class.getName()
+						+ " for extension point \""
+						+ extensionPointIdConsumerServiceFactory
+						+ "\" is valid!");
+			}
+			ConsumerFactoryServiceFactory factory = (ConsumerFactoryServiceFactory) executableExtension;
+			context.registerService(ConsumerFactoryService.class.getName(),
+					factory.createConsumerFactoryService(), null);
 		}
-		
+
 		// ProducerFactoryServiceFactory
-		String extensionPointIdProducerServiceFactory = ProducerFactoryServiceFactory.class.getName();
-		IConfigurationElement[] elementsProducerFactoryServiceFactory = Platform.getExtensionRegistry()
-				.getConfigurationElementsFor(extensionPointIdProducerServiceFactory);
+		String extensionPointIdProducerServiceFactory = ProducerFactoryServiceFactory.class
+				.getName();
+		IConfigurationElement[] elementsProducerFactoryServiceFactory = Platform
+				.getExtensionRegistry().getConfigurationElementsFor(
+						extensionPointIdProducerServiceFactory);
 
 		if (elementsProducerFactoryServiceFactory.length != 1) {
-			throw new RuntimeException("One and only one extension for extension point \""
-							+ extensionPointIdProducerServiceFactory + "\" should be present in current runtime configuration!");
+			throw new RuntimeException(
+					"One and only one extension for extension point \""
+							+ extensionPointIdProducerServiceFactory
+							+ "\" should be present in current runtime configuration!");
 		} else {
-			// TODO
+			Object executableExtension = elementsProducerFactoryServiceFactory[0]
+					.createExecutableExtension(NAME_OF_IMPLEMENTATION_ELEMENT_OF_EXTENSION_POINT);
+			if (!(executableExtension instanceof ProducerFactoryServiceFactory)) {
+				throw new RuntimeException("Only a extension of type "
+						+ ProducerFactoryServiceFactory.class.getName()
+						+ " for extension point \""
+						+ extensionPointIdProducerServiceFactory
+						+ "\" is valid!");
+			}
+			ProducerFactoryServiceFactory factory = (ProducerFactoryServiceFactory) executableExtension;
+			context.registerService(ProducerFactoryService.class.getName(),
+					factory.createProducerFactoryService(), null);
 		}
 	}
 
@@ -58,6 +92,6 @@ public class MessagingActivator extends Plugin {
 	 */
 	public void stop(BundleContext context) throws Exception {
 		super.stop(context);
-//		_ensureJustOneInstanceTracker.close();
+		// _ensureJustOneInstanceTracker.close();
 	}
 }
