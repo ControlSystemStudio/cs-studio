fb530cb8cffece6a7ada82b72185d5d3f99e4bf6
mzeimer <mzeimer>
2008-04-28 22:58:38 +0000

    *** empty log message ***

--- applications/plugins/org.csstudio.nams.application.department.decision/src/org/csstudio/nams/application/department/decision/DecisionDepartmentActivator.java
+++ applications/plugins/org.csstudio.nams.application.department.decision/src/org/csstudio/nams/application/department/decision/DecisionDepartmentActivator.java
@@ -1,101 +1,144 @@
+/* 
+ * Copyright (c) C1 WPS mbH, HAMBURG, GERMANY. All Rights Reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED UNDER THIS LICENSE ON AN "../AS IS" BASIS. 
+ * WITHOUT WARRANTY OF ANY KIND, EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR PARTICULAR
+ * PURPOSE AND  NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE. SHOULD THE SOFTWARE PROVE DEFECTIVE 
+ * IN ANY RESPECT, THE USER ASSUMES THE COST OF ANY NECESSARY SERVICING, 
+ * REPAIR OR CORRECTION. THIS DISCLAIMER OF WARRANTY CONSTITUTES AN ESSENTIAL
+ * PART OF THIS LICENSE. NO USE OF ANY SOFTWARE IS AUTHORIZED HEREUNDER 
+ * EXCEPT UNDER THIS DISCLAIMER.
+ * C1 WPS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, 
+ * ENHANCEMENTS, OR MODIFICATIONS. THE FULL LICENSE SPECIFYING FOR THE 
+ * SOFTWARE THE REDISTRIBUTION, MODIFICATION, USAGE AND OTHER RIGHTS AND 
+ * OBLIGATIONS IS INCLUDED WITH THE DISTRIBUTION OF THIS 
+ * PROJECT IN THE FILE LICENSE.HTML. IF THE LICENSE IS NOT INCLUDED YOU 
+ * MAY FIND A COPY AT
+ * {@link http://www.eclipse.org/org/documents/epl-v10.html}.
+ */
 package org.csstudio.nams.application.department.decision;
 
+import org.csstudio.nams.common.plugin.utils.BundleActivatorUtils;
 import org.csstudio.nams.service.logging.declaration.Logger;
-import org.eclipse.core.runtime.Plugin;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.util.tracker.ServiceTracker;
 
 /**
- * The activator and application class controls the plug-in and application life
- * cycle.
+ * <p>
+ * The decision department or more precise the activator and application class
+ * to controls their life cycle.
+ * </p>
+ * 
+ * <p>
+ * <strong>Pay attention:</strong> There are always exactly two instances of
+ * this class present: The <emph>bundle activator instance</emph> and the
+ * <emph>bundle application instance</emph>. The communication of both is
+ * hidden in this class to hide the dirty static singleton communication. This
+ * is required during the instantation of extensions (like {@link IApplication})
+ * is done in the framework and not by the plug in itself like it should be.
+ * Cause of this all service field filled by the <emph>bundles activator</emph>
+ * start operation are static to be accessible from the <emph>bundles
+ * application</emph> start.
+ * </p>
+ * 
+ * @author <a href="mailto:mz@c1-wps.de">Matthias Zeimer</a>
+ * @author <a href="mailto:gs@c1-wps.de">Goesta Steen</a>
+ * 
+ * @version 0.1-2008-04-25: Created.
+ * @version 0.1.1-2008-04-28 (MZ): Change to use {@link BundleActivatorUtils}.
  */
-public class DecisionDepartmentActivator extends Plugin implements IApplication {
+public class DecisionDepartmentActivator implements IApplication,
+		BundleActivator {
 
-	/** The plug-in ID */
+	/**
+	 * The plug-in ID of this bundle.
+	 */
 	public static final String PLUGIN_ID = "org.csstudio.nams.application.department.decision";
 
 	/**
-	 * Feld des Activators: ServiceTracker des Loggers.
-	 */
-	private ServiceTracker _serviceTrackerLogger;
-
-	/**
-	 * Gemeinsames Feld des Activators und der Application: Der Logger.
+	 * Gemeinsames Attribut des Activators und der Application: Der Logger.
 	 */
 	private static Logger logger;
 
 	/**
-	 * Feld der Application: Feld das angibt, ob die Application weiter arbeiten
-	 * soll.
+	 * Indicates if the application instance should continue working. Unused in
+	 * the activator instance.
+	 * 
+	 * This field is set by another thread to indicate that application should
+	 * shut down.
 	 */
-	private volatile boolean _arbeitFortsetzen = false;
+	private volatile boolean _continueWorking;
 
 	/**
-	 * TODO Beachten: Eclipse wird 2 Exemplare dieser Klasse anlegen! Dies
-	 * bedeutet, alle felder die zwischen PlugIn Activator und Application
-	 * geteilt werden, mŸssen static sein! Also alle Servicde-Felder. Die
-	 * Tracker werden nur von Activator benutzt und mŸssen nicht static sein.
-	 */
-
-	/**
-	 * The constructor
-	 */
-	public DecisionDepartmentActivator() {
-	}
-
-	/**
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 * Starts the bundle activator instance. First Step.
+	 * 
+	 * @see BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		super.start(context);
-
-		_serviceTrackerLogger = new ServiceTracker(context, Logger.class
-				.getName(), null);
-		_serviceTrackerLogger.open();
-		logger = (Logger) _serviceTrackerLogger.getService();
+		logger = BundleActivatorUtils.getCurrentAvailService(context,
+				Logger.class);
+		logger.logInfoMessage(this, "bundle " + PLUGIN_ID
+				+ " started succesfully.");
 	}
 
 	/**
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 * Stops the bundle activator instance. Last Step.
+	 * 
+	 * @see BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-
-		_serviceTrackerLogger.close();
+		logger.logInfoMessage(this, "bundle " + PLUGIN_ID
+				+ " stopped succesfully.");
 	}
 
 	/**
-	 * Start the applciation.
+	 * Starts the bundle application instance. Second Step.
+	 * 
+	 * @see IApplication#start(IApplicationContext)
 	 */
 	public Object start(IApplicationContext context) throws Exception {
-		// TODO Lade configuration, konvertiere diese und ewrzeuge die bŸros und
+		// TODO Lade configuration, konvertiere diese und ewrzeuge die bueros
+		// und
 		// starte deren Arbeit
 		// lock until application ready to quit.
-		logger.logInfoMessage(this, "Decision department application is going to be initialized...");
-		_arbeitFortsetzen = true;
+		logger
+				.logInfoMessage(this,
+						"Decision department application is going to be initialized...");
+		_continueWorking = true;
 
-		logger.logInfoMessage(this, "Decision department application successfully initialized, begining work...");
-		while (_arbeitFortsetzen) {
+		logger
+				.logInfoMessage(this,
+						"Decision department application successfully initialized, begining work...");
+		while (_continueWorking) {
 			/*-
 			 * try jms.receive if result != null (tue was)
 			 */
-			
+
 			Thread.yield();
 		}
-		// TODO stoppe bŸros
+		// TODO stoppe bueros
 
-		logger.logInfoMessage(this, "Decision department application successfully shuted down.");
+		logger.logInfoMessage(this,
+				"Decision department application successfully shuted down.");
 		return IApplication.EXIT_OK;
 	}
 
 	/**
-	 * Stop the application.
+	 * Stops the bundle application instance.Ppenultimate Step.
+	 * 
+	 * @see IApplication#start(IApplicationContext)
 	 */
 	public void stop() {
-		logger.logInfoMessage(this, "Shuting down decision department application...");
-		_arbeitFortsetzen = false;
+		logger.logInfoMessage(this,
+				"Shuting down decision department application...");
+		_continueWorking = false;
 		// TODO JMS Close -> recieve abgebrochen mit null return
 	}
 }
---
+++ applications/plugins/org.csstudio.nams.common/src/org/csstudio/nams/common/plugin/utils/AbstractBundleActivator.java
@@ -0,0 +1,23 @@
+package org.csstudio.nams.common.plugin.utils;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * An abstract bundle activator to provide common behaviour.
+ * 
+ * TODO Decide if use this abstract class or {@link BundleActivatorUtils}.
+ */
+public abstract class AbstractBundleActivator implements BundleActivator {
+
+	final public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	final public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
