253beb7801be70a639e9bd60741e7e63b72be91d
gsteen <gsteen>
2008-05-14 13:40:39 +0000

    *** empty log message ***

---
+++ applications/plugins/org.csstudio.nams.common/src/org/csstudio/nams/common/testhelper/ForTesting.java
@@ -0,0 +1,10 @@
+package org.csstudio.nams.common.testhelper;
+
+/**
+ * Marks methods and classes as for testing only
+ * 
+ * @author Gösta Steen, Tobias Rathjen
+ */
+public @interface ForTesting {
+
+}
