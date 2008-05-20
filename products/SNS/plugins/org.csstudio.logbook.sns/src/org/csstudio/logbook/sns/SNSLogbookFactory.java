67ff8eab230069fb0902834f3c83bc78a9a9ecb2
Kay Kasemir <kasemirk@ornl.gov>
2008-05-20 19:48:52 +0000

    Fixed some wrong lib. settings after moving RDB jars to ..platform.libs.jdbc.
    ..utility.rdb no longer exports junit4.
    
    Cleanup in logbook.sns in the process of finding the missing preferences.ini export to binary build.

--- products/SNS/plugins/org.csstudio.logbook.sns/src/org/csstudio/logbook/sns/SNSLogbookFactory.java
+++ products/SNS/plugins/org.csstudio.logbook.sns/src/org/csstudio/logbook/sns/SNSLogbookFactory.java
@@ -1,7 +1,9 @@
 package org.csstudio.logbook.sns;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
+import java.util.Enumeration;
 
 import org.csstudio.logbook.ILogbook;
 import org.csstudio.logbook.ILogbookFactory;
@@ -19,8 +21,21 @@ public class SNSLogbookFactory implements ILogbookFactory
         // Connect to the SNS RDB
         // Get class loader to find the driver
         Class.forName("oracle.jdbc.driver.OracleDriver").newInstance(); //$NON-NLS-1$
+        
+        System.out.println("JDBC Drivers:");
+        final Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements())
+        {
+            final Driver driver = drivers.nextElement();
+            System.out.println(driver.getClass().getName());
+        }
+        
+        final String url = Preferences.getURL();
+        
+        System.out.println("URL: '" + url + "'");
+        
         final Connection connection = DriverManager.getConnection(
-                Preferences.getURL(), user, password);
+                url, user, password);
         return new SNSLogbook(connection, user, Preferences.getLogBookName());
     }
 }
