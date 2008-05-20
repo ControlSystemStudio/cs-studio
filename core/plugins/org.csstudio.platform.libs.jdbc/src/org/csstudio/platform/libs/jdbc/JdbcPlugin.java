8653d9c4790a6919901adaf322d452ca16ae2fad
Kay Kasemir <kasemirk@ornl.gov>
2008-05-20 17:03:03 +0000

    Put all RDB jars into ...platform.libs.jdbc.
    From platform.libs.jdbc:
    Removed unused html,
    turned activator into the non-UI version,
    added Readme.html

--- core/plugins/org.csstudio.platform.libs.jdbc/src/org/csstudio/platform/libs/jdbc/JdbcPlugin.java
+++ core/plugins/org.csstudio.platform.libs.jdbc/src/org/csstudio/platform/libs/jdbc/JdbcPlugin.java
@@ -21,55 +21,32 @@
  */
 package org.csstudio.platform.libs.jdbc;
 
-import org.eclipse.ui.plugin.*;
-import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
-/**
- * The main plugin class to be used in the desktop.
- */
-public class JdbcPlugin extends AbstractUIPlugin {
+/** Plugin activator */
+public class JdbcPlugin extends Plugin {
 
-	//The shared instance.
+	/** The shared instance. */
 	private static JdbcPlugin plugin;
 	
-	/**
-	 * The constructor.
-	 */
-	public JdbcPlugin() {
+	/** The constructor. */
+	public JdbcPlugin()
+	{
 		plugin = this;
 	}
 
-	/**
-	 * This method is called upon plug-in activation
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
-
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	public void stop(BundleContext context) throws Exception {
+	/** This method is called when the plug-in is stopped */
+	@Override
+    public void stop(BundleContext context) throws Exception
+    {
 		super.stop(context);
 		plugin = null;
 	}
 
-	/**
-	 * Returns the shared instance.
-	 */
-	public static JdbcPlugin getDefault() {
+	/** @return The shared instance. */
+	public static JdbcPlugin getDefault()
+	{
 		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin("org.csstudio.platform.libs.jdbc", path); //$NON-NLS-1$
 	}
 }
