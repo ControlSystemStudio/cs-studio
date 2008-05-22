e7e0ad6789c6ba2c657616c64043adddbe4f08c6
reiswich <reiswich>
2008-05-22 08:08:01 +0000

    er: kommentar hinzugef?gt

--- applications/plugins/org.csstudio.nams.configurator/src/org/csstudio/nams/configurator/treeviewer/model/treecomponents/SortGroupBean.java
+++ applications/plugins/org.csstudio.nams.configurator/src/org/csstudio/nams/configurator/treeviewer/model/treecomponents/SortGroupBean.java
@@ -2,8 +2,17 @@ package org.csstudio.nams.configurator.treeviewer.model.treecomponents;
 
 import java.util.Collection;
 
+/**
+ * Die {@link SortGroupBean} enthält Kider, die im TreeViewer des Configurators
+ * dargestellt werden. Da eine Gruppe "global" verwendet wird, kann (muss aber
+ * nicht) eine Gruppe verschiedene Kinder beinhalten (siehe die privaten
+ * Collections).
+ * 
+ * @author Eugen Reiswich
+ * 
+ */
 public class SortGroupBean {
-	
+
 	String groupName;
 
 	private Collection<AlarmbearbeiterBean> alarmbearbeiterBeans;
