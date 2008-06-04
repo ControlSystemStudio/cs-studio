81895d37cc18e55a0925802b68a11f9ef5151d50
jrathlev <jrathlev>
2008-06-04 09:25:16 +0000

    updated documentation

--- core/plugins/org.csstudio.platform/src/org/csstudio/platform/security/ILoginCallbackHandler.java
+++ core/plugins/org.csstudio.platform/src/org/csstudio/platform/security/ILoginCallbackHandler.java
@@ -22,12 +22,15 @@
  package org.csstudio.platform.security;
 
 /**
- * Retrieves the username and password that will be used to authenticate
- * the user. Implementations of this interface are used by login modules.
+ * <p>Retrieves the username and password that will be used to authenticate
+ * the user. Implementations of this interface are used by login modules.</p>
  * 
  * <p>Implementations of this interface will usually display a login prompt
  * to the user (for example, a graphical login dialog), but this is not a
- * strict requirement.
+ * strict requirement.</p>
+ * 
+ * <p>Note: implementations of this interface should not assume that they will
+ * be called in the UI thread.</p>
  * 
  * @see ILoginModule
  * 
@@ -36,19 +39,19 @@
 public interface ILoginCallbackHandler {
 	
 	/**
-	 * Returns the username and password that will be used to authenticate
+	 * <p>Returns the username and password that will be used to authenticate
 	 * the user. Returns <code>null</code> if username and password could not
-	 * be retrieved.
+	 * be retrieved.</p>
 	 * 
 	 * <p>Implementations of this method may prompt the user for username and
 	 * password, for example by displaying a login dialog. Such implementations
 	 * have to wait for user input before returning. Login modules calling this
-	 * method therefore should not assume that it will return quickly.
+	 * method therefore should not assume that it will return quickly.</p>
 	 * 
 	 * <p>The username and password are returned in a {@link Credentials}
 	 * object. Implementations must not keep references to this object, and
 	 * callers should discard all references to the returned object as early as
-	 * possible to prevent passwords from being kept in memory unnecessarily.
+	 * possible to prevent passwords from being kept in memory unnecessarily.</p>
 	 * 
 	 * @return the username and password, or <code>null</code> if username and
 	 *         password could not be retrieved.
@@ -56,14 +59,14 @@ public interface ILoginCallbackHandler {
 	Credentials getCredentials();
 	
 	/**
-	 * Informs this callback handler that the previous login attempt has failed.
-	 * If this callback handler is interactive, it will display an error
-	 * message to the user.
+	 * <p>Informs this callback handler that the previous login attempt has
+	 * failed. If this callback handler is interactive, it will display an error
+	 * message to the user.</p>
 	 * 
 	 * <p>If the login failed due to a wrong username/password, clients
 	 * <em>must</em> call this method before calling {@link getCredentials}
 	 * again, otherwise <code>getCredentials</code> may return the same
-	 * credentials as from the previous call.
+	 * credentials as from the previous call.</p>
 	 */
 	void signalFailedLoginAttempt();
 }
