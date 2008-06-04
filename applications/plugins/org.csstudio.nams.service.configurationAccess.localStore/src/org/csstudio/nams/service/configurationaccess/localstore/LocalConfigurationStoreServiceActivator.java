089d6db964c2b61f741ffee56084bbc284b947b7
mzeimer <mzeimer>
2008-06-04 14:56:48 +0000

    *** empty log message ***

--- applications/plugins/org.csstudio.nams.service.configurationAccess.localStore/src/org/csstudio/nams/service/configurationaccess/localstore/LocalConfigurationStoreServiceActivator.java
+++ applications/plugins/org.csstudio.nams.service.configurationAccess.localStore/src/org/csstudio/nams/service/configurationaccess/localstore/LocalConfigurationStoreServiceActivator.java
@@ -1,31 +1,44 @@
 package org.csstudio.nams.service.configurationaccess.localstore;
 
-import org.csstudio.ams.configurationStoreService.declaration.ConfigurationStoreService;
-import org.csstudio.nams.common.plugin.utils.BundleActivatorUtils;
-import org.eclipse.core.runtime.Plugin;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.classic.Session;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class LocalConfigurationStoreServiceActivator extends Plugin {
+public class LocalConfigurationStoreServiceActivator implements BundleActivator {
 
 	/** The plug-in ID */
 	public static final String PLUGIN_ID = "org.csstudio.nams.service.configurationAccess.localStore";
+	private SessionFactory sessionFactory;
+	private Session session;
 
 	public void start(BundleContext context) throws Exception {
-		super.start(context);
+		try {
+			initializeHibernate();
+			session = sessionFactory.openSession();
+			
+		} catch (Throwable t) {
+			throw new RuntimeException("Failed to start LocalConfigurationStoreService's bundle", t);
+		}
+	}
 
-		ConfigurationStoreService amsConfStoreService = BundleActivatorUtils
-				.getAvailableService(context, ConfigurationStoreService.class);
-
-//		LocalConfigurationStoreService serviceImpl = new LocalConfigurationStoreServiceImpl(
-//				amsConfStoreService);
-//		context.registerService(LocalConfigurationStoreService.class.getName(),
-//				serviceImpl, null);
+	private void initializeHibernate() {
+		AnnotationConfiguration configuration = new AnnotationConfiguration();
+//    	configuration= configuration.addAnnotatedClass(TopicDTO.class);
+    	
+    	AnnotationConfiguration configured = configuration.configure();
+    	sessionFactory = configured.buildSessionFactory();
+    	
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
+		session.close();
+		sessionFactory.close();
 	}
 }
+
