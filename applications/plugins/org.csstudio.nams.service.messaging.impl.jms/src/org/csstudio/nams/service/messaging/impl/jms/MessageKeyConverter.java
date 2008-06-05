47190404ad54bfdf3a989eb186005950730374dc
mzeimer <mzeimer>
2008-06-05 14:25:55 +0000

    mz, mw, gs: MessageKeyConverter

--- applications/plugins/org.csstudio.nams.service.messaging.impl.jms/src/org/csstudio/nams/service/messaging/impl/jms/MessageKeyConverter.java
+++ applications/plugins/org.csstudio.nams.service.messaging.impl.jms/src/org/csstudio/nams/service/messaging/impl/jms/MessageKeyConverter.java
@@ -5,19 +5,36 @@ import java.util.Map;
 
 import org.csstudio.nams.common.fachwert.MessageKeyEnum;
 
-public class MessageKeyConverter {
+public final class MessageKeyConverter {
 
 	private static Map<String, MessageKeyEnum> keyMapping = new HashMap<String, MessageKeyEnum>();
 	static {
-		keyMapping.put("type", MessageKeyEnum.TYPE);
+		keyMapping.put("ams-filterid", MessageKeyEnum.AMS_REINSERTED);
+		keyMapping.put("application-id", MessageKeyEnum.APPLICATION_ID);
+		keyMapping.put("class", MessageKeyEnum.CLASS);
+		keyMapping.put("destination", MessageKeyEnum.DESTINATION);
+		keyMapping.put("domain", MessageKeyEnum.DOMAIN);
+		keyMapping.put("eventtime", MessageKeyEnum.EVENTTIME);
+		keyMapping.put("facility", MessageKeyEnum.FACILITY);
+		keyMapping.put("host", MessageKeyEnum.HOST);
+		keyMapping.put("location", MessageKeyEnum.LOCATION);
 		keyMapping.put("command", MessageKeyEnum.MSGPROP_COMMAND); // COMMAND
-		// ....
+		keyMapping.put("name", MessageKeyEnum.NAME);
+		keyMapping.put("process-id", MessageKeyEnum.PROCESS_ID);
+		keyMapping.put("severity", MessageKeyEnum.SEVERITY);
+		keyMapping.put("status", MessageKeyEnum.STATUS);
+		keyMapping.put("text", MessageKeyEnum.TEXT);
+		keyMapping.put("type", MessageKeyEnum.TYPE);
+		keyMapping.put("user", MessageKeyEnum.USER);
+		keyMapping.put("value", MessageKeyEnum.VALUE);
 	}
 
 	public static MessageKeyEnum getEnumKeyFor(String jmsKey) {
 		MessageKeyEnum result = keyMapping.get(jmsKey.toLowerCase());
 		return result;
 	}
+	
+	private MessageKeyConverter() {}
 	
 	private final static String MSGVALUE_TCMD_RELOAD = "AMS_RELOAD_CFG";
 	private final static String MSGVALUE_TCMD_RELOAD_CFG_START = MSGVALUE_TCMD_RELOAD + "_START";
