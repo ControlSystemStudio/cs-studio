d535481326267163078f08236d70c6a5badb8220
gsteen <gsteen>
2008-06-05 09:06:00 +0000

    gs: comment added

--- applications/plugins/org.csstudio.nams.service.configurationAccess.localStore/src/org/csstudio/nams/service/configurationaccess/localstore/declaration/SyncronizeFlagDTO.java
+++ applications/plugins/org.csstudio.nams.service.configurationAccess.localStore/src/org/csstudio/nams/service/configurationaccess/localstore/declaration/SyncronizeFlagDTO.java
@@ -4,4 +4,12 @@ public class SyncronizeFlagDTO {
 	// TODO Design this DTO to fit table
 	// TODO Register this DTO class in mapping file
 	// TODO Register this DTO class in annotation configuration
+	
+	/** TODO 
+	 * table name: AMS_FLAG
+	 * columns:
+	 * CFLAGNAME ("ReplicationState")
+	 * SFLAGVALUE ( 0, 1, 2 oder 3 kp was diese bedeuten)
+	 */
+	
 }
