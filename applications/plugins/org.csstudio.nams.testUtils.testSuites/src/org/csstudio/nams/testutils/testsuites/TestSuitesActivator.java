5bafb47e8375b79b0db57f39b62960e8fecdb39e
mzeimer <mzeimer>
2008-06-05 21:47:24 +0000

    mz: common test plug in

---
+++ applications/plugins/org.csstudio.nams.testUtils.testSuites/src/org/csstudio/nams/testutils/testsuites/TestSuitesActivator.java
@@ -0,0 +1,19 @@
+package org.csstudio.nams.testutils.testsuites;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class TestSuitesActivator implements BundleActivator {
+
+	/** The plug-in ID */
+	public static final String PLUGIN_ID = "org.csstudio.nams.testUtils.testSuites";
+
+	public void start(BundleContext context) throws Exception {
+	}
+
+	public void stop(BundleContext context) throws Exception {
+	}
+}
