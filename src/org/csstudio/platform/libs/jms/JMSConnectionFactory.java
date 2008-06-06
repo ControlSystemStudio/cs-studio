c2ff31dfc027640b0d37f155181a620da107e523
Kay Kasemir <kasemirk@ornl.gov>
2008-06-06 20:59:29 +0000

    added

---
+++ src/org/csstudio/platform/libs/jms/JMSConnectionFactory.java
@@ -0,0 +1,36 @@
+package org.csstudio.platform.libs.jms;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+/** Helper for connecting to a JMS server.
+ *  Shields from the underling ActiveMQ API,
+ *  only providing a <code>javax.jms.Connection</code>.
+ *  
+ *  @author Kay Kasemir
+ */
+public class JMSConnectionFactory
+{
+    /** Connect to JMS
+     *  @param url URL of server. Details can differ for ActiveMQ
+     *             or other implementations.
+     *  @return Connection
+     *  @throws JMSException on error
+     */
+    public static Connection connect(final String url) throws JMSException
+    {
+        // Instead of using JNDI lookup like this...
+        //   Context ctx = new InitialContext();
+        //   QueueConnectionFactory queueConnectionFactory = 
+        //     (QueueConnectionFactory) ctx.lookup("SomeConnectionFactory");
+        // ... which requires an appropriate jndi.properties file,
+        // we directly use the ActiveMQConnectionFactory.
+        final ActiveMQConnectionFactory factory =
+            new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER,
+                ActiveMQConnection.DEFAULT_PASSWORD, url);
+        return factory.createConnection();
+    }
+}
