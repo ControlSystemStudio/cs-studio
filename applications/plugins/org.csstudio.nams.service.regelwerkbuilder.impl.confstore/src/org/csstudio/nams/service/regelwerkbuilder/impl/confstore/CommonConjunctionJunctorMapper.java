b20d3ab50c31d70581d876fb7a6042d956016059
trathjen <trathjen>
2008-06-06 08:52:39 +0000

    added CommonConjunctionJunctorMapper

---
+++ applications/plugins/org.csstudio.nams.service.regelwerkbuilder.impl.confstore/src/org/csstudio/nams/service/regelwerkbuilder/impl/confstore/CommonConjunctionJunctorMapper.java
@@ -0,0 +1,18 @@
+package org.csstudio.nams.service.regelwerkbuilder.impl.confstore;
+
+public enum CommonConjunctionJunctorMapper {
+	OR, AND, NOT;
+
+	public CommonConjunctionJunctorMapper valueOf(int value) {
+		switch (value) {
+		case 0:
+			return OR;
+		case 1:
+			return AND;
+		case 2:
+			return NOT;
+		default:
+			throw new IllegalArgumentException("Unsupported Junctor");
+		}
+	}
+}
