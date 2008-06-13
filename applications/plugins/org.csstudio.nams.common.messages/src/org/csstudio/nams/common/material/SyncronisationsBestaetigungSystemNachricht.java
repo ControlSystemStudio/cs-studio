54f03fcb190016a140732152b629f7ae242ef489
trathjen <trathjen>
2008-06-13 15:59:26 +0000

    tr introduced messages plugin

---
+++ applications/plugins/org.csstudio.nams.common.messages/src/org/csstudio/nams/common/material/SyncronisationsBestaetigungSystemNachricht.java
@@ -0,0 +1,13 @@
+package org.csstudio.nams.common.material;
+
+public class SyncronisationsBestaetigungSystemNachricht implements SystemNachricht {
+
+	public boolean istSyncronisationsAufforderung() {
+		return false;
+	}
+
+	public boolean istSyncronisationsBestaetigung() {
+		return true;
+	}
+
+}
