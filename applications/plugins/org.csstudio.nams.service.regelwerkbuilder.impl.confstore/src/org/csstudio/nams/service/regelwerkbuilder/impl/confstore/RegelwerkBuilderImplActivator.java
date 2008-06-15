d8652aa88d5b74adb1320580b8014737197f9203
mzeimer <mzeimer>
2008-06-15 02:10:41 +0000

    *** empty log message ***

--- applications/plugins/org.csstudio.nams.service.regelwerkbuilder.impl.confstore/src/org/csstudio/nams/service/regelwerkbuilder/impl/confstore/RegelwerkBuilderImplActivator.java
+++ applications/plugins/org.csstudio.nams.service.regelwerkbuilder.impl.confstore/src/org/csstudio/nams/service/regelwerkbuilder/impl/confstore/RegelwerkBuilderImplActivator.java
@@ -2,6 +2,8 @@ package org.csstudio.nams.service.regelwerkbuilder.impl.confstore;
 
 import org.csstudio.nams.common.activatorUtils.AbstractBundleActivator;
 import org.csstudio.nams.common.activatorUtils.OSGiBundleActivationMethod;
+import org.csstudio.nams.common.activatorUtils.OSGiService;
+import org.csstudio.nams.common.activatorUtils.Required;
 import org.csstudio.platform.simpledal.IProcessVariableConnectionService;
 import org.csstudio.platform.simpledal.ProcessVariableConnectionServiceFactory;
 import org.osgi.framework.BundleActivator;
@@ -10,19 +12,19 @@ import org.osgi.framework.BundleActivator;
  * The activator class controls the plug-in life cycle
  */
 public class RegelwerkBuilderImplActivator extends AbstractBundleActivator
-implements BundleActivator {
+		implements BundleActivator {
 
 	/** The plug-in ID */
 	public static final String PLUGIN_ID = "org.csstudio.nams.service.regelwerkbuilder.impl.confstore";
-	
+
 	@OSGiBundleActivationMethod
 	public void startBundle(
-//			@OSGiService @Required ConfigurationStoreService configurationStoreService
+			@OSGiService @Required
+	        ProcessVariableConnectionServiceFactory pvConnectionServiceFactory
 	) {
-		//TODO pvConnectionService auf nicht statischen Weg beziehen!
-		IProcessVariableConnectionService pvConnectionService = ProcessVariableConnectionServiceFactory.getProcessVariableConnectionService();
-		
+		IProcessVariableConnectionService pvConnectionService = pvConnectionServiceFactory
+				.createProcessVariableConnectionService();
+
 		RegelwerkBuilderServiceImpl.staticInject(pvConnectionService);
-//		RegelwerkBuilderServiceImpl.staticInject(configurationStoreService);
 	}
 }
