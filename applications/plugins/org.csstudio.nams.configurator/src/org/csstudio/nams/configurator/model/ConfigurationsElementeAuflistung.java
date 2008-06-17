44f5185449c4b0bd799716eafc214e964da0303d
mzeimer <mzeimer>
2008-06-17 12:35:11 +0000

    *** empty log message ***

--- applications/plugins/org.csstudio.nams.configurator/src/org/csstudio/nams/configurator/model/ConfigurationsElementeAuflistung.java
+++ applications/plugins/org.csstudio.nams.configurator/src/org/csstudio/nams/configurator/model/ConfigurationsElementeAuflistung.java
@@ -6,14 +6,24 @@ import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import org.csstudio.nams.configurator.branch.composite.FilteredListVarianteA;
 import org.csstudio.nams.service.configurationaccess.localstore.declaration.NewAMSConfigurationElementDTO;
 
 /**
- * Model für die Auflistungskomponenten der UI für die Elemente einer Konfigurationsart, z.B.: Alarmbearbeiter, Filter.
+ * Model für die Auflistungskomponenten der UI für die Elemente einer
+ * Konfigurationsart, z.B.: Alarmbearbeiter, Filter.
+ * 
+ * XXX Das Modell-PlugIn kännte einen OSGi-Service hinterlegen, über den die
+ * Views (UI PlugIn) die Modelle bezieht. Somit bliebe die
+ * Modell-Exemplar-Erzeugung vollständig im Modell, die UI benutzte die Modelle
+ * legiglich.
+ * 
+ * @see FilteredListVarianteA
  * 
  * @author <a href="mailto:mz@c1-wps.de">Matthias Zeimer</a>
- *
- * @param <T> Der Konfigurationselementtyp.
+ * 
+ * @param <T>
+ *            Der Konfigurationselementtyp.
  */
 public class ConfigurationsElementeAuflistung<T extends NewAMSConfigurationElementDTO> {
 
@@ -30,8 +40,7 @@ public class ConfigurationsElementeAuflistung<T extends NewAMSConfigurationEleme
 	 */
 	public void setElements(Set<T> elements) {
 		Comparator<T> comparator = new Comparator<T>() {
-			public int compare(T bearbeiter1,
-					T bearbeiter2) {
+			public int compare(T bearbeiter1, T bearbeiter2) {
 				return bearbeiter1.getUniqueHumanReadableName().compareTo(
 						bearbeiter2.getUniqueHumanReadableName());
 			}
@@ -44,6 +53,17 @@ public class ConfigurationsElementeAuflistung<T extends NewAMSConfigurationEleme
 		this.updateVisibleElements();
 	}
 
+	/**
+	 * Returns the "visible" elements for current filtering settings.
+	 * 
+	 * @return A by
+	 *         {@link NewAMSConfigurationElementDTO#getUniqueHumanReadableName()}
+	 *         sorted set to make sure that no element is duplicated; returned
+	 *         set is unmodifiable.
+	 *         
+	 * @see ConfigurationsElementeAuflistung#setCategoryFilter(int)
+	 * @see ConfigurationsElementeAuflistung#setNameFilter(String)
+	 */
 	public SortedSet<T> getVisibleElementsInAscendingOrder() {
 		return Collections.unmodifiableSortedSet(this.visibleElements);
 	}
@@ -62,20 +82,20 @@ public class ConfigurationsElementeAuflistung<T extends NewAMSConfigurationEleme
 		}
 	}
 
-	private boolean matchCurrentFilterSettings(
-			T element) {
+	private boolean matchCurrentFilterSettings(T element) {
 		boolean result = true;
-		
+
 		if (this.currentNameFilter != null) {
-			result = element.getUniqueHumanReadableName().toLowerCase().contains(this.currentNameFilter.toLowerCase());
+			result = element.getUniqueHumanReadableName().toLowerCase()
+					.contains(this.currentNameFilter.toLowerCase());
 		}
-		
-		if( result && this.currentCategoryFilter != null ) {
+
+		if (result && this.currentCategoryFilter != null) {
 			int categoryDBId = this.currentCategoryFilter.intValue();
-			
+
 			result = element.isInCategory(categoryDBId);
 		}
-		
+
 		return result;
 	}
 
