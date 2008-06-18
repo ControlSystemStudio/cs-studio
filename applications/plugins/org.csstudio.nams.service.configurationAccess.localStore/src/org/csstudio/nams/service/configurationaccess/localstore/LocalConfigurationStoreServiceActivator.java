7a275f4f47cef1eaddd9d7a74416b8f1077946ef
gsteen <gsteen>
2008-06-18 09:19:11 +0000

    gs: hibernate log messages ein wenig unterdrueckt

--- applications/plugins/org.csstudio.nams.service.configurationAccess.localStore/src/org/csstudio/nams/service/configurationaccess/localstore/LocalConfigurationStoreServiceActivator.java
+++ applications/plugins/org.csstudio.nams.service.configurationAccess.localStore/src/org/csstudio/nams/service/configurationaccess/localstore/LocalConfigurationStoreServiceActivator.java
@@ -1,6 +1,8 @@
 package org.csstudio.nams.service.configurationaccess.localstore;
 
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.csstudio.nams.common.activatorUtils.AbstractBundleActivator;
 import org.csstudio.nams.common.activatorUtils.OSGiBundleActivationMethod;
 import org.csstudio.nams.common.activatorUtils.OSGiBundleDeactivationMethod;
@@ -59,21 +61,29 @@ public class LocalConfigurationStoreServiceActivator extends
 				.addAnnotatedClass(ProcessVariableFilterConditionDTO.class)
 				.addAnnotatedClass(FilterDTO.class)
 				.addAnnotatedClass(HistoryDTO.class)
-//				.setProperty("hibernate.connection.driver_class", "org.apache.derby.jdbc.ClientDriver")
-				.setProperty("hibernate.connection.driver_class", "oracle.jdbc.driver.OracleDriver")
-//				.setProperty("hibernate.connection.url", "jdbc:derby://134.100.12.94:1527/amsdb")
-				.setProperty("hibernate.connection.url", "jdbc:oracle:thin:@(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 134.100.7.235)(PORT = 1521))(LOAD_BALANCE = yes)(CONNECT_DATA =(SERVER = DEDICATED)(FAILOVER_MODE =(TYPE = NONE)(METHOD = BASIC)(RETRIES = 180)(DELAY = 5))))")
-				.setProperty("hibernate.connection.username", "DESY")
-				.setProperty("hibernate.connection.password", "DESY")
+
+//				.setProperty("hibernate.connection.driver_class", "oracle.jdbc.driver.OracleDriver")
+//				.setProperty("hibernate.connection.url", "jdbc:oracle:thin:@(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 134.100.7.235)(PORT = 1521))(LOAD_BALANCE = yes)(CONNECT_DATA =(SERVER = DEDICATED)(FAILOVER_MODE =(TYPE = NONE)(METHOD = BASIC)(RETRIES = 180)(DELAY = 5))))")
+//				.setProperty("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect")
+//				.setProperty("hibernate.connection.username", "DESY")
+//				.setProperty("hibernate.connection.password", "DESY")
+
+				.setProperty("hibernate.connection.driver_class", "org.apache.derby.jdbc.ClientDriver")
+				.setProperty("hibernate.connection.url", "jdbc:derby://134.100.12.94:1527/amsdb")
+				.setProperty("hibernate.dialect", "org.hibernate.dialect.DerbyDialect")
+				.setProperty("hibernate.connection.username", "APP")
+				.setProperty("hibernate.connection.password", "APP")
+
 				.setProperty("hibernate.connection.pool_size", "1")
-//				.setProperty("hibernate.dialect", "org.hibernate.dialect.DerbyDialect")
-				.setProperty("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect")
 				.setProperty("current_session_context_class", "thread")
 				.setProperty("cache.provider_class", "org.hibernate.cache.NoCacheProvider")
 				.setProperty("show_sql", "true")
 				.setProperty("hbm2ddl.auto", "update")
 				.setProperty("hibernate.mapping.precedence", "class");
 
+		//TODO in die config auslagern
+		Logger.getLogger("org.hibernate").setLevel(Level.WARN);
+		
 //		final AnnotationConfiguration configured = configuration.configure();
 		this.sessionFactory = configuration.buildSessionFactory();
 	}
