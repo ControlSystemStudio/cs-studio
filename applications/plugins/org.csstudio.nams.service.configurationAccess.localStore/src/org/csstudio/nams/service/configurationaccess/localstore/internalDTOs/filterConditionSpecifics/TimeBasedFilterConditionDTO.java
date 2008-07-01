605fc8d982718e81b563450a9159f45b7369919a
mwalczak <mwalczak>
2008-07-01 15:07:54 +0000

    mw: organize imports

--- applications/plugins/org.csstudio.nams.service.configurationAccess.localStore/src/org/csstudio/nams/service/configurationaccess/localstore/internalDTOs/filterConditionSpecifics/TimeBasedFilterConditionDTO.java
+++ applications/plugins/org.csstudio.nams.service.configurationAccess.localStore/src/org/csstudio/nams/service/configurationaccess/localstore/internalDTOs/filterConditionSpecifics/TimeBasedFilterConditionDTO.java
@@ -4,7 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import org.csstudio.nams.common.fachwert.MessageKeyEnum;
 import org.csstudio.nams.common.fachwert.Millisekunden;
