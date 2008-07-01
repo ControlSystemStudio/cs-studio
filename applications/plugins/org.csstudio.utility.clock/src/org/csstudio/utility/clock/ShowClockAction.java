b7266345940c7d5831e67b9316f3c0911333832e
Kay Kasemir <kasemirk@ornl.gov>
2008-07-01 21:21:07 +0000

    Use OpenViewAction

--- applications/plugins/org.csstudio.utility.clock/src/org/csstudio/utility/clock/ShowClockAction.java
+++ applications/plugins/org.csstudio.utility.clock/src/org/csstudio/utility/clock/ShowClockAction.java
@@ -1,45 +1,14 @@
 package org.csstudio.utility.clock;
 
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PlatformUI;
+import org.csstudio.platform.ui.workbench.OpenViewAction;
 
 /** Action connected to workbench menu action set for showing the view.
  *  @author Kay Kasemir
  */
-public class ShowClockAction implements IWorkbenchWindowActionDelegate
+public class ShowClockAction extends OpenViewAction
 {
-	public void init(IWorkbenchWindow window)
-	{
-	    // NOP
-    }
-
-	public void selectionChanged(IAction action, ISelection selection)
+    public ShowClockAction()
     {
-        // NOP
-    }
-
-	public void run(IAction action)
-	{
-	    try
-	    {
-	        IWorkbench workbench = PlatformUI.getWorkbench();
-	        IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
-	        IWorkbenchPage page = window.getActivePage();
-	        page.showView(ClockView.ID);
-	    }
-	    catch (Exception e)
-	    {
-	        e.printStackTrace();
-	    }
-	}
-
-	public void dispose()
-    {
-        // NOP
+        super(ClockView.ID);
     }
 }
--- applications/plugins/org.csstudio.utility.eliza/src/org/csstudio/utility/eliza/ShowElizaAction.java
+++ applications/plugins/org.csstudio.utility.eliza/src/org/csstudio/utility/eliza/ShowElizaAction.java
@@ -1,45 +1,14 @@
 package org.csstudio.utility.eliza;
 
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PlatformUI;
+import org.csstudio.platform.ui.workbench.OpenViewAction;
 
 /** Menu action to open Eliza View
  *  @author Kay Kasemir
  */
-public class ShowElizaAction implements IWorkbenchWindowActionDelegate
+public class ShowElizaAction extends OpenViewAction
 {
-    public void init(IWorkbenchWindow window)
+    public ShowElizaAction()
     {
-        // NOP
-    }
-
-    public void selectionChanged(IAction action, ISelection selection)
-    {
-        // NOP
-    }
-
-    public void run(IAction action)
-    {
-        try
-        {
-            IWorkbench workbench = PlatformUI.getWorkbench();
-            IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
-            IWorkbenchPage page = window.getActivePage();
-            page.showView(ElizaView.ID);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    public void dispose()
-    {
-        // NOP
+        super(ElizaView.ID);
     }
 }
