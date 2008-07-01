36396bceeef0ae3b3df4b901b6a4a78515246ba1
trathjen <trathjen>
2008-07-01 13:54:17 +0000

    tr: implemented PVFilterConditionBean

--- applications/plugins/org.csstudio.nams.configurator.ui/src/org/csstudio/nams/configurator/beans/filters/PVFilterConditionBean.java
+++ applications/plugins/org.csstudio.nams.configurator.ui/src/org/csstudio/nams/configurator/beans/filters/PVFilterConditionBean.java
@@ -1,30 +1,71 @@
 package org.csstudio.nams.configurator.beans.filters;
 
+import org.csstudio.nams.common.material.regelwerk.Operator;
+import org.csstudio.nams.common.material.regelwerk.SuggestedProcessVariableType;
 import org.csstudio.nams.configurator.beans.AbstractConfigurationBean;
-import org.csstudio.nams.configurator.beans.IConfigurationBean;
 
-public class PVFilterConditionBean extends AbstractConfigurationBean<PVFilterConditionBean> implements
-		AddOnBean {
+public class PVFilterConditionBean extends
+		AbstractConfigurationBean<PVFilterConditionBean> implements AddOnBean {
+
+	private SuggestedProcessVariableType suggestedType;
+	private String channelName;
+	private Operator operator;
+	private String compareValue;
+
+	public SuggestedProcessVariableType getSuggestedType() {
+		return suggestedType;
+	}
+
+	public void setSuggestedType(SuggestedProcessVariableType suggestedType) {
+		this.suggestedType = suggestedType;
+	}
+
+	public String getChannelName() {
+		return channelName;
+	}
+
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+
+	public Operator getOperator() {
+		return operator;
+	}
+
+	public void setOperator(Operator operator) {
+		this.operator = operator;
+	}
+
+	public String getCompareValue() {
+		return compareValue;
+	}
+
+	public void setCompareValue(String compareValue) {
+		this.compareValue = compareValue;
+	}
 
 	@Override
 	public PVFilterConditionBean getClone() {
-		// TODO Auto-generated method stub
-		return null;
+		PVFilterConditionBean bean = new PVFilterConditionBean();
+		bean.setChannelName(channelName);
+		bean.setCompareValue(compareValue);
+		bean.setOperator(operator);
+		bean.setSuggestedType(suggestedType);
+		return bean;
 	}
 
 	@Override
 	public void updateState(PVFilterConditionBean bean) {
 		// TODO Auto-generated method stub
-		
+		throw new RuntimeException("Unimplemented method");
 	}
 
 	public String getDisplayName() {
-		// TODO Auto-generated method stub
-		return null;
+		return channelName + " " + operator.toString() + " " + compareValue
+				+ " " + suggestedType;
 	}
 
 	public int getID() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
