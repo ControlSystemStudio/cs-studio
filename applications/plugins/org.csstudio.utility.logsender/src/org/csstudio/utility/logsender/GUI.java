8b6917520dbb3bcb2b8ff16c9ebc2a751d63a0c0
Kay Kasemir <kasemirk@ornl.gov>
2008-07-02 13:37:33 +0000

    Status, send-on-ENTER

--- applications/plugins/org.csstudio.utility.logsender/src/org/csstudio/utility/logsender/GUI.java
+++ applications/plugins/org.csstudio.utility.logsender/src/org/csstudio/utility/logsender/GUI.java
@@ -1,7 +1,11 @@
 package org.csstudio.utility.logsender;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.apache.log4j.Logger;
 import org.csstudio.platform.logging.CentralLogger;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -21,6 +25,7 @@ public class GUI
     private Text text;
     private Combo level;
     private Button send;
+    private Label status;
 
     enum Level
     {
@@ -30,10 +35,19 @@ public class GUI
     public GUI(final Composite parent)
     {
         createComponents(parent);
+        // Pressing 'send' button or 'ENTER' in text will send the message
         send.addSelectionListener(new SelectionAdapter()
         {
             @Override
             public void widgetSelected(SelectionEvent e)
+            {
+                sendMessage();
+            }
+        });
+        text.addSelectionListener(new SelectionAdapter()
+        {
+            @Override
+            public void widgetDefaultSelected(SelectionEvent e)
             {
                 sendMessage();
             }
@@ -48,11 +62,14 @@ public class GUI
         
         // Text:  __________________
         // Level: [level]     [send]
+        //   -------
+        // Status
         Label l = new Label(parent, 0);
         l.setText(Messages.Text);
         l.setLayoutData(new GridData());
         
         text = new Text(parent, SWT.BORDER);
+        text.setText(Messages.InitialText);
         text.setToolTipText(Messages.TextTT);
         text.setLayoutData(new GridData(SWT.FILL, 0, true, false, 2, 1));
     
@@ -72,6 +89,20 @@ public class GUI
         send.setText(Messages.Send);
         send.setToolTipText(Messages.SendTT);
         send.setLayoutData(new GridData());
+
+        // New row
+        l = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
+        l.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, true, layout.numColumns, 1));
+
+        status = new Label(parent, SWT.SHADOW_IN);
+        status.setLayoutData(new GridData(SWT.FILL, 0, true, false, layout.numColumns, 1));
+    }
+
+    /** Set status to initial component */
+    public void setFocus()
+    {
+        text.setFocus();
+        text.selectAll();
     }
 
     /** Make log entry with current GUI content */
@@ -82,29 +113,37 @@ public class GUI
         if (level.getSelectionIndex() == Level.Debug.ordinal())
         {
             logger.debug(message);
+            updateStatus(message);
             return;
         }
         else if (level.getSelectionIndex() == Level.Info.ordinal())
         {
             logger.info(message);
+            updateStatus(message);
             return;
         }
         else if (level.getSelectionIndex() == Level.Warning.ordinal())
         {
             logger.warn(message);
+            updateStatus(message);
             return;
         }
         else if (level.getSelectionIndex() == Level.Fatal.ordinal())
         {
             logger.fatal(message);
+            updateStatus(message);
             return;
         }
         // else: Should be error. Use that as default
         logger.error(message);
+        updateStatus(message);
     }
-
-    public void setFocus()
+    
+    /** Update status line */
+    private void updateStatus(final String message)
     {
-        text.setFocus();
+        final SimpleDateFormat format = new SimpleDateFormat(Messages.DateFmt);
+        final Calendar now = Calendar.getInstance();
+        status.setText(NLS.bind(Messages.StatusFmt, format.format(now.getTime()), message));
     }
 }
--- applications/plugins/org.csstudio.utility.logsender/src/org/csstudio/utility/logsender/Messages.java
+++ applications/plugins/org.csstudio.utility.logsender/src/org/csstudio/utility/logsender/Messages.java
@@ -5,10 +5,13 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS
 {
     private static final String BUNDLE_NAME = "org.csstudio.utility.logsender.messages"; //$NON-NLS-1$
+    public static String DateFmt;
+    public static String InitialText;
     public static String Level;
     public static String LevelTT;
     public static String Send;
     public static String SendTT;
+    public static String StatusFmt;
     public static String Text;
     public static String TextTT;
     static
