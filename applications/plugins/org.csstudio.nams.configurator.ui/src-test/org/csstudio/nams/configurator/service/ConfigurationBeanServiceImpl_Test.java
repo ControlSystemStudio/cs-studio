35b7ca9ab62a017215c512a9ea2555027b19acc8
mzeimer <mzeimer>
2008-07-23 17:58:24 +0000

    *** empty log message ***

--- applications/plugins/org.csstudio.nams.configurator.ui/src-test/org/csstudio/nams/configurator/service/ConfigurationBeanServiceImpl_Test.java
+++ applications/plugins/org.csstudio.nams.configurator.ui/src-test/org/csstudio/nams/configurator/service/ConfigurationBeanServiceImpl_Test.java
@@ -1,9 +1,22 @@
 package org.csstudio.nams.configurator.service;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 import junit.framework.TestCase;
 
+import org.csstudio.nams.service.configurationaccess.localstore.declaration.AlarmbearbeiterDTO;
+import org.csstudio.nams.service.configurationaccess.localstore.declaration.AlarmbearbeiterGruppenDTO;
 import org.csstudio.nams.service.configurationaccess.localstore.declaration.Configuration;
+import org.csstudio.nams.service.configurationaccess.localstore.declaration.FilterDTO;
 import org.csstudio.nams.service.configurationaccess.localstore.declaration.LocalStoreConfigurationService;
+import org.csstudio.nams.service.configurationaccess.localstore.declaration.TopicDTO;
+import org.csstudio.nams.service.configurationaccess.localstore.internalDTOs.FilterConditionDTO;
+import org.csstudio.nams.service.configurationaccess.localstore.internalDTOs.RubrikDTO;
+import org.csstudio.nams.service.configurationaccess.localstore.internalDTOs.User2UserGroupDTO;
+import org.csstudio.nams.service.configurationaccess.localstore.internalDTOs.filterConditionSpecifics.FilterConditionsToFilterDTO;
+import org.csstudio.nams.service.configurationaccess.localstore.internalDTOs.filterConditionSpecifics.StringArrayFilterConditionCompareValuesDTO;
 import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.Before;
@@ -12,14 +25,10 @@ import org.junit.Test;
 public class ConfigurationBeanServiceImpl_Test extends TestCase {
 
 	private LocalStoreConfigurationService confService;
-	private Configuration testDaten;
 
 	@Before
 	public void setUp() throws Exception {
-		testDaten = null;
-		
 		confService = EasyMock.createMock(LocalStoreConfigurationService.class);
-		EasyMock.replay(confService);
 	}
 
 	@After
@@ -27,15 +36,38 @@ public class ConfigurationBeanServiceImpl_Test extends TestCase {
 		EasyMock.verify(confService);
 		confService = null;
 	}
+	
+	private Configuration createTestData() {
+		Collection<AlarmbearbeiterDTO> alleAlarmbarbeiter = new LinkedList<AlarmbearbeiterDTO>();
+		Collection<TopicDTO> alleAlarmtopics = new LinkedList<TopicDTO>();
+		Collection<AlarmbearbeiterGruppenDTO> alleAlarmbearbeiterGruppen = new LinkedList<AlarmbearbeiterGruppenDTO>();
+		Collection<FilterDTO> allFilters = new LinkedList<FilterDTO>();
+		Collection<FilterConditionsToFilterDTO> allFilterConditionMappings = new LinkedList<FilterConditionsToFilterDTO>();
+		Collection<FilterConditionDTO> allFilterConditions = new LinkedList<FilterConditionDTO>();
+		Collection<RubrikDTO> alleRubriken = new LinkedList<RubrikDTO>();
+		List<User2UserGroupDTO> alleUser2UserGroupMappings = new LinkedList<User2UserGroupDTO>();
+		Collection<StringArrayFilterConditionCompareValuesDTO> allCompareValues = new LinkedList<StringArrayFilterConditionCompareValuesDTO>();
+		
+		// TODO Mit konsistenen!! daten füllen
+		
+		return new Configuration(alleAlarmbarbeiter, alleAlarmtopics, alleAlarmbearbeiterGruppen, allFilters, allFilterConditionMappings, allFilterConditions, alleRubriken, alleUser2UserGroupMappings, allCompareValues);
+	}
 
 	@Test
-	public void testConfigurationBeanServiceImpl() {
+	public void testConfigurationBeanServiceImpl() throws Throwable {
 		fail("Diesen Test implementieren, wenn der Conf-Service aufgeräumt ist, also das Configuration ein reines Material!");
+		
+		Configuration startConfiguration = createTestData();
+		
+		EasyMock.expect(confService.getEntireConfiguration()).andReturn(startConfiguration).once();
+		EasyMock.replay(confService);
 		
 		ConfigurationBeanService service = new ConfigurationBeanServiceImpl();
 		((ConfigurationBeanServiceImpl)service).setNewConfigurationStore(confService);
 		
+		service.refreshData();
 		
+		// TODO check here...
 	}
 
 }
