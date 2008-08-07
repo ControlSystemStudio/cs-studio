ba796b96d91fcfff0b729fef3620de2d9484aee6
avrecko <avrecko>
2008-08-07 07:47:04 +0000

    Initial release.

--- applications/plugins/org.csstudio.config.kryoNameBrowser/src/org/csstudio/config/kryonamebrowser/model/resolved/KryoNameResolved.java
+++ applications/plugins/org.csstudio.config.kryoNameBrowser/src/org/csstudio/config/kryonamebrowser/model/resolved/KryoNameResolved.java
@@ -1,19 +1,61 @@
 package org.csstudio.config.kryonamebrowser.model.resolved;
 
-import java.util.List;
-
 import org.csstudio.config.kryonamebrowser.model.entry.KryoObjectEntry;
 import org.csstudio.config.kryonamebrowser.model.entry.KryoPlantEntry;
 import org.csstudio.config.kryonamebrowser.model.entry.KryoProcessEntry;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class KryoNameResolved {
 
-	private String name;
+    private String name;
 
-	private List<KryoPlantEntry> plants;
+    private String label;
 
-	private List<KryoObjectEntry> objects;
+    private List<KryoPlantEntry> plants = new LinkedList<KryoPlantEntry>();
 
-	private KryoProcessEntry process;
+    private List<KryoObjectEntry> objects = new LinkedList<KryoObjectEntry>();
 
+    private KryoProcessEntry process;
+
+    private int id;
+
+    private int seqKryoNumber;
+
+    public KryoNameResolved(String name, String label, int id, int seqKryoNumber, KryoProcessEntry process) {
+        this.name = name;
+        this.label = label;
+        this.id = id;
+        this.seqKryoNumber = seqKryoNumber;
+        this.process = process;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public List<KryoPlantEntry> getPlants() {
+        return plants;
+    }
+
+    public List<KryoObjectEntry> getObjects() {
+        return objects;
+    }
+
+    public KryoProcessEntry getProcess() {
+        return process;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSeqKryoNumber() {
+        return seqKryoNumber;
+    }
 }
