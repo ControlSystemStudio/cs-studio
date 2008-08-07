34c1717362ac6a45f330b6c7bf2b31f85b87f316
mmoeller <mmoeller>
2008-08-07 09:59:05 +0000

    Small marginal change

--- applications/plugins/org.csstudio.ams.distributor/src/org/csstudio/ams/distributor/DistributorStart.java
+++ applications/plugins/org.csstudio.ams.distributor/src/org/csstudio/ams/distributor/DistributorStart.java
@@ -262,12 +262,12 @@ public class DistributorStart implements IApplication
                     storeAct.getString(org.csstudio.ams.internal.SampleService.P_JMS_EXTERN_CONNECTION_FACTORY));
             extConnection = extFactory.createConnection();
             
-            // ADDED BY: Markus Möller, 25.05.2007
+            // ADDED BY: Markus MÃ¶ller, 25.05.2007
             extConnection.setClientID("DistributorStartSenderExternal");
             
             extSession = extConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
             
-            // CHANGED BY: Markus Möller, 25.05.2007
+            // CHANGED BY: Markus MÃ¶ller, 25.05.2007
             /*extPublisherStatusChange = extSession.createProducer((Topic)extContext.lookup(
                     storeAct.getString(org.csstudio.ams.internal.SampleService.P_JMS_EXT_TOPIC_STATUSCHANGE)));
             */
