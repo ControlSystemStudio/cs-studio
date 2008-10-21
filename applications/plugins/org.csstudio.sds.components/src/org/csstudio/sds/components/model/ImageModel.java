f417a89614344657a0bd53c3feba8a5de47d9434
kmeyer <kmeyer>
2008-10-21 16:47:39 +0000

    KM: Bugfix

--- applications/plugins/org.csstudio.sds.components/src/org/csstudio/sds/components/model/ImageModel.java
+++ applications/plugins/org.csstudio.sds.components/src/org/csstudio/sds/components/model/ImageModel.java
@@ -68,7 +68,7 @@ public final class ImageModel extends AbstractWidgetModel {
 	/**
 	 * The default value for the file extensions.
 	 */
-	private static final String[] FILE_EXTENSIONS = new String[] {"*", "jpg", "jepg", "gif", "bmp", "png"};
+	private static final String[] FILE_EXTENSIONS = new String[] {"*", "jpg", "jpeg", "gif", "bmp", "png"};
 	
 	/**
 	 * {@inheritDoc}
