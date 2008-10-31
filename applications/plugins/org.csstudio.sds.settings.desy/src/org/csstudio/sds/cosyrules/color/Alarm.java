6640c3869689d94dd9d3508c1fece17f8c4f53c7
Jan Hatje <jan.hatje@desy.de>
2008-10-31 08:59:28 +0000

    add DAL severity ID

--- applications/plugins/org.csstudio.sds.settings.desy/src/org/csstudio/sds/cosyrules/color/Alarm.java
+++ applications/plugins/org.csstudio.sds.settings.desy/src/org/csstudio/sds/cosyrules/color/Alarm.java
@@ -19,16 +19,17 @@
  * PROJECT IN THE FILE LICENSE.HTML. IF THE LICENSE IS NOT INCLUDED YOU MAY FIND A COPY 
  * AT HTTP://WWW.DESY.DE/LEGAL/LICENSE.HTM
  */
- package org.csstudio.sds.cosyrules.color;
+package org.csstudio.sds.cosyrules.color;
 
 import org.csstudio.sds.model.logic.IRule;
 import org.eclipse.swt.graphics.RGB;
+import org.epics.css.dal.DynamicValueState;
 
 /**
  * Color rule Alarm, translated from an ADL file.
  * 
- * @author jbercic
- *
+ * @author jbercic, jhatje
+ * 
  */
 public final class Alarm implements IRule {
 	/**
@@ -43,31 +44,43 @@ public final class Alarm implements IRule {
 	}
 
 	/**
+	 * Map the severity of a pv value to a color. For DAL severities
+	 * (pv_name[severity]) the parameter 'arguments' is of type
+	 * DynamicValueState.ID. Using the EPICS field (record.SEVR) the severity is
+	 * a number.
+	 * 
 	 * {@inheritDoc}
 	 */
 	public Object evaluate(final Object[] arguments) {
 		if ((arguments != null) && (arguments.length > 0)) {
-            double d = 0.0;
-            if (arguments[0] instanceof Double) {
-                 d = (Double) arguments[0];
-            }else if (arguments[0] instanceof Long) {
-                d = ((Long)  arguments[0]).doubleValue();
-            }
-			
-			if (Math.abs(d-0.0)<0.00001) {
-				return new RGB(0,216,0);
+			double d = 300.0;
+			String s = "init";
+			if (arguments[0] instanceof Double) {
+				d = (Double) arguments[0];
+			} else if (arguments[0] instanceof Long) {
+				d = ((Long) arguments[0]).doubleValue();
+			} else if (arguments[0] instanceof String) {
+				s = (String) arguments[0];
 			}
-			if (Math.abs(d-1.0)<0.00001) {
-				return new RGB(251,243,74);
+
+			if ((Math.abs(d - 0.0) < 0.00001)
+					|| (s.equals(DynamicValueState.NORMAL.toString()))) {
+				return new RGB(0, 216, 0);
 			}
-			if (Math.abs(d-2.0)<0.00001) {
-				return new RGB(253,0,0);
+			if ((Math.abs(d - 1.0) < 0.00001)
+					|| (s.equals(DynamicValueState.WARNING.toString()))) {
+				return new RGB(251, 243, 74);
 			}
-			if (d>=3.0 && d<=255.0) {
-				return new RGB(255,255,255);
+			if ((Math.abs(d - 2.0) < 0.00001)
+					|| (s.equals(DynamicValueState.ALARM.toString()))) {
+				return new RGB(253, 0, 0);
+			}
+			if ((d >= 3.0 && d <= 255.0)
+					|| (s.equals(DynamicValueState.ERROR.toString()))) {
+				return new RGB(255, 255, 255);
 			}
 		}
 
-		return new RGB(0,0,0);
+		return new RGB(0, 0, 0);
 	}
 }
