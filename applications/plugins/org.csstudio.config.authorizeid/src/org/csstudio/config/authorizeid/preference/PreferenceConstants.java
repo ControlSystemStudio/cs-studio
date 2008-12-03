fd9b5acdc1f8047940f1a366e1b0f0641f9a3fea
jrathlev <jrathlev>
2008-12-03 16:31:36 +0000

    Removed unused class.

--- applications/plugins/org.csstudio.config.authorizeid/src/org/csstudio/config/authorizeid/preference/PreferenceConstants.java
+++
@@ -1,6 +0,0 @@
-package org.csstudio.config.authorizeid.preference;
-
-public class PreferenceConstants {
-	public static final String P_STRING_RECORD_ATTRIEBUT = "P_STRING_RECORD_ATTRIEBUT";
-	public static final String P_STRING_SEARCH_ROOT = "P_STRING_SEARCH_ROOT";
-}
