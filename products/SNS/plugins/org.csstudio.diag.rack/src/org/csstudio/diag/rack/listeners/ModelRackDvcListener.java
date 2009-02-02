a155d724e5b25c92d56e7dd02c0a9dd161b9ce08
Kay Kasemir <kasemirk@ornl.gov>
2009-02-02 19:15:27 +0000

    tweak

--- products/SNS/plugins/org.csstudio.diag.rack/src/org/csstudio/diag/rack/listeners/ModelRackDvcListener.java
+++
@@ -1,8 +0,0 @@
-package org.csstudio.diag.rack.listeners;
-
-public interface ModelRackDvcListener
-{
-    /** Something about the Device model changed: New entries, deleted entries, renamed entries, ... */
-    public void modelRackDvcChanged();
-
-}
