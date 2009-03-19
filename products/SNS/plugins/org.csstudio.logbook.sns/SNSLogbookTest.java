be82cef629acee0f80b5d8fbed1a8cc522025dc1
Delphy Armstrong <nypaverdj@ornl.gov>
2009-03-19 20:08:42 +0000

    Determines if it's an attachment or an image, based on input file extension. Creates an elog entry based on the type of file.  If the input text is >4000, it is converted into an attachment. SNSLogbookTest created for testing large text entries to attachment.

---
+++ products/SNS/plugins/org.csstudio.logbook.sns/SNSLogbookTest.java
@@ -0,0 +1,61 @@
+package org.csstudio.logbook.sns;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.csstudio.logbook.ILogbook;
+import org.junit.Test;
+
+/** JUnit test of the SNS Logbook.
+ *  <p>
+ *  Application code should use org.csstudio.logbook.LogbookFactory
+ *  and not directly access a specific implementation like SNS...
+ *  
+ *  @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class SNSLogbookTest
+{
+    private static final String URL =
+        "jdbc:oracle:thin:@//snsdb1.sns.ornl.gov:1521/prod";
+    private static final String LOGBOOK = "Scratch Pad";
+
+    @Test
+    public void testLoogbook() throws Exception
+    {
+        final BufferedReader command_line
+            = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("User      : ");
+        final String user = command_line.readLine();
+        
+        System.out.print("Password  : ");
+        final String password = command_line.readLine();
+
+        System.out.print("Image File: ");
+        final String image = command_line.readLine();
+
+        ILogbook logbook =
+            new SNSLogbookFactory().connect(URL, LOGBOOK, user, password);
+        assertNotNull(logbook);
+
+        try
+        {
+            String title = "Test Entry";
+            final String text = "This is a test entry";
+            logbook.createEntry(title, text, null);
+    
+            if (image.trim().length() > 0)
+            {
+                title = "Another Test Entry";
+                logbook.createEntry(title, text, image);
+            }
+        }
+        finally
+        {
+            logbook.close();
+        }
+    }
+}
--- products/SNS/plugins/org.csstudio.logbook.sns/src/org/csstudio/logbook/sns/SNSLogbook.java
+++ products/SNS/plugins/org.csstudio.logbook.sns/src/org/csstudio/logbook/sns/SNSLogbook.java
@@ -1,10 +1,17 @@
 package org.csstudio.logbook.sns;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.csstudio.logbook.ILogbook;
 import org.csstudio.platform.utility.rdb.RDBUtil;
@@ -36,11 +43,15 @@ public class SNSLogbook implements ILogbook
         badge_number = getBadgeNumber(user);
     }
 
+    /** Create the Elog entry */
     @SuppressWarnings("nls")
     public void createEntry(String title, String text, String imageName)
             throws Exception
     {
-        if (imageName == null)
+       /** If the input imageName is null - meaning there's no image to attach
+        *  and the test is not too large for an elog entry, create one.
+        */
+        if (imageName == null && text.length()<4000)
         {
             final String mysql = "call logbook.logbook_pkg.insert_logbook_entry"
                     + "(?, ?, ?, ?, ?)";
@@ -62,35 +73,159 @@ public class SNSLogbook implements ILogbook
                 statement.close();
             }
         }
+        /** Add text and image to the elog. */
+        else if(text.length()<4000)
+        {
+           addFileToElog(imageName, title, text);
+        }
+        /** Copy the text to a text file and create and elog entry with
+         *  this text file as an attachment.
+         */
         else
         {
-            final File imgFile = new File(imageName);
-            final int img_size = (int) imgFile.length();
-            final FileInputStream img_stream = new FileInputStream(imgFile);
-            CallableStatement statement = null;
-            try
-            {
-                final String mysql = "call logbook.logbook_pkg.insert_logbook_entry"
-                            + "(?, ?, ?, ?, ?, ?, ?, ?)";
-                statement = rdb.getConnection().prepareCall(mysql);
-                statement.setString(1, badge_number);
-                statement.setString(2, logbook);
-                statement.setString(3, title);
-                statement.setString(4, text);
-                statement.setString(5, "I");
-                statement.setString(6, imageName);
-                statement.setLong(7, 1);
-                statement.setBinaryStream(8, img_stream, img_size);
-                statement.executeQuery();
-            }
-            finally
-            {
-                if (statement != null)
-                    statement.close();
-                img_stream.close();
-            }
+           String fname = "/tmp/logbook.txt";
+           File logFile = new File(fname);
+           logFile.createNewFile();
+           setContents(logFile, text);
+           addFileToElog(fname, title, "See Attachment");
+           logFile.deleteOnExit();
         }
     }
+    
+    private void addFileToElog(String fname, String title, String text)
+    {
+       final int ndx = fname.lastIndexOf(".");
+       final String extension = fname.substring(ndx+1);
+       String fileType = "";
+       long fileTypeID = -1;
+        try
+        {
+           fileTypeID=fetchImageTypes( extension );
+           if(fileTypeID>=0) fileType = "I";
+        }
+        catch(SQLException e)
+        {
+           e.toString();
+        }
+      catch (Exception e)
+      {
+         e.toString();
+      }
+      if(fileTypeID==-1)
+      {
+         try
+         {
+            fileTypeID=fetchAttachmentTypes( extension );
+            if(fileTypeID>=0) fileType = "A";
+         }
+         catch(SQLException e)
+         {
+            e.toString();
+         }
+       catch (Exception e)
+       {
+          e.toString();
+       }
+      }
+      if(fileTypeID < 0)
+      {
+         System.out.println(extension + " is an invalid file type.");
+         return;
+      }
+       final File inputFile = new File(fname);
+       final int file_size = (int) inputFile.length();
+       FileInputStream input_stream = null;
+      try
+      {
+         input_stream = new FileInputStream(inputFile);
+      }
+      catch (FileNotFoundException e1)
+      {
+         e1.getMessage();
+      }
+       CallableStatement statement = null;
+       try
+       {
+           final String mysql = "call logbook.logbook_pkg.insert_logbook_entry"
+                       + "(?, ?, ?, ?, ?, ?, ?, ?)";
+           statement = rdb.getConnection().prepareCall(mysql);
+           statement.setString(1, badge_number);
+           statement.setString(2, logbook);
+           statement.setString(3, title);
+           statement.setString(4, text);
+           statement.setString(5, fileType);
+           statement.setString(6, fname);
+           statement.setLong(7, fileTypeID);
+           statement.setBinaryStream(8, input_stream, file_size);
+           statement.executeQuery();
+       }
+      catch (SQLException e)
+      {
+         e.getMessage();
+      }
+      catch (Exception e)
+      {
+         e.getMessage();
+      }
+ 
+           if (statement != null)
+            try
+            {
+               statement.close();
+            }
+            catch (SQLException e)
+            {
+               e.getMessage();
+            }
+            if (input_stream != null)
+           try
+         {
+            input_stream.close();
+         }
+         catch (IOException e)
+         {
+            e.getMessage();
+         }
+
+    }
+    
+    /**
+     * Change the contents of text file in its entirety, overwriting any
+     * existing text.
+     *
+     * This style of implementation throws all exceptions to the caller.
+     *
+     * @param aFile is an existing file which can be written to.
+     * @throws IllegalArgumentException if param does not comply.
+     * @throws FileNotFoundException if the file does not exist.
+     * @throws IOException if problem encountered during write.
+     */
+     static public void setContents(File aFile, String aContents)
+                                    throws FileNotFoundException, IOException {
+       if (aFile == null) {
+         throw new IllegalArgumentException("File should not be null.");
+       }
+       if (!aFile.exists()) {
+         throw new FileNotFoundException ("File does not exist: " + aFile);
+       }
+       if (!aFile.isFile()) {
+         throw new IllegalArgumentException("Should not be a directory: " + aFile);
+       }
+       if (!aFile.canWrite()) {
+         throw new IllegalArgumentException("File cannot be written: " + aFile);
+       }
+
+       //use buffering
+       Writer output = new BufferedWriter(new FileWriter(aFile));
+       try {
+         //FileWriter always assumes default encoding is OK!
+         output.write( aContents );
+       }
+       finally {
+         output.close();
+       }
+     }
+
     
     /** Get the badge number for the user in the connection dictionary
      *  @return the badge number for the specified user or a default
@@ -125,4 +260,48 @@ public class SNSLogbook implements ILogbook
     {
         rdb.close();
     }
+    
+    /** Fetch the available image types. 
+    * @throws Exception 
+    * @throws SQLException */
+    private long fetchImageTypes( String image_type ) throws SQLException, Exception  {
+       final Statement statement = rdb.getConnection().createStatement();
+       try {
+          final ResultSet result = statement.executeQuery( "select * from LOGBOOK.IMAGE_TYPE" );
+                    
+          while ( result.next() ) {
+             long ID = result.getLong( "image_type_id" );
+             String extension = result.getString( "file_extension" );
+             if(image_type.equalsIgnoreCase(extension))
+                return ID;
+          }
+       }
+       finally
+       {
+           statement.close();
+       }
+      return -1;
+    }
+    
+    
+    /** Fetch the available attachment types. */
+    private long fetchAttachmentTypes( String attachment_type ) throws SQLException, Exception  {
+       final Statement statement = rdb.getConnection().createStatement();
+       try {
+          final ResultSet result = statement.executeQuery( "select * from LOGBOOK.ATTACHMENT_TYPE" );
+                    
+          while ( result.next() ) {
+             long ID = result.getLong( "attachment_type_id" );
+             String extension = result.getString( "file_extension" );
+             if(attachment_type.equalsIgnoreCase(extension))
+                return ID;
+          }
+       }
+       finally
+       {
+           statement.close();
+       }
+      return -1;
+    }
+          
 }
