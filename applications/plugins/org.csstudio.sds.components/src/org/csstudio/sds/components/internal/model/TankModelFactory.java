552560d7abdee689ff9f4463b413d8c28e24f78f
Xihui Chen <chenx1@ornl.gov>
2009-03-26 23:45:37 +0000

    improve comments

--- applications/plugins/org.csstudio.sds.components/src/org/csstudio/sds/components/internal/model/TankModelFactory.java
+++ applications/plugins/org.csstudio.sds.components/src/org/csstudio/sds/components/internal/model/TankModelFactory.java
@@ -1,12 +1,11 @@
 package org.csstudio.sds.components.internal.model;
 
 import org.csstudio.sds.components.model.TankModel;
-import org.csstudio.sds.components.model.ThermometerModel;
 import org.csstudio.sds.model.AbstractWidgetModel;
 import org.csstudio.sds.model.IWidgetModelFactory;
 
 /**
- * This class defines a widget model factory for thermometer widget models.
+ * This class defines a widget model factory for tank widget models.
  * 
  * @author Xihui Chen
  * 
