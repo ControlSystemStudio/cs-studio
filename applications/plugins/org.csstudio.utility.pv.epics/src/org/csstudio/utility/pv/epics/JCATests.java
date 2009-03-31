9b8ed83df71dd1ab7dd8d8b4a8e0da28f2062d47
Kay Kasemir <kasemirk@ornl.gov>
2009-03-31 18:46:39 +0000

    run in loop for mem leak check

--- applications/plugins/org.csstudio.utility.pv.epics/src/org/csstudio/utility/pv/epics/JCATests.java
+++ applications/plugins/org.csstudio.utility.pv.epics/src/org/csstudio/utility/pv/epics/JCATests.java
@@ -8,6 +8,10 @@ import gov.aps.jca.dbr.DBR_TIME_Double;
 import gov.aps.jca.event.MonitorEvent;
 import gov.aps.jca.event.MonitorListener;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /** JCA Tests
  *  <p>
  *  Must set LD_LIBRARY_PATH to locate JCA lib,
@@ -18,10 +22,17 @@ import gov.aps.jca.event.MonitorListener;
 @SuppressWarnings("nls")
 public class JCATests
 {
+    private static final String PV_NAME = "RFQ_Vac:Pump3:Pressure";
+    private static final String ADDR_LIST = "160.91.230.38";
+    private static final int TESTRUNS = 10;
+
     /** Set <code>true</code> to check for cleanup error. */
-    private static final boolean CLEANUP = false;
+    private static final boolean CLEANUP = true;
     private static JCALibrary jca = null;
     private static Context jca_context = null;
+    
+    final private static DateFormat format =
+        new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
     
     /** Monitor given PV for some time. */
     static private void run(final String pv_name) throws Exception
@@ -52,10 +63,20 @@ public class JCATests
         jca_context.flushIO();
 
         // Run
-        Thread.sleep(5 * 1000);
+        Thread.sleep(3 * 1000);
 
         // Cleanup. More or less
         channel.destroy();
+
+        // With jca-2.3.0, built against EPICS base R3.14.7,
+        // it's OK to clean up.
+        //
+        // But with jca-2.3.0 or jca-2.3.1, built against EPICS base R3.14.8.2,
+        // this results in errors in the jca_context.createChannel() call
+        // after handling the first channel with 'CLEANUP':
+        // "pthread_create error Invalid argument
+        //  CAC: exception during virtual circuit creation:
+        //  epicsThread class was unable to  create a new thread"
         if (CLEANUP)
         {
             jca_context.destroy();
@@ -63,21 +84,28 @@ public class JCATests
             jca = null;
         }
     }
-    
+
+    private static void dumpMeminfo()
+    {
+        final double MB = 1024.0*1024.0;
+        final double free = Runtime.getRuntime().freeMemory() / MB;
+        final double total = Runtime.getRuntime().totalMemory() / MB;
+        final double max = Runtime.getRuntime().maxMemory() / MB;
+        
+        System.out.format("%s == Alarm Server Memory: Max %.2f MB, Free %.2f MB (%.1f %%), total %.2f MB (%.1f %%)\n",
+                format.format(new Date()), max, free, 100.0*free/max, total, 100.0*total/max);
+    }
+
     public static void main(String[] args) throws Exception
     {
-        // First channel runs OK: Gets values, completes w/o errors
-        JCATests.run("fred");
-
-        // With jca-2.3.0, built against EPICS base R3.14.7,
-        // the second attempt works fine, too.
-        //
-        // But with jca-2.3.0 or jca-2.3.1, built against EPICS base R3.14.8.2,
-        // this results in errors in the jca_context.createChannel() call
-        // when I try to use 'CLEANUP':
-        // "pthread_create error Invalid argument
-        //  CAC: exception during virtual circuit creation:
-        //  epicsThread class was unable to  create a new thread"
-        JCATests.run("jane");
+        System.setProperty("gov.aps.jca.jni.JNIContext.addr_list", ADDR_LIST);
+        System.setProperty("gov.aps.jca.jni.JNIContext.auto_addr_list", "true");
+        
+        
+        for (int i=0; i<TESTRUNS; ++i)
+        {
+            JCATests.run(PV_NAME);
+            dumpMeminfo();
+        }
     }
 }
