db911afc35e4ec9fd824243c6af8878dcfef775b
Jan Hatje <jan.hatje@desy.de>
2009-05-04 06:16:19 +0000

    -remove obsolete properties for alarm table

--- applications/plugins/org.csstudio.alarm.table/src/org/csstudio/alarm/table/preferences/LogViewPreferenceConstants.java
+++ applications/plugins/org.csstudio.alarm.table/src/org/csstudio/alarm/table/preferences/LogViewPreferenceConstants.java
@@ -27,8 +27,7 @@
 public class LogViewPreferenceConstants {
 
 	public static final String P_STRING = "Log.column_names";//$NON-NLS-1$
-	public static final String MAX = Messages.getString("LogViewPreferenceConstants.rows.max"); //$NON-NLS-1$
-	public static final String REMOVE = Messages.getString("LogViewPreferenceConstants.rows.min"); //$NON-NLS-1$
+	public static final String MAX = "Log.max_size"; //$NON-NLS-1$
 	//Sets of JMS topics to be monitored. List separated with
 	//';'. The optional name for the menu is separated with '?'.
 	public static final String TOPIC_SET = "Log.topic_set";
