6a3fb0de5231f51ae2999028555aee4aa5aec922
kmeyer <kmeyer>
2009-05-18 08:23:31 +0000

    Unnecessary Code deleted Perspective for SNL Development created

---
+++ products/DESY/plugins/de.desy.language.snl.ui/src/de/desy/language/snl/ui/Perspective.java
@@ -0,0 +1,15 @@
+package de.desy.language.snl.ui;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(true);
+		layout.addView(IPageLayout.ID_RES_NAV, IPageLayout.LEFT, 0.30f, IPageLayout.ID_EDITOR_AREA);
+		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.RIGHT, 0.75f, IPageLayout.ID_EDITOR_AREA);
+	}
+
+}
