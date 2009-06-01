9bff600ee57f549919d5d1f41389b7f673699ebd
Xihui Chen <chenx1@ornl.gov>
2009-06-01 14:19:07 +0000

    set transparent to false

--- applications/plugins/org.csstudio.sds.components/src/org/csstudio/sds/components/model/XMeterModel.java
+++ applications/plugins/org.csstudio.sds.components/src/org/csstudio/sds/components/model/XMeterModel.java
@@ -49,6 +49,7 @@ public class XMeterModel extends AbstractMarkedWidgetModel{
 				WidgetPropertyCategory.Display, true));	
 		
 		setPropertyDescription(PROP_SHOW_MARKERS, "Show Ramp");		
+		setPropertyValue(PROP_TRANSPARENT, false);
 	}	
 
 	@Override
