f02268c1dcba24e79b5fe119d29076971d407da7
Xihui Chen <chenx1@ornl.gov>
2009-06-01 14:41:43 +0000

    add inRange()

--- applications/plugins/org.csstudio.sds.components.ui/src/org/csstudio/sds/components/ui/internal/figureparts/Range.java
+++ applications/plugins/org.csstudio.sds.components.ui/src/org/csstudio/sds/components/ui/internal/figureparts/Range.java
@@ -20,8 +20,8 @@ public class Range {
      *            the end value of range
      */
     public Range(double start, double end) {
-    	if(end == start)
-    		end = start + 1;
+    	//if(end == start)
+    	//	end = start + 1;
         this.lower = (end > start) ? start : end;
         this.upper = (end > start) ? end : start;
     }
@@ -37,6 +37,26 @@ public class Range {
         upper = (range.upper > range.lower) ? upper : lower;
     }
 
+    
+    /**If a value in the range or not.
+     * @param value
+     * @param includeBoundary true if the boundary should be considered.
+     * @return true if the value is in the range. Otherwise false.
+     */
+    public boolean inRange(double value, boolean includeBoundary){
+    	if(includeBoundary)
+    		return (value >= lower && value <= upper);
+    	else
+    		return (value > lower && value < upper);
+    }
+    
+    /**If a value in the range or not. The boundary is included.
+     * @param value
+     * @return true if the value is in the range. Otherwise false.
+     */
+    public boolean inRange(double value){
+    	return inRange(value, true);
+    }
     /*
      * @see Object#toString()
      */
