a5ec1876aa576f26c4e3f8055438aa16484b2f50
Kay Kasemir <kasemirk@ornl.gov>
2009-06-10 14:50:47 +0000

    Enforce 1 sec min. period. Show period for 'monitor' channels in HTTPD

--- applications/plugins/org.csstudio.archive.engine/src/org/csstudio/archive/engine/model/EngineModel.java
+++ applications/plugins/org.csstudio.archive.engine/src/org/csstudio/archive/engine/model/EngineModel.java
@@ -23,7 +23,7 @@ import org.eclipse.core.runtime.preferences.IPreferencesService;
 public class EngineModel
 {
     /** Version code */
-    final public static String VERSION = "1.0.0"; //$NON-NLS-1$
+    final public static String VERSION = "1.0.1"; //$NON-NLS-1$
 
     /** Name of this model */
     private String name = "Archive Engine";  //$NON-NLS-1$
@@ -264,7 +264,8 @@ public class EngineModel
             // Create new channel
             if (monitor)
                 channel = new MonitoredArchiveChannel(name, enablement,
-                                                 buffer_capacity, last_sample);
+                                                 buffer_capacity, last_sample,
+                                                 period);
             else
             {
                 channel = new ScannedArchiveChannel(name, enablement,
--- applications/plugins/org.csstudio.archive.engine/src/org/csstudio/archive/engine/model/MonitoredArchiveChannel.java
+++ applications/plugins/org.csstudio.archive.engine/src/org/csstudio/archive/engine/model/MonitoredArchiveChannel.java
@@ -1,5 +1,6 @@
 package org.csstudio.archive.engine.model;
 
+import org.csstudio.apputil.time.PeriodFormat;
 import org.csstudio.platform.data.IValue;
 
 /** An ArchiveChannel that stores each incoming value.
@@ -7,19 +8,25 @@ import org.csstudio.platform.data.IValue;
  */
 public class MonitoredArchiveChannel extends ArchiveChannel
 {
+    /** Estimated period of change in seconds */
+    final private double period_estimate;
+
     /** @see ArchiveChannel#ArchiveChannel(String, int, IValue) */
     public MonitoredArchiveChannel(final String name,
                                    final Enablement enablement,
                                    final int buffer_capacity,
-                                   final IValue last_archived_value) throws Exception
+                                   final IValue last_archived_value,
+                                   final double period_estimate) throws Exception
     {
         super(name, enablement, buffer_capacity, last_archived_value);
+        this.period_estimate = period_estimate;
     }
 
+    @SuppressWarnings("nls")
     @Override
     public String getMechanism()
     {
-        return "on change"; //$NON-NLS-1$
+        return "on change [" + PeriodFormat.formatSeconds(period_estimate) + "]";
     }
 
     /** Attempt to add each new value to the buffer. */
