093d3a60971ed76b5865c43331daff6a4a85be55
Kay Kasemir <kasemirk@ornl.gov>
2009-06-24 20:28:46 +0000

    RDB lib

--- applications/plugins/org.csstudio.archive.engine/src/org/csstudio/archive/engine/model/EngineModel.java
+++ applications/plugins/org.csstudio.archive.engine/src/org/csstudio/archive/engine/model/EngineModel.java
@@ -23,7 +23,7 @@ import org.eclipse.core.runtime.preferences.IPreferencesService;
 public class EngineModel
 {
     /** Version code */
-    final public static String VERSION = "1.0.2c"; //$NON-NLS-1$
+    final public static String VERSION = "1.0.2d"; //$NON-NLS-1$
 
     /** Name of this model */
     private String name = "Archive Engine";  //$NON-NLS-1$
