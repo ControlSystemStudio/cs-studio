87da24152d569ca75a250857663cadf46cd34626
Kay Kasemir <kasemirk@ornl.gov>
2009-06-25 20:32:40 +0000

    Simpler cancel test

--- applications/plugins/org.csstudio.archive.rdb/src/org/csstudio/archive/rdb/internal/test/CancelTest.java
+++ applications/plugins/org.csstudio.archive.rdb/src/org/csstudio/archive/rdb/internal/test/CancelTest.java
@@ -8,10 +8,6 @@ import java.sql.ResultSet;
 
 import org.csstudio.archive.rdb.RDBArchive;
 import org.csstudio.archive.rdb.TestSetup;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /** Try to 'cancel' a long running query.
@@ -20,47 +16,16 @@ import org.junit.Test;
 @SuppressWarnings("nls")
 public class CancelTest
 {
-    private static RDBArchive archive;
-    private PreparedStatement query;
-
-    @BeforeClass
-    public static void connect() throws Exception
-    {
-        archive = RDBArchive.connect(TestSetup.URL, TestSetup.USER, TestSetup.PASSWORD);
-    }
-    
-    @AfterClass
-    public static void disconnect()
-    {
-        archive.close();
-    }
-
     @Test
     public void testCancelLongRunningRequest() throws Exception
     {
+        final RDBArchive archive =
+            RDBArchive.connect(TestSetup.URL, TestSetup.USER, TestSetup.PASSWORD);
         // The database connection
         final Connection connection = archive.getRDB().getConnection();
-        
-        // This RDB request would normally run in a background thread,
-        // so that the GUI stays responsive while we wait for the
-        // database.
-        // One would probably use an Eclipse "Job" instead
-        // of a plain Java "Thread" because that way we get
-        // a progress monitor that's connected to the Eclipse
-        // Progress View where users can see active jobs, request
-        // to cancel them etc.
-        // In this test, we use a fake progress monitor:
-        final IProgressMonitor progress = new NullProgressMonitor();
 
-        // Generate a query that could take a long time...
-        progress.beginTask("Getting sample count", IProgressMonitor.UNKNOWN);
-        query = connection.prepareStatement("SELECT COUNT(*) FROM chan_arch.sample");
-
-        // We will be stuck executing the query.
-        // Create background thread to check if the user tried to cancel.
-        final StatementCancelThread cancel_check =
-            new StatementCancelThread("CancelCheck", progress, query);
-        cancel_check.start();
+        final PreparedStatement query =
+            connection.prepareStatement("SELECT COUNT(*) FROM chan_arch.sample");
 
         // This thread simulates a user who pressed "cancel" in the progress
         // view after 4 seconds
@@ -72,7 +37,10 @@ public class CancelTest
                 try
                 {
                     Thread.sleep(4000);
-                    progress.setCanceled(true);
+                    // Note that connection.close() does NOT stop
+                    // an ongoing Oracle query!
+                    // Only this seems to do it:
+                    query.cancel();
                 }
                 catch (Exception ex)
                 {
@@ -84,8 +52,6 @@ public class CancelTest
         try
         {
             final ResultSet rs = query.executeQuery();
-            cancel_check.end();
-            progress.beginTask("Analysing result", IProgressMonitor.UNKNOWN);
             assertTrue(rs.next());
             System.out.println("Samples: " + rs.getInt(1));
         }
@@ -93,7 +59,10 @@ public class CancelTest
         {
             ex.printStackTrace();
         }
-        query.close();
-        progress.done();
+        finally
+        {
+            query.close();
+            archive.close();
+        }
     }
 }
