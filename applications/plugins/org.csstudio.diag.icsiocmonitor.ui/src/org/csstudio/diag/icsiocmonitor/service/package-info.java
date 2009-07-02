e19ff457ea9a5d944329e8398190545dd00a1813
jrathlev <jrathlev>
2009-07-02 12:01:57 +0000

    Added package documentation for service package

---
+++ applications/plugins/org.csstudio.diag.icsiocmonitor.ui/src/org/csstudio/diag/icsiocmonitor/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains the service which the interconnection servers implement to report
+ * the connection state of the IOCs to which they are connected.
+ */
+package org.csstudio.diag.icsiocmonitor.service;
