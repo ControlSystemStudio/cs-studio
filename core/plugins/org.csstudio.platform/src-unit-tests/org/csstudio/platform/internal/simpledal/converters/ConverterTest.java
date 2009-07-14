d6314eab6368c487c8ddadf609ed2e31c40f4bbe
Helge Rickens <helge.rickens@desy.de>
2009-07-14 06:06:43 +0000

    change double test to use delta

--- core/plugins/org.csstudio.platform/src-unit-tests/org/csstudio/platform/internal/simpledal/converters/ConverterTest.java
+++ core/plugins/org.csstudio.platform/src-unit-tests/org/csstudio/platform/internal/simpledal/converters/ConverterTest.java
@@ -1,7 +1,6 @@
 package org.csstudio.platform.internal.simpledal.converters;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,29 +17,29 @@ public class ConverterTest {
         DoubleConverter doubleConverter = new DoubleConverter();
         // Integer
         int i = 0;
-        assertEquals(i, doubleConverter.convert(i));
+        assertEquals(i, doubleConverter.convert(i), 0.0001);
         i = 12345;
-        assertEquals(i, doubleConverter.convert(i));
+        assertEquals(i, doubleConverter.convert(i), 0.0001);
         i = -12345;
-        assertEquals(i, doubleConverter.convert(i));
+        assertEquals(i, doubleConverter.convert(i), 0.0001);
         // Long
         long l = 0;
-        assertEquals(l, doubleConverter.convert(l));
+        assertEquals(l, doubleConverter.convert(l), 0.0001);
         l = 12345;
-        assertEquals(l, doubleConverter.convert(l));
+        assertEquals(l, doubleConverter.convert(l), 0.0001);
         l = -12345;
-        assertEquals(l, doubleConverter.convert(l));
+        assertEquals(l, doubleConverter.convert(l), 0.0001);
         // Double
         double d = 0;
-        assertEquals(d, doubleConverter.convert(d));
+        assertEquals(d, doubleConverter.convert(d), 0.0001);
         d = 12345;
-        assertEquals(d, doubleConverter.convert(d));
+        assertEquals(d, doubleConverter.convert(d), 0.0001);
         d = -12345;
-        assertEquals(d, doubleConverter.convert(d));
+        assertEquals(d, doubleConverter.convert(d), 0.0001);
         d = 123.45;
-        assertEquals(d, doubleConverter.convert(d));
+        assertEquals(d, doubleConverter.convert(d), 0.0001);
         d = -123.45;
-        assertEquals(d, doubleConverter.convert(d));
+        assertEquals(d, doubleConverter.convert(d), 0.0001);
         // Wrong Object
         assertNuberFormatExeption(doubleConverter, new Object());
         // String
@@ -48,22 +47,22 @@ public class ConverterTest {
         assertNuberFormatExeption(doubleConverter, "1qayXSW,.-+#");
         assertNuberFormatExeption(doubleConverter, "qayXSW,.-+#2454");
         // Int as String
-        assertEquals(0, doubleConverter.convert("0"));
-        assertEquals(1234567890, doubleConverter.convert("1234567890"));
-        assertEquals(-1234567890, doubleConverter.convert("-1234567890"));
+        assertEquals(0, doubleConverter.convert("0"), 0.0001);
+        assertEquals(1234567890, doubleConverter.convert("1234567890"), 0.0001);
+        assertEquals(-1234567890, doubleConverter.convert("-1234567890"), 0.0001);
         
         // Float as String
-        assertEquals(12345.67890, doubleConverter.convert("12345.67890"));
-        assertEquals(-12345.67890, doubleConverter.convert("-12345.67890"));
+        assertEquals(12345.67890, doubleConverter.convert("12345.67890"), 0.0001);
+        assertEquals(-12345.67890, doubleConverter.convert("-12345.67890"), 0.0001);
         assertNuberFormatExeption(doubleConverter, "-12345.-67890");
 
         // Exp as Stirng
-        assertEquals(123400, doubleConverter.convert("1234e2"));
-        assertEquals(-123.4, doubleConverter.convert("-1234e-1"));
-        assertEquals(123400, doubleConverter.convert("+1234e+2"));
-        assertEquals(1234, doubleConverter.convert("+12.34e+2"));
+        assertEquals(123400, doubleConverter.convert("1234e2"), 0.0001);
+        assertEquals(-123.4, doubleConverter.convert("-1234e-1"), 0.0001);
+        assertEquals(123400, doubleConverter.convert("+1234e+2"), 0.0001);
+        assertEquals(1234, doubleConverter.convert("+12.34e+2"), 0.0001);
         assertNuberFormatExeption(doubleConverter, "+12.34e+12.3");
-        assertEquals(-.01234, doubleConverter.convert("-12.34e-3"));
+        assertEquals(-.01234, doubleConverter.convert("-12.34e-3"), 0.0001);
         assertNuberFormatExeption(doubleConverter, "-12.34e-12.3");
         
         // Hex as String
@@ -75,14 +74,14 @@ public class ConverterTest {
         assertNuberFormatExeption(doubleConverter, "+AF");
         assertNuberFormatExeption(doubleConverter, "-1af");
         assertNuberFormatExeption(doubleConverter, "-AF");
-        assertEquals(431, doubleConverter.convert("0x1AF"));
-        assertEquals(175, doubleConverter.convert("0xaf"));
-        assertEquals(175, doubleConverter.convert("+0xaf"));
-        assertEquals(18, doubleConverter.convert("0x12"));
-        assertEquals(18, doubleConverter.convert("+0x12"));
-        assertEquals(-18, doubleConverter.convert("-0x12"));
-        assertEquals(-431, doubleConverter.convert("-0X1AF"));
-        assertEquals(-175, doubleConverter.convert("-0xaf"));
+        assertEquals(431, doubleConverter.convert("0x1AF"), 0.0001);
+        assertEquals(175, doubleConverter.convert("0xaf"), 0.0001);
+        assertEquals(175, doubleConverter.convert("+0xaf"), 0.0001);
+        assertEquals(18, doubleConverter.convert("0x12"), 0.0001);
+        assertEquals(18, doubleConverter.convert("+0x12"), 0.0001);
+        assertEquals(-18, doubleConverter.convert("-0x12"), 0.0001);
+        assertEquals(-431, doubleConverter.convert("-0X1AF"), 0.0001);
+        assertEquals(-175, doubleConverter.convert("-0xaf"), 0.0001);
 
     }
 
@@ -99,29 +98,29 @@ public class ConverterTest {
         LongConverter longConverter = new LongConverter();
         // Integer
         int i = 0;
-        assertEquals(i, longConverter.convert(i));
+        assertEquals(i, longConverter.convert(i), 0.0001);
         i = 12345;
-        assertEquals(i, longConverter.convert(i));
+        assertEquals(i, longConverter.convert(i), 0.0001);
         i = -12345;
-        assertEquals(i, longConverter.convert(i));
+        assertEquals(i, longConverter.convert(i), 0.0001);
         // Long
         long l = 0;
-        assertEquals(l, longConverter.convert(l));
+        assertEquals(l, longConverter.convert(l), 0.0001);
         l = 12345;
-        assertEquals(l, longConverter.convert(l));
+        assertEquals(l, longConverter.convert(l), 0.0001);
         l = -12345;
-        assertEquals(l, longConverter.convert(l));
+        assertEquals(l, longConverter.convert(l), 0.0001);
         // Double
         double d = 0;
-        assertEquals(d, longConverter.convert(d));
+        assertEquals(d, longConverter.convert(d), 0.0001);
         d = 12345;
-        assertEquals(d, longConverter.convert(d));
+        assertEquals(d, longConverter.convert(d), 0.0001);
         d = -12345;
-        assertEquals(d, longConverter.convert(d));
+        assertEquals(d, longConverter.convert(d), 0.0001);
         d = 123.45;
-        assertEquals(d, longConverter.convert(d));
+        assertEquals(d, longConverter.convert(d), 0.0001);
         d = -123.45;
-        assertEquals(d, longConverter.convert(d));
+        assertEquals(d, longConverter.convert(d), 0.0001);
         // Wrong Object
         assertNuberFormatExeption(longConverter, new Object());
         // String
@@ -129,9 +128,9 @@ public class ConverterTest {
         assertNuberFormatExeption(longConverter, "1qayXSW,.-+#");
         assertNuberFormatExeption(longConverter, "qayXSW,.-+#2454");
         // Int as String
-        assertEquals(0, longConverter.convert("0"));
-        assertEquals(1234567890, longConverter.convert("1234567890"));
-        assertEquals(-1234567890, longConverter.convert("-1234567890"));
+        assertEquals(0, longConverter.convert("0"), 0.0001);
+        assertEquals(1234567890, longConverter.convert("1234567890"), 0.0001);
+        assertEquals(-1234567890, longConverter.convert("-1234567890"), 0.0001);
         
         // Float as String
         assertNuberFormatExeption(longConverter, "12345.67890");
@@ -158,13 +157,13 @@ public class ConverterTest {
         assertNuberFormatExeption(longConverter, "+AF");
         assertNuberFormatExeption(longConverter, "-1af");
         assertNuberFormatExeption(longConverter, "-AF");
-        assertEquals(431, longConverter.convert("0x1AF"));
-        assertEquals(175, longConverter.convert("0xaf"));
-        assertEquals(175, longConverter.convert("+0xaf"));
-        assertEquals(18, longConverter.convert("0x12"));
-        assertEquals(18, longConverter.convert("+0x12"));
-        assertEquals(-18, longConverter.convert("-0x12"));
-        assertEquals(-431, longConverter.convert("-0X1AF"));
-        assertEquals(-175, longConverter.convert("-0xaf"));
+        assertEquals(431, longConverter.convert("0x1AF"), 0.0001);
+        assertEquals(175, longConverter.convert("0xaf"), 0.0001);
+        assertEquals(175, longConverter.convert("+0xaf"), 0.0001);
+        assertEquals(18, longConverter.convert("0x12"), 0.0001);
+        assertEquals(18, longConverter.convert("+0x12"), 0.0001);
+        assertEquals(-18, longConverter.convert("-0x12"), 0.0001);
+        assertEquals(-431, longConverter.convert("-0X1AF"), 0.0001);
+        assertEquals(-175, longConverter.convert("-0xaf"), 0.0001);
     }
 }
