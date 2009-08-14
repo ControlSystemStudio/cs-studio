dcca7bd7543eb1e26e4e8bd41f9901f350248248
Kay Kasemir <kasemirk@ornl.gov>
2009-08-14 12:23:02 +0000

    switch persepctive

--- applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/NewDataBrowserAction.java
+++ applications/plugins/org.csstudio.trends.databrowser/src/org/csstudio/trends/databrowser/NewDataBrowserAction.java
@@ -3,8 +3,11 @@ package org.csstudio.trends.databrowser;
 import org.csstudio.trends.databrowser.ploteditor.PlotEditor;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
 
 /** Action connected to workbench menu action set for opening a new editor.
  *  @author Kay Kasemir
@@ -20,6 +23,18 @@ public class NewDataBrowserAction implements IWorkbenchWindowActionDelegate
 	public void run(IAction action)
 	{
 		PlotEditor.createInstance();
+		// Try to switch to the DataBrowser perspective
+		// (suggested by Helge Rickens)
+        final IWorkbench workbench = PlatformUI.getWorkbench();
+        final IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+        try
+        {
+            workbench.showPerspective(Perspective.ID, window);
+        }
+        catch (WorkbenchException ex)
+        {
+            // Never mind
+        }
 	}
 
 	public void dispose()
