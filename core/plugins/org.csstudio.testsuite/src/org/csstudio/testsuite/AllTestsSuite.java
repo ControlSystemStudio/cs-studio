bd770b529c4b041b6bbb5e81adc15f2688edf430
cweber <cweber>
2009-10-13 12:45:26 +0000

    added Fragment Option, bundleRoot & packageRoot as "List"

--- core/plugins/org.csstudio.testsuite/src/org/csstudio/testsuite/AllTestsSuite.java
+++ core/plugins/org.csstudio.testsuite/src/org/csstudio/testsuite/AllTestsSuite.java
@@ -14,18 +14,24 @@ import junit.framework.TestSuite;
 
 public class AllTestsSuite {
 	
-	private static final String bundleRoot = "org.csstudio";
-	private static final String packageRoot = "org.csstudio";
+	//Comma separated list, if list is empty, every root will be chosen
+	private static final String bundleRoot = "org.,de.desy.";
+	
+	//Comma separated list, must contain one entry!
+	private static final String packageRoot = "org.csstudio,org.epics,org.remotercp,de.desy";
+	
+	//Only tests in fragments will be used
+	private static boolean onlyfragments = false;
+	
 	private static final String testClassFilter = "*Test";
 	private static final String testsuitename = "CSSTestsSuite";
 	
 	public static Test suite() {
 		TestSuite suite = new TestSuite(testsuitename);
 		for (Bundle bundle : Activator.getBundles()) {
-			if (isFragment(bundle)
-					&& bundle.getSymbolicName().startsWith(bundleRoot)) {
+			// check fragment & bundleRoot conditions
+			if (checkFragment(bundle) && checkBundleRoot(bundle) ) {
 				List<Class> testClasses = getTestClasesInBundle(bundle);
-
 				for (Class clazz : testClasses) {
 					suite.addTest(new JUnit4TestAdapter(clazz));				
 				}
@@ -34,25 +40,46 @@ public class AllTestsSuite {
 		return suite;
 	}
 
+	private static boolean checkFragment(Bundle bundle) {
+		boolean res = true;
+		if (onlyfragments) 
+			res = isFragment(bundle);	
+		return res;
+	}
+	
+	private static boolean checkBundleRoot(Bundle bundle) {
+		boolean res = false;
+		if (bundleRoot.contains(",")) {
+			String[] bundleRoots = bundleRoot.split(",");
+			for(String root:bundleRoots) {
+				if (bundle.getSymbolicName().startsWith(root)) {
+					res = true;
+					break;
+				}
+			}	
+		} else {
+			if (bundleRoot.equals("")) {
+				res = true;
+			} else {
+				res = bundle.getSymbolicName().startsWith(bundleRoot);
+			}
+		}
+		return res;
+	}
+	
+
 	private static List<Class> getTestClasesInBundle(Bundle bundle) {
 		List<Class> testClassesInBundle = new ArrayList<Class>();
-
-		Enumeration testClassNames = bundle.findEntries("/", testClassFilter + ".class", true); 
+		Enumeration testClassNames = bundle.findEntries("/", testClassFilter + ".class", true);
+		
 		if (testClassNames != null) {
 			while (testClassNames.hasMoreElements()) {
 
-				/*
-				 * Take relative path produced by findEntries method and convert
-				 * it into a properly formatted class name. The package root is
-				 * used to determine the start of the qualified class name in
-				 * the path.
-				 */
-				String testClassPath = ((URL) testClassNames.nextElement())
-						.getPath();
+				String testClassPath = ((URL) testClassNames.nextElement()).getPath();
 				testClassPath = testClassPath.replace('/', '.');
 
-				int packageRootStart = testClassPath.indexOf(packageRoot);
-
+				int packageRootStart = getPackageRoot(testClassPath);
+				
 				/* if class does not begin with package root, just ignore it */
 				if (packageRootStart == -1) {
 					continue;
@@ -87,7 +114,6 @@ public class AllTestsSuite {
 				 * If the class is not abstract, add it to list
 				 */
 				if (!Modifier.isAbstract(testClass.getModifiers())) {
-//					System.out.println("Add class " + testClassPath);
 					testClassesInBundle.add(testClass);
 				}
 			}
@@ -95,8 +121,25 @@ public class AllTestsSuite {
 		return testClassesInBundle;
 	}
 	
+	
+	private static int getPackageRoot(String testClassPath) {
+		int res = -1;
+		if (packageRoot.contains(",")) {
+			String[] packageRoots = packageRoot.split(",");
+			for(String root:packageRoots) {
+				res = testClassPath.indexOf(root);
+				if (res > -1)
+					break;
+			}
+		} else {
+			res = testClassPath.indexOf(packageRoot);
+		}
+		return res;
+	}
+	
+	
 	private static boolean isFragment(Bundle bundle) {
-		Enumeration headerKeys = bundle.getHeaders().keys();
+		Enumeration<String> headerKeys = bundle.getHeaders().keys();
 		while (headerKeys.hasMoreElements()) {
 			if (headerKeys.nextElement().toString().equals("Fragment-Host")) {
 				return true;
