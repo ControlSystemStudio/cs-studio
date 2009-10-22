ffa870ba4925fb68b9c21ecb50468be35a121e5e
Xihui Chen <chenx1@ornl.gov>
2009-10-22 17:13:20 +0000

    update

--- applications/plugins/org.csstudio.opibuilder.examples/examples/BOY Examples/scripts/KnobValueDialog.js
+++ applications/plugins/org.csstudio.opibuilder.examples/examples/BOY Examples/scripts/KnobValueDialog.js
@@ -1,9 +1,24 @@
 importPackage(Packages.org.eclipse.jface.dialogs);
 importPackage(Packages.org.csstudio.platform.data);
+importPackage(Packages.java.lang);
 
-if(ValueUtil.getDouble(pvArray[0].getValue()) == 100){
-		var op = MessageDialog.openWarning(
-			null, "Warning", "You reach the value of 100!");
+var flagName = "popped";
+
+if(widgetController.getExternalObject(flagName) == null){
+	widgetController.setExternalObject(flagName, false);	
+}
+
+var b = widgetController.getExternalObject(flagName);
+
+if(ValueUtil.getDouble(pvArray[0].getValue()) > 80){		
+		if( b == false){
+			MessageDialog.openWarning(
+				null, "Warning", "The temperature you set is too high!");
+			widgetController.setExternalObject(flagName, true);
+		}
+		
+}else if (b == true){
+	widgetController.setExternalObject(flagName, false);
 }
 	
 
