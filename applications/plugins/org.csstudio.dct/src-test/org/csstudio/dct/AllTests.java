9e5f410aa3d811be600479ec50bcc5076f25abf4
Jan Hatje <jan.hatje@desy.de>
2009-11-06 08:34:09 +0000

    add new Test classes to AllTests suite

--- applications/plugins/org.csstudio.dct/src-test/org/csstudio/dct/AllTests.java
+++ applications/plugins/org.csstudio.dct/src-test/org/csstudio/dct/AllTests.java
@@ -5,6 +5,24 @@ import org.csstudio.dct.metamodel.internal.DatabaseDefinitionTest;
 import org.csstudio.dct.metamodel.internal.FieldDefinitionTest;
 import org.csstudio.dct.metamodel.internal.MenuDefinitionTest;
 import org.csstudio.dct.metamodel.internal.RecordDefinitionTest;
+import org.csstudio.dct.model.commands.AbstractCommandTest;
+import org.csstudio.dct.model.commands.AddInstanceCommandTest;
+import org.csstudio.dct.model.commands.AddPrototypeCommandTest;
+import org.csstudio.dct.model.commands.AddRecordCommandTest;
+import org.csstudio.dct.model.commands.ChangeFieldValueCommandTest;
+import org.csstudio.dct.model.commands.ChangePropertyKeyCommandTest;
+import org.csstudio.dct.model.commands.ChangePropertyValueCommandTest;
+import org.csstudio.dct.model.commands.InitInstanceCommandTest;
+import org.csstudio.dct.model.commands.RemoveInstanceCommandTest;
+import org.csstudio.dct.model.commands.RemoveRecordCommandTest;
+import org.csstudio.dct.model.internal.AbstractElementTest;
+import org.csstudio.dct.model.internal.AbstractPropertyContainerTest;
+import org.csstudio.dct.model.internal.FolderTest;
+import org.csstudio.dct.model.internal.InstanceTest;
+import org.csstudio.dct.model.internal.ParameterTest;
+import org.csstudio.dct.model.internal.PrototypeTest;
+import org.csstudio.dct.model.internal.RecordTest;
+import org.csstudio.dct.model.persistence.internal.ProjectToXmlTest;
 import org.csstudio.dct.nameresolution.RecordFinderTest;
 import org.csstudio.dct.nameresolution.internal.FieldFunctionServiceTest;
 import org.csstudio.dct.util.AliasResolutionUtilTest;
@@ -15,10 +33,40 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses( { ChoiceTest.class, DatabaseDefinitionTest.class,
-	FieldDefinitionTest.class, MenuDefinitionTest.class, RecordDefinitionTest.class,
-	FieldFunctionServiceTest.class, RecordFinderTest.class, AliasResolutionUtilTest.class,
-	CompareUtilTest.class, ModelValidationUtilTest.class})
+@SuiteClasses( { ChoiceTest.class, 
+	DatabaseDefinitionTest.class,
+	FieldDefinitionTest.class,
+	MenuDefinitionTest.class,
+	RecordDefinitionTest.class,
+	
+	AbstractCommandTest.class,
+	AddInstanceCommandTest.class,
+	AddPrototypeCommandTest.class,
+	AddRecordCommandTest.class,
+	ChangeFieldValueCommandTest.class,
+	ChangePropertyKeyCommandTest.class,
+	ChangePropertyValueCommandTest.class,
+	InitInstanceCommandTest.class,
+	RemoveInstanceCommandTest.class,
+	RemoveRecordCommandTest.class,
+	
+	AbstractElementTest.class,
+	AbstractPropertyContainerTest.class,
+	FolderTest.class,
+	InstanceTest.class,
+	ParameterTest.class,
+	PrototypeTest.class,
+	RecordTest.class,
+	
+	ProjectToXmlTest.class,
+	
+	RecordFinderTest.class,
+
+	FieldFunctionServiceTest.class,
+	
+	AliasResolutionUtilTest.class,
+	CompareUtilTest.class, 
+	ModelValidationUtilTest.class})
 public class AllTests {
 }
 
