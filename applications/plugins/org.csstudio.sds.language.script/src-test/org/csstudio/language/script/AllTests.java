9d97652c2e110df549c59ea0f5ae2adb318f6cdf
Jan Hatje <jan.hatje@desy.de>
2009-11-06 13:33:56 +0000

    add AllTests Suite for headless build

--- applications/plugins/org.csstudio.sds.language.script/src-test/org/csstudio/language/script/AllTests.java
+++ applications/plugins/org.csstudio.sds.language.script/src-test/org/csstudio/language/script/AllTests.java
@@ -1,18 +1,25 @@
 package org.csstudio.language.script;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-
 import org.csstudio.language.script.parser.statementParser.AllStatementParserTests;
+import org.csstudio.language.script.parser.statementParser.VariableParser_Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
+@RunWith(Suite.class)
+@SuiteClasses( {AllStatementParserTests.class,
+		VariableParser_Test.class} )
 public class AllTests {
-	
-	public static Test suite() {
-		final TestSuite suite = new TestSuite("Test for org.csstudio.language.script");
-		// $JUnit-BEGIN$
-		suite.addTest(AllStatementParserTests.suite());
-		// $JUnit-END$
-		return suite;
-	}
-
 }
+
+//public class AllTests {
+//	
+//	public static Test suite() {
+//		final TestSuite suite = new TestSuite("Test for org.csstudio.language.script");
+//		// $JUnit-BEGIN$
+//		suite.addTest(AllStatementParserTests.suite());
+//		// $JUnit-END$
+//		return suite;
+//	}
+//
+//}
