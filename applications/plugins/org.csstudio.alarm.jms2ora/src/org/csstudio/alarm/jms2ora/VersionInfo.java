aaaccd5f8bc447ae8f2a8e381094fb0ea1273e70
mmoeller <mmoeller>
2009-11-06 11:05:59 +0000

    *** empty log message ***

--- applications/plugins/org.csstudio.alarm.jms2ora/src/org/csstudio/alarm/jms2ora/VersionInfo.java
+++ applications/plugins/org.csstudio.alarm.jms2ora/src/org/csstudio/alarm/jms2ora/VersionInfo.java
@@ -30,8 +30,8 @@ package org.csstudio.alarm.jms2ora;
 public class VersionInfo
 {
     public static final String NAME = "Jms2Ora";
-    public static final String VERSION = " 2.3.0";
-    public static final String BUILD = " - BUILD 2009-11-05 16:30";
+    public static final String VERSION = " 2.3.1";
+    public static final String BUILD = " - BUILD 2009-11-06 12:00";
     
     public static String getAll()
     {
