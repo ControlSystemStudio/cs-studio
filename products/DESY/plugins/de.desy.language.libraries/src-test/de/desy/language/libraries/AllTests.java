9cb6789a8d893cd1a68033c5088682e0e7e82aef
Jan Hatje <jan.hatje@desy.de>
2009-11-09 14:00:15 +0000

    *** empty log message ***

--- products/DESY/plugins/de.desy.language.libraries/src-test/de/desy/language/libraries/AllTests.java
+++ products/DESY/plugins/de.desy.language.libraries/src-test/de/desy/language/libraries/AllTests.java
@@ -1,17 +1,24 @@
 package de.desy.language.libraries;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
 import de.desy.language.libraries.utils.contract.Contract_Test;
-import junit.framework.Test;
-import junit.framework.TestSuite;
 
+
+@RunWith(Suite.class)
+@SuiteClasses( { Contract_Test.class})
 public class AllTests {
-
-	public static Test suite() {
-		TestSuite suite = new TestSuite("Test for de.desy.language.libraries");
-		//$JUnit-BEGIN$
-		suite.addTestSuite(Contract_Test.class);
-		//$JUnit-END$
-		return suite;
-	}
-
 }
+//public class AllTests {
+//
+//	public static Test suite() {
+//		TestSuite suite = new TestSuite("Test for de.desy.language.libraries");
+//		//$JUnit-BEGIN$
+//		suite.addTestSuite(Contract_Test.class);
+//		//$JUnit-END$
+//		return suite;
+//	}
+//
+//}
