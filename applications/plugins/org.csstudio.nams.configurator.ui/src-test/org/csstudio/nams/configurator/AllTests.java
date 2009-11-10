e291f67ae45b3345ec4be197ebbf52b7762922b1
Jan Hatje <jan.hatje@desy.de>
2009-11-10 09:12:22 +0000

    remove tests

--- applications/plugins/org.csstudio.nams.configurator.ui/src-test/org/csstudio/nams/configurator/AllTests.java
+++ applications/plugins/org.csstudio.nams.configurator.ui/src-test/org/csstudio/nams/configurator/AllTests.java
@@ -14,6 +14,7 @@ import org.junit.runners.Suite.SuiteClasses;
 	
 //	ConfigurationBeanServiceImpl_Test.class,
 	
-	SynchronizeServiceImpl_Test.class})
+//	SynchronizeServiceImpl_Test.class
+	})
 public class AllTests {
 }
