7682342fc8d03621735034bf9688ce21da298eb3
Jan Hatje <jan.hatje@desy.de>
2009-11-10 07:38:51 +0000

    remove duplicated test suite

--- applications/plugins/org.csstudio.sds.language.script/src-test/org/csstudio/language/script/AllTests.java
+++ applications/plugins/org.csstudio.sds.language.script/src-test/org/csstudio/language/script/AllTests.java
@@ -1,14 +1,12 @@
 package org.csstudio.language.script;
 
-import org.csstudio.language.script.parser.statementParser.AllStatementParserTests;
 import org.csstudio.language.script.parser.statementParser.VariableParser_Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses( {AllStatementParserTests.class,
-		VariableParser_Test.class} )
+@SuiteClasses( { VariableParser_Test.class} )
 public class AllTests {
 }
 
