a3d41c5ac5bd15721c16d89b799a807e2f82a24c
Kay Kasemir <kasemirk@ornl.gov>
2009-11-10 15:49:08 +0000

    axisRangeChanged event

--- applications/plugins/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/figures/IAxisListener.java
+++ applications/plugins/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/figures/IAxisListener.java
@@ -1,5 +1,7 @@
 package org.csstudio.swt.xygraph.figures;
 
+import org.csstudio.swt.xygraph.linearscale.Range;
+
 /**
  * A listener on the axis when axis was revalidated.
  * @author Xihui Chen
@@ -7,6 +9,11 @@ package org.csstudio.swt.xygraph.figures;
  */
 public interface IAxisListener {
 	
+    /**
+     * This event indicates a change in the axis' value range
+     */
+    public void axisRangeChanged(Axis axis, Range old_range, Range new_range);
+    
 	/**
 	 * This method will be notified by axis whenever the axis is revalidated.
 	 */
