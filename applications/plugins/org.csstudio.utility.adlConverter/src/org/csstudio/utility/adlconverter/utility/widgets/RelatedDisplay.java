1fedaee658bab4f9659d674934f1734847400693
Helge Rickens <helge.rickens@desy.de>
2009-11-13 14:44:48 +0000

    set the menu button to the Action Layer

--- applications/plugins/org.csstudio.utility.adlConverter/src/org/csstudio/utility/adlconverter/utility/widgets/RelatedDisplay.java
+++ applications/plugins/org.csstudio.utility.adlConverter/src/org/csstudio/utility/adlconverter/utility/widgets/RelatedDisplay.java
@@ -168,6 +168,7 @@ public class RelatedDisplay extends Widget {
         _widget.setPropertyValue(MenuButtonModel.PROP_BORDER_STYLE, 3);
         int h = ADLHelper.getFontSize("Times New Roman",label, getObject().getHeight(), getObject().getWidth(),"0"); //$NON-NLS-1$ //$NON-NLS-2$
         _widget.setPropertyValue(MenuButtonModel.PROP_FONT, new FontData("Times New Roman",h,SWT.NONE)); //$NON-NLS-1$
+        _widget.setLayer(Messages.ADLDisplayImporter_ADLActionLayerName);
     }
 
     /**
