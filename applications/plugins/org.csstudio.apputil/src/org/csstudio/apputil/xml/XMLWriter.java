643c23e4180a86b6c0ba196dda6c69f39d79e72f
Kay Kasemir <kasemirk@ornl.gov>
2009-12-18 17:55:53 +0000

    added XMLWriter

---
+++ applications/plugins/org.csstudio.apputil/src/org/csstudio/apputil/xml/XMLWriter.java
@@ -0,0 +1,89 @@
+package org.csstudio.apputil.xml;
+
+import java.io.PrintWriter;
+
+/** Helper for XML output.
+ *  
+ *  @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class XMLWriter
+{
+    /** Add string for indentation to given level to buffer.
+     *  @param out    PrintWriter
+     *  @param level  Intendation level
+     */
+    public static final void indent(final PrintWriter out, int level)
+    {
+        while (level > 0)
+        {
+            out.print("    ");
+            --level;
+        }
+    }
+
+    /** @return Returns text with less-than and ampersands replaced by XML escapes.
+     *  @param out    PrintWriter
+     *  @param text
+     */
+    public static final void escapeXMLstring(final PrintWriter out, final String text)
+    {
+        int i;
+        for (i=0; i<text.length(); ++i)
+        {
+            char c = text.charAt(i);
+            // Escape '&' into '&amp;'.
+            if (c == '&')
+                out.append("&amp;");
+            // Escape '<' into '&lt;'.
+            else if (c == '<')
+                out.append("&lt;");
+            else if (c < 32 || c > 126)
+            {   // Other non-printable. Exact definition not clear.
+                out.append("&#");
+                out.append(Integer.toString((int) c));
+                out.append(";");            
+            }
+            else
+                out.append(c);
+        }
+    }
+
+    /** Start XML tag
+     *  @param out    PrintWriter
+     *  @param level  Indentation level
+     *  @param tag    XML tag
+     */
+    public static final void start(final PrintWriter out, final int level, final String tag)
+    {
+        indent(out, level);
+        out.append("<").append(tag).append(">");
+    }
+
+    /** End XML tag
+     *  @param out    PrintWriter
+     *  @param level  Indentation level
+     *  @param tag    XML tag
+     */
+    public static final void end(final PrintWriter out, final int level, final String tag)
+    {
+        indent(out, level);
+        out.append("</").append(tag).append(">");
+    }
+
+    
+    /** Add XML for a one-line tagged value to buffer
+     *  @param out    PrintWriter
+     *  @param level  Indentation level
+     *  @param tag    XML tag
+     *  @param value  Value to place in tag
+     */
+    public static final void XML(final PrintWriter out, final int level,
+    		final String tag, final Object value)
+    {
+        start(out, level, tag);
+        escapeXMLstring(out, value.toString());
+        end(out, 0, tag);
+        out.append(">\n");
+    }
+}
