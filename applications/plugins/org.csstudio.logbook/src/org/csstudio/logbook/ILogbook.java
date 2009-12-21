b53b8d4cf62bbbd3303d9cc815922269ece49e54
Kay Kasemir <kasemirk@ornl.gov>
2009-12-21 17:07:23 +0000

    semantics of file to attach

--- applications/plugins/org.csstudio.logbook/src/org/csstudio/logbook/ILogbook.java
+++ applications/plugins/org.csstudio.logbook/src/org/csstudio/logbook/ILogbook.java
@@ -9,14 +9,17 @@ public interface ILogbook
 	/** Add new entry to the logbook.
 	 *  @param title Title
 	 *  @param text Text of the entry. Plain ASCII.
-	 *  @param image_file_name Name of image file or <code>null</code>.
-	 *         Has to contain the full path to the image. Exact path format
-	 *         depends on the operating system.
-	 *         File types that the logbook support (GIF, JPG) depend on
-	 *         implementation.
+     *  @param file_name Name of a file to attach or <code>null</code>.
+     *         Has to contain the full path to the file including file ending.
+     *         Exact path format depends on the operating system.
+     *         File types that the logbook support depend on
+     *         implementation but should include    
+     *           *.gif, *.jpg:  File will be attached as image
+     *           *.html, *.htm: File will be attached as web page
+     *           *.txt:         File will be attached as plain ASCII file
 	 *  @throws Exception on error
 	 */
-   void createEntry(String title, String text, String image_file_name) 
+   void createEntry(String title, String text, String file_name) 
         throws Exception;
 
 	/** Close the logbook. Should be called when done to reclaim resources. */
