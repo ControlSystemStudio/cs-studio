5a50afd2917127a9cab5b625f132bc60c17f9243
ikriznar <ikriznar>
2010-01-25 12:03:11 +0000

    Demonstration of the combined waveform.

---
+++ core/plugins/org.csstudio.platform.libs.dal.epics/src/org/epics/css/dal/epics/demo/WaveformDemo.java
@@ -0,0 +1,23 @@
+package org.epics.css.dal.epics.demo;
+
+import org.epics.css.dal.StringProperty;
+import org.epics.css.dal.epics.EPICSApplicationContext;
+import org.epics.css.dal.spi.DefaultPropertyFactoryService;
+import org.epics.css.dal.spi.LinkPolicy;
+import org.epics.css.dal.spi.PropertyFactory;
+
+public class WaveformDemo {
+
+	public static void main(String[] args) throws Exception {
+		EPICSApplicationContext ctx = new EPICSApplicationContext("Test");
+		PropertyFactory pf = DefaultPropertyFactoryService.getPropertyFactoryService().getPropertyFactory(ctx, LinkPolicy.SYNC_LINK_POLICY);
+		
+		StringProperty sp = pf.getProperty("wave", StringProperty.class, null);
+		System.out.println("Waveform value: " + sp.getValue());
+		String val = "2345678900";
+		System.out.println("Set value: " + val);
+		sp.setValue(val);
+		Thread.sleep(2000);
+		System.out.println("New Value: " + sp.getValue());
+	}
+}
