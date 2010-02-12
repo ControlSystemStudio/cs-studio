a68a05399b9626bfb96f12cf47969fccb9d03682
kmeyer <kmeyer>
2010-02-12 15:00:46 +0000

    Behavior have generics

--- applications/plugins/org.csstudio.sds.components.epics/src/org/csstudio/sds/behavior/desy/TestLabelBehavior.java
+++ applications/plugins/org.csstudio.sds.components.epics/src/org/csstudio/sds/behavior/desy/TestLabelBehavior.java
@@ -3,23 +3,22 @@ package org.csstudio.sds.behavior.desy;
 import org.csstudio.sds.components.ui.internal.figures.RefreshableLabelFigure;
 import org.csstudio.sds.model.LabelModel;
 import org.csstudio.sds.ui.behaviors.AbstractBehavior;
-import org.eclipse.draw2d.IFigure;
 import org.epics.css.dal.simple.AnyData;
 import org.epics.css.dal.simple.MetaData;
 
-public class TestLabelBehavior extends AbstractBehavior {
+public class TestLabelBehavior extends
+		AbstractBehavior<LabelModel, RefreshableLabelFigure> {
 
 	@Override
 	protected String[] doGetInvisiblePropertyIds() {
-		return new String[] {LabelModel.PROP_TEXTVALUE};
+		return new String[] { LabelModel.PROP_TEXTVALUE };
 	}
 
 	@Override
-	protected void doUpdate(IFigure figure, AnyData value) {
-		RefreshableLabelFigure label = (RefreshableLabelFigure) figure;
-		
+	protected void doUpdate(LabelModel model, RefreshableLabelFigure figure,
+			AnyData value) {
 		StringBuffer buffer = new StringBuffer();
-		
+
 		MetaData metaData = value.getMetaData();
 		if (metaData == null) {
 			buffer.append("No Metadata available");
@@ -48,16 +47,17 @@ public class TestLabelBehavior extends AbstractBehavior {
 			buffer.append("WarnLow: ");
 			buffer.append(metaData.getWarnLow());
 			buffer.append("\n");
-			buffer.append("States: ");	
-			for (String state : metaData.getStates()) {
-				buffer.append("\t");				
-				buffer.append(state);
-				buffer.append("\n");
+			buffer.append("States: ");
+			if (metaData.getStates() != null) {
+				for (String state : metaData.getStates()) {
+					buffer.append("\t");
+					buffer.append(state);
+					buffer.append("\n");
+				}
 			}
-			
 		}
-		
-		label.setTextValue(buffer.toString());
+
+		figure.setTextValue(buffer.toString());
 	}
 
 }
--- applications/plugins/org.csstudio.sds.components.epics/src/org/csstudio/sds/behavior/desy/TestRectangleBehaviour.java
+++ applications/plugins/org.csstudio.sds.components.epics/src/org/csstudio/sds/behavior/desy/TestRectangleBehaviour.java
@@ -4,11 +4,11 @@ import org.csstudio.platform.ui.util.CustomMediaFactory;
 import org.csstudio.sds.components.model.RectangleModel;
 import org.csstudio.sds.components.ui.internal.figures.RefreshableRectangleFigure;
 import org.csstudio.sds.ui.behaviors.AbstractBehavior;
-import org.eclipse.draw2d.IFigure;
 import org.epics.css.dal.simple.AnyData;
 import org.epics.css.dal.simple.MetaData;
 
-public class TestRectangleBehaviour extends AbstractBehavior {
+public class TestRectangleBehaviour extends
+		AbstractBehavior<RectangleModel, RefreshableRectangleFigure> {
 
 	@Override
 	protected String[] doGetInvisiblePropertyIds() {
@@ -18,24 +18,22 @@ public class TestRectangleBehaviour extends AbstractBehavior {
 	}
 
 	@Override
-	protected void doUpdate(IFigure figure, AnyData value) {
-		if (figure instanceof RefreshableRectangleFigure) {
-			RefreshableRectangleFigure rectangle = (RefreshableRectangleFigure) figure;
-			if (!value.getSeverity().isOK()) {
-				rectangle.setForegroundColor(CustomMediaFactory.getInstance()
-						.getColor(CustomMediaFactory.COLOR_RED));
-			} else {
-				rectangle.setForegroundColor(CustomMediaFactory.getInstance()
-						.getColor(CustomMediaFactory.COLOR_BLACK));
-			}
-			
-			rectangle.setFill(value.doubleValue());
-			MetaData metaData = value.getMetaData();
-			if (metaData != null) {
-				rectangle.setSize(100, (int)metaData.getAlarmHigh());
-			} else {
-				rectangle.setSize(100, 50);
-			}
+	protected void doUpdate(RectangleModel model,
+			RefreshableRectangleFigure figure, AnyData value) {
+		if (!value.getSeverity().isOK()) {
+			figure.setForegroundColor(CustomMediaFactory.getInstance()
+					.getColor(CustomMediaFactory.COLOR_RED));
+		} else {
+			figure.setForegroundColor(CustomMediaFactory.getInstance()
+					.getColor(CustomMediaFactory.COLOR_BLACK));
+		}
+
+		figure.setFill(value.doubleValue());
+		MetaData metaData = value.getMetaData();
+		if (metaData != null) {
+			figure.setSize(100, (int) metaData.getAlarmHigh());
+		} else {
+			figure.setSize(100, 50);
 		}
 	}
 
