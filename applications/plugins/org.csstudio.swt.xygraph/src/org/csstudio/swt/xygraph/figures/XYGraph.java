b9b9e58c94e6c93b57029a6352697abd245d5e63
Kay Kasemir <kasemirk@ornl.gov>
2010-02-15 16:35:26 +0000

    transparent?

--- applications/plugins/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/figures/XYGraph.java
+++ applications/plugins/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/figures/XYGraph.java
@@ -52,6 +52,11 @@ public class XYGraph extends Figure{
 	};
 	
 	private int traceNum = 0;
+	// TODO Is something wrong with the initial 'transparent' setting?
+	// It's 'true', and shows like that in the ToolbarArmedXYGraph's
+	// configuration panel, but it otherwise looks like a non-transparent graph.
+	// When simply opening the config panel, doing nothing but press 'OK',
+	// the plot really becomes transparent.
 	private boolean transparent = true;
 	private boolean showLegend = true;
 	
