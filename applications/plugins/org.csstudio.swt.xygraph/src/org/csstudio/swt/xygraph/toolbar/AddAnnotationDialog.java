1824cacac8ddbf5b5c65b80a213bdb93edf289b3
Kay Kasemir <kasemirk@ornl.gov>
2010-02-15 16:12:59 +0000

    initial settings

--- applications/plugins/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/toolbar/AddAnnotationDialog.java
+++ applications/plugins/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/toolbar/AddAnnotationDialog.java
@@ -1,8 +1,12 @@
 package org.csstudio.swt.xygraph.toolbar;
 
+import java.util.List;
+
 import org.csstudio.swt.xygraph.figures.Annotation;
+import org.csstudio.swt.xygraph.figures.Trace;
 import org.csstudio.swt.xygraph.figures.XYGraph;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -10,16 +14,30 @@ import org.eclipse.swt.widgets.Shell;
 
 /**The dialog for adding annotation.
  * @author Xihui Chen
- *
+ * @author Kay Kasemir Initial defaults
  */
 public class AddAnnotationDialog extends Dialog {
-	
 	private AnnotationConfigPage configPage;
 	
-	protected AddAnnotationDialog(Shell parentShell, XYGraph xyGraph) {
+	protected AddAnnotationDialog(final Shell parentShell, final XYGraph xyGraph) {
 		super(parentShell);	
-		configPage = new AnnotationConfigPage(xyGraph, new Annotation("Annotation",
-				xyGraph.primaryXAxis, xyGraph.primaryYAxis));
+		
+		// Unique annotation names help when trying to edit/delete annotations.
+		// Default name: Annotation 1, Annotation 2, ...
+		final int num = xyGraph.getPlotArea().getAnnotationList().size();
+        final String name = NLS.bind("Annotation {0}", (num+1));
+        
+        // If there are traces, default to 'snapping' to the first trace
+        final Annotation annotation;
+		final List<Trace> traces = xyGraph.getPlotArea().getTraceList();
+		if (traces.size() > 0)
+            annotation = new Annotation(name, traces.get(0));
+        else
+		    annotation = new Annotation(name,
+	                xyGraph.primaryXAxis, xyGraph.primaryYAxis);
+
+		// Allow user to tweak the settings
+	    configPage = new AnnotationConfigPage(xyGraph, annotation);
 	}
 	
 	@Override
@@ -48,5 +66,4 @@ public class AddAnnotationDialog extends Dialog {
 	public Annotation getAnnotation() {
 		return configPage.getAnnotation();
 	}
-	
 }
