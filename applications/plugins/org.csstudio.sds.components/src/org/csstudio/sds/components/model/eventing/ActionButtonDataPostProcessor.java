aed4132335965b531086946d265d366753d5c18c
kmeyer <kmeyer>
2010-02-18 13:15:39 +0000

    *** empty log message ***

---
+++ applications/plugins/org.csstudio.sds.components/src/org/csstudio/sds/components/model/eventing/ActionButtonDataPostProcessor.java
@@ -0,0 +1,69 @@
+package org.csstudio.sds.components.model.eventing;
+
+import org.csstudio.sds.components.model.ActionButtonModel;
+import org.csstudio.sds.model.eventing.AbstractWidgetPropertyPostProcessor;
+import org.csstudio.sds.model.eventing.HidePropertyCommand;
+import org.csstudio.sds.model.eventing.ShowPropertyCommand;
+import org.csstudio.sds.model.properties.ActionData;
+import org.csstudio.sds.model.properties.ActionDataProperty;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.commands.CompoundCommand;
+
+public class ActionButtonDataPostProcessor
+		extends
+		AbstractWidgetPropertyPostProcessor<ActionButtonModel, ActionDataProperty> {
+
+	@Override
+	protected Command doCreateCommand(ActionButtonModel widget,
+			ActionDataProperty property) {
+		assert widget != null : "widget != null";
+		assert property != null : "property != null";
+		return new EnsureInvariantsCommand(widget, property);
+	}
+
+	private static final class EnsureInvariantsCommand extends Command {
+		private ActionButtonModel widget;
+		private ActionDataProperty property;
+		private CompoundCommand chain;
+
+		private EnsureInvariantsCommand(ActionButtonModel widget,
+				ActionDataProperty property) {
+			this.widget = widget;
+			this.property = property;
+		}
+
+		@Override
+		public void execute() {
+			if (chain == null) {
+				chain = new CompoundCommand();
+
+				ActionData data = property.getPropertyValue();
+				int size = data.getWidgetActions().size();
+
+				String[] propertyIds = new String[] {
+						ActionButtonModel.PROP_ACTION_PRESSED_INDEX,
+						ActionButtonModel.PROP_ACTION_RELEASED_INDEX };
+
+				if (size == 0) {
+					for (String propertyId : propertyIds) {
+						chain.add(new HidePropertyCommand(widget
+								.getProperty(propertyId), property));
+					}
+				} else {
+					for (String propertyId : propertyIds) {
+						chain.add(new ShowPropertyCommand(widget
+								.getProperty(propertyId), property));
+					}
+				}
+			}
+
+			chain.execute();
+		}
+
+		@Override
+		public void undo() {
+			chain.undo();
+		}
+	}
+
+}
