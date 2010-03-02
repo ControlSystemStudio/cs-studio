e85027f124f6fc93096b28d5d719b665d4a8def7
Xihui Chen <chenx1@ornl.gov>
2010-03-02 19:38:17 +0000

    add this

---
+++ applications/plugins/org.csstudio.opibuilder.converter/src/main/org/csstudio/opibuilder/converter/TODO.java
@@ -0,0 +1,13 @@
+package org.csstudio.opibuilder.converter;
+
+/** Placeholder for 'TO DO' items
+ *  @author Xihui Chen
+ */
+public interface TODO
+{
+    /**
+     * TODO Avoid restart after color list file preference setting changed.
+     * 
+     * TODO Support HEX color presentation in color list file.
+     */
+}
