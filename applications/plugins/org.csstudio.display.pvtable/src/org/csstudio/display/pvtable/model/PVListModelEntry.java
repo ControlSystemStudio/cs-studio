44fd5a05d9e0b07e15af2dbafe36d86df0197c0d
kshroff <kshroff>
2010-03-08 17:23:58 +0000

    use of XMLWriter to accomodate pv names with special characters.

--- applications/plugins/org.csstudio.display.pvtable/src/org/csstudio/display/pvtable/model/PVListModelEntry.java
+++ applications/plugins/org.csstudio.display.pvtable/src/org/csstudio/display/pvtable/model/PVListModelEntry.java
@@ -1,5 +1,11 @@
 package org.csstudio.display.pvtable.model;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+
+import org.csstudio.apputil.xml.XMLWriter;
 import org.csstudio.display.pvtable.Plugin;
 import org.csstudio.platform.model.IProcessVariable;
 import org.csstudio.utility.pv.PV;
@@ -7,13 +13,14 @@ import org.csstudio.utility.pv.PVFactory;
 import org.csstudio.utility.pv.PVListener;
 import org.eclipse.core.runtime.PlatformObject;
 
-/** Implementation of the PVListEntry as used by the PVListModel.
- *  @see PVListModel
- *  
- *  @author Kay Kasemir
+/**
+ * Implementation of the PVListEntry as used by the PVListModel.
+ * 
+ * @see PVListModel
+ * 
+ * @author Kay Kasemir, Kunal Shroff
  */
-public class PVListModelEntry extends PlatformObject implements PVListEntry 
-{
+public class PVListModelEntry extends PlatformObject implements PVListEntry {
     private boolean selected;
     private PV pv, readback_pv;
     private final SavedValue saved_value, saved_readback_value;
@@ -21,138 +28,130 @@ public class PVListModelEntry extends PlatformObject implements PVListEntry
     private PVListener pv_listener;
 
     /** Create new entry from pieces. */
-    public PVListModelEntry(boolean selected, 
-            String pv_name, SavedValue saved_value,
-            String readback_name, SavedValue readback_value)
-    {
+    public PVListModelEntry(boolean selected, String pv_name,
+            SavedValue saved_value, String readback_name,
+            SavedValue readback_value) {
         this.selected = selected;
         this.pv = createPV(pv_name);
         this.saved_value = saved_value;
-        
-        if (readback_name != null  &&  readback_name.length() > 0)
+
+        if (readback_name != null && readback_name.length() > 0)
             this.readback_pv = createPV(readback_name);
         else
             this.readback_pv = null;
         this.saved_readback_value = readback_value;
         new_values = 0;
 
-        pv_listener = new PVListener()
-        {
-            public void pvValueUpdate(PV pv)
-            {   addNewValue(); }
-    
-            public void pvDisconnected(PV pv)
-            {   addNewValue(); // handled the same way: mark for redraw
+        pv_listener = new PVListener() {
+            public void pvValueUpdate(PV pv) {
+                addNewValue();
+            }
+
+            public void pvDisconnected(PV pv) {
+                addNewValue(); // handled the same way: mark for redraw
             }
         };
     }
-    
+
     /** @return PV for the given name */
     @SuppressWarnings("nls")
-    private PV createPV(final String name)
-    {
-        try
-        {
+    private PV createPV(final String name) {
+        try {
             return PVFactory.createPV(name);
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             ex.printStackTrace();
             Plugin.getLogger().error("Cannot create PV '" + name + "'", ex);
         }
         return null;
     }
-    
+
     /** @see org.csstudio.platform.model.IProcessVariable */
-    public String getName()
-    {   return pv.getName(); }
-    
+    public String getName() {
+        return pv.getName();
+    }
+
     /** @see org.csstudio.platform.model.IProcessVariable */
-    public final String getTypeId()
-    {   return IProcessVariable.TYPE_ID;  }
-    
-    public boolean isDisposed()
-    {   return pv == null; }
-    
-    public boolean isSelected()
-    {   return selected; }
-    
-    public void setSelected(boolean new_state)
-    {   selected = new_state; }
+    public final String getTypeId() {
+        return IProcessVariable.TYPE_ID;
+    }
 
-    public PV getPV()
-    {   return pv; }
+    public boolean isDisposed() {
+        return pv == null;
+    }
 
-    public SavedValue getSavedValue()
-    {   return saved_value; }
+    public boolean isSelected() {
+        return selected;
+    }
 
-    public PV getReadbackPV()
-    {   return readback_pv; }
+    public void setSelected(boolean new_state) {
+        selected = new_state;
+    }
 
-    public SavedValue getSavedReadbackValue()
-    {   return saved_readback_value; }
+    public PV getPV() {
+        return pv;
+    }
+
+    public SavedValue getSavedValue() {
+        return saved_value;
+    }
+
+    public PV getReadbackPV() {
+        return readback_pv;
+    }
+
+    public SavedValue getSavedReadbackValue() {
+        return saved_readback_value;
+    }
 
     /** Mark entry as disposed. */
-    public void dispose()
-    {
+    public void dispose() {
         stop();
         pv = null;
         readback_pv = null;
     }
-    
-    public void start()
-    {
+
+    public void start() {
         pv.addListener(pv_listener);
-        try
-        {
+        try {
             pv.start();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        if (readback_pv != null)
-        {
+        if (readback_pv != null) {
             readback_pv.addListener(pv_listener);
-            try
-            {
+            try {
                 readback_pv.start();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public void stop()
-    {
-        if (readback_pv != null)
-        {
-            if (readback_pv.isRunning())
-            {
+    public void stop() {
+        if (readback_pv != null) {
+            if (readback_pv.isRunning()) {
                 readback_pv.stop();
                 readback_pv.removeListener(pv_listener);
             }
         }
-        if (pv.isRunning())
-        {
+        if (pv.isRunning()) {
             pv.stop();
             pv.removeListener(pv_listener);
         }
     }
 
     /** Save the current values of the PVs. */
-    public void takeSnapshot()
-    {
+    public void takeSnapshot() {
         saved_value.readFromPV(pv);
         saved_readback_value.readFromPV(readback_pv);
     }
-    
-    /** Restore values from snapshot.
-     *  @return Returns <code>false</code> if the entry is not selected. */
-    public boolean restore() throws Exception
-    {
+
+    /**
+     * Restore values from snapshot.
+     * 
+     * @return Returns <code>false</code> if the entry is not selected.
+     */
+    public boolean restore() throws Exception {
         if (!selected)
             return false;
         saved_value.restoreToPV(pv);
@@ -160,80 +159,68 @@ public class PVListModelEntry extends PlatformObject implements PVListEntry
         return true;
     }
 
-    public void setReadbackPV(String readback_name)
-    {
+    public void setReadbackPV(String readback_name) {
         boolean running = pv.isRunning();
         if (running)
             stop();
-        if (readback_name != null  &&  readback_name.length() > 0)
+        if (readback_name != null && readback_name.length() > 0)
             readback_pv = createPV(readback_name);
         else
             readback_pv = null;
-        if (running)
-        {
-            try
-            {
+        if (running) {
+            try {
                 start();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-    
+
     /** Used by PVListModel to indicate that a PV of this entry has a new value. */
-    private synchronized void addNewValue()
-    {
+    private synchronized void addNewValue() {
         ++new_values;
     }
-    
+
     /** Used by the PVListModel to check if this entry has a new value. */
-    public synchronized int testAndResetNewValues()
-    {
+    public synchronized int testAndResetNewValues() {
         int result = new_values;
         new_values = 0;
         return result;
     }
-    
-    @SuppressWarnings("nls")
-    public String toXML()
-    {
-        StringBuffer buf = new StringBuffer();
+
+    public String toXML() {
+        StringWriter buf = new StringWriter();
+        PrintWriter pw = new PrintWriter(buf);
         buf.append("<pv>");
 
         buf.append(" <selected>");
         buf.append(isSelected() ? "true" : "false");
         buf.append("</selected>");
-        
+
         buf.append(" <name>");
-        buf.append(pv.getName());
+        XMLWriter.escapeXMLstring(pw, pv.getName());
         buf.append("</name>");
-        if (saved_value != null)
-        {
+        if (saved_value != null) {
             buf.append(" <saved_value>");
-            buf.append(saved_value);
+            buf.append(saved_value.toString());
             buf.append("</saved_value>");
         }
-        if (readback_pv != null)
-        {
+        if (readback_pv != null) {
             buf.append(" <readback>");
-            buf.append(readback_pv.getName());
+            XMLWriter.escapeXMLstring(pw, readback_pv.getName());
             buf.append("</readback>");
         }
-        if (saved_readback_value != null)
-        {
+        if (saved_readback_value != null) {
             buf.append(" <readback_value>");
-            buf.append(saved_readback_value);
+            buf.append(saved_readback_value.toString());
             buf.append("</readback_value>");
         }
         buf.append(" </pv>");
         return buf.toString();
     }
-    
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         return toXML();
     }
 }
