6763d2db1f15ebff8790704d8b6f5ff47aec5217
Joerg Penning <joerg.penning@desy.de>
2010-03-10 15:20:36 +0000

    Samples with preference for source-directory. jp

--- applications/plugins/org.csstudio.samples/src/org/csstudio/samples/internal/InstallExamplesAction.java
+++ applications/plugins/org.csstudio.samples/src/org/csstudio/samples/internal/InstallExamplesAction.java
@@ -30,6 +30,7 @@ import java.net.URL;
 import org.apache.log4j.Logger;
 import org.csstudio.platform.logging.CentralLogger;
 import org.csstudio.samples.SamplesActivator;
+import org.csstudio.samples.internal.preferences.PreferenceConstants;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -43,8 +44,10 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.preferences.IPreferencesService;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -52,9 +55,13 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 /**
- * Provides an action to install sample projects for SDS, DCT and SNL Editor.
- * If the action is started from within the IDE, the CVS information is present in the
- * source directories. To prevent it from being copied, it is filtered.
+ * Provides an action to install sample projects.
+ * 
+ * Via Preferences a list of semicolon-separated source-directories may be
+ * provided. The default is DEFAULT_SOURCE_DIRS.
+ * 
+ * If the action is started from within the IDE, the CVS information is present
+ * in the source directories. To prevent it from being copied, it is filtered.
  * 
  * Replacement for SDS plug-in 'org.csstudio.sds.samples'.
  * 
@@ -65,6 +72,8 @@ public class InstallExamplesAction extends Action implements
 		IWorkbenchWindowActionDelegate {
 
 	private final Logger log = CentralLogger.getInstance().getLogger(this);
+
+	private static final String DEFAULT_SOURCE_DIRS = "SDS Demo Display;DCT Demo Project;SNL Demo Project;";
 
 	public void dispose() {
 		// Nothing to dispose
@@ -93,22 +102,26 @@ public class InstallExamplesAction extends Action implements
 		job.schedule();
 	}
 
-	IStatus copySamples(final IWorkspaceRoot root, IProgressMonitor monitor) {
-		IStatus resultSDS = copySampleFromProjectPathToWorkspaceRoot(root,
-				monitor, "SDS Demo Display");
-		IStatus resultDCT = copySampleFromProjectPathToWorkspaceRoot(root,
-				monitor, "DCT Demo Project");
-		IStatus resultSNL = copySampleFromProjectPathToWorkspaceRoot(root,
-				monitor, "SNL Demo Project");
+	// visibility is protected to make this method accessible for the job
+	// inside the run method.
+	protected IStatus copySamples(final IWorkspaceRoot root,
+			IProgressMonitor monitor) {
+		IPreferencesService prefs = Platform.getPreferencesService();
+		String sourceDirPrefs = prefs.getString(SamplesActivator.PLUGIN_ID,
+				PreferenceConstants.SOURCE_DIRECTORIES, DEFAULT_SOURCE_DIRS, null);
+
+		String[] sourceDirectorise = sourceDirPrefs.split(";");
+
+		IStatus result = Status.OK_STATUS;
+		for (String sourceDirectory : sourceDirectorise) {
+			IStatus singleResult = copySampleFromProjectPathToWorkspaceRoot(
+					root, monitor, sourceDirectory);
+			result = singleResult.equals(Status.OK_STATUS) ? result
+					: singleResult;
+		}
 
 		// TODO: 2008-10-07: Kopieren von Scripted Rules einbauen,
 		// die in den Sample-Displays benötigt werden!!
-
-		// copySampleFromProjectPathToWorkspaceRoot returns OK_STATUS or ERROR
-		// If any result is ERROR, it will be returned.
-		IStatus result = resultSDS;
-		result = resultDCT.equals(Status.OK_STATUS) ? result : resultDCT;
-		result = resultSNL.equals(Status.OK_STATUS) ? result : resultSNL;
 
 		return result;
 	}
@@ -142,7 +155,7 @@ public class InstallExamplesAction extends Action implements
 				if (directory.isDirectory()) {
 					File[] files = directory.listFiles();
 					int totalFileNumber = count(files, 0);
-					monitor.beginTask("Copying Samples for " + projectPath,
+					monitor.beginTask("Copying Samples from " + projectPath,
 							totalFileNumber);
 					copyWithoutCVS(files, project, monitor);
 					// Now we succeeded, Status is OK
