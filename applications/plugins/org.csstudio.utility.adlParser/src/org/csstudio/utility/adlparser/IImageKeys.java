a5849f6b985d6e9fb2ec5cf6c68d30c1b5774d96
hammonds <hammonds>
2010-03-26 19:28:27 +0000

    Add to identify images to represent widgets

---
+++ applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/IImageKeys.java
@@ -0,0 +1,28 @@
+package org.csstudio.utility.adlparser;
+
+public interface IImageKeys {
+	public final static String ADL_ARC = "icons/ADLArc.JPG";
+	public final static String ADL_BAR_MONITOR = "icons/ADLBarMonitor.JPG";
+	public final static String ADL_BYTE_MONITOR = "icons/ADLByteMonitor.JPG";
+	public final static String ADL_CARTESIAN = "icons/ADLCartesian.JPG";
+	public final static String ADL_CHOICE_BUTTON = "icons/ADLChoiceButton.JPG";
+	public final static String ADL_IMAGE = "icons/ADLImage.JPG";
+	public final static String ADL_LINE = "icons/ADLLine.JPG";
+	public final static String ADL_MENU = "icons/ADLMenu.JPG";
+	public final static String ADL_MESSAGE_BUTTON = "icons/ADLMessageButton.JPG";
+	public final static String ADL_METER = "icons/ADLMeter.JPG";
+	public final static String ADL_OVAL = "icons/ADLOval.JPG";
+	public final static String ADL_POLYGON = "icons/ADLPolygon.JPG";
+	public final static String ADL_POLYLINE = "icons/ADLPolyline.JPG";
+	public final static String ADL_RECTANGLE = "icons/ADLRectangle.JPG";
+	public final static String ADL_RELATED_DISPLAY = "icons/ADLRelatedDisplay.JPG";
+	public final static String ADL_SCALE = "icons/ADLScale.JPG";
+	public final static String ADL_SHELL_COMMAND = "icons/ADLShellCommand.JPG";
+	public final static String ADL_SLIDER = "icons/ADLSlider.JPG";
+	public final static String ADL_STRIP_CHART = "icons/ADLStripChart.JPG";
+	public final static String ADL_TEXT = "icons/ADLText.JPG";
+	public final static String ADL_TEXT_ENTRY = "icons/ADLTextEntry.JPG";
+	public final static String ADL_TEXT_MONITOR = "icons/ADLTextMonitor.JPG";
+	public final static String ADL_WHEEL_SWITCH = "icons/ADLWheelSwitch.JPG";
+	public final static String MEDM = "icons/";
+}
