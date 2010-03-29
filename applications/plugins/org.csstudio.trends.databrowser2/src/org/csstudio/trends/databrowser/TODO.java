f0b3f615481b9f58e1467c7f942f96220acf1f95
Kay Kasemir <kasemirk@ornl.gov>
2010-03-29 13:10:41 +0000

    added

---
+++ applications/plugins/org.csstudio.trends.databrowser2/src/org/csstudio/trends/databrowser/TODO.java
@@ -0,0 +1,20 @@
+package org.csstudio.trends.databrowser;
+
+/** Placeholder for to-do items
+ *  @author Kay Kasemir
+ */
+public interface TODO
+{
+/**
+ * TODO Zoom single value axis.
+ * Example: Allow vertical zoom in/out by clicking (and holding) on axis.
+ * Similar to clicking on plot, but limited to value axis.
+ * 
+ * TODO 'Lines' plot type, separate lines for min, max avg.
+ * 
+ * TODO Option to hide value axes.
+ * 
+ * TODO Option to plot PV limits.
+ * Do this via syntax PV{low_warning}?
+ */
+}
