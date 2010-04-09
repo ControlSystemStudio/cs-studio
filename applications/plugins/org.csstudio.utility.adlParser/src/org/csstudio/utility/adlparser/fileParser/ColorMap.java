7c3009eacc59d0819a74b4a9ee4ddac0a04d4fee
hammonds <hammonds>
2010-04-09 21:01:01 +0000

    Add in dl_color interpretation

--- applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/ColorMap.java
+++ applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/ColorMap.java
@@ -39,6 +39,32 @@ public class ColorMap {
 			}
 			
 		}
+		else if ( colorsWidget.getType().equals("dl_color")){
+			ArrayList<ADLWidget> colorList = colorMap.getObjects();
+			if (colorList.size() == numColors) {
+				for (int ii=0; ii<numColors; ii++){
+					int red=0, green=0, blue =0;
+					ADLWidget dl_color = colorList.get(ii);
+					for ( FileLine fileLine : dl_color.getBody()){
+						String bodyPart = fileLine.getLine();
+						String[] row = bodyPart.trim().split("=");
+						if (row.length < 2){
+							throw new WrongADLFormatException(Messages.Label_WrongADLFormatException_Parameter_Begin + bodyPart + Messages.Label_WrongADLFormatException_Parameter_End);
+						}
+						if (row[0].trim().equals("r")){
+							red = Integer.parseInt(row[1].replaceAll("\"", "").trim());
+						}
+						if (row[0].trim().equals("g")){
+							green = Integer.parseInt(row[1].replaceAll("\"", "").trim());
+						}
+						if (row[0].trim().equals("b")){
+							blue = Integer.parseInt(row[1].replaceAll("\"", "").trim());							
+						}
+						colors[ii] = new RGB(red,green,blue);
+					}
+				}
+			}
+		}
 	}
 
 	/** 
@@ -58,7 +84,6 @@ public class ColorMap {
 		int red = Integer.decode(redStr);
 		int green = Integer.decode(greenStr);
 		int blue = Integer.decode(blueStr);
-		System.out.println("Red, Green, Blue:"+ red +", " + green + ", " +blue);
 		return new RGB(red, green, blue);
 	}
 }
