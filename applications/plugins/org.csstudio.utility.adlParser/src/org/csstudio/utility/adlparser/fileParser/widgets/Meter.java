477fe000bb345c639fe4db3b64b778c235137c1d
hammonds <hammonds>
2010-04-11 23:07:01 +0000

    New widget to handle meters.

---
+++ applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/widgets/Meter.java
@@ -0,0 +1,34 @@
+package org.csstudio.utility.adlparser.fileParser.widgets;
+
+import org.csstudio.utility.adlparser.fileParser.ADLWidget;
+import org.csstudio.utility.adlparser.fileParser.WrongADLFormatException;
+import org.csstudio.utility.adlparser.fileParser.widgetParts.ADLMonitor;
+import org.csstudio.utility.adlparser.fileParser.widgetParts.ADLObject;
+
+public class Meter extends ADLAbstractWidget {
+
+	public Meter(ADLWidget adlWidget) {
+		super(adlWidget);
+		try {
+			for (ADLWidget childWidget : adlWidget.getObjects()) {
+	        	if (childWidget.getType().equals("object")){
+	        		_adlObject = new ADLObject(childWidget);
+	        		if (_adlObject != null){
+	        			_hasObject = true;
+	        		}
+	        		
+	        	}
+	        	else if (childWidget.getType().equals("monitor")){
+	        		_adlMonitor = new ADLMonitor(childWidget);
+	        		if (_adlMonitor != null){
+	        			_hasMonitor = true;
+	        		}
+	        	}
+	        }
+		}
+		catch (WrongADLFormatException ex) {
+			
+		}
+	}
+
+}
