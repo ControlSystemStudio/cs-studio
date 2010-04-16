5810a5b4eb30c7911d3a451b3c3159781eb0b047
Helge Rickens <helge.rickens@desy.de>
2010-04-16 12:21:30 +0000

    The scrollbar shown now the actual value on disable

--- applications/plugins/org.csstudio.sds.components.ui/src/org/csstudio/sds/components/ui/internal/figures/SimpleSliderFigure.java
+++ applications/plugins/org.csstudio.sds.components.ui/src/org/csstudio/sds/components/ui/internal/figures/SimpleSliderFigure.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2006 Stiftung Deutsches Elektronen-Synchroton, Member of the Helmholtz Association,
  * (DESY), HAMBURG, GERMANY.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED UNDER THIS LICENSE ON AN "../AS IS" BASIS. WITHOUT WARRANTY OF ANY
  * KIND, EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
@@ -38,149 +38,146 @@ import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.Orientable;
 import org.eclipse.draw2d.Panel;
-import org.eclipse.draw2d.Polyline;
 import org.eclipse.draw2d.RangeModel;
 import org.eclipse.draw2d.SchemeBorder;
 import org.eclipse.draw2d.ScrollBar;
 import org.eclipse.draw2d.geometry.Dimension;
-import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 
 /**
  * A slider figure.
- * 
+ *
  * @author Sven Wende
  * @version $Revision$
- * 
+ *
  */
 public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
     /**
      * Listeners that react on slider events.
      */
-    private List<ISliderListener> _sliderListeners;
-    
+    private final List<ISliderListener> _sliderListeners;
+
     /**
      * A border adapter, which covers all border handlings.
      */
     private IBorderEquippedWidget _borderAdapter;
-    
+
     /**
      * The scroll bar figure.
      */
-    private ScrollBar _scrollBar;
-    
+    private final MyScrollBar _scrollBar;
+
     /**
      * A label, which display the current value.
      */
-    private Label _valueLabel;
-    
+    private final Label _valueLabel;
+
     /**
      * Lower border of the displayed value.
      */
     private int _min = 0;
-    
+
     /**
      * Upper border of the displayed value.
      */
     private int _max = 100;
-    
+
     /**
      * The current value.
      */
     private int _currentValue = 30;
-    
+
     /**
      * The precision of the scrollbar.
      */
     private int _scrollbarPrecision = 100;
-    
+
     /**
      * The potenz for the precision.
      */
     private int _decimalPlaces = 2;
-    
+
     /**
      * The minimum wide for the slider.
      */
     private int _sliderWide = 5;
-    
+
     /**
      * The "show value as text" flag.
      */
     private boolean _showValueAsText = false;
-    
+
     /**
      * The original minimum value.
      */
     private double _originalMin;
-    
+
     /**
      * The original maximum value.
      */
     private double _originalMax;
-    
+
     /**
      * The original manual value.
      */
     private double _originalManVal;
-    
+
     /**
      * The original value.
      */
     private double _originalVal;
-    
+
     /**
      * Flag which is used to disable slider events. When the current value is set on the scrollbar,
      * eventing must be turned off.
      */
     private boolean _populateEvents = true;
-    
-    private Polyline _figure1 = new Polyline();
-    
-    private Polyline _figure2 = new Polyline();
-    
-    private CrossedPaintHelper _crossedPaintHelper;
-    
+
+    private final CrossedPaintHelper _crossedPaintHelper;
+
+    private boolean _enable;
+
     /**
      * Standard constructor.
      */
     public SimpleSliderFigure() {
         _crossedPaintHelper = new CrossedPaintHelper();
-        
+
         _sliderListeners = new ArrayList<ISliderListener>();
-        
+
         BorderLayout layout = new BorderLayout();
         layout.setVerticalSpacing(2);
         layout.setHorizontalSpacing(2);
-        
+
         setLayoutManager(layout);
-        
+
         _valueLabel = new Label();
         add(_valueLabel, BorderLayout.TOP);
         _valueLabel.setVisible(_showValueAsText);
-        
+
         _scrollBar = createScrollbarFigure();
         add(_scrollBar, BorderLayout.CENTER);
-        
+
     }
-    
+
     @Override
-    public void paint(Graphics graphics) {
+    public void paint(final Graphics graphics) {
         super.paint(graphics);
         Rectangle figureBounds = getBounds();
+        _scrollBar.enableButtons(_enable);
         _crossedPaintHelper.paintCross(graphics, figureBounds);
-        
+
     }
-    
+
     /**
      * Creates the scrollbar.
-     * 
+     *
      * @return the scrollbar figure
      */
-    private ScrollBar createScrollbarFigure() {
-        ScrollBar bar = new ScrollBar();
+    private MyScrollBar createScrollbarFigure() {
+        MyScrollBar bar = new MyScrollBar();
         bar.setExtent(5);
         bar.setMaximum(1000);
         bar.setMinimum(1);
@@ -192,56 +189,56 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
         thumb.setSize(new Dimension(40, 40));
         thumb.setFont(CustomMediaFactory.getInstance().getDefaultFont(SWT.BOLD));
         thumb.setBackgroundColor(ColorConstants.red);
-        
+
         thumb.setBorder(new SchemeBorder(SchemeBorder.SCHEMES.RIDGED));
         bar.validate();
-        
+
         // add listener
         bar.addPropertyChangeListener(RangeModel.PROPERTY_VALUE, new PropertyChangeListener() {
             public void propertyChange(final PropertyChangeEvent event) {
                 fireManualValueChange((Integer) event.getNewValue());
             }
         });
-        
+
         return bar;
     }
-    
+
     /**
      * Inform all slider listeners, that the manual value has changed.
-     * 
+     *
      * @param newManualValue
      *            the new manual value
      */
     private void fireManualValueChange(final int newManualValue) {
         int tmp = newManualValue;
-        
+
         if (tmp < _min) {
             tmp = _min;
         }
         if (tmp > _max) {
             tmp = _max;
         }
-        
+
         if (_populateEvents) {
             for (ISliderListener l : _sliderListeners) {
                 l.sliderValueChanged( ((double) tmp) / _scrollbarPrecision);
             }
         }
     }
-    
+
     /**
      * Set the "populate events" flag.
-     * 
+     *
      * @param populateEvents
      *            the "populate events" flag.
      */
     public void setPopulateEvents(final boolean populateEvents) {
         _populateEvents = populateEvents;
     }
-    
+
     /**
      * Sets if the current value should also be shown as text.
-     * 
+     *
      * @param showValueAsText
      *            True if the value should be shown as text, false otherwise
      */
@@ -249,10 +246,10 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
         _showValueAsText = showValueAsText;
         _valueLabel.setVisible(_showValueAsText);
     }
-    
+
     /**
      * Set the minimum value.
-     * 
+     *
      * @param min
      *            The minimum value.
      */
@@ -260,10 +257,10 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
         _originalMin = min;
         updateScrollbar();
     }
-    
+
     /**
      * Set the maximum value.
-     * 
+     *
      * @param max
      *            The maximum value.
      */
@@ -271,21 +268,21 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
         _originalMax = max;
         updateScrollbar();
     }
-    
+
     /**
      * Refreshes the scroolbar.
      */
     private void updateScrollbar() {
-        if (isEnabled()) {
+//        if (isEnabled()) {
             _min = (int) (_originalMin * _scrollbarPrecision);
             _max = (int) (_originalMax * _scrollbarPrecision);
             _scrollBar.setMinimum(_min);
             CentralLogger.getInstance().debug(this, "Scrollbar minimum set to: " + _min);
             _scrollBar.setMaximum(_max + _sliderWide);
             CentralLogger.getInstance().debug(this, "Scrollbar maximum set to: " + _max);
-            
+
             _currentValue = (int) (_originalVal * _scrollbarPrecision);
-            
+
             int settedValue = _currentValue;
             // update scrollbar
             if (_currentValue < _min) {
@@ -301,24 +298,24 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
             _scrollBar.setValue(settedValue);
             CentralLogger.getInstance().debug(this, "Scrollbar value set to: " + settedValue);
             _scrollBar.invalidate();
-        }
+//        }
     }
-    
+
     /**
      * Sets the real max value for the scrollbar based on the given max value.
-     * 
+     *
      * @param max
      *            The base for the real max value
      */
     private void setScrollbarMax(final int max) {
-        
+
         _scrollBar.setMaximum(max + _sliderWide);
-        
+
     }
-    
+
     /**
      * Sets the count of decimal places for this slider.
-     * 
+     *
      * @param decimalPlaces
      *            The precision
      */
@@ -326,26 +323,26 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
         _decimalPlaces = decimalPlaces;
         this.updateValueText();
     }
-    
+
     /**
      * Gets the precision of this slider.
-     * 
+     *
      * @return The precision
      */
     public int getDecimalPlaces() {
         return _decimalPlaces;
     }
-    
+
     /**
      * Sets the value for the precision of this slider.
-     * 
+     *
      * @param precision
      *            The precision
      */
     private void setScrollbarPrecision(final int precision) {
-        
+
         CentralLogger.getInstance().debug(this, "precision set to " + precision);
-        
+
         // double min = this.getDoubleFor(_min);
         // double max = this.getDoubleFor(_max);
         int minWide = _sliderWide / _scrollbarPrecision;
@@ -357,55 +354,55 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
         this.setSliderWide(minWide);
         // this.setValue(value);
         // this.setManualValue(manualValue);
-        
+
         updateScrollbar();
     }
-    
+
     /**
      * Sets the wide for the slider.
-     * 
+     *
      * @param wide
      *            The wide
      */
     public void setSliderWide(final int wide) {
-        
+
         _sliderWide = wide * _scrollbarPrecision;
         this.setScrollbarMax(_max);
         _scrollBar.setExtent(_sliderWide);
-        
+
     }
-    
+
     /**
      * Gets the slider wide.
-     * 
+     *
      * @return int The slider wide
      */
     public int getSliderWide() {
         return (_sliderWide / _scrollbarPrecision);
     }
-    
+
     /**
      * Set the increment value.
-     * 
+     *
      * @param increment
      *            The increment value.
      */
     public void setIncrement(final double increment) {
-        
+
         this.setScrollbarPrecision((int) Math.pow(10, this.getCountOfDecimals(increment)));
         int inc = (int) (increment * _scrollbarPrecision);
         if (inc == 0) {
             inc = 1;
         }
-        
+
         _scrollBar.setStepIncrement(inc);
         _scrollBar.setPageIncrement(inc);
-        
+
     }
-    
+
     /**
      * Gets the count of numbers after the comma.
-     * 
+     *
      * @param value
      *            The double value
      * @return int The count of numbers after the comma
@@ -421,47 +418,47 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
         // string to a BigDecimal in order to get its scale.
         BigDecimal bd = new BigDecimal(Double.toString(value));
         int scale = bd.scale();
-        
+
         // The value is constrained to 0..3 to prevent precisions that get too
         // large.
         CentralLogger.getInstance().debug(this, "scale=" + scale);
         return Math.max(0, Math.min(scale, 3));
     }
-    
+
     /**
      * Sets the orientation.
-     * 
+     *
      * @param horizontal
      *            true for horizontal and false for vertical layout
      */
     public void setOrientation(final boolean horizontal) {
         _scrollBar.setOrientation(horizontal ? Orientable.HORIZONTAL : Orientable.VERTICAL);
     }
-    
+
     @Override
-    public void setForegroundColor(Color fg) {
+    public void setForegroundColor(final Color fg) {
         _scrollBar.setForegroundColor(fg);
         // super.setForegroundColor(fg);
     }
-    
+
     @Override
-    public void setBackgroundColor(Color bg) {
+    public void setBackgroundColor(final Color bg) {
         _scrollBar.setBackgroundColor(bg);
         // super.setForegroundColor(fg);
     }
-    
+
     /**
      * Set the current slider value.
-     * 
+     *
      * <b>Important:</b> This method should only get called by the Controller and not by the figure
      * itself!
-     * 
+     *
      * @param value
      *            the current slider value
      */
     public void setValue(final double value) {
         _originalVal = value;
-        
+
         // CentralLogger.getInstance().debug(this, "setValue("+value+")");
         // // store current value
         // _currentValue = (int) (value * _scrollbarPrecision);
@@ -476,19 +473,19 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
         // _scrollBar.setValue(_currentValue);
         // _scrollBar.invalidate();
         // }
-        
+
         updateScrollbar();
-        
+
         updateValueText();
-        
+
     }
-    
+
     /**
      * Set the current manual value.
-     * 
+     *
      * <b>Important:</b> This method should only get called by the Controller and not by the figure
      * itself!
-     * 
+     *
      * @param value
      *            the current slider value
      */
@@ -496,7 +493,7 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
         _originalManVal = value;
         updateValueText();
     }
-    
+
     /**
      * Updates the value labels text.
      */
@@ -509,62 +506,71 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
                 .setText("" + format.format(_originalVal) + " [MAN: " + format.format(_originalManVal) + "]"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         // }
     }
-    
+
     /**
      * Add a slider listener.
-     * 
+     *
      * @param listener
      *            The slider listener to add.
      */
     public void addSliderListener(final ISliderListener listener) {
         _sliderListeners.add(listener);
     }
-    
+
     /**
      * Remove a slider listener.
-     * 
+     *
      * @param listener
      *            The slider listener that is to be removed.
      */
     public void removeSliderListener(final ISliderListener listener) {
         _sliderListeners.remove(listener);
     }
-    
+
     /**
      * This method is a tribute to unit tests, which need a way to test the performance of the
      * figure implementation. Implementors should produce some random changes and refresh the
      * figure, when this method is called.
-     * 
+     *
      */
     public void randomNoiseRefresh() {
     }
-    
+
     /**
      * {@inheritDoc}
      */
     @Override
     public void setEnabled(final boolean value) {
+        _enable = value;
+
+        // Im edit: Knop ja,    Move ja   l gr r gr
+        // Im run:  Knop nein,  Move nein l no r gr
+//        _scrollBar.setEnabled(true);
+//        super.setEnabled(true);
+
+        // Im edit: Knop ja,    Move nein l gr r gr
+        // Im run:  Knop nein,  Move nein l no r gr
+//        _scrollBar.setEnabled(value);
         _scrollBar.setEnabled(true);
-        // if(!value) {
-        _figure1.setStart(new Point(2, 2));
-        _figure1.setEnd(new Point(20, 20));
-        _figure1.setBackgroundColor(bgColor);
-        _figure1.setBackgroundColor(fgColor);
-        _figure1.setFill(true);
-        _figure1.setLineCap(3);
-        _figure1.setLineWidth(3);
-        this.add(_figure1);
-        this.add(_figure2);
-        this.updateScrollbar();
-        // listen to figure movement events
-        // }else {
-        // remove(_figure1);
-        // remove(_figure2);
-        // }
+        super.setEnabled(value);
+//
+        // Im edit: Knop nein, Move nein l gr r gr
+        // Im run:  Knop nein, Move nein l no r gr
+//        _scrollBar.setEnabled(false);
+//        super.setEnabled(true);
+//
+        // Im edit: Knop nein, Move nein l gr r gr
+        // Im run:  Knop nein, Move nein l no r gr
+//        _scrollBar.setEnabled(false);
+//        super.setEnabled(false);
+
+//        _scrollBar.setEnabled(value);
+//        super.setEnabled(value);
+
     }
-    
+
     @Override
-    protected void paintFigure(Graphics graphics) {
+    protected void paintFigure(final Graphics graphics) {
         graphics.setBackgroundColor(getBackgroundColor());
         Rectangle bounds = this.getBounds().getCopy();
         bounds.crop(this.getInsets());
@@ -572,7 +578,7 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
         graphics.setBackgroundColor(getBackgroundColor());
         graphics.setForegroundColor(getForegroundColor());
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -586,28 +592,43 @@ public final class SimpleSliderFigure extends Panel implements ICrossedFigure {
         }
         return null;
     }
-    
+
     /**
      * Definition of listeners that react on slider events.
-     * 
+     *
      * @author Sven Wende
      * @version $Revision$
-     * 
+     *
      */
     public interface ISliderListener {
         /**
          * React on a slider event.
-         * 
+         *
          * @param newValue
          *            The new slider value.
          */
         void sliderValueChanged(double newValue);
     }
-    
+
     @Override
-    public void setCrossedOut(boolean newValue) {
+    public void setCrossedOut(final boolean newValue) {
         _crossedPaintHelper.setCrossed(newValue);
-        
     }
-    
+
+    /**
+     *
+     * A Scrollbar how can disable separately the Buttons
+     *
+     * @author hrickens
+     * @author $Author$
+     * @version $Revision$
+     * @since 16.04.2010
+     */
+    private class MyScrollBar extends ScrollBar{
+
+        public void enableButtons(final boolean enable) {
+            getButtonUp().setEnabled(enable);
+            getButtonDown().setEnabled(enable);
+        }
+    }
 }
