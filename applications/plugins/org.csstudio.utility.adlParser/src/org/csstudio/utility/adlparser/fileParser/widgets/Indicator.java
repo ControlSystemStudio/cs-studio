67e7519c848a3a6fcba1190f44c053fbe751a4b4
hammonds <hammonds>
2010-04-19 15:08:20 +0000

    New class to hold indicator.  This is currently empty.

---
+++ applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/widgets/Indicator.java
@@ -0,0 +1,12 @@
+package org.csstudio.utility.adlparser.fileParser.widgets;
+
+import org.csstudio.utility.adlparser.fileParser.ADLWidget;
+
+public class Indicator extends ADLAbstractWidget {
+
+	public Indicator(ADLWidget adlWidget) {
+		super(adlWidget);
+		// TODO Auto-generated constructor stub
+	}
+
+}
