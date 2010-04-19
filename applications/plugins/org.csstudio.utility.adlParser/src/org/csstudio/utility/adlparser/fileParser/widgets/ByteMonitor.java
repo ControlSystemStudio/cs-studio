d13dc20b93bccb48a6b441579836e69e3c92e39d
hammonds <hammonds>
2010-04-19 15:07:03 +0000

    New class to hold Byte monitor.  This is currently empty.

---
+++ applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/widgets/ByteMonitor.java
@@ -0,0 +1,12 @@
+package org.csstudio.utility.adlparser.fileParser.widgets;
+
+import org.csstudio.utility.adlparser.fileParser.ADLWidget;
+
+public class ByteMonitor extends ADLAbstractWidget {
+
+	public ByteMonitor(ADLWidget adlWidget) {
+		super(adlWidget);
+		// TODO Auto-generated constructor stub
+	}
+
+}
