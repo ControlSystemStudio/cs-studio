3ef9bf72d41dc7556682842947e19183d8e75456
hammonds <hammonds>
2010-04-19 15:14:46 +0000

    Add the first round of handling code.

--- applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Composite2Model.java
+++ applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Composite2Model.java
@@ -1,20 +1,53 @@
 package org.csstudio.opibuilder.adl2boy.translator;
 
+import org.csstudio.opibuilder.model.AbstractContainerModel;
 import org.csstudio.opibuilder.model.AbstractWidgetModel;
+import org.csstudio.opibuilder.widgets.model.GroupingContainerModel;
+import org.csstudio.opibuilder.widgets.model.LabelModel;
+import org.csstudio.opibuilder.widgets.model.LinkingContainerModel;
 import org.csstudio.utility.adlparser.fileParser.ADLWidget;
+import org.csstudio.utility.adlparser.fileParser.widgets.Composite;
 import org.eclipse.swt.graphics.RGB;
 
 public class Composite2Model extends AbstractADL2Model {
+	AbstractContainerModel containerModel;
 
 	public Composite2Model(ADLWidget adlWidget, RGB[] colorMap) {
 		super(adlWidget, colorMap);
-		// TODO Auto-generated constructor stub
+		Composite compositeWidget = new Composite(adlWidget);
+		if (!compositeWidget.hasCompositeFile()) {
+			containerModel = new LinkingContainerModel();
+		}
+		else {
+			containerModel = new GroupingContainerModel();
+			if (compositeWidget != null) {
+				setADLObjectProps(compositeWidget, containerModel);
+			}
+			TranslatorUtils.ConvertChildren(compositeWidget.getChildren(), containerModel, colorMap);
+			((GroupingContainerModel)(containerModel)).setPropertyValue(GroupingContainerModel.PROP_SHOW_SCROLLBAR, false);
+			FixChildPositions();
+		}
+		if (compositeWidget != null) {
+			setADLObjectProps(compositeWidget, containerModel);
+			if (compositeWidget != null) {
+				setADLObjectProps(compositeWidget, containerModel);
+			}
+		}
+
 	}
 
 	@Override
 	public	AbstractWidgetModel getWidgetModel() {
-		// TODO Auto-generated method stub
-		return null;
+		return containerModel;
 	}
 
+	private void FixChildPositions() {
+		int compositeX = containerModel.getX();
+		int compositeY = containerModel.getY();
+		
+		for (AbstractWidgetModel model : containerModel.getChildren()){
+			model.setX(model.getX() - compositeX);
+			model.setY(model.getY() - compositeY);
+		}
+	}
 }
