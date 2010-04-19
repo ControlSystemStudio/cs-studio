501022b90b5e3e730f38f17ce07b2657e2856109
hammonds <hammonds>
2010-04-19 15:16:03 +0000

    Mostly refactor to take out the code that builds the translated widgets.  This is now in TranslatorUtils.

--- applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/EditADLHandler.java
+++ applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/EditADLHandler.java
@@ -5,23 +5,8 @@ import java.io.File;
 import java.io.InputStream;
 import java.util.Iterator;
 
-import org.csstudio.opibuilder.adl2boy.translator.Arc2Model;
-import org.csstudio.opibuilder.adl2boy.translator.Bar2Model;
-import org.csstudio.opibuilder.adl2boy.translator.CartesianPlot2Model;
 import org.csstudio.opibuilder.adl2boy.translator.Display2Model;
-import org.csstudio.opibuilder.adl2boy.translator.Image2Model;
-import org.csstudio.opibuilder.adl2boy.translator.Menu2Model;
-import org.csstudio.opibuilder.adl2boy.translator.MessageButton2Model;
-import org.csstudio.opibuilder.adl2boy.translator.Meter2Model;
-import org.csstudio.opibuilder.adl2boy.translator.Oval2Model;
-import org.csstudio.opibuilder.adl2boy.translator.PolyLine2Model;
-import org.csstudio.opibuilder.adl2boy.translator.Polygon2Model;
-import org.csstudio.opibuilder.adl2boy.translator.Rectangle2Model;
-import org.csstudio.opibuilder.adl2boy.translator.RelatedDisplay2Model;
-import org.csstudio.opibuilder.adl2boy.translator.Text2Model;
-import org.csstudio.opibuilder.adl2boy.translator.TextEntry2Model;
-import org.csstudio.opibuilder.adl2boy.translator.TextUpdate2Model;
-import org.csstudio.opibuilder.adl2boy.translator.Valuator2Model;
+import org.csstudio.opibuilder.adl2boy.translator.TranslatorUtils;
 import org.csstudio.opibuilder.model.DisplayModel;
 import org.csstudio.opibuilder.persistence.XMLUtil;
 import org.csstudio.utility.adlparser.fileParser.ADLWidget;
@@ -101,137 +86,7 @@ public class EditADLHandler implements IHandler {
 						displayModel = (DisplayModel)(new Display2Model(adlWidget, colorMap)).getWidgetModel();
 					}
 				}
-				for (ADLWidget adlWidget : root.getObjects()){
-					try {
-						String widgetType = adlWidget.getType();
-						if (widgetType.equals("arc")){
-							displayModel.addChild((new Arc2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-						}
-						else if (widgetType.equals("bar")){
-							displayModel.addChild((new Bar2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-						
-						}
-						else if (widgetType.equals("byte")){
-							printNotHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("cartesian plot")){
-							displayModel.addChild((new CartesianPlot2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("choice button")){
-							printNotHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("composite")){
-							printNotHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("dynamic symbol")){
-							printNotHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("file")){
-							printNotHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("image")){
-							displayModel.addChild((new Image2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("indicator")){
-							printNotHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("menu")){
-							displayModel.addChild((new Menu2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("message button")){
-							displayModel.addChild((new MessageButton2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("toggle button")){
-							printNotHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("meter")){
-							displayModel.addChild((new Meter2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotHandledMessage(widgetType);
-								
-						}
-						else if (widgetType.equals("oval")){
-							displayModel.addChild((new Oval2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("polygon")){
-							displayModel.addChild((new Polygon2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("polyline")){
-							displayModel.addChild((new PolyLine2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("line")){
-							printNotHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("rectangle")){
-							displayModel.addChild((new Rectangle2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("related display")){
-							displayModel.addChild((new RelatedDisplay2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("strip chart")){
-							printNotHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("text")){
-							displayModel.addChild((new Text2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("text update")){
-							displayModel.addChild((new TextUpdate2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("text entry")){
-							displayModel.addChild((new TextEntry2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("valuator")){
-							displayModel.addChild((new Valuator2Model(adlWidget, colorMap)).getWidgetModel());
-							printNotCompletelyHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("basic attribute")){
-							printNotHandledMessage(widgetType);
-							
-						}
-						else if (widgetType.equals("dynamic attribute")){
-							printNotHandledMessage(widgetType);
-							
-						}
-					}
-					catch (Exception ex) {
-						ex.printStackTrace();
-					}
-				}
+				TranslatorUtils.ConvertChildren(root.getObjects(), displayModel, colorMap);
 				
 				String s = XMLUtil.WidgetToXMLString(displayModel, true);
 				InputStream is = new ByteArrayInputStream(s.getBytes());
