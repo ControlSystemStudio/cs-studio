009b2d8e689ae57c08a2b5c8a2af98117f666829
hammonds <hammonds>
2010-04-19 15:09:48 +0000

    New class to handle menus.

---
+++ applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/widgets/Composite.java
@@ -0,0 +1,83 @@
+package org.csstudio.utility.adlparser.fileParser.widgets;
+
+import java.util.ArrayList;
+
+import org.csstudio.utility.adlparser.internationalization.Messages;
+import org.csstudio.utility.adlparser.fileParser.FileLine;
+import org.csstudio.utility.adlparser.fileParser.ADLWidget;
+import org.csstudio.utility.adlparser.fileParser.WrongADLFormatException;
+import org.csstudio.utility.adlparser.fileParser.widgetParts.ADLChildren;
+import org.csstudio.utility.adlparser.fileParser.widgetParts.ADLObject;
+
+public class Composite extends ADLAbstractWidget {
+	private String _compositeFile = new String();
+	private boolean _hasCompositeFile = true;
+	private ArrayList<ADLWidget> _children;
+	
+	public Composite(ADLWidget adlWidget) {
+		super(adlWidget);
+		try {
+			for (ADLWidget childWidget : adlWidget.getObjects()) {
+	        	if (childWidget.getType().equals("object")){
+	        		_adlObject = new ADLObject(childWidget);
+	        		if (_adlObject != null){
+	        			_hasObject = true;
+	        		}
+	        		
+	        	}
+	        	else if (childWidget.getType().equals("children")){
+	        		ADLChildren adlChildren = new ADLChildren(childWidget);
+	        		if (adlChildren != null){
+	        			_children = adlChildren.getAdlChildrens();
+	        		}
+	        	}
+	        }
+	        for (FileLine fileLine : adlWidget.getBody()) {
+	            String obj = fileLine.getLine();
+	            String[] row = obj.trim().split("="); //$NON-NLS-1$
+	            if(row.length<2){
+	                throw new WrongADLFormatException(Messages.Label_WrongADLFormatException_Parameter_Begin+obj+Messages.Label_WrongADLFormatException_Parameter_End);
+	            }
+	            if(row[0].equals("composite file")){ //$NON-NLS-1$
+	                set_compositeFile(row[1].trim().replaceAll("\"", ""));
+	                if (!(_compositeFile.equals(""))){
+	                	_hasCompositeFile = false;
+	                }
+	            }
+	        }
+		}
+		catch (WrongADLFormatException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param _compositeFile the _compositeFile to set
+	 */
+	private void set_compositeFile(String _compositeFile) {
+		this._compositeFile = _compositeFile;
+	}
+
+	/**
+	 * @return the _compositeFile
+	 */
+	public String get_compositeFile() {
+		return _compositeFile;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean hasCompositeFile(){
+		return _hasCompositeFile;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<ADLWidget> getChildren() {
+		return _children;
+	}
+}
---
+++ applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/widgets/Menu.java
@@ -0,0 +1,34 @@
+package org.csstudio.utility.adlparser.fileParser.widgets;
+
+import org.csstudio.utility.adlparser.fileParser.ADLWidget;
+import org.csstudio.utility.adlparser.fileParser.WrongADLFormatException;
+import org.csstudio.utility.adlparser.fileParser.widgetParts.ADLControl;
+import org.csstudio.utility.adlparser.fileParser.widgetParts.ADLObject;
+
+public class Menu extends ADLAbstractWidget {
+
+	public Menu(ADLWidget adlWidget) {
+		super(adlWidget);
+		try {
+			for (ADLWidget childWidget : adlWidget.getObjects()) {
+	        	if (childWidget.getType().equals("control")){
+	        		_adlControl = new ADLControl(childWidget);
+	        		if (_adlControl != null){
+	        			_hasControl = true;
+	        		}
+	        	}
+	        	else if (childWidget.getType().equals("object")){
+	        		_adlObject = new ADLObject(childWidget);
+	        		if (_adlObject != null){
+	        			_hasObject = true;
+	        		}
+	        		
+	        	}
+	        }
+		}
+		catch (WrongADLFormatException ex) {
+			
+		}
+	}
+
+}
