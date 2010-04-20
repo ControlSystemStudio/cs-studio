38d3e188a96bfd8702c3a68f7d937ed40bdd2740
hammonds <hammonds>
2010-04-20 19:02:29 +0000

    Add Begin & End path Add Basic attributes (i.e. fill & line style/width)

--- applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Arc2Model.java
+++ applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Arc2Model.java
@@ -2,6 +2,8 @@ package org.csstudio.opibuilder.adl2boy.translator;
 
 import org.csstudio.opibuilder.model.AbstractContainerModel;
 import org.csstudio.opibuilder.model.AbstractWidgetModel;
+import org.csstudio.opibuilder.util.OPIColor;
+import org.csstudio.opibuilder.widgets.model.AbstractShapeModel;
 import org.csstudio.opibuilder.widgets.model.ArcModel;
 import org.csstudio.utility.adlparser.fileParser.ADLWidget;
 import org.csstudio.utility.adlparser.fileParser.widgets.Arc;
@@ -18,9 +20,32 @@ public class Arc2Model extends AbstractADL2Model {
 			setADLObjectProps(arcWidget, arcModel);
 			setADLBasicAttributeProps(arcWidget, arcModel, false);
 		}
+		arcModel.setPropertyValue(ArcModel.PROP_START_ANGLE, (float)arcWidget.get_begin()/64);
+		arcModel.setPropertyValue(ArcModel.PROP_TOTAL_ANGLE, (float)arcWidget.get_path()/64);
+
+		//check fill parameters
+		if ( arcWidget.hasADLBasicAttribute() ) {
+			if (arcWidget.getAdlBasicAttribute().getFill().equals("solid") ) {
+				System.out.println("RECTANGLE fill is solid");				
+				arcModel.setPropertyValue(ArcModel.PROP_FILL, true);
+				
+			}
+			else if (arcWidget.getAdlBasicAttribute().getFill().equals("outline")) {
+				OPIColor fColor = (OPIColor)arcModel.getPropertyValue(AbstractWidgetModel.PROP_COLOR_FOREGROUND);
+				arcModel.setPropertyValue(AbstractShapeModel.PROP_LINE_COLOR, fColor);
+				if ( arcWidget.getAdlBasicAttribute().getStyle().equals("solid") ) {
+					arcModel.setPropertyValue(AbstractShapeModel.PROP_LINE_STYLE, "Solid");
+				}
+				if ( arcWidget.getAdlBasicAttribute().getStyle().equals("dash") ) {
+					arcModel.setPropertyValue(AbstractShapeModel.PROP_LINE_STYLE, "Dash");
+					
+				}
+				arcModel.setPropertyValue(AbstractShapeModel.PROP_LINE_WIDTH, arcWidget.getAdlBasicAttribute().getWidth());
+			}
+			
+		}
+
 		//TODO Add Dynamic Properties to Arc2Model
-		//TODO Add Basic Properties to Arc2Model
-		//TODO Add Begin and Path Angle to Arc2Model
 	}
 
 	@Override
