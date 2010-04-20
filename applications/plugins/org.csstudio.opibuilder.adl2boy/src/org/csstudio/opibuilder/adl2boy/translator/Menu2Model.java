3f01c8ea8686132f8d88b80340b48e225cf415d6
hammonds <hammonds>
2010-04-20 19:46:36 +0000

    Add code to get alarm sensitive color mode.

--- applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Menu2Model.java
+++ applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Menu2Model.java
@@ -2,6 +2,7 @@ package org.csstudio.opibuilder.adl2boy.translator;
 
 import org.csstudio.opibuilder.model.AbstractContainerModel;
 import org.csstudio.opibuilder.model.AbstractWidgetModel;
+import org.csstudio.opibuilder.widgets.model.ActionButtonModel;
 import org.csstudio.opibuilder.widgets.model.MenuButtonModel;
 import org.csstudio.utility.adlparser.fileParser.ADLWidget;
 import org.csstudio.utility.adlparser.fileParser.widgets.Menu;
@@ -20,12 +21,23 @@ public class Menu2Model extends AbstractADL2Model {
 			setADLControlProps(menuWidget, menuModel);
 		}
 		menuModel.setPropertyValue(MenuButtonModel.PROP_ACTIONS_FROM_PV, true);
-		//TODO add color Mode to Menu2Model
+		String color_mode = menuWidget.getColor_mode();
+		if ( color_mode.equals("static") ){
+			menuModel.setPropertyValue(ActionButtonModel.PROP_FORECOLOR_ALARMSENSITIVE, false);
+		}
+		else if (color_mode.equals("alarm") ){
+			menuModel.setPropertyValue(ActionButtonModel.PROP_FORECOLOR_ALARMSENSITIVE, true);
+		}
+		else if (color_mode.equals("discrete") ){
+			menuModel.setPropertyValue(ActionButtonModel.PROP_FORECOLOR_ALARMSENSITIVE, false);
+			//TODO Menu2Model Figure out what to do if colorMode is discrete
+		}
 	}
 
 	@Override
 	public	AbstractWidgetModel getWidgetModel() {
 		return menuModel;
 	}
+
 
 }
