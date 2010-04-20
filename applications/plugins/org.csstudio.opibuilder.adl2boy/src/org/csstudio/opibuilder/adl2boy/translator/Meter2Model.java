b501124715b520d87ac3a3e8d163920981a487d7
hammonds <hammonds>
2010-04-20 20:23:37 +0000

    Add color mode

--- applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Meter2Model.java
+++ applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Meter2Model.java
@@ -2,6 +2,7 @@ package org.csstudio.opibuilder.adl2boy.translator;
 
 import org.csstudio.opibuilder.model.AbstractContainerModel;
 import org.csstudio.opibuilder.model.AbstractWidgetModel;
+import org.csstudio.opibuilder.widgets.model.ActionButtonModel;
 import org.csstudio.opibuilder.widgets.model.XMeterModel;
 import org.csstudio.utility.adlparser.fileParser.ADLWidget;
 import org.csstudio.utility.adlparser.fileParser.widgets.Meter;
@@ -18,9 +19,23 @@ public class Meter2Model extends AbstractADL2Model {
 			setADLObjectProps(meterWidget, meterModel);
 			setADLMonitorProps(meterWidget, meterModel);
 		}
+
+		//set color mode
+		String color_mode = meterWidget.getColor_mode();
+		System.out.println("Meter Color mode " + color_mode);
+		if ( color_mode.equals("static") ){
+			meterModel.setPropertyValue(XMeterModel.PROP_FORECOLOR_ALARMSENSITIVE, false);
+		}
+		else if (color_mode.equals("alarm") ){
+			meterModel.setPropertyValue(XMeterModel.PROP_FORECOLOR_ALARMSENSITIVE, true);
+		}
+		else if (color_mode.equals("discrete") ){
+			meterModel.setPropertyValue(XMeterModel.PROP_FORECOLOR_ALARMSENSITIVE, false);
+			//TODO Meter2Model Figure out what to do if colorMode is discrete
+		}
+		
 		//TODO Add PV Limits to Meter2Model
 		//TODO Add Label to Meter2Model
-		//TODO Add color mode to Meter2Model
 	}
 
 	@Override
