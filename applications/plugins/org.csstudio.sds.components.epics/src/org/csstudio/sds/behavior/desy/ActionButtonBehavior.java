6f6bd7210ebe1117f350a40e3530bdcfa0da66d7
Helge Rickens <helge.rickens@desy.de>
2010-04-21 07:06:48 +0000

    add ActionButtonBehavior

---
+++ applications/plugins/org.csstudio.sds.components.epics/src/org/csstudio/sds/behavior/desy/ActionButtonBehavior.java
@@ -0,0 +1,78 @@
+/*
+		* Copyright (c) 2010 Stiftung Deutsches Elektronen-Synchrotron,
+		* Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY.
+		*
+		* THIS SOFTWARE IS PROVIDED UNDER THIS LICENSE ON AN "../AS IS" BASIS.
+		* WITHOUT WARRANTY OF ANY KIND, EXPRESSED OR IMPLIED, INCLUDING BUT
+		NOT LIMITED
+		* TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR PARTICULAR PURPOSE
+		AND
+		* NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+		BE LIABLE
+		* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+		CONTRACT,
+		* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+		SOFTWARE OR
+		* THE USE OR OTHER DEALINGS IN THE SOFTWARE. SHOULD THE SOFTWARE PROVE
+		DEFECTIVE
+		* IN ANY RESPECT, THE USER ASSUMES THE COST OF ANY NECESSARY SERVICING,
+		REPAIR OR
+		* CORRECTION. THIS DISCLAIMER OF WARRANTY CONSTITUTES AN ESSENTIAL PART
+		OF THIS LICENSE.
+		* NO USE OF ANY SOFTWARE IS AUTHORIZED HEREUNDER EXCEPT UNDER THIS
+		DISCLAIMER.
+		* DESY HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+		ENHANCEMENTS,
+		* OR MODIFICATIONS.
+		* THE FULL LICENSE SPECIFYING FOR THE SOFTWARE THE REDISTRIBUTION,
+		MODIFICATION,
+		* USAGE AND OTHER RIGHTS AND OBLIGATIONS IS INCLUDED WITH THE
+		DISTRIBUTION OF THIS
+		* PROJECT IN THE FILE LICENSE.HTML. IF THE LICENSE IS NOT INCLUDED YOU
+		MAY FIND A COPY
+		* AT HTTP://WWW.DESY.DE/LEGAL/LICENSE.HTM
+		*/
+package org.csstudio.sds.behavior.desy;
+
+import org.csstudio.sds.components.model.ActionButtonModel;
+import org.epics.css.dal.simple.AnyData;
+import org.epics.css.dal.simple.MetaData;
+
+/**
+ * Default DESY-Behavior for the {@link ActionButtonModel} widget with Connection state.
+ *
+ * @author hrickens
+ * @author $Author$
+ * @version $Revision$
+ * @since 21.04.2010
+ */
+public class ActionButtonBehavior extends AbstractDesyConnectionBehavior<ActionButtonModel> {
+
+    /**
+     * Constructor.
+     */
+    public ActionButtonBehavior() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void doProcessValueChange(final ActionButtonModel model, final AnyData anyData) {
+        super.doProcessValueChange(model, anyData);
+        // .. update button value
+        model.setPropertyValue(ActionButtonModel.PROP_LABEL, anyData.stringValue());
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void doProcessMetaDataChange(final ActionButtonModel widget, final MetaData metaData) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
