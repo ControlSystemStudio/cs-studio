1288e408121969ef09f1cee5cbb94a4ee226da5d
hammonds <hammonds>
2010-04-21 16:29:33 +0000

    Add code to: 		//TODO Add ImageType to Image 		//TODO Add ImageName to Image 		//TODO Add ImageCalc to Image

--- applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/widgets/Image.java
+++ applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/widgets/Image.java
@@ -1,13 +1,17 @@
 package org.csstudio.utility.adlparser.fileParser.widgets;
 
 import org.csstudio.utility.adlparser.fileParser.ADLWidget;
+import org.csstudio.utility.adlparser.fileParser.FileLine;
 import org.csstudio.utility.adlparser.fileParser.WrongADLFormatException;
-import org.csstudio.utility.adlparser.fileParser.widgetParts.ADLBasicAttribute;
 import org.csstudio.utility.adlparser.fileParser.widgetParts.ADLDynamicAttribute;
 import org.csstudio.utility.adlparser.fileParser.widgetParts.ADLObject;
+import org.csstudio.utility.adlparser.internationalization.Messages;
 
 public class Image extends ADLAbstractWidget {
-
+	private String imageType = new String("gif");
+	private String imageName =  new String();
+	private String imageCalc = new String();
+	
 	public Image(ADLWidget adlWidget) {
 		super(adlWidget);
 		try {
@@ -26,14 +30,68 @@ public class Image extends ADLAbstractWidget {
 	        		}
 	        	}
 	        }
+			for (FileLine fileLine : adlWidget.getBody()){
+				String bodyPart = fileLine.getLine();
+				String[] row = bodyPart.trim().split("=");
+				if (row.length < 2){
+					throw new WrongADLFormatException(Messages.Label_WrongADLFormatException_Parameter_Begin + bodyPart + Messages.Label_WrongADLFormatException_Parameter_End);
+				}
+				if (FileLine.argEquals(row[0], "type")){
+					setImageType(FileLine.getTrimmedValue(row[1]));
+				}
+				else if (FileLine.argEquals(row[0], "image_name")){
+					setImageName(FileLine.getTrimmedValue(row[1]));
+				}
+				else if (FileLine.argEquals(row[0], "calc")){
+					setImageCalc(FileLine.getTrimmedValue(row[1]));
+				}
+			}
 		}
 		catch (WrongADLFormatException ex) {
-			
+			ex.printStackTrace();
 		}
-		//TODO Add ImageType to Image
-		//TODO Add ImageName to Image
-		//TODO Add ImageCalc to Image
-		
+	}
+
+	/**
+	 * @param imageType the imageType to set
+	 */
+	private void setImageType(String imageType) {
+		this.imageType = imageType;
+	}
+
+	/**
+	 * @return the imageType
+	 */
+	public String getImageType() {
+		return imageType;
+	}
+
+	/**
+	 * @param imageName the imageName to set
+	 */
+	private void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	/**
+	 * @return the imageName
+	 */
+	public String getImageName() {
+		return imageName;
+	}
+
+	/**
+	 * @param imageCalc the imageCalc to set
+	 */
+	private void setImageCalc(String imageCalc) {
+		this.imageCalc = imageCalc;
+	}
+
+	/**
+	 * @return the imageCalc
+	 */
+	public String getImageCalc() {
+		return imageCalc;
 	}
 
 }
