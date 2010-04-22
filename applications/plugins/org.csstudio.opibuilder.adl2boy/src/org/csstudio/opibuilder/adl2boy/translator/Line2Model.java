43ca548934161e99a23b94cb0362b0ed0427e915
hammonds <hammonds>
2010-04-22 19:01:39 +0000

    Just change to subclass polyline.

--- applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Line2Model.java
+++ applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Line2Model.java
@@ -1,21 +1,12 @@
 package org.csstudio.opibuilder.adl2boy.translator;
 
 import org.csstudio.opibuilder.model.AbstractContainerModel;
-import org.csstudio.opibuilder.model.AbstractWidgetModel;
 import org.csstudio.utility.adlparser.fileParser.ADLWidget;
 import org.eclipse.swt.graphics.RGB;
 
-public class Line2Model extends AbstractADL2Model {
+public class Line2Model extends PolyLine2Model {
 
 	public Line2Model(ADLWidget adlWidget, RGB[] colorMap, AbstractContainerModel parentModel) {
 		super(adlWidget, colorMap, parentModel);
-		// TODO Figure out how to map Line to Polyline
 	}
-
-	@Override
-	public	AbstractWidgetModel getWidgetModel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
