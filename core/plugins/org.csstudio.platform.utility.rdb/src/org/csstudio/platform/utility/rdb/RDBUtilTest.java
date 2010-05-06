5c88277a381d5e6e504da0a31f77af2fad828f25
Kay Kasemir <kasemirk@ornl.gov>
2010-05-06 18:05:26 +0000

    connect test

--- core/plugins/org.csstudio.platform.utility.rdb/src/org/csstudio/platform/utility/rdb/RDBUtilTest.java
+++ core/plugins/org.csstudio.platform.utility.rdb/src/org/csstudio/platform/utility/rdb/RDBUtilTest.java
@@ -2,28 +2,70 @@ package org.csstudio.platform.utility.rdb;
 
 import java.sql.Connection;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
-/** JUnit Test of automated re-connect.
- *  <p>
- *  Not really a test because it has no way to force a connection error.
- *  While running this test, one needs to stop the RDB or disconnect
- *  the network cable, see if the disconnect is noticed (after a rather
- *  long timeout).
- *  Then see if reconnection succeeds when the network cable or RDB are restored.
- *  
+/** JUnit Tests for RDBUtil
  *  @author Kay Kasemir
  */
 @SuppressWarnings("nls")
 public class RDBUtilTest
 {
+    /** Must adjust these for your site! */
+//    private static final String URL = "jdbc:mysql://ics-web.sns.ornl.gov/alarm";
+//    private static final String USER = "alarm";
+//    private static final String PASSWORD = "$alarm";
 
-    private static final String URL = "jdbc:mysql://ics-web.sns.ornl.gov/alarm";
-    private static final String USER = "alarm";
-    private static final String PASSWORD = "$alarm";
+    private static final String URL1 = 
+        "jdbc:oracle:thin:@" +
+        "(DESCRIPTION=" +
+        " (ADDRESS_LIST=(LOAD_BALANCE=OFF)" +
+        "  (ADDRESS=(PROTOCOL=TCP)(HOST=172.31.75.138)(PORT=1521))" +
+        "  (ADDRESS=(PROTOCOL=TCP)(HOST=172.31.75.141)(PORT=1521))" +
+        "  (ADDRESS=(PROTOCOL=TCP)(HOST=172.31.73.93) (PORT=1521))" +
+        " )" +
+        " (CONNECT_DATA=(SERVICE_NAME=ics_prod_lba))" +
+        ")";
+    
+    private static final String URL = 
+        "jdbc:oracle:thin:@" +
+        "(DESCRIPTION=(SOURCE_ROUTE=YES)" +
+        " (ADDRESS_LIST=(LOAD_BALANCE=OFF)(FAILOVER=ON)" +
+        "  (ADDRESS=(PROTOCOL=TCP)(HOST=snsapp1a.sns.ornl.gov)(PORT=1521))" +
+        "  (ADDRESS=(PROTOCOL=TCP)(HOST=snsapp1b.sns.ornl.gov)(PORT=1521))" +
+        " )" +
+        " (ADDRESS_LIST=(LOAD_BALANCE=OFF)" +
+        "  (ADDRESS=(PROTOCOL=TCP)(HOST=172.31.75.138)(PORT=1521))" +
+        "  (ADDRESS=(PROTOCOL=TCP)(HOST=172.31.75.141)(PORT=1521))" +
+        "  (ADDRESS=(PROTOCOL=TCP)(HOST=172.31.73.93) (PORT=1521))" +
+        " )" +
+        " (CONNECT_DATA=(SERVICE_NAME=ics_prod_lba))" +
+        ")";
 
+    private static final String USER = "sns_reports";
+    private static final String PASSWORD = "sns";
+
+    
+    /** Basic connection */
     @Test
-    public void testGetConnection() throws Exception
+    public void testConnection() throws Exception
+    {
+        final RDBUtil rdb = RDBUtil.connect(URL, USER, PASSWORD, true);
+        final Connection connection = rdb.getConnection();
+        System.out.println("Connection: " + connection);
+    }
+
+    
+    /** Check re-connect
+     *  Not really a test because it has no way to force a connection error.
+     *  While running this test, one needs to stop the RDB or disconnect
+     *  the network cable, see if the disconnect is noticed (after a rather
+     *  long timeout).
+     *  Then see if reconnection succeeds when the network cable or RDB are restored.
+     */
+    @Test
+    @Ignore
+    public void testReconnect() throws Exception
     {
         final RDBUtil rdb = RDBUtil.connect(URL, USER, PASSWORD, true);
 
