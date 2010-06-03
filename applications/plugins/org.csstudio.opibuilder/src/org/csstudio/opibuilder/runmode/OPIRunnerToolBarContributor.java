e6ea00e8c2f61dd977c3cb19a3acf4c92e680e9d
Xihui Chen <chenx1@ornl.gov>
2010-06-03 19:58:12 +0000

    add the code for auto navigation between OPIs. Need further investigation to enable this feature

--- applications/plugins/org.csstudio.opibuilder/src/org/csstudio/opibuilder/runmode/OPIRunnerToolBarContributor.java
+++ applications/plugins/org.csstudio.opibuilder/src/org/csstudio/opibuilder/runmode/OPIRunnerToolBarContributor.java
@@ -1,4 +1,5 @@
 package org.csstudio.opibuilder.runmode;
+import org.csstudio.opibuilder.actions.AutoNavigateAction;
 import org.csstudio.opibuilder.actions.NavigateOPIsAction;
 import org.eclipse.gef.ui.actions.ActionRegistry;
 import org.eclipse.gef.ui.actions.ZoomComboContributionItem;
@@ -20,13 +21,14 @@ public class OPIRunnerToolBarContributor extends EditorActionBarContributor {
 	private NavigateOPIsAction backwardAction, forwardAction;
 	private ZoomInRetargetAction zoomInAction;
 	private ZoomOutRetargetAction zoomOutAction;
-
+	//private AutoNavigateAction autoNavigateAction;
+	
 	public OPIRunnerToolBarContributor() {
 		backwardAction = new NavigateOPIsAction(false);
 		forwardAction = new NavigateOPIsAction(true);
 		zoomInAction = new ZoomInRetargetAction();
 		zoomOutAction = new ZoomOutRetargetAction();
-		
+		//autoNavigateAction = new AutoNavigateAction();
 	}
 	
 	@Override
@@ -38,6 +40,7 @@ public class OPIRunnerToolBarContributor extends EditorActionBarContributor {
 		toolBarManager.add(new ZoomComboContributionItem(getPage()));
 		toolBarManager.add(backwardAction);
 		toolBarManager.add(forwardAction);
+		//toolBarManager.add(autoNavigateAction);
 	}
 	
 	@Override
@@ -58,6 +61,9 @@ public class OPIRunnerToolBarContributor extends EditorActionBarContributor {
 				actionRegistry.getAction(zoomInAction.getId()));
 		bars.setGlobalActionHandler(zoomOutAction.getId(),
 				actionRegistry.getAction(zoomOutAction.getId()));
+		
+		//autoNavigateAction.setPart(targetEditor);
+		
 		
 		
 	}
