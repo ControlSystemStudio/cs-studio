53493f6b6ffc0597389f0e6f1d065b5a2f751deb
Kay Kasemir <kasemirk@ornl.gov>
2010-07-16 15:52:59 +0000

    XML 'header', unit test, copyright

--- applications/plugins/org.csstudio.apputil/src/org/csstudio/apputil/xml/XMLWriter.java
+++ applications/plugins/org.csstudio.apputil/src/org/csstudio/apputil/xml/XMLWriter.java
@@ -1,14 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package org.csstudio.apputil.xml;
 
 import java.io.PrintWriter;
 
 /** Helper for XML output.
  *  
+ *  Not 100% clear how Unicode etc. all work, but the general idea is this:
+ *  The XML will only contain ASCII chars.
+ *  Every code point > 126 could be UTF-8 encoded, but we're not doing that.
+ * 
+ *  Instead, the XML might include things like "&#246;", i.e. plain
+ *  ASCII '&', '#', '2', ... to represent the German u-Umlaut, which
+ *  itself is not UTF-8 but recognized by XML parsers as a Unicode
+ *  code point.
+ * 
  *  @author Kay Kasemir, Kunal Shroff
  */
 @SuppressWarnings("nls")
 public class XMLWriter
 {
+    /** Create the XML header for standalone UTF-8
+     *  @param out    PrintWriter
+     */
+    public static void header(final PrintWriter out)
+    {
+        out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
+    }
+
     /** Add string for indentation to given level to buffer.
      *  @param out    PrintWriter
      *  @param level  Intendation level
@@ -22,14 +46,15 @@ public class XMLWriter
         }
     }
 
-    /** @return Returns text with less-than and ampersands replaced by XML escapes.
+    /** Prints text with certain characters replaced by XML escapes.
+     *  Seems to work for German Umlaut as well as Russian Cyrillic chars.
      *  @param out    PrintWriter
      *  @param text
      */
     public static final void escapeXMLstring(final PrintWriter out, final String text)
     {
         int i;
-        for (i=0; i<text.length(); ++i)
+        for (i = 0; i < text.length(); ++i)
         {
             char c = text.charAt(i);
             // Escape '&' into '&amp;'.
@@ -39,19 +64,19 @@ public class XMLWriter
             else if (c == '<')
                 out.append("&lt;");
             // Escape '>' into '&gt;'.
-	    else if (c == '>')
-		out.append("&gt;");
-	    // Escape '"' into '&quot;'.
-	    else if (c == '"')
-		out.append("&quot;");
-	    // Escape ''' into '&#039;'.
-	    else if (c == '\'')
-		out.append("&#039;");
+            else if (c == '>')
+                out.append("&gt;");
+            // Escape '"' into '&quot;'.
+            else if (c == '"')
+                out.append("&quot;");
+            // Escape ''' into '&#039;'.
+            else if (c == '\'')
+                out.append("&#039;");
             else if (c < 32 || c > 126)
             {   // Other non-printable. Exact definition not clear.
                 out.append("&#");
                 out.append(Integer.toString((int) c));
-                out.append(";");            
+                out.append(";");
             }
             else
                 out.append(c);
@@ -80,7 +105,6 @@ public class XMLWriter
         out.append("</").append(tag).append(">");
     }
 
-    
     /** Add XML for a one-line tagged value to buffer. Includes newline.
      *  @param out    PrintWriter
      *  @param level  Indentation level
@@ -88,7 +112,7 @@ public class XMLWriter
      *  @param value  Value to place in tag
      */
     public static final void XML(final PrintWriter out, final int level,
-    		final String tag, final Object value)
+            final String tag, final Object value)
     {
         start(out, level, tag);
         escapeXMLstring(out, value.toString());
