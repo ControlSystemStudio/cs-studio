a99b9783d2d18b4fcc723627ab92e549910a5f61
hammonds <hammonds>
2010-07-23 16:00:19 +0000

    Add translation for byte widget

--- applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Byte2Model.java
+++ applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/Byte2Model.java
@@ -2,20 +2,29 @@ package org.csstudio.opibuilder.adl2boy.translator;
 
 import org.csstudio.opibuilder.model.AbstractContainerModel;
 import org.csstudio.opibuilder.model.AbstractWidgetModel;
+import org.csstudio.opibuilder.widgets.model.ByteMonitorModel;
 import org.csstudio.utility.adlparser.fileParser.ADLWidget;
+import org.csstudio.utility.adlparser.fileParser.widgets.ByteMonitor;
+import org.csstudio.utility.adlparser.fileParser.widgets.Meter;
 import org.eclipse.swt.graphics.RGB;
 
 public class Byte2Model extends AbstractADL2Model {
+	ByteMonitorModel byteModel = new ByteMonitorModel();
 
 	public Byte2Model(ADLWidget adlWidget, RGB[] colorMap, AbstractContainerModel parentModel) {
 		super(adlWidget, colorMap, parentModel);
-		// TODO Auto-generated constructor stub
+		parentModel.addChild(byteModel, true);
+		ByteMonitor byteWidget = new ByteMonitor(adlWidget);
+		if (byteWidget != null) {
+			setADLObjectProps(byteWidget, byteModel);
+			setADLMonitorProps(byteWidget, byteModel);
+		}
+
 	}
 
 	@Override
 	public AbstractWidgetModel getWidgetModel() {
-		// TODO Auto-generated method stub
-		return null;
+		return byteModel;
 	}
 
 }
