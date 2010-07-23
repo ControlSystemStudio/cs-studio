a61c12e50511dc9c601ffa11a617e51ac832fd45
hammonds <hammonds>
2010-07-23 15:49:26 +0000

    Add code to implement ChoiceButton Conversion.

--- applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/ChoiceButton2Model.java
+++ applications/plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/translator/ChoiceButton2Model.java
@@ -2,20 +2,48 @@ package org.csstudio.opibuilder.adl2boy.translator;
 
 import org.csstudio.opibuilder.model.AbstractContainerModel;
 import org.csstudio.opibuilder.model.AbstractWidgetModel;
+import org.csstudio.opibuilder.widgets.model.ActionButtonModel;
+import org.csstudio.opibuilder.widgets.model.ChoiceButtonModel;
 import org.csstudio.utility.adlparser.fileParser.ADLWidget;
+import org.csstudio.utility.adlparser.fileParser.widgets.ChoiceButton;
 import org.eclipse.swt.graphics.RGB;
 
 public class ChoiceButton2Model extends AbstractADL2Model {
+	ChoiceButtonModel choiceModel = new ChoiceButtonModel();
 
 	public ChoiceButton2Model(ADLWidget adlWidget, RGB[] colorMap, AbstractContainerModel parentModel) {
 		super(adlWidget, colorMap, parentModel);
-		// TODO Auto-generated constructor stub
+		parentModel.addChild(choiceModel, true);
+
+		ChoiceButton choiceWidget = new ChoiceButton(adlWidget);
+		if (choiceWidget != null) {
+			setADLObjectProps(choiceWidget, choiceModel);
+			setADLControlProps(choiceWidget, choiceModel);
+		}
+		if (choiceWidget.getStacking() != null) {
+			if (choiceWidget.getStacking().equals("column")){
+				choiceModel.setPropertyValue(ChoiceButtonModel.PROP_HORIZONTAL, true);
+			}
+			else if (choiceWidget.getStacking().equals("row")){
+				choiceModel.setPropertyValue(ChoiceButtonModel.PROP_HORIZONTAL, false);
+			}
+		}
+		String color_mode = choiceWidget.getColor_mode();
+		if ( color_mode.equals("static") ){
+			choiceModel.setPropertyValue(ChoiceButtonModel.PROP_FORECOLOR_ALARMSENSITIVE, false);
+		}
+		else if (color_mode.equals("alarm") ){
+			choiceModel.setPropertyValue(ChoiceButtonModel.PROP_FORECOLOR_ALARMSENSITIVE, true);
+		}
+		else if (color_mode.equals("discrete") ){
+			choiceModel.setPropertyValue(ChoiceButtonModel.PROP_FORECOLOR_ALARMSENSITIVE, false);
+			//TODO Menu2Model Figure out what to do if colorMode is discrete
+		}
 	}
 
 	@Override
 	public AbstractWidgetModel getWidgetModel() {
-		// TODO Auto-generated method stub
-		return null;
+		return choiceModel;
 	}
 
 }
