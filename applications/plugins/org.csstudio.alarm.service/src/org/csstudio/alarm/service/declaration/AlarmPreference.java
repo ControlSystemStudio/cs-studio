4935d567f64bee96a85f851df9b494db35d131d1
claus <claus>
2010-07-28 12:06:43 +0000

    Reduce Chunk wait from 200 to 100

--- applications/plugins/org.csstudio.alarm.service/src/org/csstudio/alarm/service/declaration/AlarmPreference.java
+++ applications/plugins/org.csstudio.alarm.service/src/org/csstudio/alarm/service/declaration/AlarmPreference.java
@@ -72,7 +72,7 @@ public final class AlarmPreference<T> extends AbstractPreference<T> {
         new AlarmPreference<Integer>("pvChunkSize", 100);
 
     public static final AlarmPreference<Integer> ALARMSERVICE_PV_CHUNK_WAIT_MSEC =
-        new AlarmPreference<Integer>("pvChunkWaitMsec", 200);
+        new AlarmPreference<Integer>("pvChunkWaitMsec", 100);	// 200 -> 100
 
     public static final AlarmPreference<Integer> ALARMSERVICE_PV_REGISTER_WAIT_MSEC =
         new AlarmPreference<Integer>("pvRegisterWaitMsec", 2000);
