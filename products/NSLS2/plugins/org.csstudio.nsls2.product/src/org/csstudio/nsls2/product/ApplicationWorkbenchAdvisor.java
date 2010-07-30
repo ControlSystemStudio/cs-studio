8b62972f9a346479808be6e3321d49e77b602767
Kunal Shroff <shroffk@bnl.gov>
2010-07-30 15:44:12 -0400

    modified .hgignore Added nsls2 product, features and plugins

---
+++ products/NSLS2/plugins/org.csstudio.nsls2.product/src/org/csstudio/nsls2/product/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,27 @@
+package org.csstudio.nsls2.product;
+
+import org.csstudio.nsls2.product.CSS_Perspective;
+import org.eclipse.ui.application.IWorkbenchConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+//	private static final String PERSPECTIVE_ID = "org.csstudio.nsls2.product.perspective";
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+    
+    @Override
+    public void initialize(IWorkbenchConfigurer configurer)
+    {
+        // Per default, state is not preserved (RCP book 5.1.1)
+        configurer.setSaveAndRestore(true);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return CSS_Perspective.ID;
+	}
+}
