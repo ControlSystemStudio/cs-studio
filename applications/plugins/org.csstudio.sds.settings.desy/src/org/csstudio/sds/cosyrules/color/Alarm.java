52d74827c9bb8556306b4c3cebc65c347681e0d7
Helge Rickens <helge.rickens@desy.de>
2010-09-17 14:28:16 +0200

    o.s.sds.settings.desy: the rule can now mulit channel inputs. Return the highest alarm color

--- applications/plugins/org.csstudio.sds.settings.desy/src/org/csstudio/sds/cosyrules/color/Alarm.java
+++ applications/plugins/org.csstudio.sds.settings.desy/src/org/csstudio/sds/cosyrules/color/Alarm.java
@@ -21,7 +21,6 @@
  */
 package org.csstudio.sds.cosyrules.color;
 
-import org.csstudio.sds.model.BorderStyleEnum;
 import org.csstudio.sds.model.IRule;
 import org.csstudio.sds.util.ColorAndFontUtil;
 import org.epics.css.dal.DynamicValueCondition;
@@ -29,132 +28,165 @@ import org.epics.css.dal.DynamicValueState;
 
 /**
  * Color rule Alarm, translated from an ADL file.
- *
+ * 
  * @author jbercic, jhatje
- *
+ * 
  */
 public final class Alarm implements IRule {
-    /**
-     * The ID for this rule.
-     */
-    public static final String TYPE_ID = "cosyrules.color.alarm";
+	/**
+	 * The ID for this rule.
+	 */
+	public static final String TYPE_ID = "cosyrules.color.alarm";
 
-    /**
-     * Standard constructor.
-     */
-    public Alarm() {
-        // Nothing to do.
-    }
+	/**
+	 * The rule can not calculate the State color!<br>
+	 * Purple
+	 */
+	public static final String UNKNOW = "#8000FF";
+	/**
+	 *  Red.
+	 */
+	public static final String ALARM = ColorAndFontUtil.toHex(253, 0, 0);
+	/**
+	 * Green.
+	 */
+	public static final String NORMAL = ColorAndFontUtil.toHex(0, 216, 0);
+	/**
+	 * Yellow.
+	 */
+	public static final String WARNING = ColorAndFontUtil.toHex(251, 243, 74);
+	/**
+	 * Black.
+	 */
+	public static final String ERROR = ColorAndFontUtil.toHex(255, 255, 255);
+	
+	/**
+	 * Standard constructor.
+	 */
+	public Alarm() {
+		// Nothing to do.
+	}
 
-    /**
-     * Map the severity of a pv value to a color. For DAL severities
-     * (pv_name[severity]) the parameter 'arguments' is of type
-     * DynamicValueState.ID. Using the EPICS field (record.SEVR) the severity is
-     * a number.
-     *
-     * {@inheritDoc}
-     */
-    public Object evaluate(final Object[] arguments) {
-        DynamicValueState dvc = null;
-        // Wrong State violet
-        String color = "#8000FF"; 
-        if ( (arguments != null) && (arguments.length > 0)) {
+	/**
+	 * Map the severity of a pv value to a color. For DAL severities
+	 * (pv_name[severity]) the parameter 'arguments' is of type
+	 * DynamicValueState.ID. Using the EPICS field (record.SEVR) the severity is
+	 * a number.
+	 * 
+	 * {@inheritDoc}
+	 */
+	public Object evaluate(final Object[] arguments) {
+		DynamicValueState dvc = null;
+		// Wrong State violet
+		String color = UNKNOW;
+		if ((arguments != null) && (arguments.length > 0)) {
+			for (int i = 0; i < arguments.length; i++) {
+				DynamicValueState dvcTemp = null;
+				if (arguments[i] instanceof Double) {
+					dvcTemp = getDynamicValueCondition((Double) arguments[i]);
+				} else if (arguments[i] instanceof Long) {
+					dvcTemp = getDynamicValueCondition((Long) arguments[i]);
+				} else if (arguments[i] instanceof String) {
+					dvcTemp = getDynamicValueCondition((String) arguments[i]);
+				} else if (arguments[i] instanceof DynamicValueCondition) {
+					dvcTemp = getDynamicValueCondition((DynamicValueCondition) arguments[i]);
+				}
+				if(dvc==null|| (dvcTemp!=null && dvc.ordinal()<dvcTemp.ordinal())){
+					dvc = dvcTemp;
+				}
+			}
 
-            if (arguments[0] instanceof Double) {
-                dvc = getDynamicValueCondition((Double) arguments[0]);
-            } else if (arguments[0] instanceof Long) {
-                dvc = getDynamicValueCondition((Long) arguments[0]);
-            } else if (arguments[0] instanceof String) {
-                dvc = getDynamicValueCondition((String) arguments[0]);
-            } else if (arguments[0] instanceof DynamicValueCondition) {
-                dvc = getDynamicValueCondition((DynamicValueCondition) arguments[0]);
-            }
+			if (dvc != null) {
+				switch (dvc) {
+				case ALARM:
+					// RED
+					color = ALARM;
+					break;
+				case NORMAL:
+					// Green
+					color = ColorAndFontUtil.toHex(0, 216, 0);
+					break;
+				case WARNING:
+					// Yellow
+					return ColorAndFontUtil.toHex(251, 243, 74);
+				case TIMELAG:
+				case LINK_NOT_AVAILABLE:
+				case TIMEOUT:
+				case ERROR:
+					// white
+					color = ColorAndFontUtil.toHex(255, 255, 255);
+				}
+			}
+		}
 
-            if (dvc != null) {
-                switch (dvc) {
-                    case ALARM:
-                        // RED
-                    	color = ColorAndFontUtil.toHex(253, 0, 0);
-                    	break;
-                    case NORMAL:
-                        // Green
-                    	color =  ColorAndFontUtil.toHex(0, 216, 0);
-                    	break;
-                    case WARNING:
-                        // Yellow
-                        return ColorAndFontUtil.toHex(251, 243, 74);
-                    case TIMELAG:
-                    case LINK_NOT_AVAILABLE:
-                    case TIMEOUT:
-                    case ERROR:
-                        // white
-                    	color = ColorAndFontUtil.toHex(255, 255, 255);
-                }
-            }
-        }
-        System.out.println("Border color: "+color+"\r\n");
-        return color;
-    }
+		return color;
+	}
 
-    /**
-     * @param dynamicValueCondition
-     * @return
-     */
-    private DynamicValueState getDynamicValueCondition(final DynamicValueCondition dynamicValueCondition) {
-        if(dynamicValueCondition.containsStates(DynamicValueState.ALARM)) {
-            return DynamicValueState.ALARM;
-        } else if(dynamicValueCondition.containsStates(DynamicValueState.WARNING)) {
-            return DynamicValueState.WARNING;
-        } else if(dynamicValueCondition.containsStates(DynamicValueState.NORMAL)) {
-            return DynamicValueState.NORMAL;
-        }
-        return DynamicValueState.ERROR;
-    }
+	/**
+	 * @param dynamicValueCondition
+	 * @return
+	 */
+	private DynamicValueState getDynamicValueCondition(
+			final DynamicValueCondition dynamicValueCondition) {
+		if (dynamicValueCondition.containsStates(DynamicValueState.ALARM)) {
+			return DynamicValueState.ALARM;
+		} else if (dynamicValueCondition
+				.containsStates(DynamicValueState.WARNING)) {
+			return DynamicValueState.WARNING;
+		} else if (dynamicValueCondition
+				.containsStates(DynamicValueState.NORMAL)) {
+			return DynamicValueState.NORMAL;
+		}
+		return DynamicValueState.ERROR;
+	}
 
-    /**
-     * @param string
-     * @return
-     */
-    private DynamicValueState getDynamicValueCondition(final String alarmState) {
-        if (alarmState.equals("NO_ALARM") ||alarmState.equals(DynamicValueState.NORMAL.toString())) {
-            return DynamicValueState.NORMAL;
-        } else  if (alarmState.equals("MINOR") || alarmState.equals(DynamicValueState.WARNING.toString())) {
-            return DynamicValueState.WARNING;
-        } else  if (alarmState.equals("MAJOR") || alarmState.equals(DynamicValueState.ALARM.toString())) {
-            return DynamicValueState.ALARM;
-        }
-        return DynamicValueState.ERROR;
-    }
+	/**
+	 * @param string
+	 * @return
+	 */
+	private DynamicValueState getDynamicValueCondition(final String alarmState) {
+		if (alarmState.equals("NO_ALARM")
+				|| alarmState.equals(DynamicValueState.NORMAL.toString())) {
+			return DynamicValueState.NORMAL;
+		} else if (alarmState.equals("MINOR")
+				|| alarmState.equals(DynamicValueState.WARNING.toString())) {
+			return DynamicValueState.WARNING;
+		} else if (alarmState.equals("MAJOR")
+				|| alarmState.equals(DynamicValueState.ALARM.toString())) {
+			return DynamicValueState.ALARM;
+		}
+		return DynamicValueState.ERROR;
+	}
 
-    /**
-     * @param alarmState
-     * @return
-     */
-    private DynamicValueState getDynamicValueCondition(final Long alarmState) {
-        return getDynamicValueCondition(alarmState.doubleValue());
-    }
+	/**
+	 * @param alarmState
+	 * @return
+	 */
+	private DynamicValueState getDynamicValueCondition(final Long alarmState) {
+		return getDynamicValueCondition(alarmState.doubleValue());
+	}
 
-    /**
-     * @param double1
-     * @return
-     */
-    private DynamicValueState getDynamicValueCondition(final Double alarmState) {
-        if ( (Math.abs(alarmState - 0.0) < 0.00001)) {
-            return DynamicValueState.NORMAL;
-        } else  if ( (Math.abs(alarmState - 1.0) < 0.00001)) {
-            return DynamicValueState.WARNING;
-        } else  if ( (Math.abs(alarmState - 2.0) < 0.00001)) {
-            return DynamicValueState.ALARM;
-        }
-        return DynamicValueState.ERROR;
-    }
+	/**
+	 * @param double1
+	 * @return
+	 */
+	private DynamicValueState getDynamicValueCondition(final Double alarmState) {
+		if ((Math.abs(alarmState - 0.0) < 0.00001)) {
+			return DynamicValueState.NORMAL;
+		} else if ((Math.abs(alarmState - 1.0) < 0.00001)) {
+			return DynamicValueState.WARNING;
+		} else if ((Math.abs(alarmState - 2.0) < 0.00001)) {
+			return DynamicValueState.ALARM;
+		}
+		return DynamicValueState.ERROR;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getDescription() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
