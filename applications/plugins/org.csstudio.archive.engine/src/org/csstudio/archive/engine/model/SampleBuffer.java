4f1f6488f0c6e5a45252f5d55d6a3e34c0776462
Kay Kasemir <kasemirk@ornl.gov>
2010-10-28 11:45:03 -0400

    o.c.archive.engine: Test tweaks, file headers

--- applications/plugins/org.csstudio.archive.engine/src/org/csstudio/archive/engine/model/SampleBuffer.java
+++ applications/plugins/org.csstudio.archive.engine/src/org/csstudio/archive/engine/model/SampleBuffer.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package org.csstudio.archive.engine.model;
 
 import java.util.concurrent.ArrayBlockingQueue;
@@ -25,6 +32,8 @@ public class SampleBuffer
     /** Max. number of samples in buffer. */
     final private int capacity;
     
+    // TODO Use LinkedList: Faster, but have to add synchronization
+    // TODO Add SampleBufferUnitTest
     /** The actual samples in a thread-save queue. */
     final private ArrayBlockingQueue<IValue> samples;
     
