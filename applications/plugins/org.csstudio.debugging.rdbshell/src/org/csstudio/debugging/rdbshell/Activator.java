0f3f52e252bea68c2c4905c401bf73d31391bf6c
Kay Kasemir <kasemirk@ornl.gov>
2010-11-12 16:41:37 -0500

    o.c.debugging.rdbshell: Preferences

---
+++ applications/plugins/org.csstudio.debugging.rdbshell/src/org/csstudio/debugging/rdbshell/Activator.java
@@ -0,0 +1,13 @@
+package org.csstudio.debugging.rdbshell;
+
+/** Plugin activator
+ *  
+ *  At this time, not really. Only used to keep the plugin ID
+ *  @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class Activator
+{
+	/** Plugin ID registered in MANIFEST.MF */
+    final public static String ID = "org.csstudio.debugging.rdbshell";
+}
