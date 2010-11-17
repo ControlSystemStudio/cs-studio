36b7a9ae78694bc134ac15cd0d673b0e2ccf6607
jphammonds <devnull@localhost>
2010-11-17 17:00:55 -0600

    o.c.opibuilder.adl2boy: Empty Test for RelatedDisplay2Model

---
+++ applications/plugins/org.csstudio.opibuilder.adl2boy/test/org/csstudio/opibuilder/adl2boy/translator/RelatedDisplay2ModelTest.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.csstudio.opibuilder.adl2boy.translator;
+
+import junit.framework.TestCase;
+
+/**
+ * @author hammonds
+ *
+ */
+public class RelatedDisplay2ModelTest extends TestCase {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	protected void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	protected void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.csstudio.opibuilder.adl2boy.translator.RelatedDisplay2Model#getWidgetModel()}.
+	 */
+	public void testGetWidgetModel() {
+	}
+
+	/**
+	 * Test method for {@link org.csstudio.opibuilder.adl2boy.translator.RelatedDisplay2Model#RelatedDisplay2Model(org.csstudio.utility.adlparser.fileParser.ADLWidget, org.eclipse.swt.graphics.RGB[], org.csstudio.opibuilder.model.AbstractContainerModel)}.
+	 */
+	public void testRelatedDisplay2Model() {
+	}
+
+}
