df3720d332c38eba3242c62242e0e99db89c2eb4
jphammonds <devnull@localhost>
2010-12-13 18:43:13 -0600

    o.c.opibuilder.adl2boy:  Cleanup.  Reformat.

--- applications/plugins/org.csstudio.opibuilder.adl2boy/test/org/csstudio/opibuilder/adl2boy/translator/AbstractADL2ModelUiPluginTest.java
+++ applications/plugins/org.csstudio.opibuilder.adl2boy/test/org/csstudio/opibuilder/adl2boy/translator/AbstractADL2ModelUiPluginTest.java
@@ -5,20 +5,21 @@
 /*************************************************************************/
 package org.csstudio.opibuilder.adl2boy.translator;
 
+import junit.framework.TestCase;
+
+import org.csstudio.opibuilder.OPIBuilderPlugin;
 import org.csstudio.opibuilder.model.AbstractWidgetModel;
 import org.csstudio.opibuilder.model.DisplayModel;
+import org.csstudio.opibuilder.preferences.PreferencesHelper;
 import org.csstudio.opibuilder.util.OPIColor;
 import org.csstudio.opibuilder.widgets.model.PolyLineModel;
 import org.csstudio.utility.adlparser.fileParser.ADLWidget;
 import org.csstudio.utility.adlparser.fileParser.widgetParts.ADLTestObjects;
 import org.csstudio.utility.adlparser.fileParser.widgets.ADLDisplay;
-import org.csstudio.utility.adlparser.fileParser.widgets.Line;
 import org.csstudio.utility.adlparser.fileParser.widgets.Oval;
-import org.csstudio.utility.adlparser.fileParser.widgets.PolyLine;
 import org.eclipse.swt.graphics.RGB;
 import org.junit.After;
 import org.junit.Before;
-import junit.framework.TestCase;
 
 /**
  * @author hammonds
@@ -27,6 +28,7 @@ import junit.framework.TestCase;
 public class AbstractADL2ModelUiPluginTest extends TestCase {
 	TestDisp2Model dispModel;
 	TestWidget2Model testWidgetModel;
+
 	protected class TestDisp2Model extends AbstractADL2Model {
 
 		public TestDisp2Model(RGB[] colorMap) {
@@ -42,8 +44,10 @@ public class AbstractADL2ModelUiPluginTest extends TestCase {
 
 		public void processWidget(ADLWidget adlWidget) {
 			ADLDisplay adlDisp = new ADLDisplay(adlWidget);
+
 		}
 	}
+
 	protected class TestWidget2Model extends AbstractADL2Model {
 
 		public TestWidget2Model(RGB[] colorMap) {
@@ -68,12 +72,10 @@ public class AbstractADL2ModelUiPluginTest extends TestCase {
 	 */
 	@Before
 	public void setUp() throws Exception {
-		dispModel = new TestDisp2Model(
-				ADLTestObjects.makeColorMap());
+		dispModel = new TestDisp2Model(ADLTestObjects.makeColorMap());
 		dispModel.processWidget(ADLTestObjects.setupBasicDisplay());
-		testWidgetModel = new TestWidget2Model(
-				ADLTestObjects.makeColorMap());
-		DisplayModel d = (DisplayModel)(dispModel.getWidgetModel());
+		testWidgetModel = new TestWidget2Model(ADLTestObjects.makeColorMap());
+		DisplayModel d = (DisplayModel) (dispModel.getWidgetModel());
 		d.addChild(testWidgetModel.getWidgetModel());
 	}
 
@@ -84,9 +86,11 @@ public class AbstractADL2ModelUiPluginTest extends TestCase {
 	public void tearDown() throws Exception {
 	}
 
-	public void testADL2ModelColors(){
+	public void testADL2ModelColors() {
 		testWidgetModel.processWidget(ADLTestObjects.setupBasicLine());
-		OPIColor frgd = (OPIColor)testWidgetModel.getWidgetModel().getPropertyValue(AbstractWidgetModel.PROP_COLOR_FOREGROUND);
-		assertEquals ("Foreground Color", ADLTestObjects.makeColorMap()[2], frgd.getRGBValue());
+		OPIColor frgd = (OPIColor) testWidgetModel.getWidgetModel()
+				.getPropertyValue(AbstractWidgetModel.PROP_COLOR_FOREGROUND);
+		assertEquals("Foreground Color", ADLTestObjects.makeColorMap()[2],
+				frgd.getRGBValue());
 	}
 }
