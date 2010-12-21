929afc0c91daae7602413b74b38d7d8bead7558b
Jan Hatje <jan.hatje@desy.de>
2010-12-21 08:15:05 +0100

    o.c.archivereader.aapi: add aapi sources and test

--- applications/plugins/org.csstudio.archivereader.aapi/src/org/csstudio/archivereader/aapi/AapiArchiveReader.java
+++ applications/plugins/org.csstudio.archivereader.aapi/src/org/csstudio/archivereader/aapi/AapiArchiveReader.java
@@ -8,6 +8,9 @@ import org.csstudio.platform.data.ITimestamp;
 import org.csstudio.platform.logging.CentralLogger;
 
 import de.desy.aapi.AapiClient;
+import de.desy.aapi.AapiReductionMethod;
+import de.desy.aapi.AnswerData;
+import de.desy.aapi.RequestData;
 
 /**
  * Access to aapi server.
@@ -20,21 +23,26 @@ import de.desy.aapi.AapiClient;
 public class AapiArchiveReader implements ArchiveReader {
 
 	private String _url;
+	private AapiClient _aapiClient;
+	private String _host;
+	private Integer _port;
 
 	public AapiArchiveReader(String url) throws Exception {
 		_url = url;
+		_aapiClient = createAapiClient(url);
+	}
+
+	private AapiClient createAapiClient(String url) {
 		String[] urlParts = url.split("//");
 		String[] hostAndPort = urlParts[1].split(":");
-		String host = hostAndPort[0];
-		Integer port = null;
+		_host = hostAndPort[0];
+		_port = null;
 		try {
-			port = Integer.parseInt(hostAndPort[1]);
+			_port = Integer.parseInt(hostAndPort[1]);
 		} catch (NumberFormatException e) {
 			CentralLogger.getInstance().getLogger(this).error("invalid url format");
 		}
-		AapiClient _aapiClient = new AapiClient(host, port);
-		String[] channelList = _aapiClient.getChannelList();
-		System.out.println();
+		return new AapiClient(_host, _port);
 	}
 
 	/** 
@@ -42,41 +50,48 @@ public class AapiArchiveReader implements ArchiveReader {
 	 */
 	@Override
 	public String getServerName() {
+		System.out.println();
 		return "Aapi Server";
 	}
 
 	@Override
 	public String getURL() {
+		System.out.println();
 		return _url;
 	}
 
 	@Override
 	public String getDescription() {
+		System.out.println();
+		System.out.println();
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int getVersion() {
+		System.out.println();
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public ArchiveInfo[] getArchiveInfos() {
-		// TODO Auto-generated method stub
-		return null;
+        return new ArchiveInfo[] {
+               new ArchiveInfo("aapi", _host + ":" + Integer.toString(_port), 1)};
 	}
 
 	@Override
 	public String[] getNamesByPattern(int key, String glob_pattern)
 			throws Exception {
+		System.out.println();
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String[] getNamesByRegExp(int key, String reg_exp) throws Exception {
+		System.out.println();
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -84,7 +99,6 @@ public class AapiArchiveReader implements ArchiveReader {
 	@Override
 	public ValueIterator getRawValues(int key, String name, ITimestamp start,
 			ITimestamp end) throws UnknownChannelException, Exception {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -92,20 +106,23 @@ public class AapiArchiveReader implements ArchiveReader {
 	public ValueIterator getOptimizedValues(int key, String name,
 			ITimestamp start, ITimestamp end, int count)
 			throws UnknownChannelException, Exception {
-		// TODO Auto-generated method stub
+		RequestData requestData = new RequestData();
+		requestData.setFromTime((int) start.seconds());
+		requestData.setToTime((int) end.seconds());
+		requestData.setNum(500);
+		requestData.setPV(new String[] {name});
+		requestData.setConversParam(AapiReductionMethod.MIN_MAX_AVERAGE_METHOD.getMethodNumber());
+		AnswerData data = _aapiClient.getData(requestData);
+			System.out.println();
 		return null;
 	}
 
 	@Override
 	public void cancel() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
-
 	}
 
 }
