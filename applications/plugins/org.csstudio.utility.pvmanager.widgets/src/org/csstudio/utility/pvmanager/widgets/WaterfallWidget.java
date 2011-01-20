34d72a360d41e5aab17f2fb10dae824f9fa85ef7
Gabriele Carcassi <carcassi@bnl.gov>
2011-01-20 10:32:05 -0500

    ..pvmanager.widgets: waterfall plot now display the error message.

--- applications/plugins/org.csstudio.utility.pvmanager.widgets/src/org/csstudio/utility/pvmanager/widgets/WaterfallWidget.java
+++ applications/plugins/org.csstudio.utility.pvmanager.widgets/src/org/csstudio/utility/pvmanager/widgets/WaterfallWidget.java
@@ -17,6 +17,11 @@ import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Label;
+import com.swtdesigner.ResourceManager;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.GridData;
 
 /**
  * A widget that connects to an array and display a waterfall plot based on it.
@@ -27,6 +32,8 @@ public class WaterfallWidget extends Composite {
 	
 	private VImageDisplay imageDisplay;
 	private WaterfallPlotParameters parameters = new WaterfallPlotParameters();
+	private CLabel errorLabel;
+	private Label errorImage;
 
 	/**
 	 * Creates a new widget.
@@ -55,13 +62,27 @@ public class WaterfallWidget extends Composite {
 			}
 		});
 		imageDisplay.setStretched(true);
+		GridLayout gl_imageDisplay = new GridLayout(2, false);
+		gl_imageDisplay.marginWidth = 0;
+		gl_imageDisplay.marginHeight = 0;
+		imageDisplay.setLayout(gl_imageDisplay);
 		FormData fd_imageDisplay = new FormData();
 		fd_imageDisplay.bottom = new FormAttachment(100);
 		fd_imageDisplay.right = new FormAttachment(100);
 		fd_imageDisplay.top = new FormAttachment(0);
 		fd_imageDisplay.left = new FormAttachment(0);
 		imageDisplay.setLayoutData(fd_imageDisplay);
-
+		
+		errorImage = new Label(imageDisplay, SWT.NONE);
+		errorImage.setImage(ResourceManager.getPluginImage("org.eclipse.ui", "/icons/full/obj16/warn_tsk.gif"));
+		errorImage.setVisible(false);
+		
+		errorLabel = new CLabel(imageDisplay, SWT.NONE);
+		GridData gd_errorLabel = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		gd_errorLabel.widthHint = 221;
+		errorLabel.setLayoutData(gd_errorLabel);
+		errorLabel.setText("");
+		errorLabel.setVisible(false);
 	}
 	
 	// The pv name for connection
@@ -88,12 +109,31 @@ public class WaterfallWidget extends Composite {
 		reconnect();
 	}
 	
+	// Displays the last error generated
+	private void setLastError(Exception ex) {
+		if (!isDisposed()) {
+			if (ex == null) {
+				errorImage.setVisible(false);
+				errorLabel.setVisible(false);
+			} else {
+				errorImage.setVisible(true);
+				errorLabel.setVisible(true);
+				errorLabel.setToolTipText(ex.getMessage());
+				errorLabel.setText(ex.getMessage());
+			}
+		}
+	}
+	
+	// Reconnects the pv
 	private void reconnect() {
 		// First de-allocate current pv if any
 		if (pv != null) {
 			pv.close();
 			pv = null;
 		}
+		
+		// Clean up old image if present
+		imageDisplay.setVImage(null);
 		
 		if (pvName != null) {
 			pv = PVManager.read(waterfallPlotOf(vDoubleArray(pvName), parameters))
@@ -102,6 +142,7 @@ public class WaterfallWidget extends Composite {
 				
 				@Override
 				public void pvValueChanged() {
+					setLastError(pv.lastException());
 					imageDisplay.setVImage(pv.getValue());
 				}
 			});
