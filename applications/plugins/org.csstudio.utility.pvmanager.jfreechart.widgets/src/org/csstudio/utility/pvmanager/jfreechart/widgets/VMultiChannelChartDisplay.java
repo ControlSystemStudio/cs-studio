ff339e04bdcd169a85431e74c091ca96f35ebf35
Kunal Shroff <shroffk@bnl.gov>
2011-02-04 10:41:14 -0500

    ...pvmanager.jfreechart.widgets: choose plotting criteria, add support for positions.

--- applications/plugins/org.csstudio.utility.pvmanager.jfreechart.widgets/src/org/csstudio/utility/pvmanager/jfreechart/widgets/VMultiChannelChartDisplay.java
+++ applications/plugins/org.csstudio.utility.pvmanager.jfreechart.widgets/src/org/csstudio/utility/pvmanager/jfreechart/widgets/VMultiChannelChartDisplay.java
@@ -3,18 +3,35 @@
  */
 package org.csstudio.utility.pvmanager.jfreechart.widgets;
 
+import java.util.ArrayList;
+import java.util.EventListener;
 import java.util.List;
-import java.util.Random;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.epics.pvmanager.data.VDouble;
 import org.epics.pvmanager.data.VMultiDouble;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartMouseEvent;
+import org.jfree.chart.ChartMouseListener;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.SymbolAxis;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -24,93 +41,158 @@ import org.jfree.experimental.chart.swt.ChartComposite;
  * @author shroffk
  * 
  */
-public class VMultiChannelChartDisplay extends Composite {
+public class VMultiChannelChartDisplay extends Canvas {
 
+	public static final int DOMAIN_AXIS_TYPE_AUTO = 0;
+	public static final int DOMAIN_AXIS_TYPE_POSITION = 1;
 	private ChartComposite chartDisplay;
 	private JFreeChart chart;
 	private XYSeriesCollection dataset;
 
-	private List<String> pvNames;
+	private List<String> channelNames;
 	private List<Double> positions;
-	Random generator;
+	private int domainAxisType;
+
+	/**
+	 * 
+	 * @param parent
+	 * @param style
+	 */
+	public VMultiChannelChartDisplay(Composite parent, int style) {
+		this(parent, style, 0);
+	}
 
 	/**
 	 * Creates a new display for data of type VMultiDouble
 	 * 
 	 * @param parent
 	 * @param style
+	 * @param domainAxisType
 	 */
-	public VMultiChannelChartDisplay(Composite parent, int style) {
+	public VMultiChannelChartDisplay(Composite parent, int style,
+			int domainAxisType) {
 		super(parent, style);
+		this.domainAxisType = domainAxisType;
 		setLayout(new FormLayout());
-		chartDisplay = new ChartComposite(this, SWT.BORDER, null, true);
+		chartDisplay = new ChartComposite(this, SWT.NONE, null, false, false,
+				false, false, true);
 		FormData fd_chartDisplay = new FormData();
 		fd_chartDisplay.bottom = new FormAttachment(100);
 		fd_chartDisplay.right = new FormAttachment(100);
 		fd_chartDisplay.top = new FormAttachment(0);
 		fd_chartDisplay.left = new FormAttachment(0);
 		chartDisplay.setLayoutData(fd_chartDisplay);
-
 	}
 
 	public void setPVData(List<VMultiDouble> pvdata) {
-		if (pvdata == null || pvdata.isEmpty()) {
-			createNewChart();
-		} else {
-			chartDisplay.setRedraw(false);
-			dataset.removeAllSeries();
-			for (VMultiDouble vMultiDouble : pvdata) {
-				XYSeries series = new XYSeries(vMultiDouble.toString(), false,
-						false);
-				double index = 0;
-				long startTime = System.nanoTime();
-				for (VDouble value : vMultiDouble.getValues()) {
-					if (value != null)
-						series.add(index, (double) value.getValue(), false);
-					index++;
+		if (pvdata != null) {
+			if (chartDisplay.getChart() == null) {
+				dataset = new XYSeriesCollection();
+				chart = ChartFactory.createXYLineChart("Title", "X Axis",
+						"Y Axis", dataset, PlotOrientation.VERTICAL, true,
+						true, true);
+				chartDisplay.setChart(chart);
+				chart.getXYPlot().setDomainCrosshairVisible(false);
+				chart.getXYPlot().setDomainGridlinesVisible(false);
+				chart.getXYPlot().setRangeCrosshairVisible(false);
+				chart.getXYPlot().setRangeGridlinesVisible(false);
+				// customizing the axis greatly reduces performance
+//				ValueAxis axis;
+//				if (domainAxisType == DOMAIN_AXIS_TYPE_POSITION){
+//					axis = chart.getXYPlot().getDomainAxis();
+//					axis.setLabel("Channel Position.");
+//					axis.setVerticalTickLabels(true);
+//				} else {
+//					axis = new SymbolAxis("Channel Names",
+//							this.channelNames.toArray(new String[channelNames.size()]));
+//					axis.setVerticalTickLabels(true);
+//					chart.getXYPlot().setDomainAxis(axis);
+//				}
+				
+			} else {
+				chartDisplay.setRedraw(false);
+				dataset.removeAllSeries();
+				for (VMultiDouble vMultiDouble : pvdata) {
+					// long startTime = System.nanoTime();
+					XYSeries series = createXYSeries("seriesName", vMultiDouble);
+					// System.out.println("dataset created in "
+					// + (System.nanoTime() - startTime) + " ns.");
+					dataset.addSeries(series);
 				}
-				System.out.println("dataset created in "
-						+ (System.nanoTime() - startTime) + " ns.");
-				dataset.addSeries(series);
+				chartDisplay.setRedraw(true);
+				chartDisplay.redraw();
 			}
-			chartDisplay.setRedraw(true);
-			chartDisplay.redraw();
-		}
-	}
-
-	private void createNewChart() {
-		generator = new Random();
-		dataset = new XYSeriesCollection();
-		XYSeries series = new XYSeries("PV Group1", false, true);
-		dataset.addSeries(series);
-		chart = ChartFactory.createXYLineChart("Title", "X Axis", "Y Axis",
-				dataset, PlotOrientation.VERTICAL, true, true, true);
-		chartDisplay.setChart(chart);
-	}
-
-	/**
-	 * list which is a union of all the pvNames
-	 * 
-	 * @param pvNames
-	 */
-	public void setNames(List<String> pvNames) {
-		if (pvNames != null) {
-			this.pvNames = pvNames;
 		} else {
-			throw new IllegalArgumentException("pvNames cannot be null.");
+			throw new IllegalArgumentException("pvdata cannot be null");
 		}
 	}
 
+	private XYSeries createXYSeries(String string, VMultiDouble vMultiDouble) {
+		XYSeries series = new XYSeries(vMultiDouble.toString(), false, false);
+		switch (this.domainAxisType) {
+		case DOMAIN_AXIS_TYPE_POSITION:
+			// A strong one-to-one relation is expected between the list
+			// of positions and the pv values
+			if (this.positions == null
+					|| vMultiDouble.getValues().size() != this.positions.size()) {
+				this.domainAxisType = DOMAIN_AXIS_TYPE_AUTO;
+			} else if (this.positions.size() == vMultiDouble.getValues().size()) {
+				List<VDouble> values = vMultiDouble.getValues();
+				for (int index = 0; index < values.size(); index++) {
+					series.add((double) this.positions.get(index),
+							(double) values.get(index).getValue());
+				}
+			}
+			break;
+		default:
+			double autoIndex = 0;
+			for (VDouble value : vMultiDouble.getValues()) {
+				if (value != null)
+					series.add(autoIndex, (double) value.getValue(), false);
+				autoIndex++;
+			}
+			break;
+		}
+		return series;
+	}
+
 	/**
-	 * ordered List of all the z positions
+	 * list which is a union of all the channel Names connected to via the
+	 * multidouble pv
+	 * 
+	 * @param channelNames
+	 */
+	public void setNames(List<String> channelNames) {
+		this.channelNames = channelNames;
+	}
+
+	/**
+	 * specify the position for the channels along the domain axis
 	 * 
 	 * @param positions
 	 *            null permitted
 	 */
 	public void setPositions(List<Double> positions) {
-		if (positions != null && positions.size() == this.pvNames.size()) {
-			this.positions = positions;
-		}
+		this.positions = positions;
+	}
 
+	public void setPlotUsing(int plotUsing) {
+		this.domainAxisType = plotUsing;
+	}
+
+	public void clear() {
+		this.positions = null;
+		this.channelNames = null;
+		this.chartDisplay.setChart(null);
+	}
+
+	public void addListener(EventListener listener) {
+		if (this.chartDisplay != null) {
+			this.chartDisplay.addSWTListener(listener);
+		}
+	}
+
+	public void addChartListener(EventListener listener) {
+		this.chartDisplay.addChartMouseListener((ChartMouseListener) listener);
 	}
 }
--- applications/plugins/org.csstudio.utility.pvmanager.jfreechart.widgets/src/org/csstudio/utility/pvmanager/jfreechart/widgets/XYChartWidget.java
+++ applications/plugins/org.csstudio.utility.pvmanager.jfreechart.widgets/src/org/csstudio/utility/pvmanager/jfreechart/widgets/XYChartWidget.java
@@ -3,6 +3,7 @@
  */
 package org.csstudio.utility.pvmanager.jfreechart.widgets;
 
+import static org.csstudio.utility.pvmanager.jfreechart.widgets.VMultiChannelChartDisplay.DOMAIN_AXIS_TYPE_POSITION;
 import static org.epics.pvmanager.data.ExpressionLanguage.synchronizedArrayOf;
 import static org.epics.pvmanager.data.ExpressionLanguage.vDoubles;
 import static org.epics.pvmanager.util.TimeDuration.ms;
@@ -13,6 +14,9 @@ import java.util.List;
 
 import org.csstudio.utility.pvmanager.ui.SWTUtil;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
@@ -29,31 +33,92 @@ import org.epics.pvmanager.data.VMultiDouble;
 public class XYChartWidget extends Composite {
 
 	private VMultiChannelChartDisplay multiDoubleDisplay;
+	private int plotUsing;
 
 	public XYChartWidget(Composite parent, int style) {
 		super(parent, style);
 		setLayout(new FormLayout());
-		multiDoubleDisplay = new VMultiChannelChartDisplay(this, SWT.NO_BACKGROUND);
+		multiDoubleDisplay = new VMultiChannelChartDisplay(this,
+				SWT.NO_BACKGROUND, DOMAIN_AXIS_TYPE_POSITION);
 		FormData fd_chartDisplay = new FormData();
 		fd_chartDisplay.bottom = new FormAttachment(100);
 		fd_chartDisplay.right = new FormAttachment(100);
 		fd_chartDisplay.top = new FormAttachment(0);
 		fd_chartDisplay.left = new FormAttachment(0, 0);
 		multiDoubleDisplay.setLayoutData(fd_chartDisplay);
+		multiDoubleDisplay.addListener(new MouseListener() {
+
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseDown(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseUp(MouseEvent e) {
+				if (e.button == 3) {
+					SelectChartDomainAxis dialog = new SelectChartDomainAxis(
+							getShell(), SWT.NONE, plotUsing);
+					Point position = new Point(e.x, e.y);
+					position = getDisplay().map(XYChartWidget.this, null,
+							position);
+					int result = dialog.open(position.x, position.y);
+					if (plotUsing != result) {
+						plotUsing = result;
+						reconnect();
+					}
+				}
+			}
+
+		});
+
 	}
 
-	// The pv name for connection
-	private List<String> pvNames;
+	// The channel names used in the multichannel pv connection
+	private List<String> channelNames;
+	// The positions corresponding to the each channel in channelNames
+	private List<Double> channelPositions;
+
 	// The pv created by pvmanager
 	private PV<VMultiDouble> pv;
 
-	public List<String> getPvName() {
-		return pvNames;
+	public List<String> getChannelNames() {
+		return channelNames;
 	}
 
-	public void setPvName(List<String> pvNames) {
-		this.pvNames = pvNames;
-		reconnect();
+	/**
+	 * 
+	 * @param channelNames
+	 *            null not permitted
+	 */
+	public void setChannelNames(List<String> channelNames) {
+		if (channelNames != null) {
+			this.channelNames = channelNames;
+			this.channelPositions = null;
+			reconnect();
+		} else {
+			throw new IllegalArgumentException("ChannelNames can not be null.");
+		}
+	}
+
+	/**
+	 * 
+	 * @param channelPositions
+	 */
+	public void setChannelPositions(List<Double> channelPositions) {
+		if (channelPositions != null
+				&& channelPositions.size() == channelNames.size()) {
+			this.channelPositions = channelPositions;
+			reconnect();
+		} else {
+			throw new IllegalArgumentException();
+		}
 	}
 
 	private void reconnect() {
@@ -64,13 +129,19 @@ public class XYChartWidget extends Composite {
 		}
 
 		// Clean up old chart if present
-		multiDoubleDisplay.setPVData(null);
-		// Create an empty chart
+		// multiDoubleDisplay.setPVData(null);
+		// multiDoubleDisplay.setNames(null);
+		// multiDoubleDisplay.setPositions(null);
+		multiDoubleDisplay.clear();
 
-		if (pvNames != null) {
-			pv = PVManager.read(
-					synchronizedArrayOf(ms(75),
-							vDoubles(Collections.unmodifiableList(pvNames))))
+		if (channelNames != null) {
+			multiDoubleDisplay.setNames(channelNames);
+			multiDoubleDisplay.setPositions(channelPositions);
+			multiDoubleDisplay.setPlotUsing(plotUsing);
+			pv = PVManager
+					.read(synchronizedArrayOf(
+							ms(75),
+							vDoubles(Collections.unmodifiableList(channelNames))))
 					.andNotify(SWTUtil.onSWTThread()).atHz(10);
 			pv.addPVValueChangeListener(new PVValueChangeListener() {
 
