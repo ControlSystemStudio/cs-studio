0e0eacfbb8106954f850643fded521065e4f3b6c
Kunal Shroff <shroffk@bnl.gov>
2011-02-04 10:41:55 -0500

    ..multichannelViewer: update test plot

--- applications/plugins/org.csstudio.multiChannelViewer/src/org/csstudio/multichannelviewer/ChannelsPlot.java
+++ applications/plugins/org.csstudio.multiChannelViewer/src/org/csstudio/multichannelviewer/ChannelsPlot.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import org.csstudio.multichannelviewer.model.CSSChannelGroupPV;
 import org.csstudio.multichannelviewer.model.IChannelGroup;
+import org.csstudio.utility.pvmanager.jfreechart.widgets.XYChartWidget;
 import org.csstudio.utility.pvmanager.ui.SWTUtil;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -36,28 +38,23 @@ import static org.epics.pvmanager.util.TimeDuration.ms;
 import static org.epics.pvmanager.data.ExpressionLanguage.synchronizedArrayOf;
 import static org.epics.pvmanager.data.ExpressionLanguage.vDouble;
 import static org.epics.pvmanager.data.ExpressionLanguage.vDoubles;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
 
 public class ChannelsPlot extends ViewPart {
 
 	private static int instance;
 
 	public static final String ID = "org.csstudio.orbitViewer.channelsPlot";
-
-	// GUI
-	private GridLayout layout;
 	private Button button1;
 	private Button button2;
-	private Button button3;
-
-	private ChartComposite frame;
+	private XYChartWidget chart;
 
 	// Model
 	private IChannelGroup channels;
-	private XYSeriesCollection dataset;
-	private PV<VMultiDouble> pv1;
-	private PV<VMultiDouble> pv2;
-	private PV<VMultiDouble> pv3;
-
+	
 	public ChannelsPlot() {
 	}
 
@@ -69,110 +66,70 @@ public class ChannelsPlot extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		layout = new GridLayout(3, true);
-		parent.setLayout(layout);
+		parent.setLayout(new FormLayout());
 
 		button1 = new Button(parent, SWT.PUSH);
-		button1.setText("AddGroup1");
+		FormData fd_button1 = new FormData();
+		fd_button1.top = new FormAttachment(0, 5);
+		fd_button1.left = new FormAttachment(0, 5);
+		button1.setLayoutData(fd_button1);
+		button1.setText("AddGroup1-ramp");
 		button1.setToolTipText("add channels group");
-		button1.setLayoutData(new GridData());
 		button1.addSelectionListener(new SelectionAdapter() {
 			@Override
-			public void widgetSelected(SelectionEvent e) {
-				if (pv1 != null) {
-					pv1.close();
-					dataset.removeSeries(dataset.getSeries("PV group1"));
-				}
+			public void widgetSelected(SelectionEvent e) {				
 				List<String> pvNames = new ArrayList<String>();
 				for (int i = 0; i < 200; i++) {
 					pvNames.add("sim://ramp(" + i + "," + (i+50) + ", 1, 0.1)");
 				}
-				pv1 = PVManager.read(
-						synchronizedArrayOf(ms(75), vDoubles(Collections
-								.unmodifiableList(pvNames)))).andNotify(SWTUtil.onSWTThread()).atHz(10);
-				final XYSeries series1 = new XYSeries("PV Group1", false, true);
-				dataset.addSeries(series1);
-				pv1.addPVValueChangeListener(new PVValueChangeListener() {
-					@Override
-					public void pvValueChanged() {
-						// get new series.
-						updateXYSeriesForPV(pv1, series1);
-					}
-				});
+				chart.setChannelNames(pvNames);
+				chart.setChannelPositions(generatePositions(pvNames.size(), true));
 			}
 		});
 
 		button2 = new Button(parent, SWT.PUSH);
-		button2.setText("AddGroup2");
+		FormData fd_button2 = new FormData();
+		fd_button2.top = new FormAttachment(0, 5);
+		fd_button2.left = new FormAttachment(0, 116);
+		button2.setLayoutData(fd_button2);
+		button2.setText("AddGroup2-noise");
 		button2.setToolTipText("add channels group");
-		button2.setLayoutData(new GridData());
 		button2.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				if (pv2 != null) {
-					pv2.close();
-					dataset.removeSeries(dataset.getSeries("PV group1"));
-				}
 				List<String> pvNames = new ArrayList<String>();
 				for (int i = 200; i > 0; i--) {
-					pvNames.add("sim://ramp(" + i + "," + (i+50) + ", 1, 0.1)");
+					pvNames.add("sim://noise(0,20,0.1)");
 				}
-				pv2 = PVManager.read(
-						synchronizedArrayOf(ms(75), vDoubles(Collections
-								.unmodifiableList(pvNames)))).andNotify(SWTUtil.onSWTThread()).atHz(10);
-				final XYSeries series2 = new XYSeries("PV Group2", false, true);
-				dataset.addSeries(series2);
-				pv2.addPVValueChangeListener(new PVValueChangeListener() {
-					@Override
-					public void pvValueChanged() {
-						// get new series.
-						updateXYSeriesForPV(pv2, series2);
-					}
-				});
+				chart.setChannelNames(pvNames);
+				chart.setChannelPositions(generatePositions(pvNames.size(), false));
 			}
 		});
+		
+		chart = new XYChartWidget(parent, SWT.NONE);
+		FormData fd_chart = new FormData();
+		fd_chart.bottom = new FormAttachment(100);
+		fd_chart.top = new FormAttachment(button1, 6);
+		fd_chart.left = new FormAttachment(button1, 0, SWT.LEFT);
+		fd_chart.right = new FormAttachment(100);
+		chart.setLayoutData(fd_chart);
+	}
 
-		button3 = new Button(parent, SWT.PUSH);
-		button3.setText("AddGroup3");
-		button3.setToolTipText("add channels group");
-		button3.setLayoutData(new GridData());
-		button3.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				XYSeries series3 = new XYSeries("Series 3", false, true);
-				series3.add(0, 1);
-				series3.add(1, 1);
-				series3.add(2, 1);
-				dataset.addSeries(series3);
+	protected List<Double> generatePositions(int size, boolean exponential) {
+		List<Double> positions = new ArrayList<Double>();
+		if(exponential){			
+			double step = 1;
+			for (double i = 0; i < size; i++) {
+				positions.add(i+step);
+				step=step*2;				
 			}
-		});
-
-		dataset = new XYSeriesCollection();
-
-		try {
-			JFreeChart chart = ChartFactory.createXYLineChart("title",
-					"x Axis", "y Axis", dataset, PlotOrientation.VERTICAL,
-					true, true, true);
-			XYPlot plot = (XYPlot) chart.getPlot();
-//			XYItemRenderer xyItemRenderer = plot.getRenderer();
-			NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
-			rangeAxis.setRange(0, 250);
-			plot.setDomainGridlinesVisible(true);
-			plot.setRangeGridlinesVisible(true);
-			frame = new ChartComposite(parent, SWT.NONE, chart, true);
-			// layout
-			GridData frameGD = new GridData();
-			frameGD.horizontalSpan = 3;
-			frameGD.grabExcessHorizontalSpace = true;
-			frameGD.grabExcessVerticalSpace = true;
-			frameGD.horizontalAlignment = SWT.FILL;
-			frameGD.verticalAlignment = SWT.FILL;
-			frame.setLayoutData(frameGD);
-			frame.pack();
-
-		} catch (Exception e) {
-			e.printStackTrace();
+		}else{
+			Random generator = new Random();
+			for (int i = 0; i < size; i++) {
+				positions.add(generator.nextDouble());
+			}
 		}
+		return positions;
 	}
 
 	@Override
@@ -184,28 +141,10 @@ public class ChannelsPlot extends ViewPart {
 
 	}
 
-	private void updateXYSeriesForPV(PV<VMultiDouble> pv, XYSeries series) {
-		if (pv.getValue() != null) {
-			double index = 0;
-			series.clear();
-			for (VDouble value : pv.getValue().getValues()) {
-				if (value != null)
-					series.add(index, (double)value.getValue(), false);
-				index++;
-			}
-			series.fireSeriesChanged();
-		}
-	}
-
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		if (pv1 != null)
-			pv1.close();
-		if (pv2 != null)
-			pv2.close();
-		if (pv3 != null)
-			pv3.close();
+		chart.dispose();
 		super.dispose();
 	}
 
