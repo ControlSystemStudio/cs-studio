952448a3ac99b76307991a74e6f6a12df5116f02
Kay Kasemir <kasemirk@ornl.gov>
2011-02-11 16:14:31 -0500

    o.c.utility.speech: Workaround for occasional "LINE UNAVAILABLE" errors

--- applications/plugins/org.csstudio.utility.speech/src/org/csstudio/utility/speech/FreeTTSHacks.java
+++ applications/plugins/org.csstudio.utility.speech/src/org/csstudio/utility/speech/FreeTTSHacks.java
@@ -25,12 +25,27 @@ class FreeTTSHacks
         //     -Djava.awt.headless=true
         // or the equivalent in here seems to avoid the hangup
         // (from http://www.digitalsanctuary.com/tech-blog/java/eclipse-startonfirstthread-error-and-fix.html )
-        
+
         // Look for something like "Mac OS X"
         final String os = System.getProperty("os.name");
         if (os.contains("Mac"))
             System.setProperty("java.awt.headless", "true");
-        
+
+        // If audio line is not available, try again after short delay.
+        // Keep the delay short.
+        // If too long, it can miss a short break in the output of
+        // another program where the audio line would have become available
+        System.setProperty("com.sun.speech.freetts.audio.AudioPlayer.openFailDelayMs",
+			"100");
+        // .. and keep trying for a total of 30 seconds
+        System.setProperty("com.sun.speech.freetts.audio.AudioPlayer.totalOpenFailDelayMs",
+        	"30000");
+
+        // In extreme cases, debug info may help
+        // This shows that we use the 'JavaStreamingAudioPlayer'
+        //        System.setProperty("com.sun.speech.freetts.audio.AudioPlayer.debug",
+        //    		"true");
+
         // Setting this property eliminates the need for voices.txt
         System.setProperty("freetts.voices",
                 "com.sun.speech.freetts.en.us.cmu_time_awb.AlanVoiceDirectory," +
