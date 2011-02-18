971296f652b5d9ab70dc88edb762e23df3668ca3
Kay Kasemir <kasemirk@ornl.gov>
2011-02-18 08:25:19 -0500

    o.c.alarm.beast.annunciator: Remove useless startup message

--- applications/plugins/org.csstudio.alarm.beast.annunciator/src/org/csstudio/alarm/beast/annunciator/model/JMSAnnunciator.java
+++ applications/plugins/org.csstudio.alarm.beast.annunciator/src/org/csstudio/alarm/beast/annunciator/model/JMSAnnunciator.java
@@ -129,9 +129,6 @@ public class JMSAnnunciator implements ExceptionListener, MessageListener
         // Initialize the QueueManager.
         queuemanager = new QueueManager(listener, queue, translations, threshold);
         queuemanager.start();
-
-        // Add a startup message to the queue
-        queue.add(Severity.forInfo(), "Annunciator started");
     }
 
     /** {@inhericDoc} */
