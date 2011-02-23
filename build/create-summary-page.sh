3df7aeae13c3106fb670b3f7d1ba9c4f08c5c565
build-irmis-dev <devnull@localhost>
2011-02-23 11:08:23 -0500

    Plugin page: adding build id (which has the timestamp).

--- build/create-summary-page.sh
+++ build/create-summary-page.sh
@@ -61,7 +61,7 @@ echo "</table>"
 
 if [ -n "$BUILD_TAG" ]
 then
-  echo "<p>Built by Jenkins (" $BUILD_TAG ")</p>"
+  echo "<p>Built by Jenkins (" $BUILD_TAG " -  " $BUILD_ID  ")</p>"
 fi
 echo "<p>Built by parsing each plugin's MANIFEST.MF, using properties Bundle-Name, Bundle-Version, Bundle-Vendor, Bundle-Description</p>"
 
