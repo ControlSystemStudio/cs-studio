db359b47b07aa672c1289b91aa8ab2608d82da3e
Kay Kasemir <kasemirk@ornl.gov>
2011-02-28 09:55:25 -0500

    o.c.alarm.beast.annunciator: Use java.util.logging instead of CentralLogger

--- applications/plugins/org.csstudio.alarm.beast.annunciator/src/org/csstudio/alarm/beast/annunciator/Activator.java
+++ applications/plugins/org.csstudio.alarm.beast.annunciator/src/org/csstudio/alarm/beast/annunciator/Activator.java
@@ -7,12 +7,19 @@
  ******************************************************************************/
 package org.csstudio.alarm.beast.annunciator;
 
+import java.util.logging.Logger;
+
 /** Not really an activator, just holds ID
  *  @author Kay Kasemir
  */
 public class Activator
 {
-
 	/** The plug-in ID defined in MANIFEST.MF */
 	public static final String PLUGIN_ID = "org.csstudio.alarm.beast.annunciator"; //$NON-NLS-1$
+
+	/** @return Logger for plugin ID */
+	public static Logger getLogger()
+	{
+	    return Logger.getLogger(PLUGIN_ID);
+	}
 }
