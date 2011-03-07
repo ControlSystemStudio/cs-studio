67a1bd26a325fd7879b9848b6b3b7e55cd2cb2ab
Kay Kasemir <kasemirk@ornl.gov>
2011-03-07 15:23:57 -0500

    org.csstudio.data: Created; imported IValue classes

---
+++ core/plugins/org.csstudio.data/src/org/csstudio/data/values/IMetaData.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.csstudio.data.values;
+
+/** Base interface for a sample's meta data.
+ *  @see IValue
+ *  @see INumericMetaData
+ *  @see IEnumeratedMetaData
+ *  @author Kay Kasemir
+ */
+public interface IMetaData
+{
+    // No content, only defined as base for other *MetaData interfaces
+}
