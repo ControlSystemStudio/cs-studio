799d4d1202496d63325e262609480fc5f47b0464
Gabriele Carcassi <carcassi@bnl.gov>
2011-03-08 15:39:29 -0500

    ...waterfall: using commands and menu contributions to add the waterfall view to the menu.

--- applications/plugins/org.csstudio.display.waterfall/src/org/csstudio/display/waterfall/WaterfallView.java
+++ applications/plugins/org.csstudio.display.waterfall/src/org/csstudio/display/waterfall/WaterfallView.java
@@ -13,23 +13,8 @@ import org.csstudio.apputil.ui.swt.ComboHistoryHelper;
 import org.csstudio.utility.pvmanager.widgets.WaterfallWidget;
 
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
- * <p>
+ * View that allows to create a waterfall plot out of a given PV.
  */
-
 public class WaterfallView extends ViewPart {
 
 	/**
