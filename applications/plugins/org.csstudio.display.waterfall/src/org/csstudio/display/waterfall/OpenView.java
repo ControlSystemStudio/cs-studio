9f861a6d5a85e845c8d533a1b41f2d4680e5a4c8
Gabriele Carcassi <carcassi@bnl.gov>
2011-03-09 16:11:35 -0500

    ...waterfall: registering OpenView command to popup menu.

--- applications/plugins/org.csstudio.display.waterfall/src/org/csstudio/display/waterfall/OpenView.java
+++ applications/plugins/org.csstudio.display.waterfall/src/org/csstudio/display/waterfall/OpenView.java
@@ -1,13 +1,19 @@
 package org.csstudio.display.waterfall;
 
+import java.util.Iterator;
+
+import org.csstudio.platform.model.IProcessVariable;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * Opens the waterfall view.
@@ -20,10 +26,21 @@ public class OpenView extends AbstractHandler implements IHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		try
 	    {
+			ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event)
+			.getActivePage().getSelection();
+			
 	        IWorkbench workbench = PlatformUI.getWorkbench();
 	        IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
 	        IWorkbenchPage page = window.getActivePage();
-	        page.showView(WaterfallView.ID);
+	        WaterfallView waterfall = (WaterfallView) page.showView(WaterfallView.ID);
+			
+			if (selection instanceof IStructuredSelection) {
+				IStructuredSelection strucSelection = (IStructuredSelection) selection;
+				if (!strucSelection.isEmpty()) {
+					IProcessVariable variable = (IProcessVariable) strucSelection.iterator().next();
+					waterfall.setPVName(variable.getName());
+				}
+			}
 	    }
 	    catch (Exception e)
 	    {
