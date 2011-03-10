6737adc1575a5ed3a1df4628c94f6b43bf487aba
Gabriele Carcassi <carcassi@bnl.gov>
2011-03-10 16:55:23 -0500

    ...pvmanager.probe: new probe is initialized with the same pv name. Adding plus to the icon for the new view.

--- applications/plugins/org.csstudio.diag.pvmanager.probe/src/org/csstudio/diag/pvmanager/probe/CreateNewProbe.java
+++ applications/plugins/org.csstudio.diag.pvmanager.probe/src/org/csstudio/diag/pvmanager/probe/CreateNewProbe.java
@@ -1,18 +1,14 @@
 package org.csstudio.diag.pvmanager.probe;
 
 import org.csstudio.diag.pvmanager.probe.views.PVManagerProbe;
-import org.csstudio.platform.model.IProcessVariable;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * Command handler for opening probe on the current selection.
@@ -24,13 +20,21 @@ public class CreateNewProbe extends AbstractHandler implements IHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		try {
-			// Retrieve the selection and the current page
+			// Create a new instance of the multiple view
 			IWorkbench workbench = PlatformUI.getWorkbench();
 			IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
 			IWorkbenchPage page = window.getActivePage();
-			page.showView(PVManagerProbe.MULTIPLE_VIEW_ID,
+			PVManagerProbe newProbe = (PVManagerProbe) page.showView(
+					PVManagerProbe.MULTIPLE_VIEW_ID,
 					PVManagerProbe.createNewInstance(),
 					IWorkbenchPage.VIEW_ACTIVATE);
+			
+			// Look at the pv name and set it to the new view
+			PVManagerProbe oldProbe = (PVManagerProbe) page
+					.showView(PVManagerProbe.SINGLE_VIEW_ID);
+			if (oldProbe.getPVName() != null) {
+				newProbe.setPVName(oldProbe.getPVName());
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
