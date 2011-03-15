96be129226095f4433baab07e6d62599520022df
Markus Moeller <markus.moeller@desy.de>
2011-03-15 08:24:14 +0100

    Bug fixed in classes TailRawHandler and NoFilterHandler

--- applications/plugins/org.csstudio.archive.sdds.server/src/org/csstudio/archive/sdds/server/conversion/handler/TailRawHandler.java
+++ applications/plugins/org.csstudio.archive.sdds.server/src/org/csstudio/archive/sdds/server/conversion/handler/TailRawHandler.java
@@ -61,6 +61,12 @@ public class TailRawHandler extends AlgorithmHandler {
     public Iterable<EpicsRecordData> handle(DataRequestHeader header, EpicsRecordData[] data)
     throws DataException, AlgorithmHandlerException, MethodNotImplementedException {
 
+        if (data == null) {
+            return new ArrayList<EpicsRecordData>(0);
+        } else if (data.length == 0){
+            return new ArrayList<EpicsRecordData>(0);
+        }
+
         long intervalStart = header.getFromSec();
         long intervalEnd = header.getToSec();
         
