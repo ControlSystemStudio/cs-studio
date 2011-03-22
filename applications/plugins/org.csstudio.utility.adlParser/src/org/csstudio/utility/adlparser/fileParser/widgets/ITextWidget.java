9a206a3cd06a46f93542b897640a154ffe94aca3
jphammonds <devnull@localhost>
2011-03-22 09:08:16 -0500

    Add new interface to allow dealing with text issues in a uniform manner.

---
+++ applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/widgets/ITextWidget.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.csstudio.utility.adlparser.fileParser.widgets;
+
+/**
+ * @author hammonds
+ *
+ */
+public interface ITextWidget {
+	abstract String getAlignment();
+	abstract void setAlignment(String alignment);
+	abstract String getFormat();
+	abstract void setFormat(String format);
+}
