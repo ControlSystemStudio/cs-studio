160e62317ebe07f18bc1ff0be2d423bde8ebc26a
jphammonds <devnull@localhost>
2011-03-22 09:08:57 -0500

    Add new interface to deal with common text issues.

--- applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/widgets/TextEntryWidget.java
+++ applications/plugins/org.csstudio.utility.adlParser/src/org/csstudio/utility/adlparser/fileParser/widgets/TextEntryWidget.java
@@ -18,7 +18,7 @@ import org.csstudio.utility.adlparser.internationalization.Messages;
  * @author hammonds
  *
  */
-public class TextEntryWidget extends ADLAbstractWidget {
+public class TextEntryWidget extends ADLAbstractWidget implements ITextWidget {
 	private String color_mode = new String("static");
 	private String format = new String("decimal");
 	private String alignment = new String("horiz. left");
