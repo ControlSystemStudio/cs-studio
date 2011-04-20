26939d4788040e45fc63950a7098e877dc08e2c1
Kunal Shroff <shroffk@bnl.gov>
2011-04-20 10:03:42 -0400

    ..utility.channel.adapters: plugin for providing the adapters from adapting the channel object to other CSS datatypes

---
+++ applications/plugins/org.csstudio.utility.channel.adapters/src/org/csstudio/utility/channel/adapters/ChannelAdapterFactory.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.csstudio.utility.channel.adapters;
+
+import gov.bnl.channelfinder.api.Channel;
+
+import org.csstudio.platform.model.IProcessVariable;
+import org.csstudio.platform.model.IProcessVariableWithArchive;
+import org.csstudio.utility.channel.ICSSChannel;
+import org.csstudio.utility.channel.nsls2.CSSChannelFactory;
+import org.eclipse.core.runtime.IAdapterFactory;
+
+/**
+ * @author shroffk
+ * 
+ */
+public class ChannelAdapterFactory implements IAdapterFactory {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.IAdapterFactory#getAdapter(java.lang.Object,
+	 * java.lang.Class)
+	 */
+	@Override
+	public Object getAdapter(Object adaptableObject, Class adapterType) {
+		Channel channel = ((Channel) adaptableObject);
+		if (adapterType == String.class) {
+			return channel.toString();
+		} else if (adapterType == ICSSChannel.class) {
+			return CSSChannelFactory.getInstance().getCSSChannel(channel);
+		} else if (adapterType == IProcessVariable.class) {
+			return CSSChannelFactory.getInstance().getCSSChannel(channel);
+		} else if (adapterType == IProcessVariableWithArchive.class) {
+			return null;
+		} else {
+			return null;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapterList()
+	 */
+	@Override
+	public Class[] getAdapterList() {
+		return new Class[] { String.class, ICSSChannel.class,
+				IProcessVariable.class, IProcessVariableWithArchive.class };
+	}
+
+}
