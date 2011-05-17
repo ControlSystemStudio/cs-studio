aba094df47e33bbae1833114cdcd3b160fdb0d2a
Gabriele Carcassi <carcassi@bnl.gov>
2011-05-17 14:12:42 -0400

    ..ui.menu.test: removing warnings.

--- core/plugins/org.csstudio.ui.menu.test/src/org/csstudio/ui/menu/test/CustomProcessAdapter.java
+++ core/plugins/org.csstudio.ui.menu.test/src/org/csstudio/ui/menu/test/CustomProcessAdapter.java
@@ -6,13 +6,14 @@ import org.eclipse.core.runtime.IAdapterFactory;
 public class CustomProcessAdapter implements IAdapterFactory {
 
 	@Override
-	public Object getAdapter(Object adaptableObject, Class adapterType) {
+	public Object getAdapter(Object adaptableObject, @SuppressWarnings("rawtypes") Class adapterType) {
 		if (ProcessVariable.class == adapterType) {
 			return new ProcessVariable(((CustomProcessVariable) adaptableObject).getName());
 		}
 		return null;
 	}
 
+	@SuppressWarnings("rawtypes")
 	@Override
 	public Class[] getAdapterList() {
 		return new Class[] {ProcessVariable.class};
