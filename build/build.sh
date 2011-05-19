7ffa579e73c1c2dcf654a9ea1904996f6713bfed
Kunal Shroff <shroffk@bnl.gov>
2011-05-19 14:54:58 -0400

    build.sh: updated to use the delta pack 3.6.2

--- build/build.sh
+++ build/build.sh
@@ -51,12 +51,12 @@ else
     then
     wget http://ftp.osuosl.org/pub/eclipse//technology/epp/downloads/release/helios/SR1/eclipse-rcp-helios-SR1-linux-gtk.tar.gz
     fi
-  if [[ ! -f eclipse-3.6.1-delta-pack.zip ]]
+  if [[ ! -f eclipse-3.6.2-delta-pack.zip ]]
   then
-    wget http://download.eclipse.org/eclipse/downloads/drops/R-3.6.1-201009090800/eclipse-3.6.1-delta-pack.zip
+    wget http://download.eclipse.org/eclipse/downloads/drops/R-3.6.2-201102101200/eclipse-3.6.2-delta-pack.zip
   fi
   tar -xzvf eclipse-rcp-helios-SR1-linux-gtk.tar.gz
-  unzip -o eclipse-3.6.1-delta-pack.zip
+  unzip -o eclipse-3.6.2-delta-pack.zip
   cd ..
 fi
 
