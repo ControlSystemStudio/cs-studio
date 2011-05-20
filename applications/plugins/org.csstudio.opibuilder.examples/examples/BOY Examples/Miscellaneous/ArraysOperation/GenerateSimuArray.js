c47845dfc37887ddffecc59dc91f509dfe1472bd
Xihui Chen <chenx1@ornl.gov>
2011-05-20 12:00:35 -0400

    BOY: update examples

--- applications/plugins/org.csstudio.opibuilder.examples/examples/BOY Examples/Miscellaneous/ArraysOperation/GenerateSimuArray.js
+++ applications/plugins/org.csstudio.opibuilder.examples/examples/BOY Examples/Miscellaneous/ArraysOperation/GenerateSimuArray.js
@@ -1,18 +1,19 @@
 importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
 
-var xArray = DataUtil.createDoubleArray(400);
-var yArray = DataUtil.createDoubleArray(400);
+var xArray = DataUtil.createDoubleArray(100);
+var yArray = DataUtil.createDoubleArray(100);
 
 var xPV = pvArray[0];
 var yPV = pvArray[1];
+var sPVValue = PVUtil.getDouble(pvs[2]);
 
-for(var i=0; i<400; i++){
-	xArray[i] = i*0.25;
+
+for(var i=0; i<100; i++){
+	xArray[i] = i*0.25+sPVValue;
+	yArray[i] = Math.sin(i*0.25+sPVValue);	
 }
 
-for(var i=0; i<400; i++){
-	yArray[i] = Math.random()*2-1;
-}
+
 
 xPV.setValue(xArray);
 yPV.setValue(yArray);
