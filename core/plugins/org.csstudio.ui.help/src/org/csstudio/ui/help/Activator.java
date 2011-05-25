e29d9990885ff37f0e4e1e49eb4e6e7c063836a4
Kay Kasemir <kasemirk@ornl.gov>
2011-05-25 15:36:16 -0400

    o.c.ui.help: Dummy activator, removed warnings from build.properties

--- core/plugins/org.csstudio.ui.help/src/org/csstudio/ui/help/Activator.java
+++ core/plugins/org.csstudio.ui.help/src/org/csstudio/ui/help/Activator.java
@@ -1,36 +1,10 @@
 package org.csstudio.ui.help;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-
 /**This plugin defines the CSS help hierarchy structure.
- * 
- * @author Xihui Chen
- * 
  *
+ * @author Xihui Chen
  */
-public class Activator implements BundleActivator {
-
-	private static BundleContext context;
-
-	static BundleContext getContext() {
-		return context;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-	}
-
+public class Activator
+{
+    // No actual activator needed
 }
