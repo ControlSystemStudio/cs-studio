cf7a179e522d7c853b65942120206f02e4ab76e5
chrzoller <devnull@localhost>
2011-06-06 16:29:44 +0200

    o.c.sds: Made widget moving by keys undo- and redoable.

---
+++ applications/plugins/org.csstudio.sds.ui/src/org/csstudio/sds/ui/internal/commands/AssociableCommand.java
@@ -0,0 +1,21 @@
+package org.csstudio.sds.ui.internal.commands;
+
+import org.csstudio.sds.ui.internal.editor.AssociableCommandListener;
+
+/**
+ * This interface can be implemented by Commands.  
+ * If an AssociableCommandListener is registered at the appropriate CommandStack, 
+ * Associable Commands are combined in Undo and Redo actions when they are associated and 
+ * no not associated Commands were executed in between.
+ * 
+ * @see AssociableCommandListener
+ * 
+ * @author Christian Zoller
+ */
+public interface AssociableCommand {
+    
+    public boolean isAssociated(AssociableCommand associable);
+    
+    public void skipNextStackAction();
+    
+}
