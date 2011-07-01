129c7ddf288ae2fd72856c0397a4d4775baf19ab
Kay Kasemir <kasemirk@ornl.gov>
2011-07-01 13:10:29 +0900

    KEK: Headless Build

--- products/KEK/plugins/org.csstudio.kek.build/make.sh
+++ products/KEK/plugins/org.csstudio.kek.build/make.sh
@@ -17,28 +17,33 @@ $ANT clean
 echo Fetching sources
 $ANT get_sources
 
-echo Building Products
-java -jar $ECLIPSE/plugins/org.eclipse.equinox.launcher_*.jar \
-  -application org.eclipse.ant.core.antRunner \
-  -buildfile $ECLIPSE/plugins/org.eclipse.pde.build_$PDE_VER/scripts/productBuild/productBuild.xml \
-  -Dbuilder=$TOP/products/KEK/plugins/org.csstudio.kek.build/css \
-  -DbuildDirectory=$BUILDDIR \
-  -Dbase=$ECLIPSE_BASE \
-  -Dversion=$VERSION \
-  -Dqualifier=$QUALIFIER \
-  -Ddeltapack=$DELTAPACK 2>&1 | tee css/build.log
+for prod in css archiveengine
+do
+	echo Building $prod Product
+	java -jar $ECLIPSE/plugins/org.eclipse.equinox.launcher_*.jar \
+	  -application org.eclipse.ant.core.antRunner \
+	  -buildfile $ECLIPSE/plugins/org.eclipse.pde.build_$PDE_VER/scripts/productBuild/productBuild.xml \
+	  -Dbuilder=$TOP/products/KEK/plugins/org.csstudio.kek.build/$prod \
+	  -DbuildDirectory=$BUILDDIR \
+	  -Dbase=$ECLIPSE_BASE \
+	  -Dversion=$VERSION \
+	  -Dqualifier=$QUALIFIER \
+	  -Ddeltapack=$DELTAPACK 2>&1 | tee $prod/build.log
+done
 
-echo Building Features
-# Features depend on the CSS product, so they will only compile
-# after the product compiled OK
-java -jar $ECLIPSE/plugins/org.eclipse.equinox.launcher_*.jar \
-  -application org.eclipse.ant.core.antRunner \
-  -buildfile $ECLIPSE/plugins/org.eclipse.pde.build_$PDE_VER/scripts/build.xml \
-  -Dbuilder=$TOP/products/KEK/plugins/org.csstudio.kek.build/optional \
-  -DbuildDirectory=$BUILDDIR \
-  -Dbase=$ECLIPSE_BASE \
-  -Dversion=$VERSION \
-  -Dqualifier=$QUALIFIER \
-  -Ddeltapack=$DELTAPACK 2>&1 | tee optional/build.log
-
+for feature in optional 
+do
+	echo Building $feature Features
+	# Features depend on the CSS product, so they will only compile
+	# after the product compiled OK
+	java -jar $ECLIPSE/plugins/org.eclipse.equinox.launcher_*.jar \
+	  -application org.eclipse.ant.core.antRunner \
+	  -buildfile $ECLIPSE/plugins/org.eclipse.pde.build_$PDE_VER/scripts/build.xml \
+	  -Dbuilder=$TOP/products/KEK/plugins/org.csstudio.kek.build/$feature \
+	  -DbuildDirectory=$BUILDDIR \
+	  -Dbase=$ECLIPSE_BASE \
+	  -Dversion=$VERSION \
+	  -Dqualifier=$QUALIFIER \
+	  -Ddeltapack=$DELTAPACK 2>&1 | tee $feature/build.log
+done
  
