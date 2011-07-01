35e9740efa9a5b577006a2bbd3e99ae25edc49fd
Gabriele Carcassi <carcassi@bnl.gov>
2011-07-01 10:55:54 -0400

    Adding a script to detect plug-ins that have only one file/directory

---
+++ build/find-garbage.sh
@@ -0,0 +1,15 @@
+TEMPFILE="/tmp/css-find-garbage"
+rm -f $TEMPFILE
+find ../core -maxdepth 2 | xargs -I {} bash -c "echo {}; ls {} -1a | wc -l" | grep -x "3" -B1 | grep "/" > $TEMPFILE
+find ../applications -maxdepth 2 | xargs -I {} bash -c "echo {}; ls {} -1a | wc -l" | grep -x "3" -B1 | grep "/" >> $TEMPFILE
+
+BROKENPLUGINS=`wc -l < $TEMPFILE`
+if [[ $BROKENPLUGINS == "0" ]]
+then
+exit 0
+else
+echo Found plug-ins with just one file/directory:
+cat $TEMPFILE
+exit -1
+fi
+
