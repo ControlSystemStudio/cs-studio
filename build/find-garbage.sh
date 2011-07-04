b23496a37f5db665eb9ac7667e6f0830f811bf51
bknerr <bastian.knerr@desy.de>
2011-07-04 11:16:17 +0200

    build: added products dir scanner for broken plugins and check for .project artifacts

--- build/find-garbage.sh
+++ build/find-garbage.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 TEMPFILE="/tmp/css-find-garbage"
 rm -f $TEMPFILE
+
 find ../core -maxdepth 2 | xargs -I {} bash -c "echo {}; ls {} -1a | wc -l" | grep -x "3" -B1 | grep "/" > $TEMPFILE
 find ../applications -maxdepth 2 | xargs -I {} bash -c "echo {}; ls {} -1a | wc -l" | grep -x "3" -B1 | grep "/" >> $TEMPFILE
+find ../products -maxdepth 3 | xargs -I {} bash -c "echo {}; ls {} -1a | wc -l" | grep -x "3" -B1 | grep "/" >> $TEMPFILE
 
 BROKENPLUGINS=`wc -l < $TEMPFILE`
-if [[ $BROKENPLUGINS == "0" ]]
-then
-rm -f $TEMPFILE
-exit 0
-else
-echo Found plug-ins with just one file/directory:
-cat $TEMPFILE
-rm -f $TEMPFILE
-exit -1
+error=0
+if [[ $BROKENPLUGINS != "0" ]]
+  then
+    echo Found plug-ins with just one file/directory:
+	cat $TEMPFILE
+
+	echo "Now scan these dirs for .project artifacts."
+    while read line
+    do	
+      name=$(find $line -maxdepth 1 -type f -name ".project")
+	  if [[ "$name" != "" ]]
+         then 
+		   error=-1
+		   echo ".project artifact found in $line"
+      fi
+    done <$TEMPFILE
 fi
 
+rm -f $TEMPFILE
+exit $error
