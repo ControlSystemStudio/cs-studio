db05733f843616b621caa7729a3aa93de0f43a65
Gabriele Carcassi <carcassi@bnl.gov>
2011-07-12 11:39:47 -0400

    ..display.waterfall: no need to dispose the widget anymore.

--- applications/plugins/org.csstudio.display.waterfall/src/org/csstudio/display/waterfall/WaterfallView.java
+++ applications/plugins/org.csstudio.display.waterfall/src/org/csstudio/display/waterfall/WaterfallView.java
@@ -109,13 +109,5 @@ public class WaterfallView extends ViewPart {
 			setPVName(memento.getString(MEMENTO_PVNAME));
 		}
 	}
-	
-	@Override
-	public void dispose() {
-		if (waterfallComposite != null) {
-			waterfallComposite.dispose();
-			waterfallComposite = null;
-		}
-		super.dispose();
-	}
+
 }
