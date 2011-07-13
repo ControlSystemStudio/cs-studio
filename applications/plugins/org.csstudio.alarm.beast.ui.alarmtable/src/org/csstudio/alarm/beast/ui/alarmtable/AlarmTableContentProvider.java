5f88e924b0aae8334985cd04bd0ddf0ab30f3f7a
Kay Kasemir <kasemirk@ornl.gov>
2011-07-13 16:42:11 +0900

    o.c.alarm.beast.ui.alarmtable: Limit rows in alarm table to (by default) 2500 for each section

--- applications/plugins/org.csstudio.alarm.beast.ui.alarmtable/src/org/csstudio/alarm/beast/ui/alarmtable/AlarmTableContentProvider.java
+++ applications/plugins/org.csstudio.alarm.beast.ui.alarmtable/src/org/csstudio/alarm/beast/ui/alarmtable/AlarmTableContentProvider.java
@@ -15,6 +15,7 @@ import org.csstudio.alarm.beast.ui.alarmtable.AlarmTableLabelProvider.ColumnInfo
 import org.eclipse.jface.viewers.ILazyContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.osgi.util.NLS;
 
 /** Lazy content provider that links model (current alarms) to table viewer.
  *  <p>
@@ -28,25 +29,40 @@ import org.eclipse.jface.viewers.Viewer;
  */
 public class AlarmTableContentProvider implements ILazyContentProvider
 {
+	final private int alarm_table_row_limit = Preferences.getAlarmTableRowLimit();
     private TableViewer table_viewer;
     private AlarmTreePV[] alarms;
     private Comparator<AlarmTreePV> comparator = AlarmComparator.getComparator(ColumnInfo.SEVERITY, false);
-
+	
     /** Update the list of alarms to display.
      *  @param alarms
      */
     public void setAlarms(final AlarmTreePV alarms[])
     {
-        this.alarms = alarms;
         if (alarms == null)
-            table_viewer.setItemCount(0);
-        else
         {
-        	// TODO Limit size to ~5000 alarms, add "...more" entry when capping it?
+        	this.alarms = null;
+            table_viewer.setItemCount(0);
+        }
+        else if (alarms.length > alarm_table_row_limit)
+    	{	// Use only a subset of actual alarms
+    		this.alarms = new AlarmTreePV[alarm_table_row_limit + 1];
+    		System.arraycopy(alarms, 0, this.alarms, 0, alarm_table_row_limit);
+    		// Add explanatory entry to end
+    		final AlarmTreePV info = new AlarmTreePV(null, "More ...", -1);
+    		info.setDescription(NLS.bind("Display limited to {0} alarms", alarm_table_row_limit));
+			this.alarms[alarm_table_row_limit] = info;
+			// Sort all but that explanatory entry
+            Arrays.sort(this.alarms, 0, alarm_table_row_limit, comparator);
+            table_viewer.setItemCount(alarm_table_row_limit + 1);
+    	}
+    	else
+    	{	// Use alarms as received
+            this.alarms = alarms;
             Arrays.sort(this.alarms, comparator);
             table_viewer.setItemCount(alarms.length);
-            table_viewer.refresh();
-        }
+    	}
+        table_viewer.refresh();
     }
 
     /** @return Alarms to be shown in table */
