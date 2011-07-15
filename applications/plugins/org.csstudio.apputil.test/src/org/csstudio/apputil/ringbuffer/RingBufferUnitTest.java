54bacb437c9bf011190e362a0bda8f63ddbe54d2
Kay Kasemir <kasemirk@ornl.gov>
2011-07-15 15:29:22 +0900

    o.c.apputil: Added RingBuffer.toArray

--- applications/plugins/org.csstudio.apputil.test/src/org/csstudio/apputil/ringbuffer/RingBufferUnitTest.java
+++ applications/plugins/org.csstudio.apputil.test/src/org/csstudio/apputil/ringbuffer/RingBufferUnitTest.java
@@ -25,21 +25,26 @@ public class RingBufferUnitTest
         
         // Add/remove one item
         ring.add(1);
+        dump(ring);
         assertFalse(ring.isEmpty());
+
         assertEquals(Integer.valueOf(1), ring.remove());
         assertNull(ring.remove());
         assertTrue(ring.isEmpty());
-
+        dump(ring);
+        
         // Add 4 items
         for (int i=1; i<=4; ++i)
             ring.add(i);
         assertFalse(ring.isFull());
+        dump(ring);
         
         // Fill with 5th element
         ring.add(5);
         assertTrue(ring.isFull());
+        dump(ring);
         
-        // Fill to 10, but ring only remembers the last 5 items
+        // Fill to 9, but ring only remembers the last 5 items
         for (int i=6; i<10; ++i)
             ring.add(i);
         assertTrue(ring.isFull());
@@ -51,6 +56,7 @@ public class RingBufferUnitTest
         // Changing the capacity will preserve those items
         final int cap = ring.getCapacity() * 2;
         ring.setCapacity(cap);
+        dump(ring);
         assertFalse(ring.isFull());
         assertEquals(cap, ring.getCapacity());
         assertEquals(5, ring.size());
@@ -58,6 +64,7 @@ public class RingBufferUnitTest
         assertEquals(Integer.valueOf(9), ring.get(4));
         // .. but now we can add more
         ring.add(10);
+        dump(ring);
         assertEquals(6, ring.size());
         assertEquals(Integer.valueOf(5), ring.get(0));
         assertEquals(Integer.valueOf(10), ring.get(5));
@@ -72,8 +79,9 @@ public class RingBufferUnitTest
 
     private void dump(final RingBuffer<Integer> ring)
     {
-        final int N = ring.size();
-        for (int i=0; i<N; ++i)
-            System.out.println(ring.get(i));
+    	final Integer[] values = ring.toArray(new Integer[0]);
+    	for (Integer i : values)
+            System.out.print(i + " "); //$NON-NLS-1$
+    	System.out.println();
     }
 }
