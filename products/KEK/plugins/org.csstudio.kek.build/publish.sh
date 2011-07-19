309e5afc112f7f00be3cf44d85fd0c20066cdfe6
Kay Kasemir <kasemirk@ornl.gov>
2011-07-19 09:13:47 +0900

    KEK build

--- products/KEK/plugins/org.csstudio.kek.build/publish.sh
+++ products/KEK/plugins/org.csstudio.kek.build/publish.sh
@@ -14,5 +14,5 @@ chmod 664 $CYGDRIVE/$BUILDDIR/I.css_kek_$VERSION/*.zip $CYGDRIVE/$BUILDDIR/css_k
 chmod -R 777 $CYGDRIVE/$BUILDDIR/buildRepo
 
 scp $CYGDRIVE/$BUILDDIR/I.css_kek_$VERSION/*.zip $CYGDRIVE/$BUILDDIR/css_kek_$VERSION-src.zip kasemir@abco4.kek.jp:/mnt/linac-misc/httpd/home/cont/css/apps
-scp -r $CYGDRIVE/$BUILDDIR/buildRepo/* kasemir@abco4.kek.jp:/mnt/linac-misc/httpd/home/cont/css/updates
+scp -r $CYGDRIVE/$BUILDDIR/buildRepo kasemir@abco4.kek.jp:/mnt/linac-misc/httpd/home/cont/css/updates/repo$VERSION
 
