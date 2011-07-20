7936d1928ccd68dba689823e44ed42b5701e29a8
Gabriele Carcassi <carcassi@bnl.gov>
2011-07-20 15:09:20 -0400

    ..multichannelviewer: porting to PVManager 1.0.0 dev.

--- applications/plugins/org.csstudio.multiChannelViewer/src/org/csstudio/multichannelviewer/model/CSSChannelGroupPV.java
+++ applications/plugins/org.csstudio.multiChannelViewer/src/org/csstudio/multichannelviewer/model/CSSChannelGroupPV.java
@@ -1,22 +1,23 @@
 package org.csstudio.multichannelviewer.model;
 
 import org.epics.pvmanager.PV;
-import org.epics.pvmanager.PVValueChangeListener;
+import org.epics.pvmanager.PVReader;
+import org.epics.pvmanager.PVReaderListener;
 import org.epics.pvmanager.data.VMultiDouble;
 
 public class CSSChannelGroupPV {
 
 	// PvManager PV to define the connection with the list of specified pv's
-	private PV<VMultiDouble> pv;
+	private PVReader<VMultiDouble> pv;
 
 	/**
 	 * Add a listener to the PV which listens to events from the PV.
 	 * 
 	 * @param listener
 	 */
-	public void addPvListener(PVValueChangeListener listener) {
+	public void addPvListener(PVReaderListener listener) {
 		if (pv != null)
-			pv.addPVValueChangeListener(listener);
+			pv.addPVReaderListener(listener);
 	}
 
 	/**
@@ -24,7 +25,7 @@ public class CSSChannelGroupPV {
 	 * 
 	 * @param listener
 	 */
-	public void removePvListener(PVValueChangeListener listener) {
-		pv.removePVValueChangeListener(listener);
+	public void removePvListener(PVReaderListener listener) {
+		pv.removePVReaderListener(listener);
 	}
 }
