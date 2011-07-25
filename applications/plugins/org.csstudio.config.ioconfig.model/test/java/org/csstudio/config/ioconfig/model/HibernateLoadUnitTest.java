ccec35f54b843c33b2e365cfc7f15e079b575a14
Helge Rickens <helge.rickens@desy.de>
2011-07-25 11:20:18 +0200

    o.c.config.ioconifg: change loops

--- applications/plugins/org.csstudio.config.ioconfig.model/test/java/org/csstudio/config/ioconfig/model/HibernateLoadUnitTest.java
+++ applications/plugins/org.csstudio.config.ioconfig.model/test/java/org/csstudio/config/ioconfig/model/HibernateLoadUnitTest.java
@@ -27,18 +27,12 @@ package org.csstudio.config.ioconfig.model;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.csstudio.config.ioconfig.model.pbmodel.ChannelStructureDBO;
-import org.csstudio.config.ioconfig.model.pbmodel.MasterDBO;
-import org.csstudio.config.ioconfig.model.pbmodel.ModuleDBO;
-import org.csstudio.config.ioconfig.model.pbmodel.ProfibusSubnetDBO;
-import org.csstudio.config.ioconfig.model.pbmodel.SlaveDBO;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -65,22 +59,13 @@ public class HibernateLoadUnitTest {
     }
 
     
-    @Ignore // TODO (hrickens) : infinite loop ??? does not terminate
+    @SuppressWarnings("rawtypes")
     @Test
     public void loadFromHibernate() throws Exception {
         final List<FacilityDBO> load = Repository.load(FacilityDBO.class);
         assertNotNull(load);
         assertFalse(load.isEmpty());
-
-        final Iterator<FacilityDBO> iterator = load.iterator();
-        while (iterator.hasNext()) {
-            final FacilityDBO next = iterator.next();
-            assertNotNull(next);
-            final Set<DocumentDBO> documents = next.getDocuments();
-            assertNotNull(documents);
-            testIocs(next.getChildren());
-        }
-
+        testChildrens(new HashSet<AbstractNodeDBO>(load));
         List<DocumentDBO> loadDocument = Repository.loadDocument(false);
         assertNotNull(loadDocument);
         assertFalse(loadDocument.isEmpty());
@@ -94,105 +79,14 @@ public class HibernateLoadUnitTest {
 
     }
 
-
-    /**
-     * @param next
-     */
-    private void testIocs(final Set<IocDBO> iocs) {
-        if(!iocs.isEmpty()) {
-            final Iterator<IocDBO> iocIterator = iocs.iterator();
-            while (iocIterator.hasNext()) {
-                final IocDBO iocDBO = iocIterator.next();
-                final Set<DocumentDBO> doc = iocDBO.getDocuments();
-                assertNotNull(doc);
-                testSubnets(iocDBO.getChildren());
-            }
-
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private void testChildrens(final Set<AbstractNodeDBO> nodes) {
+        for (AbstractNodeDBO node : nodes) {
+            final Set<DocumentDBO> doc = node.getDocuments();
+            assertNotNull(doc);
+            testChildrens(node.getChildren());
         }
     }
-
-    /**
-     * @param children
-     */
-    private void testSubnets(final Set<ProfibusSubnetDBO> children) {
-        if(!children.isEmpty()) {
-            final Iterator<ProfibusSubnetDBO> subnetIterator = children.iterator();
-            while (subnetIterator.hasNext()) {
-                final ProfibusSubnetDBO subnetDBO = subnetIterator.next();
-                final Set<DocumentDBO> doc = subnetDBO.getDocuments();
-                assertNotNull(doc);
-                testMaster(subnetDBO.getChildren());
-            }
-
-        }
-    }
-
-
-    /**
-     * @param children
-     */
-    private void testMaster(final Set<MasterDBO> children) {
-        if(!children.isEmpty()) {
-            final Iterator<MasterDBO> masterIterator = children.iterator();
-            while (masterIterator.hasNext()) {
-                final MasterDBO masterDBO = masterIterator.next();
-                final Set<DocumentDBO> doc = masterDBO.getDocuments();
-                assertNotNull(doc);
-                testSlave(masterDBO.getChildren());
-            }
-
-        }
-    }
-
-
-    /**
-     * @param children
-     */
-    private void testSlave(final Set<SlaveDBO> children) {
-        if(!children.isEmpty()) {
-            final Iterator<SlaveDBO> iterator = children.iterator();
-            while (iterator.hasNext()) {
-                final SlaveDBO dbo = iterator.next();
-                final Set<DocumentDBO> doc = dbo.getDocuments();
-                assertNotNull(doc);
-                testModule(dbo.getChildren());
-            }
-        }
-    }
-
-
-    /**
-     * @param children
-     */
-    private void testModule(final Set<ModuleDBO> children) {
-        if(!children.isEmpty()) {
-            final Iterator<ModuleDBO> iterator = children.iterator();
-            while (iterator.hasNext()) {
-                final ModuleDBO dbo = iterator.next();
-                final Set<DocumentDBO> doc = dbo.getDocuments();
-                assertNotNull(doc);
-                testChannelStructure(dbo.getChildren());
-            }
-        }
-
-    }
-
-
-    /**
-     * @param children
-     */
-    private void testChannelStructure(final Set<ChannelStructureDBO> children) {
-        if(!children.isEmpty()) {
-            final Iterator<ChannelStructureDBO> iterator = children.iterator();
-            while (iterator.hasNext()) {
-                final ChannelStructureDBO dbo = iterator.next();
-                final Set<DocumentDBO> doc = dbo.getDocuments();
-                assertNotNull(doc);
-            }
-        }
-
-    }
-
 
     @Test
     public void loadDocumentWithFalseFromHibernate() throws Exception {
@@ -209,4 +103,3 @@ public class HibernateLoadUnitTest {
 
 
 }
-//CHECKSTYLE:ON
