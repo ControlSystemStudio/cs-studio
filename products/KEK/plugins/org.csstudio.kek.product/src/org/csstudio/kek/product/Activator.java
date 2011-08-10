92371950db2ada4e85789702a6a24e1fd3075d92
Kay Kasemir <kasemirk@ornl.gov>
2011-08-10 16:59:14 +0900

    KEK build on Linux

---
+++ products/KEK/plugins/org.csstudio.kek.product/src/org/csstudio/kek/product/Activator.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Oak Ridge National Laboratory.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+
+package org.csstudio.kek.product;
+
+/** Empty.
+ *  Placeholder for the 'src' directory
+ *  which might be needed later
+ *  but gets removed by source control
+ *  when empty.
+ *  @author Kay Kasemir
+ */
+class Activator
+{
+}
