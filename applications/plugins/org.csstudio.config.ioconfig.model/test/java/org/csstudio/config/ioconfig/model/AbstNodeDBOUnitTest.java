a8966d5026f77d96e789d74eb995902089626195
Helge Rickens <helge.rickens@desy.de>
2011-08-22 11:37:44 +0200

    o.c.config.ioconfig.model: change test. dosen't use the DB.

--- applications/plugins/org.csstudio.config.ioconfig.model/test/java/org/csstudio/config/ioconfig/model/AbstNodeDBOUnitTest.java
+++ applications/plugins/org.csstudio.config.ioconfig.model/test/java/org/csstudio/config/ioconfig/model/AbstNodeDBOUnitTest.java
@@ -6,13 +6,18 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.SortedMap;
 
 import org.csstudio.config.ioconfig.model.hibernate.HibernateRepository;
 import org.csstudio.config.ioconfig.model.hibernate.HibernateTestManager;
 import org.csstudio.config.ioconfig.model.hibernate.Repository;
 import org.csstudio.config.ioconfig.model.pbmodel.ProfibusSubnetDBO;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -24,23 +29,37 @@ import org.junit.Test;
  * @since 02.08.2011
  */
 public class AbstNodeDBOUnitTest {
+    
+    
 
     @Test
-    @Ignore("Need a correct TEST DB")
-    public void loadNode() throws PersistenceException {
+    public void sortedChildrenTest() throws PersistenceException {
+        
+        final Short[] sIndex = {7,4,9,};
+        final String[] names = {"7","4","9",};
+        
+        final FacilityDBO out = new FacilityDBO();
 
-        final Collection<FacilityDBO> result = Repository.load(FacilityDBO.class);
-
-        assertFalse(result.isEmpty());
-
-        for(final FacilityDBO f: result) {
-            assertNull(f.getParent());
-            if (8061 == f.getId()) {
-                assertEquals("XMTS", f.getName());
-//                assertEquals("DB Move Test", f.getName());
-                assertEquals(19, f.getChildren().size());
-            }
+        for (int i = 0; i < names.length; i++) {
+            final IocDBO createChild = out.createChild();
+            createChild.setName(names[i]);
+            createChild.setSortIndexNonHibernate(sIndex[i]);
         }
+
+        final SortedMap<Short, IocDBO> childrenAsMap = out.getChildrenAsMap();
+        Assert.assertNotNull(childrenAsMap);
+        Assert.assertEquals(3, childrenAsMap.size());
+        
+        Arrays.sort(sIndex);
+        Arrays.sort(names);
+        final Set<Entry<Short, IocDBO>> entrySet = childrenAsMap.entrySet();
+        int i =0;
+        for (Entry<Short, IocDBO> entry : entrySet) {
+            Assert.assertEquals(sIndex[i],entry.getKey());
+            Assert.assertEquals(names[i],entry.getValue().getName());
+            i++;
+        }
+
 
     }
 
