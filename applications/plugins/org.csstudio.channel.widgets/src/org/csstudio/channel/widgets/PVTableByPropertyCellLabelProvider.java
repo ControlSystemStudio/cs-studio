df4044d71a181ab241f6e2260dd2787ffdcc57f4
Gabriele Carcassi <carcassi@bnl.gov>
2011-08-25 16:46:48 -0400

    ..channel.widgets: PVTableByProperty colors the cell gray if there is no pv for it.

--- applications/plugins/org.csstudio.channel.widgets/src/org/csstudio/channel/widgets/PVTableByPropertyCellLabelProvider.java
+++ applications/plugins/org.csstudio.channel.widgets/src/org/csstudio/channel/widgets/PVTableByPropertyCellLabelProvider.java
@@ -3,12 +3,29 @@ package org.csstudio.channel.widgets;
 import java.util.List;
 
 import org.csstudio.utility.pvmanager.widgets.VTableCellLabelProvider;
+import org.csstudio.utility.pvmanager.widgets.VTableContentProvider;
+import org.eclipse.jface.viewers.ViewerCell;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class PVTableByPropertyCellLabelProvider extends VTableCellLabelProvider {
 	private final List<List<String>> channels;
 
 	public PVTableByPropertyCellLabelProvider(List<List<String>> channels) {
 		this.channels = channels;
+	}
+	
+	@Override
+	public void update(ViewerCell cell) {
+		super.update(cell);
+		if (cell.getColumnIndex() > 0) {
+			String channelName = channels.get(cell.getColumnIndex() - 1).get(((VTableContentProvider.VTableRow) cell.getElement()).getRow());
+			if (channelName == null) {
+				cell.setBackground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
+				cell.setForeground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
+			}
+		}
 	}
 	
 	@Override
