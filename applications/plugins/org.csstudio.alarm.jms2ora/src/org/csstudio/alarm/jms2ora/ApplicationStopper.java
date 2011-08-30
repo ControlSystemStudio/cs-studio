f27f43b20e8438984a6e73060d2a1edae8936fd6
Markus Moeller <markus.moeller@desy.de>
2011-08-30 17:21:17 +0200

    o.c.alarm.jms2ora: Fixed some checkstyle warnings.

--- applications/plugins/org.csstudio.alarm.jms2ora/src/org/csstudio/alarm/jms2ora/ApplicationStopper.java
+++ applications/plugins/org.csstudio.alarm.jms2ora/src/org/csstudio/alarm/jms2ora/ApplicationStopper.java
@@ -1,23 +1,23 @@
 
-/* 
- * Copyright (c) 2009 Stiftung Deutsches Elektronen-Synchrotron, 
+/*
+ * Copyright (c) 2009 Stiftung Deutsches Elektronen-Synchrotron,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY.
  *
- * THIS SOFTWARE IS PROVIDED UNDER THIS LICENSE ON AN "../AS IS" BASIS. 
- * WITHOUT WARRANTY OF ANY KIND, EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED 
- * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR PARTICULAR PURPOSE AND 
- * NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
- * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
- * THE USE OR OTHER DEALINGS IN THE SOFTWARE. SHOULD THE SOFTWARE PROVE DEFECTIVE 
- * IN ANY RESPECT, THE USER ASSUMES THE COST OF ANY NECESSARY SERVICING, REPAIR OR 
- * CORRECTION. THIS DISCLAIMER OF WARRANTY CONSTITUTES AN ESSENTIAL PART OF THIS LICENSE. 
+ * THIS SOFTWARE IS PROVIDED UNDER THIS LICENSE ON AN "../AS IS" BASIS.
+ * WITHOUT WARRANTY OF ANY KIND, EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR PARTICULAR PURPOSE AND
+ * NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE. SHOULD THE SOFTWARE PROVE DEFECTIVE
+ * IN ANY RESPECT, THE USER ASSUMES THE COST OF ANY NECESSARY SERVICING, REPAIR OR
+ * CORRECTION. THIS DISCLAIMER OF WARRANTY CONSTITUTES AN ESSENTIAL PART OF THIS LICENSE.
  * NO USE OF ANY SOFTWARE IS AUTHORIZED HEREUNDER EXCEPT UNDER THIS DISCLAIMER.
- * DESY HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, 
+ * DESY HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS,
  * OR MODIFICATIONS.
- * THE FULL LICENSE SPECIFYING FOR THE SOFTWARE THE REDISTRIBUTION, MODIFICATION, 
- * USAGE AND OTHER RIGHTS AND OBLIGATIONS IS INCLUDED WITH THE DISTRIBUTION OF THIS 
- * PROJECT IN THE FILE LICENSE.HTML. IF THE LICENSE IS NOT INCLUDED YOU MAY FIND A COPY 
+ * THE FULL LICENSE SPECIFYING FOR THE SOFTWARE THE REDISTRIBUTION, MODIFICATION,
+ * USAGE AND OTHER RIGHTS AND OBLIGATIONS IS INCLUDED WITH THE DISTRIBUTION OF THIS
+ * PROJECT IN THE FILE LICENSE.HTML. IF THE LICENSE IS NOT INCLUDED YOU MAY FIND A COPY
  * AT HTTP://WWW.DESY.DE/LEGAL/LICENSE.HTM
  *
  */
@@ -27,6 +27,7 @@ package org.csstudio.alarm.jms2ora;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
+
 import org.csstudio.alarm.jms2ora.preferences.PreferenceConstants;
 import org.csstudio.platform.management.CommandDescription;
 import org.csstudio.platform.management.CommandParameters;
@@ -34,12 +35,12 @@ import org.csstudio.platform.management.CommandResult;
 import org.csstudio.platform.management.IManagementCommandService;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.preferences.IPreferencesService;
+import org.eclipse.ecf.core.identity.ID;
 import org.eclipse.ecf.presence.roster.IRoster;
 import org.eclipse.ecf.presence.roster.IRosterEntry;
 import org.eclipse.ecf.presence.roster.IRosterGroup;
 import org.eclipse.ecf.presence.roster.IRosterItem;
 import org.eclipse.ecf.presence.roster.IRosterManager;
-import org.eclipse.ecf.core.identity.ID;
 import org.remotercp.common.tracker.IGenericServiceListener;
 import org.remotercp.service.connection.session.ISessionService;
 import org.slf4j.Logger;
@@ -50,7 +51,7 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class ApplicationStopper implements IGenericServiceListener<ISessionService> {
-    
+
     /** The class logger */
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationStopper.class);
 
@@ -60,31 +61,31 @@ public class ApplicationStopper implements IGenericServiceListener<ISessionServi
     public ApplicationStopper() {
         xmppSession = null;
     }
-    
+
     /**
-     * 
+     *
      * @param applicationName
      * @param host
      * @param user
      */
-    public boolean stopExternInstance(String applicationName, String host, String user) {
-        
+    public boolean stopExternInstance(final String applicationName, final String host, final String user) {
+
         Vector<IRosterItem> rosterItems = null;
         IRosterGroup jmsApplics = null;
         IRosterEntry currentApplic = null;
         String stopPassword = null;
         boolean success = false;
-        
-        IPreferencesService prefs = Platform.getPreferencesService();
+
+        final IPreferencesService prefs = Platform.getPreferencesService();
         stopPassword = prefs.getString(Jms2OraPlugin.PLUGIN_ID, PreferenceConstants.XMPP_SHUTDOWN_PASSWORD, "", null);
 
         Jms2OraPlugin.getDefault().addSessionServiceListener(this);
-        
-        Object lock = new Object();
+
+        final Object lock = new Object();
         synchronized (lock) {
             try {
                 lock.wait(3000);
-            } catch (InterruptedException ie) {
+            } catch (final InterruptedException ie) {
                 // Can be ignored
             }
         }
@@ -99,85 +100,83 @@ public class ApplicationStopper implements IGenericServiceListener<ISessionServi
         synchronized(lock) {
             try {
                 lock.wait(5000);
-            } catch(InterruptedException ie) {
+            } catch(final InterruptedException ie) {
                 LOG.error("[*** InterruptedException ***]: {}" + ie.getMessage());
             }
         }
-        
+
         if(rosterItems.size() == 0) {
             LOG.info("XMPP roster not found. Leaving application.");
-            
+
             // Allways 'false' here
             return success;
         }
-        
+
         LOG.info("Manager initialized");
         LOG.info("Anzahl Directory-Elemente: {}" + rosterItems.size());
-        
+
         jmsApplics = this.getRosterGroup(rosterItems, "jms-applications");
         currentApplic = this.getRosterApplication(jmsApplics, applicationName, host, user);
-        
+
         if(currentApplic != null) {
             success = this.stopApplication(currentApplic, stopPassword);
         }
-        
+
         if (xmppSession != null) {
             xmppSession.disconnect();
         }
-        
+
         return success;
     }
 
     /**
-     * 
+     *
      * @return
      */
     @SuppressWarnings("unchecked")
     private Vector<IRosterItem> getRosterItems() {
-        
+
         IRoster roster = null;
         Vector<IRosterItem> rosterItems = null;
         int count = 0;
-        
-        IRosterManager rosterManager = xmppSession.getRosterManager();
-        
+
+        final IRosterManager rosterManager = xmppSession.getRosterManager();
+
         // We have to wait until the ECF connection manager have been initialized
         synchronized(this) {
-            
+
             do {
-            
+
                 try {
                     this.wait(2000);
-                } catch(InterruptedException ie) {
+                } catch(final InterruptedException ie) {
                     LOG.error("[*** InterruptedException ***]: {}", ie.getMessage());
                 }
-  
+
                 roster = rosterManager.getRoster();
 
                 // Get the roster items
-                rosterItems = new Vector<IRosterItem>(roster.getItems());
-            
-            } while((++count <= 10) && (rosterItems.isEmpty()));
+                rosterItems = new Vector<IRosterItem> (roster.getItems());
+
+            } while(++count <= 10 && rosterItems.isEmpty());
         }
-        
+
         return rosterItems;
     }
-    
+
     /**
-     * 
+     *
      * @param rosterItems
      * @param groupName
      * @return
      */
-    private IRosterGroup getRosterGroup(Vector<IRosterItem> rosterItems, String groupName)
+    private IRosterGroup getRosterGroup(final Vector<IRosterItem> rosterItems, final String groupName)
     {
         IRosterGroup jmsApplics = null;
 
         // Get the group of JMS applications
-        for(IRosterItem ri : rosterItems)
-        {
-            if(ri.getName().compareToIgnoreCase(groupName) == 0)
-            {
+        for(final IRosterItem ri : rosterItems) {
+            if(ri.getName().compareToIgnoreCase(groupName) == 0) {
                 jmsApplics = (IRosterGroup)ri;
                 break;
             }
@@ -185,9 +184,9 @@ public class ApplicationStopper implements IGenericServiceListener<ISessionServi
 
         return jmsApplics;
     }
-    
+
     /**
-     * 
+     *
      * @param jmsApplics
      * @param applicName
      * @param host
@@ -195,81 +194,78 @@ public class ApplicationStopper implements IGenericServiceListener<ISessionServi
      * @return
      */
     @SuppressWarnings("unchecked")
-    private IRosterEntry getRosterApplication(IRosterGroup jmsApplics, String applicName, String host, String user)
-    {
+    private IRosterEntry getRosterApplication(final IRosterGroup jmsApplics, final String applicName,
+                                              final String host, final String user) {
+
         Vector<IRosterEntry> rosterEntries = null;
         IRosterEntry currentApplic = null;
         String name = null;
-        
-        if(jmsApplics != null)
-        {
+
+        if(jmsApplics != null) {
+
             rosterEntries = new Vector<IRosterEntry>(jmsApplics.getEntries());
-            
-            Iterator<IRosterEntry> list = rosterEntries.iterator();
-            while(list.hasNext())
-            {
-                IRosterEntry ce = list.next();
-                
+
+            final Iterator<IRosterEntry> list = rosterEntries.iterator();
+            while(list.hasNext()) {
+                final IRosterEntry ce = list.next();
                 name = ce.getUser().getID().toExternalForm();
-                if(name.contains(applicName))
-                {
-                    if((name.indexOf(host) > -1) && (name.indexOf(user) > -1))
-                    {
+                if(name.contains(applicName)) {
+                    if(name.indexOf(host) > -1 && name.indexOf(user) > -1) {
                         currentApplic = ce;
                         break;
                     }
                 }
             }
         }
-        
+
         return currentApplic;
     }
-    
+
     /**
-     * 
+     *
      * @param currentApplic
      * @param password
      * @return
      */
-    private boolean stopApplication(IRosterEntry currentApplic, String password) {
-        
+    private boolean stopApplication(final IRosterEntry currentApplic, final String password) {
+
         CommandDescription stopAction = null;
         CommandParameters parameter = null;
         String returnValue = null;
         boolean result = false;
 
         IManagementCommandService service = null;
-        
+
         if(currentApplic != null) {
-            
+
             LOG.info("Anwendung gefunden: {}" + currentApplic.getUser().getID().getName());
 
-            List<IManagementCommandService> managementServices =
+            final List<IManagementCommandService> managementServices =
                 xmppSession.getRemoteServiceProxies(
                     IManagementCommandService.class, new ID[] {currentApplic.getUser().getID()});
 
             if(managementServices.size() == 1) {
-                
+
                 service = managementServices.get(0);
-                CommandDescription[] commands = service.getSupportedCommands();
-                
-                for(int i = 0; i < commands.length; i++) {
-                    if(commands[i].getLabel().compareToIgnoreCase("stop") == 0) {
-                        stopAction = commands[i];
+                final CommandDescription[] commands = service.getSupportedCommands();
+
+                for (final CommandDescription command : commands) {
+                    if(command.getLabel().compareToIgnoreCase("stop") == 0) {
+                        stopAction = command;
                         break;
                     }
                 }
             }
-            
-            if((stopAction != null) && (service != null)) {
-                
+
+            if(stopAction != null && service != null) {
+
                 parameter = new CommandParameters();
                 parameter.set("Password", password);
-                
-                CommandResult retValue = service.execute(stopAction.getIdentifier(), parameter);
+
+                final CommandResult retValue = service.execute(stopAction.getIdentifier(), parameter);
                 if(retValue != null) {
                     returnValue = (String)retValue.getValue();
-                    if((returnValue.trim().startsWith("OK:")) || (returnValue.indexOf("stopping") > -1)) {
+                    if(returnValue.trim().startsWith("OK:") || returnValue.indexOf("stopping") > -1) {
                         result = true;
                         LOG.info("Application stopped: {}", result);
                         // iResult = ApplicResult.RESULT_OK.ordinal();
@@ -282,31 +278,33 @@ public class ApplicationStopper implements IGenericServiceListener<ISessionServi
                     // iResult = ApplicResult.RESULT_ERROR_UNKNOWN.ordinal();
                 }
             }
-        }        
+        }
 
         return result;
     }
 
-    public void bindService(ISessionService sessionService) {
-                
-        IPreferencesService prefs = Platform.getPreferencesService();
-        String xmppUser = prefs.getString(Jms2OraPlugin.PLUGIN_ID,
+    @Override
+    public void bindService(final ISessionService sessionService) {
+
+        final IPreferencesService prefs = Platform.getPreferencesService();
+        final String xmppUser = prefs.getString(Jms2OraPlugin.PLUGIN_ID,
                 PreferenceConstants.XMPP_REMOTE_USER_NAME, "anonymous", null);
-        String xmppPassword = prefs.getString(Jms2OraPlugin.PLUGIN_ID,
+        final String xmppPassword = prefs.getString(Jms2OraPlugin.PLUGIN_ID,
                 PreferenceConstants.XMPP_REMOTE_PASSWORD, "anonymous", null);
-        String xmppServer = prefs.getString(Jms2OraPlugin.PLUGIN_ID,
+        final String xmppServer = prefs.getString(Jms2OraPlugin.PLUGIN_ID,
                 PreferenceConstants.XMPP_SERVER, "krynfs.desy.de", null);
 
         try {
             sessionService.connect(xmppUser, xmppPassword, xmppServer);
             xmppSession = sessionService;
-        } catch (Exception e) {
+        } catch (final Exception e) {
             xmppSession = null;
             LOG.warn("XMPP connection is not available: {}", e.getMessage());
         }
     }
 
-    public void unbindService(ISessionService service) {
+    @Override
+    public void unbindService(final ISessionService service) {
         // Nothing to do here
     }
 }
