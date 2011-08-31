1d63f325b84ae1b2da772d973c42af9108de68a9
Kay Kasemir <kasemirk@ornl.gov>
2011-08-31 16:27:29 -0400

    o.c.opibuilder.examples: Updated Heater to use a rule, PVUtil, Data Browser Widget

--- applications/plugins/org.csstudio.opibuilder.examples/examples/BOY Examples/Heater/scripts/Move.js
+++ applications/plugins/org.csstudio.opibuilder.examples/examples/BOY Examples/Heater/scripts/Move.js
@@ -1,23 +1,12 @@
-importPackage(Packages.org.eclipse.swt.graphics);
-importPackage(Packages.java.lang);
-importPackage(Packages.org.csstudio.platform.ui.util);
-importPackage(Packages.org.csstudio.platform.data);
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
 
 // Simple animation, meant to be used with an input PV
 // like sim://sine(-10,10,10,0.1)
 // that runs -10...10 in 10 steps, updating every 0.1 seconds
-var sine = ValueUtil.getDouble(pvArray[0].getValue());
+var sine = PVUtil.getDouble(pvs[0]);
 
 // Update widget position
 var x = 440 + sine;
 var y = 390 + 0.2*sine;
-var widget = widgetController.getWidgetModel();
 widget.setPropertyValue("x", x);
 widget.setPropertyValue("y", y);
-
-// Update widget color
-if (sine > 0.0)
-	widget.setPropertyValue("foreground_color", CustomMediaFactory.COLOR_CYAN);
-else
-	widget.setPropertyValue("foreground_color", CustomMediaFactory.COLOR_BLUE);
-	
