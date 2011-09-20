3d5338e3c00d209064e2ea7bd1667b64a19914bf
bknerr <bastian.knerr@desy.de>
2011-09-20 07:59:31 +0200

    o.c.sds.components.ui: renamed files that are not tests, but demos or whatever

--- applications/plugins/org.csstudio.sds.components.ui/test/org/csstudio/sds/components/ui/internal/figures/WidgetFigureTestDemo.java
+++ applications/plugins/org.csstudio.sds.components.ui/test/org/csstudio/sds/components/ui/internal/figures/WidgetFigureTestDemo.java
@@ -19,51 +19,53 @@ import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 
 /**
- * To avoid to start CSS frequently to see the 
+ * To avoid to start CSS frequently to see the
  * drawing result of the widget figure, this program simply draw the widget figure on a shell.
- * <p> 
- * This is a common java program, <b>not</b> a JUnit test. 
+ * <p>
+ * This is a common java program, <b>not</b> a JUnit test.
  * </p>
  * @author Xihui Chen
  *
  */
-public class WidgetFigureTest {
-	public static void main(String[] args) {
+public class WidgetFigureTestDemo {
+	public static void main(final String[] args) {
 		final Shell shell = new Shell();
 		final LightweightSystem lws = new LightweightSystem(shell);
-		final Figure parent = new Figure();		
+		final Figure parent = new Figure();
 		parent.setLayoutManager(new XYLayout());
 		lws.setContents(parent);
-		 
+
 		shell.addListener(SWT.Paint, new Listener() {
-			public void handleEvent(Event event) {
+			@Override
+            public void handleEvent(final Event event) {
 				parent.removeAll();
-				Rectangle testFigureBounds = new Rectangle(0,
+				final Rectangle testFigureBounds = new Rectangle(0,
 						0, shell.getBounds().width, shell.getBounds().height);
-				Rectangle scaleBounds = new Rectangle(10,
+				final Rectangle scaleBounds = new Rectangle(10,
 						10, shell.getBounds().width-50, shell.getBounds().height-50);
-				
-				KnobTest testFigure = new KnobTest(scaleBounds);
-				//BulbTest testFigure = new BulbTest(scaleBounds);
-				//XMeterTest testFigure = new XMeterTest(scaleBounds);				
-				//GaugeTest testFigure = new GaugeTest(scaleBounds);
-				//AnyTest testFigure = new AnyTest(scaleBounds);				
-				//RoundScaleTest testFigure = new RoundScaleTest(scaleBounds);
-				//XSliderFigureTest testFigure = new XSliderFigureTest(scaleBounds);
-				//TankFigureTest testFigure = new TankFigureTest(scaleBounds);
-				//ThermoFigureTest testFigure = new ThermoFigureTest(scaleBounds);
+
+				final KnobTestDemo testFigure = new KnobTestDemo(scaleBounds);
+				//BulbTest testFigure = new BulbTestDemo(scaleBounds);
+				//XMeterTest testFigure = new XMeterTestDemo(scaleBounds);
+				//GaugeTest testFigure = new GaugeTestDemo(scaleBounds);
+				//AnyTest testFigure = new AnyTestDemo(scaleBounds);
+				//RoundScaleTest testFigure = new RoundScaleTestDemo(scaleBounds);
+				//XSliderFigureTest testFigure = new XSliderFigureTestDemo(scaleBounds);
+				//TankFigureTest testFigure = new TankFigureTestDemo(scaleBounds);
+				//ThermoFigureTest testFigure = new ThermoFigureTestDemo(scaleBounds);
 				parent.add(testFigure,testFigureBounds);
 				lws.paint(event.gc);
-				
+
 			}
 		});
 		shell.setSize(500, 500);
 	    shell.open();
 	    shell.setText("Widget Figure Test");
-	    Display display = Display.getDefault();
+	    final Display display = Display.getDefault();
 	    while (!shell.isDisposed()) {
-	      if (!display.readAndDispatch())
-	        display.sleep();
+	      if (!display.readAndDispatch()) {
+            display.sleep();
+        }
 	    }
 	}
 }
@@ -71,14 +73,14 @@ public class WidgetFigureTest {
 
 
 
-class KnobTest extends Figure {
-	
-	public KnobTest(Rectangle bounds) {
+class KnobTestDemo extends Figure {
 
-		KnobFigure knob = new KnobFigure();	
+	public KnobTestDemo(final Rectangle bounds) {
+
+		final KnobFigure knob = new KnobFigure();
 		//knob.setBulbColor(CustomMediaFactory.COLOR_GREEN);
 		knob.setValue(10);
-		knob.setBounds(bounds);		
+		knob.setBounds(bounds);
 		//knob.setBackgroundColor(CustomMediaFactory.getInstance().getColor(
 		//		new RGB(127,127,127)));
 		//		new RGB(142,180,227)));
@@ -89,23 +91,23 @@ class KnobTest extends Figure {
 		//knob.setLogScale(true);
 		knob.setThumbColor(CustomMediaFactory.getInstance().getColor(new RGB(127, 127, 127)));
 		knob.setForegroundColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_BLACK));
-		add(knob);		
+		add(knob);
 	}
 }
 
 
-class BulbTest extends Figure {
-	
-	public BulbTest(Rectangle bounds) {
+class BulbTestDemo extends Figure {
 
-		Bulb bulb = new Bulb();	
-		bulb.setBounds(bounds);		
+	public BulbTestDemo(final Rectangle bounds) {
+
+		final Bulb bulb = new Bulb();
+		bulb.setBounds(bounds);
 		bulb.setBulbColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_GRAY));
-		add(bulb);		
+		add(bulb);
 	}
-	
+
 	@Override
-	protected void paintFigure(Graphics graphics) {
+	protected void paintFigure(final Graphics graphics) {
 		graphics.setBackgroundColor(CustomMediaFactory.getInstance().getColor(
 				CustomMediaFactory.COLOR_GRAY));
 		//graphics.fillRectangle(bounds);
@@ -115,15 +117,15 @@ class BulbTest extends Figure {
 
 
 
-class XMeterTest extends Figure {
-	
-	public XMeterTest(Rectangle bounds) {
+class XMeterTestDemo extends Figure {
 
-		RefreshableXMeterFigure xMeter = new RefreshableXMeterFigure();	
+	public XMeterTestDemo(final Rectangle bounds) {
+
+		final RefreshableXMeterFigure xMeter = new RefreshableXMeterFigure();
 		xMeter.setNeedleColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_RED));
 		xMeter.setValue(65);
 		//xMeter.setNeedleColor(CustomMediaFactory.COLOR_BLUE);
-		xMeter.setBounds(bounds);		
+		xMeter.setBounds(bounds);
 		xMeter.setBackgroundColor(CustomMediaFactory.getInstance().getColor(
 				new RGB(255,255,255)));
 		//		new RGB(142,180,227)));
@@ -131,21 +133,21 @@ class XMeterTest extends Figure {
 		//xMeter.setEffect3D(false);
 		//xMeter.setGradient(false);
 		//xMeter.setForegroundColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_WHITE));
-		add(xMeter);		
+		add(xMeter);
 	}
 }
 
 
 
 
-class GaugeTest extends Figure {
-	
-	public GaugeTest(Rectangle bounds) {
+class GaugeTestDemo extends Figure {
 
-		RefreshableGaugeFigure gauge = new RefreshableGaugeFigure();	
+	public GaugeTestDemo(final Rectangle bounds) {
+
+		final RefreshableGaugeFigure gauge = new RefreshableGaugeFigure();
 		gauge.setNeedleColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_RED));
 		gauge.setValue(34.28);
-		gauge.setBounds(bounds);		
+		gauge.setBounds(bounds);
 		gauge.setBackgroundColor(CustomMediaFactory.getInstance().getColor(
 				new RGB(127,127,127)));
 		//		new RGB(142,180,227)));
@@ -153,42 +155,42 @@ class GaugeTest extends Figure {
 		///gauge.setEffect3D(false);
 		gauge.setGradient(false);
 		gauge.setForegroundColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_WHITE));
-		add(gauge);		
+		add(gauge);
 	}
 }
 
 
 
 
-class AnyTest extends Figure {
-	
-	public AnyTest(Rectangle bounds) {		
-		ArcTest arc = new ArcTest();
+class AnyTestDemo extends Figure {
+
+	public AnyTestDemo(final Rectangle bounds) {
+		final ArcTestDemo arc = new ArcTestDemo();
 		arc.setBounds(bounds);
 		add(arc);
-	}	
+	}
 }
 
-class ArcTest extends Figure {
-	
-	public ArcTest() {		
+class ArcTestDemo extends Figure {
+
+	public ArcTestDemo() {
 	}
-	
-	
+
+
 	@Override
-	protected void paintFigure(Graphics graphics) {
-		Color yellow = new Color(Display.getCurrent(), 255,255,0);
-		Color red = new Color(Display.getCurrent(), 255,0,0);
+	protected void paintFigure(final Graphics graphics) {
+		final Color yellow = new Color(Display.getCurrent(), 255,255,0);
+		final Color red = new Color(Display.getCurrent(), 255,0,0);
 
 		super.paintFigure(graphics);
 		graphics.fillArc(bounds, 0, 20);
 		graphics.setAntialias(SWT.ON);
 	    graphics.setLineWidth(20);
 	    //graphics.drawRectangle(bounds);
-	    Pattern pattern = new Pattern(Display.getCurrent(), bounds.x, bounds.y, 
+	    final Pattern pattern = new Pattern(Display.getCurrent(), bounds.x, bounds.y,
 	    		bounds.x + bounds.width, bounds.y + bounds.height, yellow, red);
 	    graphics.setForegroundPattern(pattern);
-	    graphics.drawArc(new Rectangle(bounds.x +10, bounds.y+10, 
+	    graphics.drawArc(new Rectangle(bounds.x +10, bounds.y+10,
 	    		bounds.width -20, bounds.height -20), 315, 27);
 	    graphics.setForegroundColor(new Color(Display.getCurrent(), 255, 0, 0));
 	    graphics.setLineWidth(1);
@@ -196,18 +198,18 @@ class ArcTest extends Figure {
 	   // graphics.drawArc(bounds, 180, 90);
 	}
 	@Override
-	protected void paintClientArea(Graphics graphics) {
+	protected void paintClientArea(final Graphics graphics) {
 		super.paintClientArea(graphics);
-		
+
 	}
 }
 
 
-class RoundScaleTest extends Figure {
-	
-	public RoundScaleTest(Rectangle bounds) {
+class RoundScaleTestDemo extends Figure {
 
-		RoundScale scale = new RoundScale();		
+	public RoundScaleTestDemo(final Rectangle bounds) {
+
+		final RoundScale scale = new RoundScale();
 		//scale.setStartAngle(135);
 		//scale.setEndAngle(45);
 		//scale.setRange(new Range(0, 12*3600000d+ 5 * 3600000d));
@@ -221,10 +223,10 @@ class RoundScaleTest extends Figure {
 		//scale.setStartAngle(90);
 		//scale.setEndAngle(90.00000001);
 		//scale.setScaleLineVisible(false);
-					
-		
-		RoundScaledRamp ramp = new RoundScaledRamp(scale);				
-		Rectangle rect = new Rectangle(bounds);
+
+
+		final RoundScaledRamp ramp = new RoundScaledRamp(scale);
+		final Rectangle rect = new Rectangle(bounds);
 		//rect.shrink(100, 100);
 		ramp.setBounds(rect);
 		//ramp.setThresholdVisibility(Threshold.LO, false);
@@ -234,77 +236,77 @@ class RoundScaleTest extends Figure {
 		//ramp.setThresholdValue(Threshold.LO, 100);
 		//ramp.setThresholdValue(Threshold.LOLO, 50);
 		ramp.setGradient(false);
-		add(ramp);	
+		add(ramp);
 		//scale.setVisible(false);
 		add(scale);
-		
+
 	}
 }
 
 
 
-class XSliderFigureTest extends Figure {
-	
-	public XSliderFigureTest(Rectangle bounds) {
+class XSliderFigureTestDemo extends Figure {
 
-		ScaledSliderFigure slider = new ScaledSliderFigure();		
+	public XSliderFigureTestDemo(final Rectangle bounds) {
+
+		final ScaledSliderFigure slider = new ScaledSliderFigure();
 		slider.setBounds(bounds);
 		slider.setBackgroundColor(CustomMediaFactory.getInstance().getColor(
 				CustomMediaFactory.COLOR_WHITE));
-		slider.setValue(50);		
+		slider.setValue(50);
 		slider.setFillColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_RED));
 		slider.setFillBackgroundColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_GRAY));
 		slider.setForegroundColor(CustomMediaFactory.getInstance().getColor(
 				CustomMediaFactory.COLOR_BLACK));
-		slider.setTransparent(true);	
+		slider.setTransparent(true);
 		slider.setHihiLevel(90);
 		slider.setLogScale(true);
 		slider.setEffect3D(true);
 		//slider.setHorizontal(true);
 		add(slider);
-		
+
 	}
 }
 
 
-class TankFigureTest extends Figure {
-	
-	public TankFigureTest(Rectangle bounds) {
+class TankFigureTestDemo extends Figure {
 
-		RefreshableTankFigure tank = new RefreshableTankFigure();		
+	public TankFigureTestDemo(final Rectangle bounds) {
+
+		final RefreshableTankFigure tank = new RefreshableTankFigure();
 		tank.setBounds(bounds);
 		tank.setBackgroundColor(CustomMediaFactory.getInstance().getColor(
 				CustomMediaFactory.COLOR_WHITE));
-		tank.setValue(50);		
+		tank.setValue(50);
 		tank.setFillColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_BLUE));
 		tank.setFillBackgroundColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_RED));
 		tank.setForegroundColor(CustomMediaFactory.getInstance().getColor(
 				CustomMediaFactory.COLOR_BLACK));
-		tank.setTransparent(true);	
+		tank.setTransparent(true);
 		tank.setHihiLevel(90);
 		add(tank);
-		
+
 	}
 
 }
 
-class ThermoFigureTest extends Figure {
-	
-	public ThermoFigureTest(Rectangle bounds) {
+class ThermoFigureTestDemo extends Figure {
 
-		RefreshableThermoFigure thermo = new RefreshableThermoFigure();		
+	public ThermoFigureTestDemo(final Rectangle bounds) {
+
+		final RefreshableThermoFigure thermo = new RefreshableThermoFigure();
 		thermo.setBounds(bounds);
 		thermo.setBackgroundColor(CustomMediaFactory.getInstance().getColor(
 				CustomMediaFactory.COLOR_WHITE));
-		thermo.setValue(28.01);		
+		thermo.setValue(28.01);
 		thermo.setFillColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_RED));
 		thermo.setFillBackgroundColor(CustomMediaFactory.getInstance().getColor(CustomMediaFactory.COLOR_GRAY));
 		thermo.setForegroundColor(CustomMediaFactory.getInstance().getColor(
 				CustomMediaFactory.COLOR_BLACK));
-		thermo.setTransparent(true);	
+		thermo.setTransparent(true);
 		thermo.setHihiLevel(90);
 		add(thermo);
-		
+
 	}
 
 }
