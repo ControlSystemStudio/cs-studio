018fc8e7aebab5775ea20713aa1ab53e1b58b625
Takashi Nakamoto <devnull@localhost>
2011-10-04 12:08:24 +0900

    KEK build: Corrected indentation and added more instructive comments

--- products/KEK/plugins/org.csstudio.kek.build/publish.sh
+++ products/KEK/plugins/org.csstudio.kek.build/publish.sh
@@ -5,7 +5,7 @@
 # Note: May have to remove a previously copied repository on the web server
 # before copying new files on top of existing ones.
 #
-# Runs on either Windows with cygwin installed or Linux
+# Runs on Linux
 #
 # Author:
 #  - Kay Kasemir
@@ -18,13 +18,11 @@ source settings.sh
 
 # Platforms
 PLATFORMS="LINUX_X64 LINUX_X86 WIN_X64 WIN_X86 MACOSX_X64 MACOSX_X86"
-#PLATFORMS="LINUX_X64 MACOSX_X64 WIN_X64"
 
 DATE=$(date +'%Y%m%d_%H%M%S')
 
-# Destionation of index.html and produced zip files.
-#DEST=/mnt/linac-misc/httpd/home/cont/css
-DEST=/usr/users/nakamoto/tmp/css
+# Destination of index.html and produced zip files.
+DEST=/mnt/linac-misc/httpd/home/cont/css
 
 LINUX_X64_NAME="Linux (64 bit)"
 LINUX_X64_PREF="linux.gtk.x86_64"
@@ -44,8 +42,8 @@ MACOSX_X64_PREF="macosx.cocoa.x86_64"
 MACOSX_X86_NAME="Mac OS X (32 bit)"
 MACOSX_X86_PREF="macosx.cocoa.x86"
 
-#MACOSX_PPC_NAME="Mac OS X (PPC)"
-#MACOSX_PPC_PREF="macosx.cocoa.ppc"
+MACOSX_PPC_NAME="Mac OS X (PPC)"
+MACOSX_PPC_PREF="macosx.cocoa.ppc"
 
 # Hack the windows file permissions for cygwin
 chmod 664 $CYGDRIVE/$BUILDDIR/I.css_kek_$VERSION/*.zip $CYGDRIVE/$BUILDDIR/css_kek_$VERSION-src.zip
@@ -57,6 +55,7 @@ PREV_DIR=$(pwd)
 # Copy CSS binaries
 mkdir -p ${DEST}/apps
 ACCS="jparc pfar pf linac superkekb jparc_office pfar_office pf_office linac_office superkekb_office"
+
 for PLATFORM in ${PLATFORMS}; do
     echo "#####################################################"
     echo " Publishing a binary for ${PLATFORM}"
@@ -70,53 +69,61 @@ for PLATFORM in ${PLATFORMS}; do
     OS=$(echo "${PLATFORM}" | cut -f1 -d'_')
     ZIP_BINARY=${CYGDRIVE}/${BUILDDIR}/I.css_kek_${VERSION}/css_kek_${VERSION}-${PREF}.zip
 
-    # Adding launcher scripts to the archive
+    # Adding launcher files to the archive.
     if [ "${OS}" = "LINUX" ]; then
-	cd ${BUILDDIR}
-	
-	cp ${SCRIPTDIR}/css_kek.sh ${BUILDDIR}/css_${VERSION}
-	zip ${ZIP_BINARY} css_${VERSION}/css_kek.sh
+        cd ${BUILDDIR}
+        
+        # Include common scripts required to launch CSS.
+        cp ${SCRIPTDIR}/css_kek.sh ${BUILDDIR}/css_${VERSION}
+        zip ${ZIP_BINARY} css_${VERSION}/css_kek.sh
 
-	cp ${SCRIPTDIR}/acc_settings.sh ${BUILDDIR}/css_${VERSION}
-	zip ${ZIP_BINARY} css_${VERSION}/acc_settings.sh
+        cp ${SCRIPTDIR}/acc_settings.sh ${BUILDDIR}/css_${VERSION}
+        zip ${ZIP_BINARY} css_${VERSION}/acc_settings.sh
 
-	for acc in ${ACCS}; do
-	    echo "#!/bin/sh" > ${BUILDDIR}/css_${VERSION}/css_kek_${acc}
-	    echo "SCRIPTDIR=\$(cd \$(dirname \$0) && pwd)" >> ${BUILDDIR}/css_${VERSION}/css_kek_${acc}
-	    echo "sh \${SCRIPTDIR}/css_kek.sh $acc LINUX" >> ${BUILDDIR}/css_${VERSION}/css_kek_${acc}
+        for acc in ${ACCS}; do
+            # Generate a launcher script and include it into the zip.
+            echo "#!/bin/sh" > ${BUILDDIR}/css_${VERSION}/css_kek_${acc}
+            echo "SCRIPTDIR=\$(cd \$(dirname \$0) && pwd)" >> ${BUILDDIR}/css_${VERSION}/css_kek_${acc}
+            echo "sh \${SCRIPTDIR}/css_kek.sh $acc LINUX" >> ${BUILDDIR}/css_${VERSION}/css_kek_${acc}
 
             chmod a+x css_${VERSION}/css_kek_${acc}
             zip ${ZIP_BINARY} css_${VERSION}/css_kek_${acc}
-	done
+        done
+
     elif [ "${OS}" = "WIN" ]; then
-	cd ${BUILDDIR}
+        cd ${BUILDDIR}
 
-	for acc in ${ACCS}; do
-	    sh ${SCRIPTDIR}/gen_wrapper.sh ${acc} WIN | perl -pe 's/\n/\r\n/' > css_${VERSION}/css_kek_${acc}.bat
-	    chmod a+x css_${VERSION}/css_kek_${acc}.bat
-	    zip ${ZIP_BINARY} css_${VERSION}/css_kek_${acc}.bat
-	done
+        for acc in ${ACCS}; do
+            # Generate a launcher batch file and include it into the zip.
+            sh ${SCRIPTDIR}/gen_wrapper.sh ${acc} WIN | perl -pe 's/\n/\r\n/' > css_${VERSION}/css_kek_${acc}.bat
+            chmod a+x css_${VERSION}/css_kek_${acc}.bat
+            zip ${ZIP_BINARY} css_${VERSION}/css_kek_${acc}.bat
+        done
+
     elif [ "${OS}" = "MACOSX" ]; then
-	cd ${BUILDDIR}
+        cd ${BUILDDIR}
 
-	unzip ${ZIP_BINARY} "css_${VERSION}/css.app/*"
-	for acc in ${ACCS}; do
-	    cp -r css_${VERSION}/css.app css_${VERSION}/css_kek_${acc}.app
-	    sed -i -e "s|<string>css</string>|<string>css_kek_${acc}</string>|" css_${VERSION}/css_kek_${acc}.app/Contents/Info.plist
+        unzip ${ZIP_BINARY} "css_${VERSION}/css.app/*"
+        for acc in ${ACCS}; do
+            # Generate an application bundle based on CSS's and include it
+            # into the zip.
+            cp -r css_${VERSION}/css.app css_${VERSION}/css_kek_${acc}.app
+            sed -i -e "s|<string>css</string>|<string>css_kek_${acc}</string>|" css_${VERSION}/css_kek_${acc}.app/Contents/Info.plist
 
-	    cp ${SCRIPTDIR}/css_kek.sh ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS
-	    cp ${SCRIPTDIR}/acc_settings.sh ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS
+            cp ${SCRIPTDIR}/css_kek.sh ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS
+            cp ${SCRIPTDIR}/acc_settings.sh ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS
 
-	    echo "#!/bin/sh" > ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS/css_kek_${acc}
-	    echo "SCRIPTDIR=\$(cd \$(dirname \$0) && pwd)" >> ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS/css_kek_${acc}
-	    echo "sh \${SCRIPTDIR}/css_kek.sh $acc MACOSX" >> ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS/css_kek_${acc}
+            echo "#!/bin/sh" > ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS/css_kek_${acc}
+            echo "SCRIPTDIR=\$(cd \$(dirname \$0) && pwd)" >> ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS/css_kek_${acc}
+            echo "sh \${SCRIPTDIR}/css_kek.sh $acc MACOSX" >> ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS/css_kek_${acc}
 
-	    chmod a+x ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS/css_kek_${acc}
-	
-	    zip -r ${ZIP_BINARY} css_${VERSION}/css_kek_${acc}.app
-	done
+            chmod a+x ${BUILDDIR}/css_${VERSION}/css_kek_${acc}.app/Contents/MacOS/css_kek_${acc}
+            
+            zip -r ${ZIP_BINARY} css_${VERSION}/css_kek_${acc}.app
+        done
     fi
 
+    # Append date and time to the zip file name and copy it to the destination.
     echo "Copying css_kek_$VERSION-$PREF.zip to ${DEST}/apps/css_kek_${VERSION}-${PREF}_${DATE}.zip"
     cp $CYGDRIVE/$BUILDDIR/I.css_kek_${VERSION}/css_kek_${VERSION}-${PREF}.zip ${DEST}/apps/css_kek_${VERSION}-${PREF}_${DATE}.zip
     DOWNLOAD_LINKS="$DOWNLOAD_LINKS<li><a href=\"apps/css_kek_${VERSION}-${PREF}_${DATE}.zip\">$NAME</a></li>\n"
