5f2e4ea606c1ba8c4d2274ccb552d87220e61477
Takashi Nakamoto <devnull@localhost>
2011-10-04 12:08:55 +0900

    KEK build: Default to Linux settings

--- products/KEK/plugins/org.csstudio.kek.build/settings.sh
+++ products/KEK/plugins/org.csstudio.kek.build/settings.sh
@@ -9,25 +9,35 @@
 export VERSION=3.0.0
 
 # Root of CSS source tree.
-export TOP=/CSS/Repo/cs-studio-3.0
+export TOP=${HOME}/work/cs-studio-3.0
 
-# On Windows with cygwin, this needs to be set.
-export CYGDRIVE=/cygdrive/c
+# Root directory of the drive on cygwin.
+#
+# On Windows with cygwin, this needs to be set to something like /cygdrive/c.
+# On Linux, this needs to be empty.
+export CYGDRIVE=
 
 # Directory where the built CSS and temporary files will be stored.
-export BUILDDIR=/CSS/build
+export BUILDDIR=${HOME}/work/CSSBuild
 
 # Root directory of Eclipse.
-export ECLIPSE_BASE=/CSS/Eclipse/RCP
+export ECLIPSE_BASE=${HOME}/work/eclipse
 
 # Root directory of Delta pack
-export DELTAPACK=$ECLIPSE_BASE/eclipse-3.6.2-delta-pack/eclipse
+export DELTAPACK=${HOME}/work/delta/eclipse
 
 # Root directory of JDK SE 6.
-export JAVA_HOME="c:\Program Files\Java\jdk1.6.0_26"
+#
+# On Windows with cygwin, this needs to be set in the form like
+# "c:\Program Files\Java\jdk1.6.0_26".
+export JAVA_HOME=${HOME}/work/jdk1.6.0_27
+
+###################################################################
+# Following parts should NOT be edited without a particuar reason.
+###################################################################
 
 # Path to the Eclipse executable.
-export ECLIPSE=$ECLIPSE_BASE/eclipse
+export ECLIPSE=${ECLIPSE_BASE}/eclipse
 
 # Could use standalone ant, or the one built into Eclipse
 export ANT="java -jar $ECLIPSE/plugins/org.eclipse.equinox.launcher_*.jar -application org.eclipse.ant.core.antRunner"
@@ -43,18 +53,6 @@ QUALIFIER=`date "+%Y%m%d"`
 
 # This can be empty unless you happen to have more than one version of
 # org.eclipse.pde.build_*, as can happen after installing updates
-#PDE_VER=3.6.2.R36x_20110203
 PDE_VER=
-
-# Linux
-# export TOP=$HOME/cs-studio-3.0
-# export CYGDRIVE=
-# export BUILDDIR=$HOME/CSS/build
-# export ECLIPSE_BASE=$HOME
-# export JAVA_HOME=/usr/new/pkg/jdk1.6.0_21
-# export ECLIPSE=$ECLIPSE_BASE/eclipse
-# export ECLIPSE_ANT="$JAVA_HOME/bin/java -jar $ECLIPSE/plugins/org.eclipse.equinox.launcher_*.jar -application org.eclipse.ant.core.antRunner"
-# export ANT=$ECLIPSE_ANT
-# export DELTAPACK=$ECLIPSE/delta
-
+#PDE_VER=3.6.2.R36x_20110203
 
