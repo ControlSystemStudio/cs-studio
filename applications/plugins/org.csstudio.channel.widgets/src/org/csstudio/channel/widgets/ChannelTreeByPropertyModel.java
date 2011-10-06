0bb3e83a36384dbb78d5d6012b89410e4410041a
Gabriele Carcassi <carcassi@bnl.gov>
2011-10-06 16:36:37 -0400

    ..channel.widgets: adding prototype of a channel tree widget

---
+++ applications/plugins/org.csstudio.channel.widgets/src/org/csstudio/channel/widgets/ChannelTreeByPropertyModel.java
@@ -0,0 +1,115 @@
+package org.csstudio.channel.widgets;
+
+import gov.bnl.channelfinder.api.Channel;
+import gov.bnl.channelfinder.api.ChannelUtil;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.csstudio.channel.widgets.ChannelTreeByPropertyModel.Node;
+
+class ChannelTreeByPropertyModel {
+	private List<Channel> allChannels;
+	private List<String> properties;
+	private Node root; 
+	
+	public ChannelTreeByPropertyModel(Collection<Channel> allChannels, List<String> properties) {
+		this.allChannels = new ArrayList<Channel>(allChannels);
+		this.properties = properties;
+		this.root = new Node(null, "");
+	}
+	
+	public Node getRoot() {
+		return root;
+	}
+	
+	class Node {
+		// Channels represented by this node and down
+		private List<Channel> nodeChannels;
+		// 0 for root, 1 for children, 2 for grandchildren...
+		private final int depth;
+		// null for root, first property value for children, second property value for grandchildren,
+		// channelName for leaf
+		private final String displayName;
+		// Next property value for root and descendents, names of channels for first to last node,
+		// null for leaf
+		private final List<String> childrenNames;
+		
+		
+		private Node(Node parentNode, String displayName) {
+			// Calculate depth
+			if (parentNode == null) {
+				depth = 0;
+			} else {
+				depth = parentNode.depth + 1;
+			}
+			
+			this.displayName = displayName;
+			
+			// Construct the Channel list
+			if (parentNode == null) {
+				// Node is root, get all channels
+				nodeChannels = allChannels;
+			} else if (getPropertyName() == null) {
+				// leaf node, no channel list
+				nodeChannels = null;
+			} else {
+				// Filter the channels that match the property name
+				nodeChannels = new ArrayList<Channel>();
+				for (Channel channel : parentNode.nodeChannels) {
+					if (this.displayName.equals(channel.getProperty(getPropertyName()).getValue())) {
+						nodeChannels.add(channel);
+					}
+				}
+			}
+			
+			if (depth < properties.size()) {
+				// Children will be property values
+				childrenNames = new ArrayList<String>(ChannelUtil.getPropValues(nodeChannels, properties.get(depth)));
+				Collections.sort(childrenNames);
+			} else if (nodeChannels != null) {
+				// Children will be channels
+				childrenNames = new ArrayList<String>(ChannelUtil.getChannelNames(nodeChannels));
+				Collections.sort(childrenNames);
+			} else {
+				childrenNames = null;
+			}
+			
+			System.out.println(nodeChannels);
+			System.out.println(childrenNames);
+		}
+		
+		/**
+		 * The property name at this level
+		 * 
+		 * @return property name or null if leaf node
+		 */
+		public String getPropertyName() {
+			// Root node does not have any property associated with it
+			if (depth == 0)
+				return null;
+			
+			int index = depth - 1;
+			// We are at the channel level
+			if (index >= properties.size())
+				return null;
+			
+			return properties.get(index);
+		}
+		
+		public String getDisplayName() {
+			return displayName;
+		}
+		
+		public List<String> getChildrenNames() {
+			return childrenNames;
+		}
+
+		public Node getChild(int index) {
+			return new Node(this, childrenNames.get(index));
+		}
+		
+	}
+}
---
+++ applications/plugins/org.csstudio.channel.widgets/src/org/csstudio/channel/widgets/ChannelTreeByPropertyWidget.java
@@ -0,0 +1,251 @@
+package org.csstudio.channel.widgets;
+
+import static org.epics.pvmanager.ExpressionLanguage.channels;
+import static org.epics.pvmanager.ExpressionLanguage.latestValueOf;
+import static org.epics.pvmanager.data.ExpressionLanguage.column;
+import static org.epics.pvmanager.data.ExpressionLanguage.vStringConstants;
+import static org.epics.pvmanager.data.ExpressionLanguage.vTable;
+import static org.epics.pvmanager.util.TimeDuration.ms;
+import gov.bnl.channelfinder.api.Channel;
+import gov.bnl.channelfinder.api.ChannelUtil;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.csstudio.channel.widgets.ChannelTreeByPropertyModel.Node;
+import org.csstudio.utility.channelfinder.CFClientManager;
+import org.csstudio.utility.channelfinder.ChannelQuery;
+import org.csstudio.utility.channelfinder.ChannelQueryListener;
+import org.csstudio.utility.pvmanager.ui.SWTUtil;
+import org.csstudio.utility.pvmanager.widgets.ErrorBar;
+import org.csstudio.utility.pvmanager.widgets.VTableDisplay;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+import org.epics.pvmanager.PVManager;
+import org.epics.pvmanager.PVReader;
+import org.epics.pvmanager.PVReaderListener;
+import org.epics.pvmanager.data.VTable;
+import org.epics.pvmanager.data.VTableColumn;
+
+public class ChannelTreeByPropertyWidget extends Composite {
+	
+	private static final int MAX_COLUMNS = 200;
+	private static final int MAX_CELLS = 50000;
+	
+	private String channelQuery = "Tags=aphla.sys.SR";;
+	
+	private Tree tree;
+	private ErrorBar errorBar;
+	private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+	
+	{
+		final ChannelQuery query = ChannelQuery.Builder.query(channelQuery).create();
+		query.addChannelQueryListener(new ChannelQueryListener() {
+			
+			@Override
+			public void getQueryResult() {
+				SWTUtil.swtThread().execute(new Runnable() {
+					
+					@Override
+					public void run() {
+						Exception e = query.getLastException();
+						if (e == null) {
+							model = new ChannelTreeByPropertyModel(query.getResult(), Arrays.asList("cell", "girder", "devName", "elemName", "elemField", "handle"));
+							setChannels(query.getResult());
+						} else {
+							errorBar.setException(e);
+						}
+						tree.setItemCount(0);
+						tree.clearAll(true);
+						tree.setItemCount(model.getRoot().getChildrenNames().size());
+					}
+				});
+				
+			}
+		});
+		query.execute();
+	}
+	
+	private ChannelTreeByPropertyModel model;
+
+	public ChannelTreeByPropertyWidget(Composite parent, int style) {
+		super(parent, style);
+		
+		GridLayout gridLayout = new GridLayout(1, false);
+		gridLayout.verticalSpacing = 0;
+		gridLayout.marginWidth = 0;
+		gridLayout.marginHeight = 0;
+		setLayout(gridLayout);
+		tree = new Tree(this, SWT.VIRTUAL | SWT.BORDER);
+		tree.addListener(SWT.SetData, new Listener() {
+			public void handleEvent(Event event) {
+				if (model != null) {
+					TreeItem item = (TreeItem)event.item;
+					TreeItem parentItem = item.getParentItem();
+					Node parentNode;
+					Node node;
+					int index;
+					if (parentItem == null) {
+						parentNode = model.getRoot();
+						index = tree.indexOf(item);
+					} else {
+						parentNode = (Node) parentItem.getData();
+						index = parentItem.indexOf(item);
+					}
+					node = parentNode.getChild(index);
+					item.setData(node);
+					item.setText(node.getDisplayName());
+					if (node.getChildrenNames() == null) {
+						item.setItemCount(0);
+					} else {
+						item.setItemCount(node.getChildrenNames().size());
+					}
+					System.out.println("Actual data: " + item.getItemCount() + " " + item.getText());
+				}
+			}
+		});
+		tree.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("Selected " + ((TreeItem) e.item).getData());
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		tree.setItemCount(0);
+		tree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		
+		errorBar = new ErrorBar(this, SWT.NONE);
+		errorBar.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		
+		addPropertyChangeListener(new PropertyChangeListener() {
+			
+			List<String> properties = Arrays.asList("channels", "rowProperty", "columnProperty");
+			
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				if (properties.contains(evt.getPropertyName())) {
+					computeTree();
+				}
+			}
+		});
+		
+		changeSupport.addPropertyChangeListener("channelQuery", new PropertyChangeListener() {
+			
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				queryChannels();
+			}
+		});
+	}
+	
+	private void setLastException(Exception ex) {
+		errorBar.setException(ex);
+	}
+	
+	private String rowProperty;
+	private String columnProperty;
+	
+	private List<List<String>> cellPvs;
+	private List<String> columnNames;
+	private List<String> rowNames;
+	
+    public void addPropertyChangeListener( PropertyChangeListener listener ) {
+        changeSupport.addPropertyChangeListener( listener );
+    }
+
+    public void removePropertyChangeListener( PropertyChangeListener listener ) {
+    	changeSupport.removePropertyChangeListener( listener );
+    }
+	
+	public String getChannelQuery() {
+		return channelQuery;
+	}
+	
+	public void setChannelQuery(String channelQuery) {
+		String oldValue = this.channelQuery;
+		this.channelQuery = channelQuery;
+		changeSupport.firePropertyChange("channelQuery", oldValue, channelQuery);
+	}
+	
+	public String getRowProperty() {
+		return rowProperty;
+	}
+	
+	public void setRowProperty(String rowProperty) {
+		String oldValue = this.rowProperty;
+		this.rowProperty = rowProperty;
+		changeSupport.firePropertyChange("rowProperty", oldValue, rowProperty);
+	}
+	
+	public String getColumnProperty() {
+		return columnProperty;
+	}
+	
+	public void setColumnProperty(String columnProperty) {
+		String oldValue = this.columnProperty;
+		this.columnProperty = columnProperty;
+		changeSupport.firePropertyChange("columnProperty", oldValue, columnProperty);
+	}
+	
+	public Collection<Channel> getChannels() {
+		return channels;
+	}
+	
+	private Collection<Channel> channels;
+	
+	private void setChannels(Collection<Channel> channels) {
+		Collection<Channel> oldChannels = this.channels;
+		this.channels = channels;
+		changeSupport.firePropertyChange("channels", oldChannels, channels);
+	}
+	
+	private void queryChannels() {
+		setChannels(null);
+		final ChannelQuery query = ChannelQuery.Builder.query(channelQuery).create();
+		query.addChannelQueryListener(new ChannelQueryListener() {
+			
+			@Override
+			public void getQueryResult() {
+				SWTUtil.swtThread().execute(new Runnable() {
+					
+					@Override
+					public void run() {
+						Exception e = query.getLastException();
+						if (e == null) {
+							setChannels(query.getResult());
+						} else {
+							errorBar.setException(e);
+						}
+					}
+				});
+				
+			}
+		});
+		query.execute();
+	}
+	
+	private void computeTree() {
+	}
+}
