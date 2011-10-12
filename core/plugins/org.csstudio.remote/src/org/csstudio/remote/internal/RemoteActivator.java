fab20615eca38d7bd727fb6fedc2cbcc684b50d4
Jan Hatje <jan.hatje@desy.de>
2011-10-12 15:11:21 +0200

    o.c.remote: remove ui dependency

--- core/plugins/org.csstudio.remote/src/org/csstudio/remote/internal/RemoteActivator.java
+++ core/plugins/org.csstudio.remote/src/org/csstudio/remote/internal/RemoteActivator.java
@@ -1,19 +1,19 @@
 package org.csstudio.remote.internal;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class RemoteActivator extends AbstractUIPlugin {
+public class RemoteActivator extends Plugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.csstudio.remote"; //$NON-NLS-1$
 
 	// The shared instance
 	private static RemoteActivator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -24,7 +24,7 @@ public class RemoteActivator extends AbstractUIPlugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext context) throws Exception {
+	public void start(final BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
@@ -33,7 +33,7 @@ public class RemoteActivator extends AbstractUIPlugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext context) throws Exception {
+	public void stop(final BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
