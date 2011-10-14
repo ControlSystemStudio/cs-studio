0483210e7c412bef1417bf6a7b20b736a095af41
Takashi Nakamoto <devnull@localhost>
2011-10-14 10:48:42 +0900

    KEK: Added a new preference page for KBLog

---
+++ products/KEK/plugins/org.csstudio.archive.reader.kblog/src/org/csstudio/archive/reader/kblog/KBLogPreferencePage.java
@@ -0,0 +1,34 @@
+package org.csstudio.archive.reader.kblog;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IPreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+
+/**
+ * Preference Page for KBLog settings
+ * 
+ * @author Takashi Nakamoto
+ */
+public class KBLogPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+	public KBLogPreferencePage() {
+		super(GRID);
+		setPreferenceStore(new ScopedPreferenceStore(new InstanceScope(), Activator.ID));
+	}
+	
+	@Override
+	protected void createFieldEditors() {
+		setMessage("ABC");
+		final Composite parent = getFieldEditorParent();
+		addField(new StringFieldEditor("path_to_kblogrd", "Path to kblogrd", parent));
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {
+		// nothing to do
+	}
+
+}
