8d8a081a5620abbc2f44183532e1ba159c1230f3
Takashi Nakamoto <devnull@localhost>
2011-10-15 23:12:27 +0900

    KEK: Converted KBLogValueIterator into Java interface

---
+++ products/KEK/plugins/org.csstudio.archive.reader.kblog/src/org/csstudio/archive/reader/kblog/KBLogValueIterator.java
@@ -0,0 +1,7 @@
+package org.csstudio.archive.reader.kblog;
+
+import org.csstudio.archive.reader.ValueIterator;
+
+public interface KBLogValueIterator extends ValueIterator {
+	public boolean isClosed();
+}
