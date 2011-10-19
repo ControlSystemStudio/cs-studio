3230ea8ab6e2a80dd8dea0ffe92bd641cc8a580e
Gabriele Carcassi <carcassi@bnl.gov>
2011-10-19 15:40:07 -0400

    ..utility.channel: removing info action, using command instead.

--- applications/plugins/org.csstudio.utility.channel/src/org/csstudio/utility/channel/actions/InfoCommandHandler.java
+++ applications/plugins/org.csstudio.utility.channel/src/org/csstudio/utility/channel/actions/InfoCommandHandler.java
@@ -5,21 +5,11 @@ package org.csstudio.utility.channel.actions;
 
 import gov.bnl.channelfinder.api.Channel;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import org.csstudio.utility.channel.ChannelCommandHandler;
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.jface.dialogs.TitleAreaDialog;
-import org.eclipse.jface.viewers.ITreeContentProvider;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.TreeViewer;
-import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 
