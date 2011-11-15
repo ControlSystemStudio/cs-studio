3c5006b1427a04c7dec2317934e7c902c809defe
Gabriele Carcassi <carcassi@bnl.gov>
2011-11-15 13:54:47 -0500

    ..channel.opiwidgets: PVTableByProperty widget extends AbstractChannel elements.

--- applications/plugins/org.csstudio.channel.opiwidgets/src/org/csstudio/channel/opiwidgets/PVTableByPropertyEditPart.java
+++ applications/plugins/org.csstudio.channel.opiwidgets/src/org/csstudio/channel/opiwidgets/PVTableByPropertyEditPart.java
@@ -1,40 +1,23 @@
 package org.csstudio.channel.opiwidgets;
 
 import org.csstudio.channel.widgets.PVTableByPropertyWidget;
-import org.csstudio.opibuilder.editparts.AbstractWidgetEditPart;
 import org.csstudio.opibuilder.editparts.ExecutionMode;
 import org.csstudio.opibuilder.properties.IWidgetPropertyChangeHandler;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.swt.widgets.Composite;
 
-public class PVTableByPropertyEditPart extends AbstractWidgetEditPart {
+public class PVTableByPropertyEditPart
+extends AbstractChannelWidgetEditPart<PVTableByPropertyFigure, PVTableByPropertyModel> {
 	
 	/**
 	 * Create and initialize figure.
 	 */
 	@Override
-	protected IFigure doCreateFigure() {
+	protected PVTableByPropertyFigure doCreateFigure() {
 		PVTableByPropertyFigure figure = new PVTableByPropertyFigure((Composite) getViewer().getControl(), getWidgetModel().getParent());
 		figure.setRunMode(getExecutionMode() == ExecutionMode.RUN_MODE);
 		configure(figure.getSWTWidget(), getWidgetModel(), figure.isRunMode());
 		return figure;
-	}
-	
-	/**Get the widget model.
-	 * It is recommended that all widget controller should override this method.
-	 *@return the widget model.
-	 */
-	@Override
-	public PVTableByPropertyModel getWidgetModel() {
-		return (PVTableByPropertyModel) super.getWidgetModel();
-	}
-	
-	private static PVTableByPropertyWidget widgetOf(IFigure figure) {
-		return ((PVTableByPropertyFigure) figure).getSWTWidget();
-	}
-	
-	private static boolean runMode(IFigure figure) {
-		return ((PVTableByPropertyFigure) figure).isRunMode();
 	}
 	
 	private static void configure(PVTableByPropertyWidget widget, PVTableByPropertyModel model, boolean runMode) {
@@ -53,7 +36,7 @@ public class PVTableByPropertyEditPart extends AbstractWidgetEditPart {
 			public boolean handleChange(final Object oldValue,
 					final Object newValue,
 					final IFigure figure) {
-				configure(widgetOf(figure), getWidgetModel(), runMode(figure));
+				configure(getFigure().getSWTWidget(), getWidgetModel(), getFigure().isRunMode());
 				return false;
 			}
 		};
