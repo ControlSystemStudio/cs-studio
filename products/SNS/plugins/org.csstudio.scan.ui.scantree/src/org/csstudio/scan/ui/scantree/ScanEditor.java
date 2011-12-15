aa18b4dd430da07b0ade5f686ebd9441c875b284
Kay Kasemir <kasemirk@ornl.gov>
2011-12-15 12:20:14 -0500

    SNS scan server: Scan tree 'save as'

--- products/SNS/plugins/org.csstudio.scan.ui.scantree/src/org/csstudio/scan/ui/scantree/ScanEditor.java
+++ products/SNS/plugins/org.csstudio.scan.ui.scantree/src/org/csstudio/scan/ui/scantree/ScanEditor.java
@@ -15,8 +15,13 @@ import org.csstudio.scan.command.ScanCommand;
 import org.csstudio.scan.command.XMLCommandReader;
 import org.csstudio.scan.command.XMLCommandWriter;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.window.Window;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorInput;
@@ -24,7 +29,9 @@ import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.dialogs.SaveAsDialog;
 import org.eclipse.ui.part.EditorPart;
+import org.eclipse.ui.part.FileEditorInput;
 
 /** Eclipse Editor for the Scan Tree
  *  
@@ -33,7 +40,6 @@ import org.eclipse.ui.part.EditorPart;
  *  {@link ScanCommandAdapterFactory} then adapts
  *  as necessary to support Properties view/editor.
  *  
- *  TODO Add scan commands
  *  TODO Context menu to submit scan
  *  TODO Context menu to load scan from server?
  *  
@@ -41,6 +47,9 @@ import org.eclipse.ui.part.EditorPart;
  */
 public class ScanEditor extends EditorPart implements ScanTreeGUIListener
 {
+    /** File extension used to save files */
+    final private static String FILE_EXTENSION = "scn"; //$NON-NLS-1$
+
     private ScanTreeGUI gui;
 
     /** @see #isDirty() */
@@ -67,8 +76,10 @@ public class ScanEditor extends EditorPart implements ScanTreeGUIListener
 
         try
         {
-            final List<ScanCommand> commands = XMLCommandReader.readXMLStream(input.getFile().getContents());
+            final IFile file = input.getFile();
+            final List<ScanCommand> commands = XMLCommandReader.readXMLStream(file.getContents());
             gui.setCommands(commands);
+            setPartName(file.getName());
         }
         catch (Exception ex)
         {
@@ -106,14 +117,16 @@ public class ScanEditor extends EditorPart implements ScanTreeGUIListener
     @Override
     public boolean isSaveAsAllowed()
     {
-        return false;
+        return true;
     }
 
-    /** {@inheritDoc} */
-    @Override
-    public void doSave(final IProgressMonitor monitor)
+    /** Save current editor content to file
+     *  @param monitor
+     *  @param file
+     *  @return <code>true</code> on success
+     */
+    private boolean saveToFile(final IProgressMonitor monitor, final IFile file)
     {
-        final IFileEditorInput input = (IFileEditorInput) getEditorInput();
         try
         {
             // Write commands as XML to buffer
@@ -122,23 +135,69 @@ public class ScanEditor extends EditorPart implements ScanTreeGUIListener
             buf.close();
 
             // Write the buffer to file
-            input.getFile().setContents(new ByteArrayInputStream(buf.toByteArray()), IFile.FORCE, monitor);
-            
+            final ByteArrayInputStream stream = new ByteArrayInputStream(buf.toByteArray());
+            if (file.exists())
+                file.setContents(stream, IFile.FORCE, monitor);
+            else
+                file.create(stream, true, monitor);
             setDirty(false);
+            return true;
         }
         catch (Exception ex)
         {
             MessageDialog.openError(getSite().getShell(), Messages.Error,
                     NLS.bind(Messages.FileSaveErrorFmt,
-                            new Object[] { input.getName(), ex.getMessage() }));
+                            new Object[] { file.getName(), ex.getMessage() }));
+            return false;
         }
     }
-
+    
+    /** {@inheritDoc} */
+    @Override
+    public void doSave(final IProgressMonitor monitor)
+    {
+        final IFileEditorInput input = (IFileEditorInput) getEditorInput();
+        saveToFile(monitor, input.getFile());
+    }
+    
     /** {@inheritDoc} */
     @Override
     public void doSaveAs()
     {
-        // see isSaveAsAllowed()
+        final IFile file = promptForFile(null);
+        if (file == null)
+            return;
+        if (saveToFile(new NullProgressMonitor(), file))
+        {
+            setInput(new FileEditorInput(file));
+            setPartName(file.getName());
+        }
+    }
+    
+    /** Prompt for file name
+     *  @param old_file Old file name or <code>null</code>
+     *  @return IFile for new file name
+     */
+    private IFile promptForFile(final IFile old_file)
+    {
+        final SaveAsDialog dlg = new SaveAsDialog(getSite().getShell());
+        dlg.setBlockOnOpen(true);
+        if (old_file != null)
+            dlg.setOriginalFile(old_file);
+        if (dlg.open() != Window.OK)
+            return null;
+
+        // The path to the new resource relative to the workspace
+        IPath path = dlg.getResult();
+        if (path == null)
+            return null;
+        // Assert it's a '.scn' file
+        final String ext = path.getFileExtension();
+        if (ext == null  ||  !ext.equals(FILE_EXTENSION))
+            path = path.removeFileExtension().addFileExtension(FILE_EXTENSION);
+        // Get the file for the new resource's path.
+        final IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+        return root.getFile(path);
     }
 
     /** {@inheritDoc} */
