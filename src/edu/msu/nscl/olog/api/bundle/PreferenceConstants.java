91a374fa59407b496e7453bba2c71d2ee619e842
Kunal Shroff <shroffk@bnl.gov>
2011-12-29 15:23:10 -0500

    ...olog.api: add activator and clientManager

---
+++ src/edu/msu/nscl/olog/api/bundle/PreferenceConstants.java
@@ -0,0 +1,14 @@
+package edu.msu.nscl.olog.api.bundle;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+	
+	public static final String Olog_URL = "olog_url";
+	public static final String Olog_jcr_URL = "olog_jcr_url";
+	public static final String Use_authentication = "use_authentication";
+	public static final String Username = "user";
+	public static final String Password = "password";
+	
+}
