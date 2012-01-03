3b3eb4d35cfe0b4caaea9ec6be3ad4244534d6d2
Kunal Shroff <shroffk@bnl.gov>
2012-01-03 14:06:24 -0500

    o.c.u.olog.ui: register a new olog client when preferences are changed.

--- applications/plugins/org.csstudio.utility.olog.ui/src/org/csstudio/utility/olog/ui/Activator.java
+++ applications/plugins/org.csstudio.utility.olog.ui/src/org/csstudio/utility/olog/ui/Activator.java
@@ -1,7 +1,18 @@
 package org.csstudio.utility.olog.ui;
 
+import java.util.logging.Logger;
+
+import org.csstudio.auth.security.SecureStorage;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.preferences.IPreferencesService;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+
+import edu.msu.nscl.olog.api.OlogClientImpl.OlogClientBuilder;
+import edu.msu.nscl.olog.api.OlogClientManager;
+import edu.msu.nscl.olog.api.bundle.PreferenceConstants;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -13,7 +24,9 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+	private IPropertyChangeListener preferenceListner;
+	private static Logger log = Logger.getLogger(PLUGIN_ID);
+
 	/**
 	 * The constructor
 	 */
@@ -22,25 +35,70 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		preferenceListner = new IPropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent event) {
+				final IPreferencesService prefs = Platform.getPreferencesService();
+				OlogClientBuilder ologClientBuilder;
+				String url = prefs.getString(Activator.PLUGIN_ID,
+						PreferenceConstants.Olog_URL,
+						"http://localhost:8080/Olog/resources", null);
+				String jcr_url = prefs.getString(Activator.PLUGIN_ID,
+						PreferenceConstants.Olog_jcr_URL,
+						"http://localhost:8080/Olog/repository/olog", null);
+				ologClientBuilder = OlogClientBuilder.serviceURL(url).jcrURI(jcr_url);
+				if (prefs.getBoolean(Activator.PLUGIN_ID,
+						PreferenceConstants.Use_authentication, false, null)) {
+					ologClientBuilder
+							.withHTTPAuthentication(true)
+							.username(
+									prefs.getString(Activator.PLUGIN_ID,
+											PreferenceConstants.Username, "username",
+											null))
+							.password(
+									SecureStorage.retrieveSecureStorage(
+											Activator.PLUGIN_ID,
+											PreferenceConstants.Password));
+				}else{
+					ologClientBuilder.withHTTPAuthentication(false);
+				}
+				log.info("Creating Olog client : " + url);
+				try {
+					OlogClientManager.registerDefaultClient(ologClientBuilder.create());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		Activator.getDefault().getPreferenceStore()
+				.addPropertyChangeListener(preferenceListner);
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
+		Activator.getDefault().getPreferenceStore()
+				.removePropertyChangeListener(preferenceListner);
 		super.stop(context);
 	}
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
