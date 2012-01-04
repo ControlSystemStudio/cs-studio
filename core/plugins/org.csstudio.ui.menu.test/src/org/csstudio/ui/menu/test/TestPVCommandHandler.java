5024454b51ab4d6f0870e0f5a4d44cc1e02ba674
Gabriele Carcassi <carcassi@bnl.gov>
2012-01-04 15:23:47 -0500

    ..ui.menu.test: modified command to use new abstract handler, and added a menu command to test/showcase the dialog.

--- core/plugins/org.csstudio.ui.menu.test/src/org/csstudio/ui/menu/test/TestPVCommandHandler.java
+++ core/plugins/org.csstudio.ui.menu.test/src/org/csstudio/ui/menu/test/TestPVCommandHandler.java
@@ -1,30 +1,28 @@
 package org.csstudio.ui.menu.test;
 
-import java.util.Arrays;
+import java.util.List;
 
 import org.csstudio.csdata.ProcessVariable;
-import org.csstudio.ui.util.AdapterUtil;
-import org.eclipse.core.commands.AbstractHandler;
+import org.csstudio.ui.util.AbstractAdaptedHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-public class TestPVCommandHandler extends AbstractHandler implements IHandler {
+public class TestPVCommandHandler extends AbstractAdaptedHandler<ProcessVariable> {
 
+	public TestPVCommandHandler() {
+		super(ProcessVariable.class);
+	}
+	
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
+	protected void execute(List<ProcessVariable> data, ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		ISelection selection = HandlerUtil.getActiveMenuSelection(event);
 		MessageDialog.openInformation(
-				window.getShell(),
-				"PV Command",
-				"PVs: " + Arrays.toString(AdapterUtil.convert(selection,
-						ProcessVariable.class)));
-		return null;
+		window.getShell(),
+		"PV Command",
+		"PVs: " + data);
 	}
 
 }
