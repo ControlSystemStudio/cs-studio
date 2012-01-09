9454309e9c032414fd37346255f89a6b64cf561f
Gabriele Carcassi <carcassi@bnl.gov>
2012-01-09 10:51:07 -0500

    ..ui.util: adding findView utility method to AbstractAdaptedHandler.

--- core/plugins/org.csstudio.ui.util/src/org/csstudio/ui/util/AbstractAdaptedHandler.java
+++ core/plugins/org.csstudio.ui.util/src/org/csstudio/ui/util/AbstractAdaptedHandler.java
@@ -1,20 +1,20 @@
 package org.csstudio.ui.util;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.List;
 
-import org.csstudio.ui.util.AdapterUtil;
 import org.csstudio.ui.util.dialogs.ExceptionDetailsErrorDialog;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
@@ -30,6 +30,22 @@ public abstract class AbstractAdaptedHandler<T> extends AbstractHandler {
 	
 	public AbstractAdaptedHandler(Class<T> dataType) {
 		this.clazz = dataType;
+	}
+	
+	/**
+	 * Searches for the view of the given class with the given view id.
+	 * 
+	 * @param clazz the view class
+	 * @param viewId the view id
+	 * @return the view
+	 * @throws PartInitException if the view was not found
+	 * @throws ClassCastException if the view is of a different type
+	 */
+	public static <T> T findView(Class<T> clazz, String viewId) throws PartInitException {
+		IWorkbench workbench = PlatformUI.getWorkbench();
+		IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+		IWorkbenchPage page = window.getActivePage();
+		return clazz.cast(page.showView(viewId));
 	}
 	
 	@Override
