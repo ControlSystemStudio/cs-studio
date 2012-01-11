8837041e23b748c3e531a80f512d0e8b9163253e
Gabriele Carcassi <carcassi@bnl.gov>
2012-01-11 16:55:57 -0500

    ..pvmanager.widget: error bar has a button to open the exception details. Works but looks awful.

--- core/plugins/org.csstudio.utility.pvmanager.widgets/src/org/csstudio/utility/pvmanager/widgets/ErrorBar.java
+++ core/plugins/org.csstudio.utility.pvmanager.widgets/src/org/csstudio/utility/pvmanager/widgets/ErrorBar.java
@@ -1,14 +1,15 @@
 package org.csstudio.utility.pvmanager.widgets;
 
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
+import org.csstudio.ui.util.dialogs.ExceptionDetailsErrorDialog;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 
 import com.swtdesigner.ResourceManager;
 
@@ -16,6 +17,8 @@ public class ErrorBar extends Composite {
 
 	private Label errorImage;
 	private CLabel errorLabel;
+	private Exception exception;
+	Button imageButton;
 	
 	/**
 	 * Create the composite.
@@ -24,16 +27,27 @@ public class ErrorBar extends Composite {
 	 */
 	public ErrorBar(Composite parent, int style) {
 		super(parent, style);
-		GridLayout gridLayout = new GridLayout(2, false);
+		GridLayout gridLayout = new GridLayout(3, false);
 		gridLayout.marginWidth = 0;
 		gridLayout.marginHeight = 0;
 		gridLayout.marginLeft = 1;
 		setLayout(gridLayout);
 		
-		errorImage = new Label(this, SWT.NONE);
-		GridData gd_errorImage = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		errorImage.setLayoutData(gd_errorImage);
-		errorImage.setImage(ResourceManager.getPluginImage("org.eclipse.ui", "/icons/full/obj16/warn_tsk.gif"));
+		//errorImage = new Label(this, SWT.NONE);
+		//GridData gd_errorImage = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		//errorImage.setLayoutData(gd_errorImage);
+		//errorImage.setImage(ResourceManager.getPluginImage("org.eclipse.ui", "/icons/full/obj16/warn_tsk.gif"));
+		
+		imageButton = new Button(this, SWT.FLAT);
+		GridData gd_imageButton = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		imageButton.setLayoutData(gd_imageButton);
+		imageButton.setImage(ResourceManager.getPluginImage("org.eclipse.ui", "/icons/full/obj16/warn_tsk.gif"));
+		imageButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				ExceptionDetailsErrorDialog.openError(getShell(), "Query failed...", exception);
+			}
+		});
 		
 		errorLabel = new CLabel(this, SWT.NONE);
 		GridData gd_errorLabel = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
@@ -44,15 +58,16 @@ public class ErrorBar extends Composite {
 	
 	public void setException(Exception ex) {
 		if (!isDisposed()) {
+			this.exception = ex;
 			if (ex == null) {
 				errorLabel.setToolTipText("");
 				errorLabel.setText("");
 				GridData gd = (GridData) errorLabel.getLayoutData();
 				gd.exclude = true;
 				errorLabel.setLayoutData(gd);
-				gd = (GridData) errorImage.getLayoutData();
+				gd = (GridData) imageButton.getLayoutData();
 				gd.exclude = true;
-				errorImage.setLayoutData(gd);
+				imageButton.setLayoutData(gd);
 			} else {
 				ex.printStackTrace();
 				errorLabel.setToolTipText(ex.getMessage());
@@ -60,9 +75,9 @@ public class ErrorBar extends Composite {
 				GridData gd = (GridData) errorLabel.getLayoutData();
 				gd.exclude = false;
 				errorLabel.setLayoutData(gd);
-				gd = (GridData) errorImage.getLayoutData();
+				gd = (GridData) imageButton.getLayoutData();
 				gd.exclude = false;
-				errorImage.setLayoutData(gd);
+				imageButton.setLayoutData(gd);
 			}
 			getParent().layout();
 		}
