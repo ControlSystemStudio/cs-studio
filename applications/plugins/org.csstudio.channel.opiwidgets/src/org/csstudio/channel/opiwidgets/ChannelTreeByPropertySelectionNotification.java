dcfcb209605137d3e492d113fecade82627b01d2
Gabriele Carcassi <carcassi@bnl.gov>
2012-01-23 12:22:35 -0500

    ..channel.opiwidgets: common selection notification logic extracted.

--- applications/plugins/org.csstudio.channel.opiwidgets/src/org/csstudio/channel/opiwidgets/ChannelTreeByPropertySelectionNotification.java
+++ applications/plugins/org.csstudio.channel.opiwidgets/src/org/csstudio/channel/opiwidgets/ChannelTreeByPropertySelectionNotification.java
@@ -1,73 +1,23 @@
 package org.csstudio.channel.opiwidgets;
 
 import org.csstudio.channel.widgets.ChannelTreeByPropertyNode;
-import org.csstudio.channel.widgets.LocalUtilityPvManagerBridge;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.DisposeListener;
-import org.eclipse.swt.widgets.Widget;
+import org.csstudio.channel.widgets.ChannelTreeByPropertyWidget;
 
-public class ChannelTreeByPropertySelectionNotification {
+public class ChannelTreeByPropertySelectionNotification extends ChannelSelectionNotification {
 	
-	private final ChannelNotificationExpression notificationExpression;
-	private final ISelectionProvider selectionProvider;
-	private final LocalUtilityPvManagerBridge notification;
+	public ChannelTreeByPropertySelectionNotification(String notificationPv,
+			String notificationExpression, ChannelTreeByPropertyWidget widget) {
+		super(notificationPv, notificationExpression, widget);
+	}
 	
-	private final ISelectionChangedListener listener = new ISelectionChangedListener() {
-		
-		@Override
-		public void selectionChanged(SelectionChangedEvent event) {
-			if (event.getSelection() instanceof IStructuredSelection) {
-				String notificationString = notificationFor(((IStructuredSelection) event.getSelection()).getFirstElement());
-				if (notificationString != null) {
-					notification.write(notificationString);
-				} else {
-					notification.write(notificationString);
-				}
-			} else {
-				notification.write("");
-			}
-		}
-	};
 	
 	protected String notificationFor(Object selection) {
 		ChannelTreeByPropertyNode node = (ChannelTreeByPropertyNode) selection;
 		if (node.isSubQuery()) {
-			return notificationExpression.notification(node.getPropertiesAndValues());
+			return getNotificationExpression().notification(node.getPropertiesAndValues());
 		} else {
-			return notificationExpression.notification(node.getNodeChannels().get(0));
+			return getNotificationExpression().notification(node.getNodeChannels().get(0));
 		}
-	}
-	
-	public <T extends Widget & ISelectionProvider>  ChannelTreeByPropertySelectionNotification(String notificationPv,
-			String notificationExpression, T widget) {
-		this(notificationPv, notificationExpression, widget, widget);
-	}
-	
-	
-	public ChannelTreeByPropertySelectionNotification(String notificationPv,
-			String notificationExpression, ISelectionProvider selectionProvider, Widget widget) {
-		this.notificationExpression = new ChannelNotificationExpression(notificationExpression);
-		this.selectionProvider = selectionProvider;
-		
-		notification = new LocalUtilityPvManagerBridge(notificationPv);
-		
-		selectionProvider.addSelectionChangedListener(listener);
-		widget.addDisposeListener(new DisposeListener() {
-			
-			@Override
-			public void widgetDisposed(DisposeEvent e) {
-				close();
-			}
-		});
-	}
-	
-	public void close() {
-		selectionProvider.removeSelectionChangedListener(listener);
-		notification.close();
 	}
 
 }
