acda40cf07347c6753bb368ae2b024345bb59e16
Helge Rickens <helge.rickens@desy.de>
2012-01-31 09:36:36 +0100

    o.c.servicelocator: add new PlugIn

--- applications/plugins/org.csstudio.servicelocator.test/src/java/org/csstudio/servicelocator/ServiceLocatorUnitTest.java
+++ applications/plugins/org.csstudio.servicelocator.test/src/java/org/csstudio/servicelocator/ServiceLocatorUnitTest.java
@@ -1,20 +1,20 @@
 package org.csstudio.servicelocator;
 
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.osgi.util.tracker.ServiceTracker;
 
-import static org.junit.Assert.*;
-
-import static org.mockito.Mockito.*;
-
 public class ServiceLocatorUnitTest {
-    
+
     @Before
     public void setUp() {
         ServiceLocator.reset();
     }
-    
+
     @Test(expected = IllegalStateException.class)
     public void testAccessAfterCloseIsDetected() {
         ServiceLocator.registerService(IService.class, new MyService());
@@ -22,49 +22,48 @@ public class ServiceLocatorUnitTest {
         ServiceLocator.close();
         ServiceLocator.getService(IService.class);
     }
-    
+
     @Test
     public void testRegisterService() {
-        MyService myService = new MyService();
-        ServiceLocator.registerService(IService.class, myService);
-        
-        IService service = ServiceLocator.getService(IService.class);
-        assertSame(myService, service);
-    }
-    
-    @SuppressWarnings({ "unchecked" })
-    @Test
-    public void testRegisterServiceFromTracker() {
         final MyService myService = new MyService();
-        final ServiceTracker<IService, Object> myServiceTracker = mock(ServiceTracker.class);
-        when(myServiceTracker.getService()).thenReturn(myService);
-        ServiceLocator.registerServiceTracker(IService.class, myServiceTracker);
-        
+        ServiceLocator.registerService(IService.class, myService);
+
         final IService service = ServiceLocator.getService(IService.class);
         assertSame(myService, service);
     }
-    
+
+    @Test
+    public void testRegisterServiceFromTracker() {
+        final MyService myService = new MyService();
+        final ServiceTracker myServiceTracker = mock(ServiceTracker.class);
+        when(myServiceTracker.getService()).thenReturn(myService);
+        ServiceLocator.registerServiceTracker(IService.class, myServiceTracker);
+
+        final IService service = ServiceLocator.getService(IService.class);
+        assertSame(myService, service);
+    }
+
     @Test(expected = IllegalStateException.class)
     public void testGetServiceFailsWithoutRegistration() {
         ServiceLocator.getService(IService.class);
     }
-    
+
     /**
      * example service
      */
-    private static interface IService {
+    private interface IService {
         // nothing to implement
     }
-    
+
     /**
      * example service implementation
      */
     private static class MyService implements IService {
-        
+
         public MyService() {
             // TODO Auto-generated constructor stub
         }
         // nothing to implement
     }
-    
+
 }
