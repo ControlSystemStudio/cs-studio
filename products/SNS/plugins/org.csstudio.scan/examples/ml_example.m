92787ccca5faaaa60d6064fcfb535a28a3943dee
Kay Kasemir <kasemirk@ornl.gov>
2012-03-09 09:06:50 -0500

    SNS Scan System: Matlab example

--- products/SNS/plugins/org.csstudio.scan/examples/ml_example.m
+++ products/SNS/plugins/org.csstudio.scan/examples/ml_example.m
@@ -3,19 +3,25 @@
 % @author: Kay Kasemir
 
 %% Set path to Java classes
-% Example for classes fetched from IDE, using the 'bin'
-% subdir of the source code.
-%
-% For compiled plugins, use '/org.csstudio.scan_1.2.3'
-% i.e. versioned plugin name, but then no 'bin' subdir.
-%
 % Instead of configuring the path in here one could also
 % update the file matlab/toolbox/local/classpath.txt
-basepath='/Kram/MerurialRepos/cs-studio/products/SNS/plugins';
-path={};
-path{1} = strcat(basepath, '/org.csstudio.scan/bin');
-path{2} = strcat(basepath, '/org.csstudio.scan.client/bin');
-javaclasspath(path)
+
+% Ideally, use the standalone client JAR
+% created from org.csstudio.scan.client/build.xml
+javaclasspath('/usr/local/css/scan.client.jar')
+
+% Alternatively, use use classes fetched from IDE,
+% using the 'bin' subdir of the source code.
+% Or the compiled plugins of a CSS installation that includes
+% the scan system, in which case you need to specify the exact
+% version number of the plugin, like '/org.csstudio.scan_1.2.3',
+% and there's no 'bin' subdir.
+%
+%basepath='/Kram/MerurialRepos/cs-studio/products/SNS/plugins';
+%path={};
+%path{1} = strcat(basepath, '/org.csstudio.scan/bin');
+%path{2} = strcat(basepath, '/org.csstudio.scan.client/bin');
+%javaclasspath(path)
 
 %% Import scan system classes
 import org.csstudio.scan.server.*
@@ -23,31 +29,29 @@ import org.csstudio.scan.client.*
 import org.csstudio.scan.command.*
 
 %% Connect to scan server
+% Default is localhost:4810. Change as needed
+import java.lang.System
+%System.setProperty(ScanServer.HOST_PROPERTY, 'ky9linux.ornl.gov');
+%System.setProperty(ScanServer.PORT_PROPERTY, '4810');
+
 server=ScanServerConnector.connect();
 server.getInfo()
 
 %% Create a scan
-% Matlab/Java quirk:
-% The LoopCommand body and the CommandSequence expect
-% an array of scan commands.
-% But Matlab turns a one-element array into a skalar.
-% For now, the easiest workaround seems to be:
-% Add dummy DelayCommands to have at least 2 elements
-% in the array.
+% Scan x/y, at each step waiting for readback to follow setpoint
 scan=[
     LoopCommand('xpos', 1, 10, 1,[
-      LoopCommand('ypos', 1, 5, 1,[
-        SetCommand('setpoint', 2, 'readback', true, 0.1, 0.0)
-        SetCommand('setpoint', 3, 'readback', true, 0.1, 0.0)
-      ]),
-      DelayCommand(0.1)
-    ]),
-    DelayCommand(0.1)
-]
+        LoopCommand('ypos', 1, 5, -1,[
+            SetCommand('setpoint', 2, 'readback', true, 0.1, 0.0)
+            SetCommand('setpoint', 3, 'readback', true, 0.1, 0.0)
+      ])
+    ])
+];
 
 seq=CommandSequence(scan);
 seq.dump();
 
+%% Submit to server
 id=server.submitScan('Matlab Scan', seq.getXML());
 
 %% Play with pause, resume
@@ -80,3 +84,5 @@ plot(xpos.time, xpos.val, readback.time, readback.val);
 legend('xpos', 'readback');
 datetick('x');
 
+%% Not absolutely necessary, but nice: Disconnect when done
+ScanServerConnector.disconnect(server)
