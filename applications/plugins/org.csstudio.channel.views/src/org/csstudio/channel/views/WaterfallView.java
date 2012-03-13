45e3bdce516db2da21b66fa9aff5e98caa788d71
Gabriele Carcassi <carcassi@bnl.gov>
2012-03-13 16:16:36 -0400

    ..channel.views: waterfall view now saves the state.

--- applications/plugins/org.csstudio.channel.views/src/org/csstudio/channel/views/WaterfallView.java
+++ applications/plugins/org.csstudio.channel.views/src/org/csstudio/channel/views/WaterfallView.java
@@ -1,7 +1,5 @@
 package org.csstudio.channel.views;
 
-import gov.bnl.channelfinder.api.ChannelQuery;
-
 import org.csstudio.channel.widgets.WaterfallWidget;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IMemento;
@@ -16,23 +14,14 @@ public class WaterfallView extends AbstractChannelQueryView<WaterfallWidget> {
 	 */
 	public static final String ID = "org.csstudio.channel.views.WaterfallView";
 
-	/** Memento tag */
-	private static final String MEMENTO_PVNAME = "PVName"; //$NON-NLS-1$
-
 	@Override
 	public void saveWidgetState(WaterfallWidget widget, IMemento memento) {
-		if (widget.getChannelQuery() != null) {
-			memento.putString(MEMENTO_PVNAME, widget.getChannelQuery()
-					.getQuery());
-		}
+		widget.saveState(memento);
 	}
 
 	@Override
 	public void loadWidgetState(WaterfallWidget widget, IMemento memento) {
-		if (memento != null && memento.getString(MEMENTO_PVNAME) != null) {
-			widget.setChannelQuery(ChannelQuery.query(
-					memento.getString(MEMENTO_PVNAME)).build());
-		}
+		widget.loadState(memento);
 	}
 
 	@Override
