5a0517288a690fc9b85c261bad1f434a24b76034
Kay Kasemir <kasemirk@ornl.gov>
2012-04-16 10:46:53 -0400

    SNS Scan System: Simulation display uses PlatformUI instead of HandlerUtil for window

--- products/SNS/plugins/org.csstudio.scan.ui.scantree/src/org/csstudio/scan/ui/scantree/operations/ShowSimulationHandler.java
+++ products/SNS/plugins/org.csstudio.scan.ui.scantree/src/org/csstudio/scan/ui/scantree/operations/ShowSimulationHandler.java
@@ -17,7 +17,7 @@ import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.PlatformUI;
 
 /** Handler for parameterized command that shows simulation result
  *
@@ -31,7 +31,11 @@ public class ShowSimulationHandler extends AbstractHandler
     @Override
     public Object execute(final ExecutionEvent event) throws ExecutionException
     {
-		final IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+    	// We should probably use HandlerUtil.getActiveWorkbenchWindowChecked(event)
+    	// to move away from PlatformUI, HandlerUtil will fail if a dialog or other
+    	// window was in focus, while PlatformUI always returns the window in which
+    	// we're running
+    	final IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
     	final String log = event.getParameter("org.csstudio.scan.ui.scantree.show_simulation.log");
 		final IEditorInput input = new StringEditorInput(Messages.ScanSimulation, log);
 		try
