0f8a94228f9f6f1e2faf62d427d55ecc5671faf1
Gabriele Carcassi <carcassi@bnl.gov>
2012-04-16 12:14:13 -0400

    ...pvmanager.graphene: merge with 0.1-SNAPSHOT

---
+++ applications/plugins/org.csstudio.utility.pvmanager.graphene/src/org/epics/pvmanager/graphene/LineGraphPlot.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2011 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.pvmanager.graphene;
+
+import org.epics.graphene.LineGraphRendererUpdate;
+import org.epics.pvmanager.data.VImage;
+import org.epics.pvmanager.expression.DesiredRateExpressionImpl;
+import org.epics.pvmanager.expression.DesiredRateExpressionList;
+
+/**
+ *
+ * @author carcassi
+ */
+public class LineGraphPlot extends DesiredRateExpressionImpl<VImage> {
+
+    public LineGraphPlot(DesiredRateExpressionList<?> childExpressions, LineGraphFunction function, String defaultName) {
+        super(childExpressions, function, defaultName);
+    }
+    
+    public void update(LineGraphRendererUpdate update) {
+        ((LineGraphFunction) getFunction()).update(update);
+    }
+}
