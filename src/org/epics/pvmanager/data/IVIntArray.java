5f531ee852a44e285901dc35dcaca171e812368a
Gabriele Carcassi <carcassi@bnl.gov>
2012-06-12 11:10:58 -0400

    pvmanager: updating to 1.1-SNAPSHOT

--- src/org/epics/pvmanager/data/IVIntArray.java
+++ src/org/epics/pvmanager/data/IVIntArray.java
@@ -7,6 +7,7 @@ package org.epics.pvmanager.data;
 import java.text.NumberFormat;
 import java.util.List;
 import org.epics.util.array.ArrayInt;
+import org.epics.util.array.ListDouble;
 import org.epics.util.array.ListInt;
 import org.epics.util.time.Timestamp;
 
@@ -17,6 +18,7 @@ import org.epics.util.time.Timestamp;
 class IVIntArray extends IVNumeric implements VIntArray {
 
     private final int[] array;
+    private final ListInt data;
     private final List<Integer> sizes;
 
     public IVIntArray(int[] array, List<Integer> sizes,
@@ -30,10 +32,30 @@ class IVIntArray extends IVNumeric implements VIntArray {
                 upperAlarmLimit, upperCtrlLimit, upperDisplayLimit);
         this.array = array;
         this.sizes = sizes;
+        this.data = new ArrayInt(array);
+    }
+
+    public IVIntArray(ListInt data, List<Integer> sizes,
+            Alarm alarm, Time time, Display display) {
+        super(alarm.getAlarmSeverity(), alarm.getAlarmStatus(), time.getTimestamp(), time.getTimeUserTag(), time.isTimeValid(),
+                display.getLowerDisplayLimit(), display.getLowerCtrlLimit(), display.getLowerAlarmLimit(), display.getLowerWarningLimit(),
+                display.getUnits(), display.getFormat(),
+                display.getUpperWarningLimit(), display.getUpperAlarmLimit(), display.getUpperCtrlLimit(), display.getUpperDisplayLimit());
+        this.array = null;
+        this.sizes = sizes;
+        this.data = data;
     }
 
     @Override
     public int[] getArray() {
+        if (array == null) {
+            int[] temp = new int[data.size()];
+            for (int i = 0; i < data.size(); i++) {
+                temp[i] = data.getInt(i);
+            }
+            return temp;
+        }
+        
         return array;
     }
 
@@ -44,7 +66,7 @@ class IVIntArray extends IVNumeric implements VIntArray {
 
     @Override
     public ListInt getData() {
-        return new ArrayInt(array);
+        return data;
     }
 
 }
