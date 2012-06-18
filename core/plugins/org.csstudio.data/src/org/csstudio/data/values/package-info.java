78925cca9d80d7fca5c7ab38f42cc288a144a5ba
Kay Kasemir <kasemirk@ornl.gov>
2012-06-18 16:59:36 -0400

    several package-info.java

---
+++ core/plugins/org.csstudio.data/src/org/csstudio/data/values/package-info.java
@@ -0,0 +1,2 @@
+/** Control system data types: PV, Device, ... */
+package org.csstudio.data.values;
---
+++ core/plugins/org.csstudio.platform.utility.rdb/src/org/csstudio/platform/utility/rdb/package-info.java
@@ -0,0 +1,7 @@
+/** RDB Utilities
+ *
+ *  <p>The key class is the {@link org.csstudio.platform.utility.rdb.RDBUtil},
+ *  providing an RDB connection for Oracle, Postgres or MySQL without
+ *  tying to calling code to a specific RDB.
+ */
+package org.csstudio.platform.utility.rdb;
