9f38af3a499f11064caccd588ad98a48d97ef8a1
Xihui Chen <chenx1@ornl.gov>
2012-07-10 13:56:44 -0400

    Add BOY Symbol widgets plugins.

---
+++ src/org/apache/batik/Activator.java
@@ -0,0 +1,53 @@
+package org.apache.batik;
+
+import java.util.logging.Logger;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.apache.batik"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	// The logger
+	private static final Logger LOGGER = Logger.getLogger(PLUGIN_ID);
+
+	public Activator() {
+	}
+
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+	
+	/**
+	 * Get the logger for Symbol plug-in.
+	 * 
+	 * @return the logger
+	 */
+	public static Logger getLogger() {
+		return LOGGER;
+	}
+
+}
