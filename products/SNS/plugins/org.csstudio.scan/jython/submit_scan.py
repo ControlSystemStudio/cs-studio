2a05109a98d9ad960c4552f3348da5ec789679b2
Kay Kasemir <kasemirk@ornl.gov>
2012-07-24 08:56:34 -0400

    SNS Scan System: submitScan callable as module

--- products/SNS/plugins/org.csstudio.scan/jython/submit_scan.py
+++ products/SNS/plugins/org.csstudio.scan/jython/submit_scan.py
@@ -1,41 +1,45 @@
+"""Example for submitting scan file (XML) from command-line
+
+   jython.sh submit_scan my_scan.py
+
+   @author: Kay Kasemir
 """
-Example for submitting scan file (XML) from command-line
-
- jython.sh submit_scan my_scan.py
-
-@author: Kay Kasemir
-"""
-
 import os, optparse
-
-# Check arguments
-parser = optparse.OptionParser("%prog {scan file name}")
-(options, args) = parser.parse_args()
-
-if len(args) != 1:
-    parser.print_help()
-    parser.error("Missing name of scan file")
-    
-filepath = args[0]
-filename = os.path.basename(filepath)
-
-if not os.path.isfile(filepath):
-    parser.error("Cannot locate scan file")
-    
-# Connect to scan server
 from scan_client import *
-print 'Connected to %s' % scan.server.getInfo()
 
-# Read scan file
-print 'Reading %s' % filepath
-f = open(filepath, 'r')
-commands = f.read()
-f.close()
+def submitScan(filepath, wait=True):
+    """Submit a scan file to server
 
-# Schedule for execution on server
-print 'Submitting to scan server'
-scan.submit(filename, commands)
-scan.waitUntilDone()
+       filepath: Full path to the *.scn file
+       wait [optional]: Wait for scan to finish?
+    """
+    if not os.path.isfile(filepath):
+        raise Exception("Cannot locate scan file %s", filepath)
+
+    filename = os.path.basename(filepath)
+
+    # Connect to scan server
+    print 'Connected to %s' % scan.server.getInfo()
+
+    # Read scan file
+    print 'Reading %s' % filepath
+    f = open(filepath, 'r')
+    commands = f.read()
+    f.close()
+
+    # Schedule for execution on server
+    print 'Submitting to scan server'
+    scan.submit(filename, commands)
+    if wait:
+        scan.waitUntilDone()
 
 
+if __name__ == '__main__':
+    # Check arguments
+    parser = optparse.OptionParser("%prog {scan file name}")
+    (options, args) = parser.parse_args()
 
+    if len(args) != 1:
+        parser.print_help()
+        parser.error("Missing name of scan file")
+    submitScan(args[0])
