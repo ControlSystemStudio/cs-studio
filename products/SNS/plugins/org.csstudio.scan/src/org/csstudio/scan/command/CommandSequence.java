c313b8753c657c7d51b48462c804cf6b2495c963
Kay Kasemir <kasemirk@ornl.gov>
2012-07-26 14:21:34 -0400

    SNS Scan System: Simplified CommandSequence. Way to build scan is now: Create list of commands, wrap as CommandSequence, submit.

--- products/SNS/plugins/org.csstudio.scan/src/org/csstudio/scan/command/CommandSequence.java
+++ products/SNS/plugins/org.csstudio.scan/src/org/csstudio/scan/command/CommandSequence.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 /** Helper for creating a sequence of commands
  *
- *  <p>Used by the client to building a sequence of commands
+ *  <p>Used by the client to build a sequence of commands
  *  which is then submitted to the server for execution as a "scan".
  *
  *  <p>Allows both <code>List&lt;Command></code> and plain <code>Command[]</code>.
@@ -77,130 +77,6 @@ public class CommandSequence
         long next = 0;
         for (ScanCommand command : commands)
             next = command.setAddress(next);
-    }
-
-    /** Add a command
-     *  @param command {@link ScanCommand}
-     */
-    public void add(final ScanCommand command)
-    {
-        commands.add(command);
-    }
-
-    /** Add all commands from other sequence
-     *  @param command_sequence {@link CommandSequence}
-     */
-    public void add(final CommandSequence command_sequence)
-    {
-        commands.addAll(command_sequence.commands);
-    }
-
-    /** Add a 'delay' command
-     *  @param seconds Delay in seconds
-     */
-    public void delay(final double seconds)
-    {
-        add(new DelayCommand(seconds));
-    }
-
-    /** Add a 'log' command
-     *  @param device_names List of device names to log
-     */
-    public void log(final String... device_names)
-    {
-        add(new LogCommand(device_names));
-    }
-
-    /** Add a 'loop' command
-     *  @param device_name Name of device to update in loop
-     *  @param start Initial loop value
-     *  @param end Final loop value
-     *  @param stepsize Loop value step size
-     *  @param body Loop body
-     */
-    public void loop(final String device_name,
-            final double start, final double end,
-            final double stepsize,
-            final ScanCommand... body)
-    {
-        add(new LoopCommand(device_name, start, end, stepsize, body));
-    }
-
-    /** Add a 'loop' command
-     *  @param device_name Name of device to update in loop
-     *  @param start Initial loop value
-     *  @param end Final loop value
-     *  @param stepsize Loop value step size
-     *  @param body Loop body
-     */
-    public void loop(final String device_name,
-            final double start, final double end,
-            final double stepsize,
-            final CommandSequence body)
-    {
-        loop(device_name, start, end, stepsize, body.getCommands());
-    }
-
-    /** Add a 'loop' command
-     *  @param device_name Name of device to update in loop
-     *  @param start Initial loop value
-     *  @param end Final loop value
-     *  @param stepsize Loop value step size
-     *  @param body Loop body
-     */
-    public void loop(final String device_name,
-            final double start, final double end,
-            final double stepsize,
-            final List<ScanCommand> body)
-    {
-        add(new LoopCommand(device_name, start, end, stepsize,
-                            body.toArray(new ScanCommand[body.size()])));
-    }
-
-    /** Add a 'set' command that writes to a device
-     *  with default readback, tolerance and no timeout
-     *  @param device_name Name of device
-     *  @param value Value to write to the device
-     */
-    public void set(final String device_name, final Object value)
-    {
-        add(new SetCommand(device_name, value));
-    }
-
-    /** Add a 'set' command that writes to a device
-     *  with default readback, tolerance and no timeout
-     *  @param device_name Name of device
-     *  @param value Value to write to the device
-     *  @param wait Wait for readback to match?
-     */
-    public void set(final String device_name, final Object value, final boolean wait)
-    {
-        add(new SetCommand(device_name, value, wait));
-    }
-
-    /** Add a 'set' command that writes to a device
-     *  @param device_name Name of device
-     *  @param value Value to write to the device
-     *  @param readback Readback device
-     *  @param tolerance Numeric tolerance when checking value
-     *  @param timeout Timeout in seconds, 0 as "forever"
-     */
-    public void set(final String device_name, final Object value,
-            final String readback,
-            final double tolerance, final double timeout)
-    {
-        add(new SetCommand(device_name, value, readback, true, tolerance, timeout));
-    }
-
-    /** Add a 'wait' command that delays the scan until a device reaches a certain value
-     *  @param device_name Name of device to check
-     *  @param desired_value Desired value of the device
-     *  @param tolerance Numeric tolerance when checking value
-     */
-    public void wait(final String device_name, final double desired_value,
-         final double tolerance)
-    {
-        add(new WaitCommand(device_name, Comparison.EQUALS, desired_value, tolerance, 0.0));
     }
 
     // Note: This was called 'print' which causes warnings in a PyDev python
