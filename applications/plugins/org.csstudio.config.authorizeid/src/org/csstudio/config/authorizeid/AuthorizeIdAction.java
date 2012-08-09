ad792e03adcf3fc5f5faa1c09d1ed293daacedf3
Jan Hatje <jan.hatje@desy.de>
2012-08-09 14:55:25 +0200

    o.c.config.authorizeid: remove platform dependency, add view to css menu

--- applications/plugins/org.csstudio.config.authorizeid/src/org/csstudio/config/authorizeid/AuthorizeIdAction.java
+++ applications/plugins/org.csstudio.config.authorizeid/src/org/csstudio/config/authorizeid/AuthorizeIdAction.java
@@ -1,12 +1,28 @@
 package org.csstudio.config.authorizeid;
 
-import org.csstudio.platform.ui.workbench.OpenViewAction;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 
 
-public class AuthorizeIdAction extends OpenViewAction {
+public class AuthorizeIdAction extends AbstractHandler {
 
-	public AuthorizeIdAction() {
-		super(AuthorizeIdView.ID);
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+        IWorkbench workbench = PlatformUI.getWorkbench();
+        IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+        IWorkbenchPage page = window.getActivePage();
+        try {
+			page.showView(AuthorizeIdView.class.getName());
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+        return null;
 	}
 
 }
