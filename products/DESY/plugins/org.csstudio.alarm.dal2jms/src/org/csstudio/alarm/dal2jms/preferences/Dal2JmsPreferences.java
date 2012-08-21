fb3b9c319fae19608951469b0d8e6b5d65f810e5
Jan Hatje <jan.hatje@desy.de>
2012-08-21 13:35:55 +0200

    o.c.alarm.dal2jms: add changes from rel branch

--- products/DESY/plugins/org.csstudio.alarm.dal2jms/src/org/csstudio/alarm/dal2jms/preferences/Dal2JmsPreferences.java
+++ products/DESY/plugins/org.csstudio.alarm.dal2jms/src/org/csstudio/alarm/dal2jms/preferences/Dal2JmsPreferences.java
@@ -23,7 +23,7 @@ package org.csstudio.alarm.dal2jms.preferences;
 
 import javax.annotation.Nonnull;
 
-import org.csstudio.alarm.dal2jms.Activator;
+import org.csstudio.alarm.dal2jms.Dal2JmsActivator;
 import org.csstudio.domain.desy.preferences.AbstractPreference;
 
 /**
@@ -37,22 +37,26 @@ import org.csstudio.domain.desy.preferences.AbstractPreference;
 
  * @param <T> the type of the preference. It must match the type of the default value.
  */
-public final class Preference<T> extends AbstractPreference<T> {
+public final class Dal2JmsPreferences<T> extends AbstractPreference<T> {
 
-    public static final Preference<Integer> JMS_TIME_TO_LIVE_ALARMS =
-        new Preference<Integer>("TimeToLiveAlarms", 3600000);
-    public static final Preference<String> JMS_ALARM_TOPIC_NAME =
-        new Preference<String>("AlarmTopicName", "ALARM");
-    public static final Preference<String> XMPP_DAL2JMS_SERVER_NAME =
-        new Preference<String>("XmppServerName", "krynfs.desy.de");
-    public static final Preference<String> XMPP_DAL2JMS_USER_NAME =
-        new Preference<String>("XmppUserName", "dal2jms");
-    public static final Preference<String> XMPP_DAL2JMS_PASSWORD =
-        new Preference<String>("XmppPassword", "dal2jms");
-    public static final Preference<String> ALARM_CONFIG_XML_FILE_NAME =
-        new Preference<String>("AlarmConfigXMLFileName", "resource/dal2jmsConfig.xml");
+    public static final Dal2JmsPreferences<Integer> JMS_TIME_TO_LIVE_ALARMS =
+        new Dal2JmsPreferences<Integer>("TimeToLiveAlarms", 3600000);
+    public static final Dal2JmsPreferences<String> JMS_ALARM_DESTINATION_TOPIC_NAME =
+        new Dal2JmsPreferences<String>("AlarmTopicName", "ALARM");
+    public static final Dal2JmsPreferences<String> JMS_ACK_SOURCE_TOPIC_NAME =
+        new Dal2JmsPreferences<String>("AckSourceTopicName", "ACK");
+    public static final Dal2JmsPreferences<String> XMPP_DAL2JMS_SERVER_NAME =
+        new Dal2JmsPreferences<String>("XmppServerName", "krynfs.desy.de");
+    public static final Dal2JmsPreferences<String> XMPP_DAL2JMS_USER_NAME =
+        new Dal2JmsPreferences<String>("XmppUserName", "dal2jms");
+    public static final Dal2JmsPreferences<String> XMPP_DAL2JMS_PASSWORD =
+        new Dal2JmsPreferences<String>("XmppPassword", "dal2jms");
+    public static final Dal2JmsPreferences<String> SNAPSHOT_FILENAME =
+        new Dal2JmsPreferences<String>("SnapshotFilename", "dal2jms.ser");
+    public static final Dal2JmsPreferences<Integer> SNAPSHOT_INTERVAL_SECS =
+        new Dal2JmsPreferences<Integer>("SnapshotIntervalSecs", 60);
 
-    private Preference(@Nonnull final String keyAsString, @Nonnull final T defaultValue) {
+    private Dal2JmsPreferences(@Nonnull final String keyAsString, @Nonnull final T defaultValue) {
         super(keyAsString, defaultValue);
     }
 
@@ -60,17 +64,19 @@ public final class Preference<T> extends AbstractPreference<T> {
      * {@inheritDoc}
      */
     @Override
+    @Nonnull
     public String getPluginID() {
-        return Activator.PLUGIN_ID;
+        return Dal2JmsActivator.PLUGIN_ID;
     }
 
     /**
      * {@inheritDoc}
      */
     @SuppressWarnings("unchecked")
+    @Nonnull
     @Override
     protected Class<? extends AbstractPreference<T>> getClassType() {
-        return (Class<? extends AbstractPreference<T>>) Preference.class;
+        return (Class<? extends AbstractPreference<T>>) Dal2JmsPreferences.class;
     }
 
 }
