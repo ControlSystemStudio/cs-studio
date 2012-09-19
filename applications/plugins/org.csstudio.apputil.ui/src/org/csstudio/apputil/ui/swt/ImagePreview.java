0710bb84576ccd37c4434d34dec6475ebccc4311
shroffk <shroffk@bnl.gov>
2012-09-19 09:51:27 -0400

    ..apputil.ui: the image is always centered
    Change-Id: I22a9a237118c09ed112838af00a3ac8834abde0a
    Signed-off-by: shroffk <shroffk@bnl.gov>

--- applications/plugins/org.csstudio.apputil.ui/src/org/csstudio/apputil/ui/swt/ImagePreview.java
+++ applications/plugins/org.csstudio.apputil.ui/src/org/csstudio/apputil/ui/swt/ImagePreview.java
@@ -19,121 +19,117 @@ import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 
-/** Widget that displays an image.
- *  <p>
- *  Similar to a Label with image, but image is resized to fit the widget.
- *  @author Kay Kasemir
+/**
+ * Widget that displays an image.
+ * <p>
+ * Similar to a Label with image, but image is resized to fit the widget.
+ * 
+ * @author Kay Kasemir
  */
-public class ImagePreview extends Canvas implements DisposeListener, PaintListener
-{
-    /** Name of image file */
-    private String filename;
+public class ImagePreview extends Canvas implements DisposeListener,
+		PaintListener {
+	/** Name of image file */
+	private String filename;
 
-    /** Image or <code>null</code> */
-    private Image image = null;
+	/** Image or <code>null</code> */
+	private Image image = null;
 
-    /** Additional short message or <code>null</code> */
-    private String message;
+	/** Additional short message or <code>null</code> */
+	private String message;
 
-    public ImagePreview(final Composite parent)
-    {
-        this(parent, null, null);
-    }
+	public ImagePreview(final Composite parent) {
+		this(parent, null, null);
+	}
 
-    public ImagePreview(final Composite parent, final String message, final String image_filename)
-    {
-        super(parent, 0);
-        this.message = message;
+	public ImagePreview(final Composite parent, final String message,
+			final String image_filename) {
+		super(parent, 0);
+		this.message = message;
 
-        addDisposeListener(this);
-        setImage(image_filename);
+		addDisposeListener(this);
+		setImage(image_filename);
 
-        addPaintListener(this);
-    }
+		addPaintListener(this);
+	}
 
+	/**
+	 * Set image to display
+	 * 
+	 * @param image_filename
+	 *            Name of image file or <code>null</code> for no image
+	 */
+	public void setImage(final String image_filename) {
+		this.filename = image_filename;
+		// Remove previous image, if there was one
+		if (image != null) {
+			image.dispose();
+			image = null;
+			setToolTipText(""); //$NON-NLS-1$
+		}
+		if (image_filename == null)
+			return;
+		try {
+			image = new Image(getDisplay(), image_filename);
+		} catch (Exception ex) {
+			setToolTipText("Error: " + ex.getMessage()); //$NON-NLS-1$
+			return;
+		}
+		setToolTipText(image_filename);
+	}
 
-    /** Set image to display
-     *  @param image_filename Name of image file or <code>null</code> for no image
-     */
-    public void setImage(final String image_filename)
-    {
-        this.filename = image_filename;
-        // Remove previous image, if there was one
-        if (image != null)
-        {
-            image.dispose();
-            image = null;
-            setToolTipText(""); //$NON-NLS-1$
-        }
-        if (image_filename == null)
-            return;
-        try
-        {
-            image = new Image(getDisplay(), image_filename);
-        }
-        catch (Exception ex)
-        {
-            setToolTipText("Error: " + ex.getMessage()); //$NON-NLS-1$
-            return;
-        }
-        setToolTipText(image_filename);
-    }
+	/** @see Control */
+	@Override
+	public Point computeSize(final int wHint, final int hHint) {
+		if (image == null)
+			return new Point(1, 1);
+		return new Point(200, 200);
+	}
 
-    /** @see Control */
-    @Override
-    public Point computeSize(final int wHint, final int hHint)
-    {
-        if (image == null)
-            return new Point(1, 1);
-        return new Point(200, 200);
-    }
+	/** @see PaintListener */
+	@Override
+	public void paintControl(final PaintEvent e) {
+		final Rectangle bounds = getBounds();
+		final GC gc = e.gc;
+		if (image != null) { // Draw image to fit widget bounds, maintaining
+								// aspect ratio
+			final Rectangle img = image.getBounds();
+			// Start with original size
+			int width = img.width;
+			int height = img.height;
+			int destX = 0;
+			int destY = 0;
+			if (width > bounds.width) { // Too wide?
+				width = bounds.width;
+				height = width * img.height / img.width;
+			}
+			if (height > bounds.height) { // Too high?
+				height = bounds.height;
+				width = height * img.width / img.height;
+			}
+			destX = (bounds.width - width) / 2;
+			destY = (bounds.height - height) / 2;
+			gc.drawImage(image, 0, 0, img.width, img.height, destX, destY,
+					width, height);
+		}
+		if (message != null) { // Show message
+			final Point extend = gc.textExtent(message, SWT.DRAW_DELIMITER);
+			final int x = (bounds.width - extend.x) / 2;
+			final int y = (bounds.height - extend.y) / 2;
+			gc.drawText(message, x, y, SWT.DRAW_DELIMITER
+					| SWT.DRAW_TRANSPARENT);
+			return;
+		}
+	}
 
-    /** @see PaintListener */
-    @Override
-    public void paintControl(final PaintEvent e)
-    {
-        final Rectangle bounds = getBounds();
-        final GC gc = e.gc;
-        if (image != null)
-        {   // Draw image to fit widget bounds, maintaining aspect ratio
-            final Rectangle img = image.getBounds();
-            // Start with original size
-            int width = img.width;
-            int height = img.height;
-            if (width > bounds.width)
-            {   // Too wide?
-                width = bounds.width;
-                height = width * img.height / img.width;
-            }
-            if (height > bounds.height)
-            {   // Too high?
-                height = bounds.height;
-                width = height * img.width / img.height;
-            }
-            gc.drawImage(image, 0, 0, img.width, img.height,
-                    0, 0, width, height);
-        }
-        if (message != null)
-        {   // Show message
-            final Point extend = gc.textExtent(message, SWT.DRAW_DELIMITER);
-            final int x = (bounds.width - extend.x)/2;
-            final int y = (bounds.height - extend.y)/2;
-            gc.drawText(message, x, y, SWT.DRAW_DELIMITER | SWT.DRAW_TRANSPARENT);
-            return;
-        }
-    }
+	/** @see DisposeListener */
+	@Override
+	public void widgetDisposed(final DisposeEvent e) {
+		if (image != null)
+			image.dispose();
+	}
 
-    /** @see DisposeListener */
-    @Override
-    public void widgetDisposed(final DisposeEvent e)
-    {
-        if (image != null)
-            image.dispose();
-    }
-
-    /** @return Image file name */
-    public String getImageFileName()
-    {
-        return filename;
-    }
+	/** @return Image file name */
+	public String getImageFileName() {
+		return filename;
+	}
 }
