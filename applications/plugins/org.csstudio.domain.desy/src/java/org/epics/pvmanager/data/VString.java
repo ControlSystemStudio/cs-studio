30fe0f9d0f46a782d302afd2823d7ace7209433c
xuwenhu <xuwenhu@krykpcg.desy.de>
2012-09-25 09:12:53 +0200

    o.c.archive.common: error free compile

---
+++ applications/plugins/org.csstudio.domain.desy/src/java/org/epics/pvmanager/data/VString.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.pvmanager.data;
+
+/**
+ * Scalar string with alarm and timestamp.
+ *
+ * @author carcassi
+ */
+public interface VString extends Scalar<String>, Alarm, Time {
+}
