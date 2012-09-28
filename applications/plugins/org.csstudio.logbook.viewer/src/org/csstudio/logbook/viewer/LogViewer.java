cd3d4ba2f3435283d4a9e742db63e30d7807fd8c
Kunal Shroff <shroffk@bnl.gov>
2012-09-28 17:22:40 -0400

    ..LogViewer: allow resizing of controls
    Change-Id: I547658a0f230ee9fee7365d26061eee6223d811d
    Signed-off-by: Kunal Shroff <shroffk@bnl.gov>

--- applications/plugins/org.csstudio.logbook.viewer/src/org/csstudio/logbook/viewer/LogViewer.java
+++ applications/plugins/org.csstudio.logbook.viewer/src/org/csstudio/logbook/viewer/LogViewer.java
@@ -16,6 +16,8 @@ import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FormAttachment;
@@ -27,6 +29,8 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.events.MouseMoveListener;
+import org.eclipse.swt.events.MouseEvent;
 
 /**
  * @author shroffk
@@ -41,6 +45,7 @@ public class LogViewer extends ViewPart {
 
 	private LogbookClient logbookClient;
 	private LogEntryWidget logEntryWidget;
+	private Label label;
 
 	public LogViewer() {
 	}
@@ -56,49 +61,62 @@ public class LogViewer extends ViewPart {
 		lblLogQuery.setText("Log Query:");
 
 		text = new Text(parent, SWT.BORDER);
-		FormData fd_text = new FormData();
-		fd_text.left = new FormAttachment(lblLogQuery, 5);
-		fd_text.top = new FormAttachment(0, 5);
-		fd_text.right = new FormAttachment(100, -100);
-		text.setLayoutData(fd_text);
-
-		Button btnSearch = new Button(parent, SWT.NONE);
-		FormData fd_btnSearch = new FormData();
-		fd_btnSearch.left = new FormAttachment(text, 5);
-		fd_btnSearch.top = new FormAttachment(0, 3);
-		fd_btnSearch.right = new FormAttachment(100, -3);
-		btnSearch.setLayoutData(fd_btnSearch);
-		btnSearch.addSelectionListener(new SelectionAdapter() {
-
+		text.addKeyListener(new KeyAdapter() {
 			@Override
-			public void widgetSelected(SelectionEvent e) {
-				if (logbookClient == null) {
-					try {
-						logbookClient = LogbookClientManager
-								.getLogbookClientFactory().getClient();
-					} catch (Exception e1) {
-						e1.printStackTrace();
-					}
-				} else {
-					Map<String, String> findAttributeMap = new HashMap<String, String>();
-					findAttributeMap.put("search", text.getText());
-					try {
-						final Collection<LogEntry> logEntries = logbookClient
-								.findLogEntries(findAttributeMap);
-						Display.getDefault().asyncExec(new Runnable() {
+			public void keyReleased(KeyEvent e) {
+				if (e.keyCode == SWT.CR || e.keyCode == SWT.KEYPAD_CR) {
+					if (logbookClient == null) {
+						try {
+							logbookClient = LogbookClientManager
+									.getLogbookClientFactory().getClient();
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+					} else {
+						Map<String, String> findAttributeMap = new HashMap<String, String>();
+						findAttributeMap.put("search", text.getText());
+						try {
+							final Collection<LogEntry> logEntries = logbookClient
+									.findLogEntries(findAttributeMap);
+							Display.getDefault().asyncExec(new Runnable() {
 
-							@Override
-							public void run() {
-								logEntryTable.setLogs(logEntries);
-							}
-						});
-					} catch (Exception e1) {
-						e1.printStackTrace();
+								@Override
+								public void run() {
+									logEntryTable.setLogs(logEntries);
+								}
+							});
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
 					}
 				}
 			}
 		});
-		btnSearch.setText("Search");
+		FormData fd_text = new FormData();
+		fd_text.left = new FormAttachment(lblLogQuery, 5);
+		fd_text.top = new FormAttachment(0, 5);
+		fd_text.right = new FormAttachment(100, -3);
+		text.setLayoutData(fd_text);
+
+		label = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
+		label.addMouseMoveListener(new MouseMoveListener() {
+			public void mouseMove(MouseEvent e) {
+				FormData fd = (FormData) label.getLayoutData();
+				long calNumerator = fd.top.numerator + (e.y * 100)
+						/ e.display.getActiveShell().getClientArea().height;
+				fd.top = new FormAttachment((int) calNumerator);
+				label.setLayoutData(fd);
+				label.getParent().layout();
+				logEntryTable.layout();
+			}
+		});
+		label.setCursor(Display.getCurrent().getSystemCursor(
+				SWT.CURSOR_SIZENS));
+		FormData fd_label = new FormData();
+		fd_label.top = new FormAttachment(100);
+		fd_label.right = new FormAttachment(100, -2);
+		fd_label.left = new FormAttachment(0, 2);
+		label.setLayoutData(fd_label);
 
 		logEntryTable = new LogEntryTable(parent, SWT.NONE);
 		fd_lblLogQuery.left = new FormAttachment(logEntryTable, 0, SWT.LEFT);
@@ -106,7 +124,8 @@ public class LogViewer extends ViewPart {
 		fd_logEntryTable.top = new FormAttachment(text, 5);
 		fd_logEntryTable.right = new FormAttachment(100, -3);
 		fd_logEntryTable.left = new FormAttachment(0, 3);
-		fd_logEntryTable.bottom = new FormAttachment(100, -5);
+		fd_logEntryTable.bottom = new FormAttachment(label, -5);
+		
 		logEntryTable.setLayoutData(fd_logEntryTable);
 		logEntryTable
 				.addSelectionChangedListener(new ISelectionChangedListener() {
@@ -117,17 +136,15 @@ public class LogViewer extends ViewPart {
 							IStructuredSelection selection = (IStructuredSelection) event
 									.getSelection();
 							if (selection != null && !selection.isEmpty()) {
-								FormData fd = (FormData) logEntryTable
-										.getLayoutData();
-								fd.bottom = new FormAttachment(60);
-								logEntryTable.setLayoutData(fd);
+								FormData fd = (FormData) label.getLayoutData();
+								fd.top = new FormAttachment(60);
+								label.setLayoutData(fd);
 								logEntryWidget.setLogEntry((LogEntry) selection
 										.getFirstElement());
 							} else {
-								FormData fd = (FormData) logEntryTable
-										.getLayoutData();
-								fd.bottom = new FormAttachment(100, -5);
-								logEntryTable.setLayoutData(fd);
+								FormData fd = (FormData) label.getLayoutData();
+								fd.top = new FormAttachment(100);
+								label.setLayoutData(fd);
 								logEntryWidget.setLogEntry(null);
 							}
 							parent.layout();
@@ -137,11 +154,11 @@ public class LogViewer extends ViewPart {
 		logEntryWidget = new LogEntryWidget(parent, SWT.NONE);
 		FormData fd_logEntryWidget = new FormData();
 		fd_logEntryWidget.right = new FormAttachment(100, -3);
-		fd_logEntryWidget.top = new FormAttachment(logEntryTable, 5);
+		fd_logEntryWidget.top = new FormAttachment(label, -5);
 		fd_logEntryWidget.left = new FormAttachment(0, 3);
 		fd_logEntryWidget.bottom = new FormAttachment(100, -5);
 		logEntryWidget.setLayoutData(fd_logEntryWidget);
-		
+
 		PopupMenuUtil.installPopupForView(logEntryTable, getSite(),
 				logEntryTable);
 	}
