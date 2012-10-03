28b453ff0a227e2adc8abeae660ecf35e954cef1
Kunal Shroff <shroffk@bnl.gov>
2012-10-03 10:40:33 -0400

    ..alarm.beast.ui.adapter: adding adapters for beast datatypes
    Change-Id: Ie42753e78ef25a663a14fd0a53fefb3738ef94b5
    Signed-off-by: Kunal Shroff <shroffk@bnl.gov>

---
+++ applications/plugins/org.csstudio.alarm.beast.ui.adapters/src/org/csstudio/beast/ui/adapters/LogbookBuilderFactory.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package org.csstudio.beast.ui.adapters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.csstudio.alarm.beast.client.AlarmTreeItem;
+import org.csstudio.alarm.beast.client.AlarmTreeLeaf;
+import org.csstudio.alarm.beast.ui.actions.AlarmTextHelper;
+import org.csstudio.logbook.LogEntry;
+import org.csstudio.logbook.LogEntryBuilder;
+import org.eclipse.core.runtime.IAdapterFactory;
+
+/**
+ * @author shroffk
+ * 
+ */
+public class LogbookBuilderFactory implements IAdapterFactory {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.IAdapterFactory#getAdapter(java.lang.Object,
+	 * java.lang.Class)
+	 */
+	@Override
+	public Object getAdapter(Object adaptableObject, Class adapterType) {
+		if (adapterType == LogEntryBuilder.class) {
+			if (adaptableObject instanceof AlarmTreeItem) {
+				AlarmTreeItem alarmTreeItem = ((AlarmTreeItem) adaptableObject);
+				return LogEntryBuilder.withText("alarmTreeItem");
+			} else if (adaptableObject instanceof AlarmTreeLeaf) {
+				AlarmTreeLeaf alarmTreeLeaf = ((AlarmTreeLeaf) adaptableObject);
+				List<AlarmTreeLeaf> alarms = new ArrayList<AlarmTreeLeaf>();
+				alarms.add(alarmTreeLeaf);
+				return LogEntryBuilder.withText(AlarmTextHelper.createAlarmInfoText(alarms));
+			}
+		}
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapterList()
+	 */
+	@Override
+	public Class[] getAdapterList() {
+		return new Class[] { LogEntryBuilder.class };
+	}
+
+}
