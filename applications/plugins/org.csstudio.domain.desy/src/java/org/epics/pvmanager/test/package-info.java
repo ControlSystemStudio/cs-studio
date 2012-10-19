42fc258399dbc68652a57dc127946adaf8299a14
jhatje <jan.hatje@desy.de>
2012-10-19 15:38:48 +0200

    o.c.domain.desy: remove accidentally merged changes from archiver branch

--- applications/plugins/org.csstudio.domain.desy/src/java/org/epics/pvmanager/test/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright (C) 2010-12 Brookhaven National Laboratory
- * All rights reserved. Use is subject to license terms.
- */
-/**
- * Support for a data source that simulates various error conditions.
- */
-package org.epics.pvmanager.test;
