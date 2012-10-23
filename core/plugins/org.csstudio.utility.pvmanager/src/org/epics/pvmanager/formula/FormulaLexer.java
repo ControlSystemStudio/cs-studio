84ff19d3c067faf67cb87aa9b9949a51770499e5
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-10-23 10:55:06 -0400

    pvmanager: update to 1.2-SNAPSHOT

--- core/plugins/org.csstudio.utility.pvmanager/src/org/epics/pvmanager/formula/FormulaLexer.java
+++ core/plugins/org.csstudio.utility.pvmanager/src/org/epics/pvmanager/formula/FormulaLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.4 org\\epics\\pvmanager\\formula\\Formula.g 2012-10-15 13:34:17
+// $ANTLR 3.4 org\\epics\\pvmanager\\formula\\Formula.g 2012-10-23 10:46:18
 
   package org.epics.pvmanager.formula;
 
