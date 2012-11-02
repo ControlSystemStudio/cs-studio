95681a22b848f1dd6b4e720115651aff3b5dd442
Kunal Shroff <shroffk@bnl.gov>
2012-11-02 10:49:16 -0400

    ..channelfinder.api: updating to version 2.2.0
    Change-Id: Iac416fb4f789573626095fddf53248c29e38e9d0
    Signed-off-by: Kunal Shroff <shroffk@bnl.gov>

--- applications/plugins/gov.bnl.channelfinder.api/src/gov/bnl/channelfinder/api/XmlChannels.java
+++ applications/plugins/gov.bnl.channelfinder.api/src/gov/bnl/channelfinder/api/XmlChannels.java
@@ -1,9 +1,8 @@
-/*
- * Copyright (c) 2010 Brookhaven National Laboratory
- * Copyright (c) 2010 Helmholtz-Zentrum Berlin fuer Materialien und Energie GmbH
- * Subject to license terms and conditions.
+/**
+ * Copyright (C) 2010-2012 Brookhaven National Laboratory
+ * Copyright (C) 2010-2012 Helmholtz-Zentrum Berlin für Materialien und Energie GmbH
+ * All rights reserved. Use is subject to license terms.
  */
-
 package gov.bnl.channelfinder.api;
 
 import java.util.ArrayList;
