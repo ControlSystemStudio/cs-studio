1f38561353698a5f03c3292dc3da76058b2cacaa
jhatje <jan.hatje@desy.de>
2012-11-16 09:10:45 +0100

    Desy plugins: update menu

--- applications/plugins/org.csstudio.ams.alarmchainmanager/src/org/csstudio/ams/alarmchainmanager/actions/ShowAlarmChain.java
+++ applications/plugins/org.csstudio.ams.alarmchainmanager/src/org/csstudio/ams/alarmchainmanager/actions/ShowAlarmChain.java
@@ -27,57 +27,32 @@
 package org.csstudio.ams.alarmchainmanager.actions;
 
 import org.csstudio.ams.alarmchainmanager.views.AlarmChainManagerView;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
 /**
  * @author Markus Moeller
  *
  */
-public class ShowViewAction implements IWorkbenchWindowActionDelegate
-{
+public class ShowAlarmChain extends AbstractHandler {
 
-    /* (non-Javadoc)
-     * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#dispose()
-     */
-    @Override
-    public void dispose() {
-        // TODO Auto-generated method stub
-    }
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
 
-    /* (non-Javadoc)
-     * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(org.eclipse.ui.IWorkbenchWindow)
-     */
-    @Override
-    public void init(IWorkbenchWindow window) {
-        // TODO Auto-generated method stub
-    }
-
-    /* (non-Javadoc)
-     * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
-     */
-    @Override
-    public void run(IAction action) {
-        try {
-            IWorkbench workbench = PlatformUI.getWorkbench();
-            IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
-            IWorkbenchPage page = window.getActivePage();
-            page.showView(AlarmChainManagerView.ID);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /* (non-Javadoc)
-     * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
-     */
-    @Override
-    public void selectionChanged(IAction action, ISelection selection) {
-        // TODO Auto-generated method stub
-    }
+        final IWorkbench workbench = PlatformUI.getWorkbench();
+        final IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+        final IWorkbenchPage page = window.getActivePage();
+	    try {
+	        page.showView(AlarmChainManagerView.ID);
+	    } catch (final PartInitException e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
 }
--- applications/plugins/org.csstudio.utility.caSnooperUi/src/org/csstudio/utility/caSnooperUi/ui/ChangeView/ShowSnooper.java
+++ applications/plugins/org.csstudio.utility.caSnooperUi/src/org/csstudio/utility/caSnooperUi/ui/ChangeView/ShowSnooper.java
@@ -1,26 +1,28 @@
-
-package org.csstudio.nams.configurator.views;
+package org.csstudio.utility.caSnooperUi.ui.ChangeView;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.WorkbenchException;
 
-public class ShowAMS extends AbstractHandler {
+
+public class ShowSnooper extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
         final IWorkbench workbench = PlatformUI.getWorkbench();
         final IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+        final IWorkbenchPage page = window.getActivePage();
 	    try {
-	        workbench.showPerspective("org.csstudio.nams.newconfigurator.perspective", window);
-	    } catch (WorkbenchException e) {
-			e.printStackTrace();
-		}
+	        page.showView(SnooperView.ID);
+	    } catch (final PartInitException e) {
+	        e.printStackTrace();
+	    }
 	    return null;
 	}
 }
