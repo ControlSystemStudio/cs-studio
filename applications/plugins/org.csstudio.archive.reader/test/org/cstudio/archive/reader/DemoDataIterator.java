8ac6d3bf621e3113631efa89386e667a2643dfb3
Kay Kasemir <kasemirk@ornl.gov>
2012-11-27 15:00:13 -0500

    o.c.archive.reader: MergingValueIterator uses VType

---
+++ applications/plugins/org.csstudio.archive.reader/test/org/cstudio/archive/reader/DemoDataIterator.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.cstudio.archive.reader;
+
+import org.csstudio.archive.reader.ValueIterator;
+import org.csstudio.archive.vtype.ArchiveVString;
+import org.epics.pvmanager.data.AlarmSeverity;
+import org.epics.pvmanager.data.VType;
+import org.epics.util.time.Timestamp;
+
+/** Value iterator that produces demo samples 1 ... 10
+ *  @author Kay Kasemir
+ */
+class DemoDataIterator implements ValueIterator
+{
+    final private String name;
+    final private int start_time;
+    private int i = 0;
+    private boolean open = true;
+
+    public DemoDataIterator(final String name, final int start_time)
+    {
+        this.name = name;
+        this.start_time = start_time;
+    }
+
+    public DemoDataIterator(final String name)
+    {
+        this(name, 0);
+    }
+
+
+    @Override
+    public boolean hasNext()
+    {
+        return i < 10;
+    }
+
+    @Override
+    public VType next() throws Exception
+    {
+        ++i;
+        return new ArchiveVString(Timestamp.of(start_time + i, 0), AlarmSeverity.NONE, "OK", name + " " + i);
+    }
+
+    @Override
+    public void close()
+    {
+        if (!open)
+            throw new IllegalStateException("Closed twice");
+        open = false;
+    }
+
+    public boolean isOpen()
+    {
+        return open;
+    }
+}
---
+++ applications/plugins/org.csstudio.archive.reader/test/org/cstudio/archive/reader/DemoValueIteratorUnitTest.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.cstudio.archive.reader;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.csstudio.archive.reader.ValueIterator;
+import org.junit.Test;
+
+/** JUnit test of the {@link DemoDataIterator}
+ *  @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class DemoValueIteratorUnitTest
+{
+    @Test
+    public void testDemoDataIterator() throws Exception
+    {
+        System.out.println("DemoDataIterator");
+        final ValueIterator iter = new DemoDataIterator("Demo");
+        int count = 0;
+        while (iter.hasNext())
+        {
+            System.out.println(iter.next());
+            ++count;
+        }
+        assertThat(count, equalTo(10));
+        iter.close();
+    }
+}
