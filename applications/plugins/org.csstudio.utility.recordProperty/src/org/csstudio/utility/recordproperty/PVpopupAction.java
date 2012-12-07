0eb5f039ea0a74b624f88bf7c46a356e27bd1820
jhatje <jan.hatje@desy.de>
2012-12-07 13:26:31 +0100

    o.c.utility.recordproperty: add object contribution and dnd

--- applications/plugins/org.csstudio.utility.recordProperty/src/org/csstudio/utility/recordproperty/PVpopupAction.java
+++ applications/plugins/org.csstudio.utility.recordProperty/src/org/csstudio/utility/recordproperty/PVpopupAction.java
@@ -1,22 +1,29 @@
 package org.csstudio.utility.recordproperty;
 
+import org.csstudio.csdata.ProcessVariable;
+import org.csstudio.ui.util.AdapterUtil;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.handlers.HandlerUtil;
 
-
-/** Handle activation of Record Property from the object contrib. context menu.
- *  @author Kay Kasemir
- *  @author Helge Rickens
- *  @author Rok Povsic
+/**
+ * Handle activation of Record Property from the object contrib. context menu.
+ * 
+ * @author Kay Kasemir
+ * @author Helge Rickens
+ * @author Rok Povsic
  */
-//TODO jhatje: implement new datatype
-public class PVpopupAction {//extends ProcessVariablePopupAction {
-//
-//	@Override
-//	public void handlePVs(IProcessVariable[] pv_names) {
-//		
-//        if (pv_names.length < 1)
-//            return;
-//        RecordPropertyView.activateWithPV(pv_names[0]);
-//		
-//	}
+public class PVpopupAction extends AbstractHandler {
 
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		final ISelection selection = HandlerUtil.getActiveMenuSelection(event);
+		final ProcessVariable[] pvs = AdapterUtil.convert(selection,
+				ProcessVariable.class);
+		RecordPropertyView.activateWithPV(pvs[0]);
+		return null;
+	}
 }
