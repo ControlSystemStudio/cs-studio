6ad20f5465258317d9733e4862e4209b2f046925
Kunal Shroff <shroffk@bnl.gov>
2012-12-19 17:11:58 -0500

    *ImageStackWidget: prototype for using InputStream instead of files
    Change-Id: If6d0fb8fe953cee0e167af7d96dd1fa336a27e7e
    Signed-off-by: Kunal Shroff <shroffk@bnl.gov>

--- core/plugins/org.csstudio.ui.util/src/org/csstudio/ui/util/widgets/ImageStackWidget.java
+++ core/plugins/org.csstudio.ui.util/src/org/csstudio/ui/util/widgets/ImageStackWidget.java
@@ -11,7 +11,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jface.viewers.ListViewer;
 import org.eclipse.swt.SWT;
@@ -43,190 +45,192 @@ import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
 /**
- * A widget to display a set of Images 
+ * A widget to display a set of Images
  * 
  * @author shroffk
  * 
  */
 public class ImageStackWidget extends Composite {
 
-	private boolean editable;
-	private List<String> imageFilenames = new ArrayList<String>();
-	private String selectedImageFile;
+    private boolean editable;
+    private String selectedImageName;
 
-	protected final PropertyChangeSupport changeSupport = new PropertyChangeSupport(
-			this);
-	private ImagePreview imagePreview;
-	private Table table;
-	private TableViewer tableViewer;
+    protected final PropertyChangeSupport changeSupport = new PropertyChangeSupport(
+	    this);
+    private ImagePreview imagePreview;
+    private Table table;
+    private TableViewer tableViewer;
+    private Map<String, InputStream> imageInputStreamsMap = new HashMap<String, InputStream>();
 
-	public void addPropertyChangeListener(PropertyChangeListener listener) {
-		changeSupport.addPropertyChangeListener(listener);
-	}
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+	changeSupport.addPropertyChangeListener(listener);
+    }
 
-	public void removePropertyChangeListener(PropertyChangeListener listener) {
-		changeSupport.removePropertyChangeListener(listener);
-	}
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+	changeSupport.removePropertyChangeListener(listener);
+    }
 
-	public ImageStackWidget(final Composite parent, int style) {
-		super(parent, style);
-		setLayout(new FormLayout());
+    public ImageStackWidget(final Composite parent, int style) {
+	super(parent, style);
+	setLayout(new FormLayout());
 
-		tableViewer = new TableViewer(this, SWT.NONE);
-		table = tableViewer.getTable();
-		table.setBackground(SWTResourceManager
-				.getColor(SWT.COLOR_WIDGET_LIGHT_SHADOW));
-		FormData fd_table = new FormData();
-		fd_table.top = new FormAttachment(0, 10);
-		fd_table.right = new FormAttachment(100, -10);
-		fd_table.bottom = new FormAttachment(100, -10);
-		fd_table.left = new FormAttachment(100, -124);
-		table.setLayoutData(fd_table);
+	tableViewer = new TableViewer(this, SWT.NONE);
+	table = tableViewer.getTable();
+	table.setBackground(SWTResourceManager
+		.getColor(SWT.COLOR_WIDGET_LIGHT_SHADOW));
+	FormData fd_table = new FormData();
+	fd_table.top = new FormAttachment(0, 10);
+	fd_table.right = new FormAttachment(100, -10);
+	fd_table.bottom = new FormAttachment(100, -10);
+	fd_table.left = new FormAttachment(100, -124);
+	table.setLayoutData(fd_table);
 
-		TableViewerColumn tableViewerColumn = new TableViewerColumn(
-				tableViewer, SWT.NONE);
-		tableViewerColumn.setLabelProvider(new StyledCellLabelProvider() {
-			@Override
-			public void update(ViewerCell cell) {
-				// TODO does not center
-				// TODO does not preserve aspect ratio
-				// use the OwnerDrawLabelProvider
-				try {
-					String fileName = cell.getElement() == null ? "" : cell
-							.getElement().toString();
-					ImageData imageData = new ImageData(new FileInputStream(
-							fileName));
-					cell.setImage(new Image(getDisplay(), imageData.scaledTo(
-							90, 90)));
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+	TableViewerColumn tableViewerColumn = new TableViewerColumn(
+		tableViewer, SWT.NONE);
+	tableViewerColumn.setLabelProvider(new StyledCellLabelProvider() {
+	    @Override
+	    public void update(ViewerCell cell) {
+		// TODO does not center
+		// TODO does not preserve aspect ratio
+		// use the OwnerDrawLabelProvider
+		String imageName = cell.getElement() == null ? "" : cell
+			.getElement().toString();
+		ImageData imageData = new ImageData(imageInputStreamsMap
+			.get(imageName));
+		cell.setImage(new Image(getDisplay(), imageData
+			.scaledTo(90, 90)));
+	    }
+	});
+	TableColumn tblclmnImage = tableViewerColumn.getColumn();
+	tblclmnImage.setResizable(false);
+	tblclmnImage.setWidth(90);
+	tableViewer.setContentProvider(new IStructuredContentProvider() {
+
+	    @Override
+	    public void inputChanged(Viewer viewer, Object oldInput,
+		    Object newInput) {
+
+	    }
+
+	    @Override
+	    public void dispose() {
+
+	    }
+
+	    @Override
+	    public Object[] getElements(Object inputElement) {
+		return (Object[]) inputElement;
+	    }
+	});
+
+	tableViewer
+		.addSelectionChangedListener(new ISelectionChangedListener() {
+
+		    @Override
+		    public void selectionChanged(SelectionChangedEvent event) {
+			ISelection selection = event.getSelection();
+			if (selection != null
+				&& selection instanceof IStructuredSelection) {
+			    IStructuredSelection sel = (IStructuredSelection) selection;
+			    if (sel.size() == 1) {
+				setSelectedImageName((String) sel.iterator()
+					.next());
+			    }
 			}
+		    }
 		});
-		TableColumn tblclmnImage = tableViewerColumn.getColumn();
-		tblclmnImage.setResizable(false);
-		tblclmnImage.setWidth(90);
-		tableViewer.setContentProvider(new IStructuredContentProvider() {
+	imagePreview = new ImagePreview(this);
+	FormData fd_imagePreview = new FormData();
+	fd_imagePreview.right = new FormAttachment(table, -10);
+	fd_imagePreview.top = new FormAttachment(0, 10);
+	fd_imagePreview.left = new FormAttachment(0, 10);
+	fd_imagePreview.bottom = new FormAttachment(100, -10);
+	imagePreview.setLayoutData(fd_imagePreview);
+	this.addPropertyChangeListener(new PropertyChangeListener() {
 
-			@Override
-			public void inputChanged(Viewer viewer, Object oldInput,
-					Object newInput) {
-
+	    @Override
+	    public void propertyChange(PropertyChangeEvent evt) {
+		switch (evt.getPropertyName()) {
+		case "editable":
+		    break;
+		case "imageInputStreamsMap":
+		    if (imageInputStreamsMap != null
+			    && !imageInputStreamsMap.isEmpty()) {
+			// Populate the list on the side
+			tableViewer.setInput(imageInputStreamsMap.keySet()
+				.toArray(
+					new String[imageInputStreamsMap
+						.keySet().size()]));
+			// tableViewer.setInput(imageInputStreamsMap.entrySet().toArray(new
+			// Entry<String,InputStream>[]));
+			// Set the Selected Image
+			if (imageInputStreamsMap.keySet().contains(
+				selectedImageName)) {
+			    imagePreview.setImage(imageInputStreamsMap
+				    .get(selectedImageName));
+			} else {
+			    imagePreview.setImage(imageInputStreamsMap.values()
+				    .iterator().next());
 			}
+		    } else {
+			tableViewer.setInput(null);
+			imagePreview.setImage((InputStream) null);
+		    }
+		    tableViewer.refresh();
+		    imagePreview.redraw();
+		    break;
+		case "selectedImageName":
+		    imagePreview.setImage(imageInputStreamsMap
+			    .get(selectedImageName));
+		    imagePreview.redraw();
+		    break;
+		default:
+		    break;
+		}
 
-			@Override
-			public void dispose() {
+	    }
+	});
+    }
 
-			}
+    public boolean isEditable() {
+	return editable;
+    }
 
-			@Override
-			public Object[] getElements(Object inputElement) {
-				return (Object[]) inputElement;
-			}
-		});
+    public void setEditable(boolean editable) {
+	boolean oldValue = this.editable;
+	this.editable = editable;
+	changeSupport.firePropertyChange("editable", oldValue, this.editable);
+    }
 
-		tableViewer
-				.addSelectionChangedListener(new ISelectionChangedListener() {
+    public void setImageInputStream(
+	    Map<String, InputStream> imageInputStreamsMap) {
+	Map<String, InputStream> oldValue = this.imageInputStreamsMap;
+	this.imageInputStreamsMap = imageInputStreamsMap == null ? new HashMap<String, InputStream>()
+		: imageInputStreamsMap;
+	changeSupport.firePropertyChange("imageInputStreamsMap", oldValue,
+		this.imageInputStreamsMap);
+    }
 
-					@Override
-					public void selectionChanged(SelectionChangedEvent event) {
-						ISelection selection = event.getSelection();
-						if (selection != null
-								&& selection instanceof IStructuredSelection) {
-							IStructuredSelection sel = (IStructuredSelection) selection;
-							if (sel.size() == 1) {
-								setSelectedImageFile((String) sel.iterator()
-										.next());
-							}
-						}
-					}
-				});
-		imagePreview = new ImagePreview(this);
-		FormData fd_imagePreview = new FormData();
-		fd_imagePreview.right = new FormAttachment(table, -10);
-		fd_imagePreview.top = new FormAttachment(0, 10);
-		fd_imagePreview.left = new FormAttachment(0, 10);
-		fd_imagePreview.bottom = new FormAttachment(100, -10);
-		imagePreview.setLayoutData(fd_imagePreview);
-		this.addPropertyChangeListener(new PropertyChangeListener() {
+    public void addImage(String imageName, InputStream imageInputStream) {
+	Map<String, InputStream> oldValue = this.imageInputStreamsMap;
+	this.imageInputStreamsMap.put(imageName, imageInputStream);
+	changeSupport.firePropertyChange("imageInputStreamsMap", oldValue,
+		this.imageInputStreamsMap);
+    }
 
-			@Override
-			public void propertyChange(PropertyChangeEvent evt) {
-				switch (evt.getPropertyName()) {
-				case "editable":
-					break;
-				case "imageFilenames":
-					if (imageFilenames != null && !imageFilenames.isEmpty()) {
-						// Populate the list on the side
-						tableViewer.setInput(imageFilenames
-								.toArray(new String[imageFilenames.size()]));
-						// Set the Selected Image
-						if (imageFilenames.contains(selectedImageFile)) {
-							imagePreview.setImage(selectedImageFile);
-						} else {
-							imagePreview.setImage(imageFilenames.iterator()
-									.next());
-						}
-					} else {
-						tableViewer.setInput(null);
-						imagePreview.setImage((InputStream)null);
-					}
-					tableViewer.refresh();
-					imagePreview.redraw();
-					break;
-				case "selectedImageFile":
-					imagePreview.setImage(selectedImageFile);
-					imagePreview.redraw();
-					break;
-				default:
-					break;
-				}
+    public List<String> getImageFilenames() {
+	return (List<String>) imageInputStreamsMap.keySet();
+    }
 
-			}
-		});
-	}
+    public String getSelectedImageFile() {
+	return selectedImageName;
+    }
 
-	public boolean isEditable() {
-		return editable;
-	}
-
-	public void setEditable(boolean editable) {
-		boolean oldValue = this.editable;
-		this.editable = editable;
-		changeSupport.firePropertyChange("editable", oldValue, this.editable);
-	}
-
-	public void setImageFilenames(List<String> imageFilenames) {
-		List<String> oldValue = this.imageFilenames;
-		this.imageFilenames = imageFilenames == null ? new ArrayList<String>()
-				: imageFilenames;
-		changeSupport.firePropertyChange("imageFilenames", oldValue,
-				this.imageFilenames);
-	}
-
-	public void addImageFilename(String imageFilename) {
-		List<String> oldValue = this.imageFilenames;
-		ArrayList<String> newList = new ArrayList<String>(this.imageFilenames);
-		newList.add(imageFilename);
-		this.imageFilenames = newList;
-		changeSupport.firePropertyChange("imageFilenames", oldValue,
-				this.imageFilenames);
-	}
-
-	public List<String> getImageFilenames() {
-		return imageFilenames;
-	}
-
-	public String getSelectedImageFile() {
-		return selectedImageFile;
-	}
-
-	public void setSelectedImageFile(String selectedImageFile) {
-		String oldValue = this.selectedImageFile;
-		this.selectedImageFile = selectedImageFile;
-		changeSupport.firePropertyChange("selectedImageFile", oldValue,
-				this.imageFilenames);
-	}
+    public void setSelectedImageName(String selectedImageName) {
+	String oldValue = this.selectedImageName;
+	this.selectedImageName = selectedImageName;
+	changeSupport.firePropertyChange("selectedImageName", oldValue,
+		this.selectedImageName);
+    }
 
 }
--- core/plugins/org.csstudio.ui.util/src/org/csstudio/ui/util/widgets/ImageStackWidgetTest.java
+++ core/plugins/org.csstudio.ui.util/src/org/csstudio/ui/util/widgets/ImageStackWidgetTest.java
@@ -4,8 +4,13 @@
 package org.csstudio.ui.util.widgets;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.jface.window.ApplicationWindow;
 import org.eclipse.swt.SWT;
@@ -29,94 +34,105 @@ import org.eclipse.ui.internal.FolderLayout;
  */
 public class ImageStackWidgetTest extends ApplicationWindow {
 
-	public ImageStackWidgetTest() {
-		super(null);
-		addToolBar(SWT.FLAT | SWT.WRAP);
-		addMenuBar();
-		addStatusLine();
-	}
+    public ImageStackWidgetTest() {
+	super(null);
+	addToolBar(SWT.FLAT | SWT.WRAP);
+	addMenuBar();
+	addStatusLine();
+    }
 
-	/**
-	 * Create contents of the application window.
-	 * 
-	 * @param parent
-	 */
-	@Override
-	protected Control createContents(Composite parent) {
-		Composite container = new Composite(parent, SWT.NONE);
-		container.setLayout(new GridLayout(1, false));
-		final ImageStackWidget imageStackWidget = new ImageStackWidget(
-				container, SWT.NONE);
-		imageStackWidget.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
-				true, 1, 1));
+    /**
+     * Create contents of the application window.
+     * 
+     * @param parent
+     */
+    @Override
+    protected Control createContents(Composite parent) {
+	Composite container = new Composite(parent, SWT.NONE);
+	container.setLayout(new GridLayout(1, false));
+	final ImageStackWidget imageStackWidget = new ImageStackWidget(
+		container, SWT.NONE);
+	imageStackWidget.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
+		true, 1, 1));
 
-		Button btnNewButton = new Button(container, SWT.NONE);
-		btnNewButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				List<String> imageFilenames = new ArrayList<String>();
-				File imagefolder = new File("Images");
-				for (File file : imagefolder.listFiles()) {
-					imageFilenames.add(file.getPath());
-				}
-				imageStackWidget.setImageFilenames(imageFilenames);
-			}
-		});
-		btnNewButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
-				false, 1, 1));
-		btnNewButton.setText("Add Test Images");
-
-		Button btnNewButton_1 = new Button(container, SWT.NONE);
-		btnNewButton_1.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				final FileDialog dlg = new FileDialog(getShell(), SWT.OPEN);
-				dlg.setFilterExtensions(new String[] { "*.png", "*.jpg" }); //$NON-NLS-1$
-				dlg.setFilterNames(new String[] { "PNG Image", "JPEG Image" }); //$NON-NLS-1$
-				final String filename = dlg.open();
-				if (filename != null) {
-					imageStackWidget.addImageFilename(filename);
-				}
-			}
-		});
-		btnNewButton_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
-				false, 1, 1));
-		btnNewButton_1.setText("Add Image");
-		return container;
-	}
-
-	/**
-	 * Launch the application.
-	 * 
-	 * @param args
-	 */
-	public static void main(String args[]) {
-		try {
-			ImageStackWidgetTest window = new ImageStackWidgetTest();
-			window.setBlockOnOpen(true);
-			window.open();
-			Display.getCurrent().dispose();
-		} catch (Exception e) {
-			e.printStackTrace();
+	Button btnNewButton = new Button(container, SWT.NONE);
+	btnNewButton.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent e) {
+		Map<String, InputStream> imageMap = new HashMap<String, InputStream>();
+		File imagefolder = new File("Images");
+		for (File file : imagefolder.listFiles()) {
+		    try {
+			imageMap.put(file.getName(), new FileInputStream(file));
+		    } catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		    }
 		}
-	}
+		imageStackWidget.setImageInputStream(imageMap);
+	    }
+	});
+	btnNewButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
+		false, 1, 1));
+	btnNewButton.setText("Add Test Images");
 
-	/**
-	 * Configure the shell.
-	 * 
-	 * @param newShell
-	 */
-	@Override
-	protected void configureShell(Shell newShell) {
-		super.configureShell(newShell);
-		newShell.setText("Test ImageStackWidgetTest");
-	}
+	Button btnNewButton_1 = new Button(container, SWT.NONE);
+	btnNewButton_1.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent e) {
+		final FileDialog dlg = new FileDialog(getShell(), SWT.OPEN);
+		dlg.setFilterExtensions(new String[] { "*.png", "*.jpg" }); //$NON-NLS-1$
+		dlg.setFilterNames(new String[] { "PNG Image", "JPEG Image" }); //$NON-NLS-1$
+		final String filename = dlg.open();
+		if (filename != null) {
+		    try {
+			imageStackWidget.addImage(filename, new FileInputStream(
+			    filename));
+		    } catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		    }
+		}
+	    }
+	});
+	btnNewButton_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
+		false, 1, 1));
+	btnNewButton_1.setText("Add Image");
+	return container;
+    }
 
-	/**
-	 * Return the initial size of the window.
-	 */
-	@Override
-	protected Point getInitialSize() {
-		return new Point(473, 541);
+    /**
+     * Launch the application.
+     * 
+     * @param args
+     */
+    public static void main(String args[]) {
+	try {
+	    ImageStackWidgetTest window = new ImageStackWidgetTest();
+	    window.setBlockOnOpen(true);
+	    window.open();
+	    Display.getCurrent().dispose();
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
+    }
+
+    /**
+     * Configure the shell.
+     * 
+     * @param newShell
+     */
+    @Override
+    protected void configureShell(Shell newShell) {
+	super.configureShell(newShell);
+	newShell.setText("Test ImageStackWidgetTest");
+    }
+
+    /**
+     * Return the initial size of the window.
+     */
+    @Override
+    protected Point getInitialSize() {
+	return new Point(473, 541);
+    }
 }
