53141b0aead46d2d84911a13ad7c51d71817df75
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-01-11 11:53:12 -0500

    o.c.u.pvmanager.sys: adding sys datasource.

---
+++ core/plugins/org.csstudio.utility.pvmanager.sys/src/org/epics/pvmanager/sys/SystemDataSource.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.pvmanager.sys;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.logging.Logger;
+import org.epics.pvmanager.ChannelHandler;
+import org.epics.pvmanager.DataSource;
+import org.epics.pvmanager.vtype.DataTypeSupport;
+import static org.epics.pvmanager.util.Executors.*;
+
+/**
+ * Data source to produce simulated signals that can be using during development
+ * and testing.
+ *
+ * @author carcassi
+ */
+public final class SystemDataSource extends DataSource {
+
+    static {
+        // Install type support for the types it generates.
+        DataTypeSupport.install();
+    }
+
+    public SystemDataSource() {
+        super(false);
+    }
+
+    private static final Logger log = Logger.getLogger(SystemDataSource.class.getName());
+
+    /**
+     * ExecutorService on which all data is polled.
+     */
+    private static ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor(namedPool("pvmanager-sys poller "));
+
+    static ScheduledExecutorService getScheduledExecutorService() {
+        return exec;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected ChannelHandler createChannel(String channelName) {
+        if ("free_mb".equals(channelName)) {
+            return new FreeMemoryChannelHandler(channelName);
+        }
+        if ("max_mb".equals(channelName)) {
+            return new MaxMemoryChannelHandler(channelName);
+        }
+        if ("used_mb".equals(channelName)) {
+            return new UsedMemoryChannelHandler(channelName);
+        }
+        if ("time".equals(channelName)) {
+            return new TimeChannelHandler(channelName);
+        }
+        if ("user".equals(channelName)) {
+            return new UserChannelHandler(channelName);
+        }
+        if ("host_name".equals(channelName)) {
+            return new HostnameChannelHandler(channelName);
+        }
+        if ("qualified_host_name".equals(channelName)) {
+            return new QualifiedHostnameChannelHandler(channelName);
+        }
+        if (channelName.startsWith(SystemPropertyChannelHandler.PREFIX)) {
+            return new SystemPropertyChannelHandler(channelName);
+        }
+        throw new IllegalArgumentException("Channel " + channelName + " does not exist");
+    }
+
+}
---
+++ core/plugins/org.csstudio.utility.pvmanager.sys/src/org/epics/pvmanager/sys/SystemPropertyChannelHandler.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.pvmanager.sys;
+
+import java.util.Objects;
+import static org.epics.vtype.ValueFactory.*;
+
+/**
+ *
+ * @author carcassi
+ */
+class SystemPropertyChannelHandler extends SystemChannelHandler {
+    
+    private final String propertyName;
+    static final String PREFIX = "system.";
+    private String previousValue = null;
+
+    public SystemPropertyChannelHandler(String channelName) {
+        super(channelName);
+        propertyName = channelName.substring(PREFIX.length());
+    }
+
+    @Override
+    protected Object createValue() {
+        String value = System.getProperty(propertyName);
+        if (value == null) {
+            value = "";
+        }
+        if (!Objects.equals(value, previousValue)) {
+            previousValue = value;
+            return newVString(value, alarmNone(), timeNow());
+        } else {
+            return null;
+        }
+    }
+    
+}
