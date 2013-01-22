39ffeca146941349b0c3649565dbc88b6fc5e1bc
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-01-22 17:11:31 -0500

    o.c.u.pvmanager.sim: added documentation.

--- core/plugins/org.csstudio.utility.pvmanager.sim/src/org/epics/pvmanager/sim/package-info.java
+++ core/plugins/org.csstudio.utility.pvmanager.sim/src/org/epics/pvmanager/sim/package-info.java
@@ -2,7 +2,8 @@
  * Copyright (C) 2010-12 Brookhaven National Laboratory
  * All rights reserved. Use is subject to license terms.
  */
+
 /**
- * Provides a data source implementation for simulated data.
+ * DataSource for simulated data (<a href="doc-files/sim-datasource.html">channel syntax</a>).
  */
 package org.epics.pvmanager.sim;
