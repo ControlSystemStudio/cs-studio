0181173611f56a8fe67f9e482fa95a1b638deb8e
Kay Kasemir <kasemirk@ornl.gov>
2013-02-13 17:56:33 -0500

    o.c.diag.probe.pvmanager: Allow multiple instances.
    No matter if opened from main menu, context menu, ..
    Prompt before opening > 5

--- applications/plugins/org.csstudio.diag.pvmanager.probe/src/org/csstudio/diag/pvmanager/probe/Activator.java
+++ applications/plugins/org.csstudio.diag.pvmanager.probe/src/org/csstudio/diag/pvmanager/probe/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.csstudio.diag.pvmanager.probe";
+	public static final String PLUGIN_ID = "org.csstudio.diag.pvmanager.probe"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
