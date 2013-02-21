62133c99ded3c63ac6e10aac8c4a18e90b6e3142
Kunal Shroff <shroffk@bnl.gov>
2013-02-21 11:18:12 -0500

    ..graphene: fixing layout of Line2DplotView
    Change-Id: I4437da22627d32c2caf3d468c7012bee81368507
    Signed-off-by: Kunal Shroff <shroffk@bnl.gov>

--- applications/plugins/org.csstudio.graphene/src/org/csstudio/graphene/Line2DPlotView.java
+++ applications/plugins/org.csstudio.graphene/src/org/csstudio/graphene/Line2DPlotView.java
@@ -22,6 +22,8 @@ import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.GridData;
 
 /**
  * @author shroffk
@@ -65,8 +67,8 @@ public class Line2DPlotView extends ViewPart {
 	super.saveState(memento);
 	// Save the currently selected variable
 	if (processVariableInputBar.getProcessVariable() != null) {
-	    memento.putString(MEMENTO_PVNAME, processVariableInputBar.getProcessVariable()
-		    .getName());
+	    memento.putString(MEMENTO_PVNAME, processVariableInputBar
+		    .getProcessVariable().getName());
 	}
     }
 
@@ -82,72 +84,59 @@ public class Line2DPlotView extends ViewPart {
 
     @Override
     public void createPartControl(Composite parent) {
-	parent.setLayout(new FormLayout());
-	
+	parent.setLayout(new GridLayout(2, false));
+
 	Label lblPvName = new Label(parent, SWT.NONE);
-	FormData fd_lblPvName = new FormData();
-	fd_lblPvName.top = new FormAttachment(0, 8);
-	fd_lblPvName.left = new FormAttachment(0, 5);
-	lblPvName.setLayoutData(fd_lblPvName);
 	lblPvName.setText("PV Name:");
 
-	processVariableInputBar = new ProcessVariableInputBar(parent, SWT.NONE, Activator
-		.getDefault().getDialogSettings(), "histogram.query");
-	FormData fd_combo = new FormData();
-	fd_combo.top = new FormAttachment(0, 5);
-	fd_combo.left = new FormAttachment(lblPvName, 6);
-	fd_combo.right = new FormAttachment(100, -5);
-	processVariableInputBar.setLayoutData(fd_combo);
-	processVariableInputBar.addPropertyChangeListener(new PropertyChangeListener() {
+	processVariableInputBar = new ProcessVariableInputBar(parent, SWT.NONE,
+		Activator.getDefault().getDialogSettings(), "histogram.query");
+	processVariableInputBar.setLayoutData(new GridData(SWT.FILL,
+		SWT.CENTER, true, false, 1, 1));
+	processVariableInputBar
+		.addPropertyChangeListener(new PropertyChangeListener() {
 
-	    @Override
-	    public void propertyChange(PropertyChangeEvent event) {
-		if ("processVariable".equals(event.getPropertyName())) {
-		    line2DPlotWidget.setPvName(processVariableInputBar.getProcessVariable()
-			    .getName());
-		}
-	    }
-	});
-	
+		    @Override
+		    public void propertyChange(PropertyChangeEvent event) {
+			if ("processVariable".equals(event.getPropertyName())) {
+			    line2DPlotWidget.setPvName(processVariableInputBar
+				    .getProcessVariable().getName());
+			}
+		    }
+		});
+
+	PopupMenuUtil.installPopupForView(processVariableInputBar, getSite(),
+		processVariableInputBar);
+
 	lblXPvName = new Label(parent, SWT.NONE);
-	FormData fd_lblXPvName = new FormData();
-	fd_lblXPvName.left = new FormAttachment(0,5);
-	fd_lblXPvName.top = new FormAttachment(processVariableInputBar, 8);
-	lblXPvName.setLayoutData(fd_lblXPvName);
-	lblXPvName.setText("X PV(optional)");
-	
-	xProcessVariableInputBar = new ProcessVariableInputBar(parent, SWT.NONE, (IDialogSettings) null, "histogram.query");
-	FormData fd_processVariableInputBar = new FormData();
-	fd_processVariableInputBar.top = new FormAttachment(processVariableInputBar, 5);
-	fd_processVariableInputBar.left = new FormAttachment(lblXPvName, 5);
-	fd_processVariableInputBar.right = new FormAttachment(100, -5);
-	xProcessVariableInputBar.setLayoutData(fd_processVariableInputBar);
-	xProcessVariableInputBar.addPropertyChangeListener(new PropertyChangeListener() {
-	    
-	    @Override
-	    public void propertyChange(PropertyChangeEvent event) {
-		if ("processVariable".equals(event.getPropertyName())) {
-		    line2DPlotWidget.setXPvName(xProcessVariableInputBar.getProcessVariable()
-			    .getName());
-		}
-	    }
-	});
+	lblXPvName.setText("X PV(optional):");
+
+	xProcessVariableInputBar = new ProcessVariableInputBar(parent,
+		SWT.NONE, (IDialogSettings) null, "histogram.query");
+	xProcessVariableInputBar.setLayoutData(new GridData(SWT.FILL,
+		SWT.CENTER, true, false, 1, 1));
+	xProcessVariableInputBar
+		.addPropertyChangeListener(new PropertyChangeListener() {
+
+		    @Override
+		    public void propertyChange(PropertyChangeEvent event) {
+			if ("processVariable".equals(event.getPropertyName())) {
+			    line2DPlotWidget
+				    .setXPvName(xProcessVariableInputBar
+					    .getProcessVariable().getName());
+			}
+		    }
+		});
 
 	line2DPlotWidget = new Line2DPlotWidget(parent, SWT.NONE);
-	FormData fd_waterfallComposite = new FormData();
-	fd_waterfallComposite.top = new FormAttachment(xProcessVariableInputBar, 5);
-	fd_waterfallComposite.bottom = new FormAttachment(100, -5);
-	fd_waterfallComposite.left = new FormAttachment(0, 5);
-	fd_waterfallComposite.right = new FormAttachment(100, -5);
-	line2DPlotWidget.setLayoutData(fd_waterfallComposite);
+	line2DPlotWidget.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
+		true, 2, 1));
+	PopupMenuUtil.installPopupForView(line2DPlotWidget, getSite(),
+		line2DPlotWidget);
 
 	if (memento != null && memento.getString(MEMENTO_PVNAME) != null) {
 	    setProcessVariable(new ProcessVariable(
 		    memento.getString(MEMENTO_PVNAME)));
 	}
-
-	PopupMenuUtil.installPopupForView(processVariableInputBar, getSite(), processVariableInputBar);
-	PopupMenuUtil.installPopupForView(line2DPlotWidget, getSite(), line2DPlotWidget);
     }
 }
-
