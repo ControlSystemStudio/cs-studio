0887e66b016a305e3bf933571db0e7d6607246f6
Kay Kasemir <kasemirk@ornl.gov>
2013-02-28 11:20:36 -0500

    o.c.trends.databrowser2.rap: Use singlesource plugin...

--- applications/plugins/org.csstudio.trends.databrowser2.rap/src/org/csstudio/trends/databrowser2/RequestUtil.java
+++ applications/plugins/org.csstudio.trends.databrowser2.rap/src/org/csstudio/trends/databrowser2/RequestUtil.java
@@ -2,12 +2,9 @@ package org.csstudio.trends.databrowser2;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.csstudio.trends.databrowser2.persistence.URLPath;
 import org.csstudio.trends.databrowser2.preferences.Preferences;
-import org.csstudio.trends.databrowser2.util.ResourceUtil;
+import org.csstudio.utility.singlesource.SingleSourcePlugin;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.rwt.RWT;
 
 /**
  * Utility for request related functions.
@@ -27,17 +24,13 @@ public class RequestUtil {
 				.getParameter(WebDataBrowserConstants.PLT_PARAMETER); //$NON-NLS-1$
 		IPath path = null;
 		if (pltPath != null && !pltPath.isEmpty()) {
-			if (ResourceUtil.isURL(pltPath))
-				path = new URLPath(pltPath);
-			else {
-				path = new Path(pltPath);
-				if (!path.isAbsolute()) {
-					IPath repoPath = Preferences.getPltRepository();
-					if (repoPath == null)
-						path = null;
-					else
-						path = repoPath.append(path);
-				}
+		    path = SingleSourcePlugin.getResourceHelper().newPath(pltPath);
+			if (!path.isAbsolute()) {
+				IPath repoPath = Preferences.getPltRepository();
+				if (repoPath == null)
+					path = null;
+				else
+					path = repoPath.append(path);
 			}
 			return path;
 		}
