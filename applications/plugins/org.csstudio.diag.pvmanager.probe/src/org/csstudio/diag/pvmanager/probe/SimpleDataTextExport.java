10609942ac3ed738f5eeb6f15856b7ef19aaa664
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-03-12 14:10:27 -0400

    o.c.d.pvmanager.probe: all scalar and array types can be exported

--- applications/plugins/org.csstudio.diag.pvmanager.probe/src/org/csstudio/diag/pvmanager/probe/SimpleDataTextExport.java
+++ applications/plugins/org.csstudio.diag.pvmanager.probe/src/org/csstudio/diag/pvmanager/probe/SimpleDataTextExport.java
@@ -2,13 +2,18 @@ package org.csstudio.diag.pvmanager.probe;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.List;
 
 import org.epics.util.array.ListNumber;
 import org.epics.util.time.TimestampFormat;
 import org.epics.vtype.Alarm;
 import org.epics.vtype.Time;
+import org.epics.vtype.VEnum;
+import org.epics.vtype.VEnumArray;
 import org.epics.vtype.VNumber;
 import org.epics.vtype.VNumberArray;
+import org.epics.vtype.VString;
+import org.epics.vtype.VStringArray;
 import org.epics.vtype.ValueUtil;
 
 public class SimpleDataTextExport implements DataExportFormat {
@@ -39,11 +44,20 @@ public class SimpleDataTextExport implements DataExportFormat {
 			}
 
 			if (value instanceof VNumber) {
-				
-				
 			    writer.append(" ")
-			      .append(Double.toString(((VNumber) value).getValue().doubleValue()));
+			          .append(Double.toString(((VNumber) value).getValue().doubleValue()));
 			}
+
+			if (value instanceof VString) {
+			    writer.append(" ")
+			          .append(((VString) value).getValue());
+			}
+
+			if (value instanceof VEnum) {
+			    writer.append(" ")
+			          .append(((VEnum) value).getValue());
+			}
+			
 			if (value instanceof VNumberArray) {
 				ListNumber data = ((VNumberArray) value).getData();
 				for (int i = 0; i < data.size(); i++) {
@@ -51,6 +65,23 @@ public class SimpleDataTextExport implements DataExportFormat {
 					      .append(Double.toString(data.getDouble(i)));
 				}
 			}
+			
+			if (value instanceof VStringArray) {
+				List<String> data = ((VStringArray) value).getData();
+				for (int i = 0; i < data.size(); i++) {
+					writer.append(" ")
+					      .append(data.get(i));
+				}
+			}
+			
+			if (value instanceof VEnumArray) {
+				List<String> data = ((VEnumArray) value).getData();
+				for (int i = 0; i < data.size(); i++) {
+					writer.append(" ")
+					      .append(data.get(i));
+				}
+			}
+			
 		} catch (IOException e) {
 			throw new RuntimeException("Write failed", e);
 		}
@@ -62,7 +93,23 @@ public class SimpleDataTextExport implements DataExportFormat {
 			return true;
 		}
 		
+		if (data instanceof VString) {
+			return true;
+		}
+		
+		if (data instanceof VEnum) {
+			return true;
+		}
+		
 		if (data instanceof VNumberArray) {
+			return true;
+		}
+		
+		if (data instanceof VStringArray) {
+			return true;
+		}
+		
+		if (data instanceof VEnumArray) {
 			return true;
 		}
 		
