47d71fa4d9b32759c87f430dca42b05585098f68
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-03-18 15:02:35 -0400

    build: adding main release scripts and list of products to release

---
+++ build/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check parameters
+VERSION=$1
+BUILD_DIR=`pwd`
+if [ -z "$VERSION" ]
+then 
+  echo You must provide the version \(e.g. \"release.sh 3.2.4\"\)
+exit -1
+fi
+
+# Prepare all products
+while read PRODUCT
+do
+  ./prepare-release.sh $PRODUCT $VERSION
+  git commit -m "Preparing product $PRODUCT for release $VERSION"
+done < release.products
+
