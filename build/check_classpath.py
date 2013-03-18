f6cbe711db425ebe8429b5e128dd0b3c27699672
Kunal Shroff <shroffk@bnl.gov>
2013-03-18 17:40:22 -0400

    check_classpath.py: a simple script to check the JRE configuration in .classpath and print a list of offending files
    Change-Id: I0bda0ed03eb76c1e2f316750bc341bc983e1051f
    Signed-off-by: Kunal Shroff <shroffk@bnl.gov>

---
+++ build/check_classpath.py
@@ -0,0 +1,24 @@
+'''
+Created on Mar 18, 2013
+
+@author: shroffk
+'''
+import os.path
+import re
+import sys
+
+nonGenericPath='(.*)<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/(.+)"/>'
+
+for dirpath, dirnames, filenames in os.walk("."):
+    incorrectFiles = []
+    for completefilename in [ os.path.join(dirpath, f) for f in filenames if f.endswith(".classpath")]:
+        for line in open(completefilename).readlines():
+            m = re.match(nonGenericPath, line)
+            if m is not None:
+                incorrectFiles.append(completefilename)
+    if len(incorrectFiles) != 0:
+        print 'Following incorrectly configured .classpath files committed to repository'
+        for f in incorrectFiles:
+            print f
+        sys.exit(-1)         
+        
