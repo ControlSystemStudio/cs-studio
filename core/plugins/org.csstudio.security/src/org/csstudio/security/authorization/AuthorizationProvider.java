b19a9febdc5b8354a015018a48647b2220978eda
Kay Kasemir <kasemirk@ornl.gov>
2013-04-01 17:14:09 -0400

    o.c.sec: Explain how to add custom AuthorizationProvider

--- core/plugins/org.csstudio.security/src/org/csstudio/security/authorization/AuthorizationProvider.java
+++ core/plugins/org.csstudio.security/src/org/csstudio/security/authorization/AuthorizationProvider.java
@@ -10,14 +10,20 @@ package org.csstudio.security.authorization;
 import javax.security.auth.Subject;
 
 /** Interface of service that determines {@link Authorizations} of a user ({@link Subject})
+ * 
+ *  <p>To define a new authorization mechanism:
+ *  <ol>
+ *  <li>Implement this interface, for example as <code>MyAuthorizationProvider</code>.
+ *  <li>Register <code>MyAuthorizationProvider</code> as an OSGi service for
+ *      class <code>AuthorizationProvider.class</code>, name it for example <code>MyAuth</code>.
+ *      Refer to examples in this plugin which register via declarative services.
+ *  <li>Set the preference
+ *      <code>org.csstudio.security/authorization_provider=MyAuth</code>
+ *  </ol>
  *  @author Kay Kasemir
  */
-@SuppressWarnings("nls")
 public interface AuthorizationProvider
 {
-    /** ID of extension point for adding implementations */
-    public static String EXT_ID = "org.csstudio.security.authorization";
-    
     /** Obtain authorizations for a user
      *  @param user JAAS {@link Subject} that describes the user
      *  @return {@link Authorizations} held by this user
