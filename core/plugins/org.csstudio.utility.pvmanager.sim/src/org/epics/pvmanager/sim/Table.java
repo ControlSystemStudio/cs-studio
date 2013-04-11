e36900294ddeedbe0ac32d89c97b9ca296b1527b
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-04-11 13:19:59 -0400

    o.c.u.pvmanager.sim: update to SNAPSHOT adding table simulation

---
+++ core/plugins/org.csstudio.utility.pvmanager.sim/src/org/epics/pvmanager/sim/Table.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.pvmanager.sim;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import org.epics.vtype.VTable;
+import static org.epics.vtype.ValueFactory.*;
+
+/**
+ * Function to simulate a signal that generates VTables.
+ *
+ * @author carcassi
+ */
+public class Table extends SimFunction<VTable> {
+
+    public Table() {
+        this(0.1);
+    }
+
+    public Table(Double interval) {
+        super(interval, VTable.class);
+        if (interval <= 0.0) {
+            throw new IllegalArgumentException("Interval must be greater than zero (was " + interval + ")");
+        }
+    }
+    
+    private final List<Class<?>> types = Arrays.asList((Class<?>) String.class, Double.TYPE, Integer.TYPE);
+    
+
+    @Override
+    VTable nextValue() {
+        return newVTable(types, Arrays.asList("text", "value", "index"),
+                Arrays.asList((Object) generateStringColumn(10), generateDoubleColumn(10), generateIntegerColumn(10)));
+    }
+    
+    private final Random rand = new Random();
+    
+    int[] generateIntegerColumn(int size) {
+        int[] column = new int[size];
+        for (int i = 0; i < column.length; i++) {
+            column[i] = i;
+        }
+        return column;
+    }
+    
+    double[] generateDoubleColumn(int size) {
+        double[] column = new double[size];
+        for (int i = 0; i < column.length; i++) {
+            column[i] = rand.nextGaussian();
+        }
+        return column;
+    }
+    
+    String[] generateStringColumn(int size) {
+        String[] column = new String[size];
+        for (int i = 0; i < column.length; i++) {
+            column[i] = generateString(i);
+        }
+        return column;
+    }
+    
+    String generateString(int id) {
+        if (id == 0) {
+            return "A";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while (id != 0) {
+            char letter = (char) ('A' + (id %  26));
+            sb.insert(0, letter);
+            id = id / 26;
+        }
+        return sb.toString();
+    }
+}
