895dcc0a7ea16f1fe100cc20748e4016fdcddf23
Kunal Shroff <shroffk@bnl.gov>
2013-04-18 17:45:36 -0400

    ..logbook: adding null check to tag and logbook builder
    Change-Id: I6c036ef5ddeb33cd00129231a9a021007fb68f14
    Signed-off-by: Kunal Shroff <shroffk@bnl.gov>

--- applications/plugins/org.csstudio.logbook/src/org/csstudio/logbook/TagBuilder.java
+++ applications/plugins/org.csstudio.logbook/src/org/csstudio/logbook/TagBuilder.java
@@ -6,72 +6,82 @@
 package org.csstudio.logbook;
 
 /**
- *
+ * 
  * A builder for a default implementation of the {@link Tag}
- *
+ * 
  * @author shroffk
  */
 public class TagBuilder {
-		// Required
-		private String name;
-		// Optional
-		private String state = null;
+    // Required
+    private String name;
+    // Optional
+    private String state = null;
 
-		public static TagBuilder tag(Tag tag) {
-			TagBuilder builder = new TagBuilder();
-			builder.name = tag.getName();
-			builder.state = tag.getState();
-			return builder;
-		}
+    public static TagBuilder tag(Tag tag) {
+	if (tag.getName() == null) {
+	    throw new NullPointerException("Tag name cannot be null");
+	} else {
+	    TagBuilder builder = new TagBuilder();
+	    builder.name = tag.getName();
+	    builder.state = tag.getState();
+	    return builder;
+	}
 
-		public static TagBuilder tag(String name) {
-			TagBuilder builder = new TagBuilder();
-			builder.name = name;
-			return builder;
-		}
+    }
 
-		public static TagBuilder tag(String name, String state) {
-			TagBuilder builder = new TagBuilder();
-			builder.name = name;
-			builder.state = state;
-			return builder;
-		}
+    public static TagBuilder tag(String name) {
+	if (name == null) {
+	    throw new NullPointerException("Tag name cannot be null");
+	} else {
+	    TagBuilder builder = new TagBuilder();
+	    builder.name = name;
+	    return builder;
+	}
+    }
 
-		public TagBuilder state(String state) {
-			this.state = state;
-			return this;
-		}
+    public static TagBuilder tag(String name, String state) {
+	TagBuilder builder = new TagBuilder();
+	builder.name = name;
+	builder.state = state;
+	return builder;
+    }
 
-		public Tag build(){
-			return new TagImpl(name, state);
-		}
+    public TagBuilder state(String state) {
+	this.state = state;
+	return this;
+    }
 
-		/**
-		 * A Default implementation for the {@link Tag}
-		 * @author shroffk
-		 *
-		 */
-		private class TagImpl implements Tag{
+    public Tag build() {
+	return new TagImpl(name, state);
+    }
 
-			private final String name;
-			private final String state;
-			
-			public TagImpl(String name, String state) {
-				super();
-				this.name = name;
-				this.state = state;
-			}
+    /**
+     * A Default implementation for the {@link Tag}
+     * 
+     * @author shroffk
+     * 
+     */
+    private class TagImpl implements Tag {
 
-			@Override
-			public String getName() {
-				return name;
-			}
+	private final String name;
+	private final String state;
 
-			@Override
-			public String getState() {
-				return state;
-			}
-			
-		}
+	public TagImpl(String name, String state) {
+	    super();
+	    this.name = name;
+	    this.state = state;
+	}
+
+	@Override
+	public String getName() {
+	    return name;
+	}
+
+	@Override
+	public String getState() {
+	    return state;
+	}
+
+    }
 
 }
