9a145445831e91e26e3a2b8890f5f99e2325d108
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-05-20 16:53:27 -0400

    o.c.u.pvmanager.file: adding file based datasource For now, it can only read tables formatted in csv.

---
+++ core/plugins/org.csstudio.utility.pvmanager.file/src/org/epics/pvmanager/file/FileDataSource.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.pvmanager.file;
+
+import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.epics.pvmanager.ChannelHandler;
+import org.epics.pvmanager.ChannelReadRecipe;
+import org.epics.pvmanager.ChannelWriteRecipe;
+import org.epics.pvmanager.DataSource;
+import org.epics.pvmanager.ReadRecipe;
+import org.epics.pvmanager.WriteRecipe;
+import org.epics.pvmanager.vtype.DataTypeSupport;
+import org.epics.pvmanager.util.FunctionParser;
+import org.epics.util.array.ArrayDouble;
+
+/**
+ * Data source for locally written data. Each instance of this
+ * data source will have its own separate channels and values.
+ *
+ * @author carcassi
+ */
+public final class FileDataSource extends DataSource {
+
+    static {
+        // Install type support for the types it generates.
+        DataTypeSupport.install();
+    }
+
+    /**
+     * Creates a new data source.
+     */
+    public FileDataSource() {
+        super(false);
+    }
+    
+    @Override
+    protected ChannelHandler createChannel(String channelName) {
+        return new FileChannelHandler(channelName, new File(URI.create("file://" + channelName)));
+    }
+
+}
