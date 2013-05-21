b41888251b3c0c093fea95ac224540be53c99dbd
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-05-20 21:01:08 -0400

    o.c.graphene: starting to integrate bubble graph

---
+++ applications/plugins/org.csstudio.graphene/src/org/csstudio/graphene/BubbleGraph2DConfigurationPanel.java
@@ -0,0 +1,14 @@
+package org.csstudio.graphene;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.SWT;
+
+public class BubbleGraph2DConfigurationPanel extends
+		AbstractPointDatasetGraph2DConfigurationPanel {
+
+	public BubbleGraph2DConfigurationPanel(Composite parent,
+			int style) {
+		super(parent, style);
+	}
+}
---
+++ applications/plugins/org.csstudio.graphene/src/org/csstudio/graphene/BubbleGraph2DSelection.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.csstudio.graphene;
+
+import org.csstudio.ui.util.ConfigurableWidget;
+import org.csstudio.ui.util.ConfigurableWidgetAdaptable;
+import org.epics.pvmanager.graphene.Graph2DResult;
+import org.epics.vtype.VType;
+
+/**
+ * @author shroffk
+ * 
+ */
+public class BubbleGraph2DSelection implements VTypeAdaptable,
+		ConfigurableWidgetAdaptable {
+
+	private final BubbleGraph2DWidget widget;
+
+	public BubbleGraph2DSelection(BubbleGraph2DWidget widget) {
+		this.widget = widget;
+	}
+
+	@Override
+	public VType toVType() {
+		Graph2DResult result = widget.getCurrentResult();
+		if (result != null) {
+			return result.getData();
+		}
+		return null;
+	}
+
+	@Override
+	public ConfigurableWidget toConfigurableWidget() {
+		return widget;
+	}
+}
