c7b8793d6d0c9e90f97aeb297582067c955d5377
Kunal Shroff <shroffk@bnl.gov>
2013-05-21 12:52:44 -0400

    ..ui.util: adding a simple multiStringSelectionCombo
    Change-Id: Iaf2e592add3c8db4ccdbb18291cb7daf6490ac47

---
+++ core/plugins/org.csstudio.ui.util/src/org/csstudio/ui/util/widgets/MultipleSelectionCombo.java
@@ -0,0 +1,407 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.csstudio.ui.util.widgets;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.FocusAdapter;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * Combo-type widget that allows selecting multiple items.
+ * 
+ * <p>
+ * Takes a list of {@link Object}s as input.
+ * 
+ * <p>
+ * The <code>toString()</code> of each Object is displayed in a drop-down list.
+ * One or more items can be selected, they're also displayed in the text field.
+ * 
+ * <p>
+ * Items can be entered in the text field, comma-separated. If entered text does
+ * not match a valid item, text is highlighted and tool-tip indicates error.
+ * 
+ * <p>
+ * Keyboard support: 'Down' key in text field opens drop-down. Inside drop-down,
+ * single item can be selected via cursor key and 'RETURN' closes the drop-down.
+ * 
+ * TODO Auto-completion while typing?
+ * 
+ * @author Kay Kasemir
+ */
+public class MultipleSelectionCombo<T> extends Composite {
+    final private static String SEPARATOR = ", "; //$NON-NLS-1$
+    final private static String SEPERATOR_PATTERN = "\\s*,\\s*"; //$NON-NLS-1$
+
+    private final PropertyChangeSupport changeSupport = new PropertyChangeSupport(
+	    this);
+
+    private Display display;
+
+    private Text text;
+
+    /** Pushing the drop_down button opens the popup */
+    private Button drop_down;
+
+    private Shell popup;
+    private org.eclipse.swt.widgets.List list;
+
+    /** Items to show in list */
+    private List<String> items = new ArrayList<String>();
+
+    /** Selection indices */
+    private List<String> selection = new ArrayList<String>();
+
+    private String tool_tip = null;
+    private Color text_color = null;
+
+    /**
+     * When list looses focus, the event time is noted here. This prevents the
+     * drop-down button from re-opening the list right away.
+     */
+    private int lost_focus = 0;
+
+    /**
+     * Initialize
+     * 
+     * @param parent
+     * @param style
+     */
+    public MultipleSelectionCombo(final Composite parent, final int style) {
+	super(parent, style);
+	createComponents(parent);
+    }
+
+    /**
+     * Create SWT components
+     * 
+     * @param parent
+     */
+    private void createComponents(final Composite parent) {
+	display = parent.getDisplay();
+	final GridLayout layout = new GridLayout();
+	layout.numColumns = 2;
+	layout.marginWidth = 0;
+	setLayout(layout);
+
+	addPropertyChangeListener(new PropertyChangeListener() {
+
+	    @Override
+	    public void propertyChange(PropertyChangeEvent e) {
+		switch (e.getPropertyName()) {
+		case "selection":
+		    validateSelection();
+		    updateText();
+		    break;
+		case "items":
+		    setSelection(Collections.<String> emptyList());
+		    break;
+		default:
+		    break;
+		}
+	    }
+	});
+
+	text = new Text(this, SWT.BORDER);
+	GridData gd = new GridData(SWT.FILL, 0, true, false);
+	text.setLayoutData(gd);
+	text.addModifyListener(new ModifyListener() {
+	    @Override
+	    public void modifyText(ModifyEvent e) {
+		// Analyze text, update selection
+		final String items_text = text.getText();
+		setSelection(items_text);
+	    }
+	});
+	text.addKeyListener(new KeyAdapter() {
+	    @Override
+	    public void keyPressed(final KeyEvent e) {
+		switch (e.keyCode) {
+		case SWT.ARROW_DOWN:
+		    drop(true);
+		    return;
+		case SWT.ARROW_UP:
+		    drop(false);
+		    return;
+		}
+	    }
+	});
+
+	drop_down = new Button(this, SWT.ARROW | SWT.DOWN);
+	gd = new GridData(SWT.FILL, SWT.FILL, false, false);
+	gd.heightHint = text.getBounds().height;
+	drop_down.setLayoutData(gd);
+	drop_down.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(final SelectionEvent e) {
+		// Was list open, user clicked this button to close,
+		// and list self-closed because is lost focus?
+		if (e.time - lost_focus <= 300)
+		    return; // Done
+
+		// If list is not open, open it
+		if (!isDropped())
+		    drop(true);
+	    }
+	});
+    }
+
+    private void validateSelection() {
+	if (items.containsAll(selection)) {
+	    text.setForeground(text_color);
+	    text.setToolTipText(tool_tip);
+	} else {
+	    text.setForeground(display.getSystemColor(SWT.COLOR_RED));
+	    text.setToolTipText("Text contains invalid items");
+	}
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setForeground(final Color color) {
+	text_color = color;
+	text.setForeground(color);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setToolTipText(final String tooltip) {
+	tool_tip = tooltip;
+	text.setToolTipText(tooltip);
+	drop_down.setToolTipText(tooltip);
+    }
+
+    /**
+     * Define items to be displayed in the list, and returned as the current
+     * selection when selected.
+     * 
+     * @param new_items
+     *            Items to display in the list
+     */
+    public void setItems(final List<String> items) {
+	List<String> oldValue = this.items;
+	this.items = items;
+	changeSupport.firePropertyChange("items", oldValue, this.items);
+    }
+
+    /**
+     * Get the list of items
+     * 
+     * @return list of selectable items
+     */
+    public List<String> getItems() {
+	return this.items;
+    }
+
+    /**
+     * Define items that should be selected.
+     * 
+     * <p>
+     * Selected items must be on the list of items provided via
+     * <code>setItems</code>
+     * 
+     * @param selection_text
+     *            Items to select in the list as comma-separated string
+     * @return <code>true</code> if all requested items could be selected,
+     *         <code>false</code> if some are not in the list
+     */
+    public void setSelection(final String selection) {
+	List<String> oldValue = this.selection;
+	this.selection = convert2List(selection);
+	changeSupport.firePropertyChange("selection", oldValue, this.selection);
+    }
+
+    /**
+     * Define items that should be selected.
+     * 
+     * <p>
+     * Selected items must be on the list of items provided via
+     * <code>setItems</code>
+     * 
+     * @param sel_items
+     *            Items to select in the list
+     * @return <code>true</code> if all requested items could be selected,
+     *         <code>false</code> if some are not in the list
+     */
+    public void setSelection(final List<String> selection) {
+	List<String> oldValue = this.selection;
+	this.selection = new ArrayList<String>(selection);
+	changeSupport.firePropertyChange("selection", oldValue, this.selection);
+    }
+
+    /**
+     * Obtain currently selected items
+     * 
+     * <p>
+     * The order of selected items is not guaranteed. In the current
+     * implementation, it will match the order of items provided to
+     * <code>setItems()</code>. It does <u>not</u> reflect the order in which
+     * items were selected inside the GUI's drop-down list. That may change in
+     * the future, so treat the result as a <code>Collection</code> rather than
+     * ordered <code>List</code>.
+     * 
+     * @return Currently selected items
+     */
+    public List<String> getSelection() {
+	return Collections.unmodifiableList(this.selection);
+    }
+
+    /**
+     * Set indices of <code>selection</code> based on desired selection.
+     * 
+     * @param selection_text
+     *            Items to select in the list as comma-separated string
+     * @return <code>true</code> if all requested items could be selected,
+     *         <code>false</code> if some are not in the list
+     */
+    private List<String> convert2List(final String selection_text) {
+	final String[] item_texts = selection_text.split(SEPERATOR_PATTERN);
+	List<String> selection = new ArrayList<String>();
+	// Locate index for each item
+	for (String text : item_texts) {
+	    selection.add(text);
+	}
+	return selection;
+    }
+
+    /** Update <code>selection</code> from <code>list</code> */
+    private void updateSelectionFromList() {
+	setSelection(Arrays.asList(list.getSelection()));
+    }
+
+    /** Update <code>text</code> to reflect <code>selection</code> */
+    private void updateText() {
+	final StringBuilder buf = new StringBuilder();
+	for (String i : selection) {
+	    if (buf.length() > 0)
+		buf.append(SEPARATOR);
+	    buf.append(i);
+	}
+	text.setText(buf.toString());
+	text.setSelection(buf.length());
+    }
+
+    /** @return <code>true</code> if drop-down is visible */
+    private boolean isDropped() {
+	return popup != null;
+    }
+
+    /**
+     * @param drop
+     *            Display drop-down?
+     */
+    private void drop(boolean drop) {
+	if (drop == isDropped())
+	    return;
+
+	if (drop)
+	    createPopup();
+	else
+	    hidePopup();
+    }
+
+    /** Create shell that simulates drop-down */
+    private void createPopup() {
+	popup = new Shell(getShell(), SWT.NO_TRIM | SWT.ON_TOP);
+	popup.setLayout(new FillLayout());
+	list = new org.eclipse.swt.widgets.List(popup, SWT.MULTI | SWT.V_SCROLL);
+	list.setToolTipText(tool_tip);
+
+	// Position popup under the text box
+	Rectangle bounds = text.getBounds();
+	bounds.y += bounds.height;
+	// As high as necessary for items
+	bounds.height = 5 + 2 * list.getBorderWidth() + list.getItemHeight()
+		* items.size();
+	// ..with limitation
+	bounds.height = Math.min(bounds.height, display.getBounds().height / 2);
+	// Map to screen coordinates
+	bounds = display.map(text, null, bounds);
+	popup.setBounds(bounds);
+	popup.open();
+
+	// Update text from changed list selection
+	list.addSelectionListener(new SelectionListener() {
+	    @Override
+	    public void widgetSelected(SelectionEvent e) {
+		updateSelectionFromList();
+		updateText();
+	    }
+
+	    @Override
+	    public void widgetDefaultSelected(SelectionEvent e) {
+		updateSelectionFromList();
+		updateText();
+		hidePopup();
+	    }
+	});
+
+	list.setItems(items.toArray(new String[items.size()]));
+	list.setSelection(selection.toArray(new String[selection.size()]));
+
+	list.addKeyListener(new KeyAdapter() {
+
+	    @Override
+	    public void keyReleased(KeyEvent e) {
+		System.out.println("key event:" + e.keyCode);
+	    }
+	});
+	// Hide popup when loosing focus
+	list.addFocusListener(new FocusAdapter() {
+	    @Override
+	    public void focusLost(final FocusEvent e) {
+		lost_focus = e.time;
+		hidePopup();
+	    }
+	});
+	list.setFocus();
+    }
+
+    /** Hide popup shell */
+    private void hidePopup() {
+	if (popup != null) {
+	    popup.close();
+	    popup.dispose();
+	    popup = null;
+	}
+	text.setFocus();
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+	changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+	changeSupport.removePropertyChangeListener(listener);
+    }
+}
---
+++ core/plugins/org.csstudio.ui.util/test/org/csstudio/ui/util/widgets/StringMultiSelectionComboDemo.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Oak Ridge National Laboratory.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.csstudio.ui.util.widgets;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * SWT Demo of the {@link MultiSelectionCombo}
+ * 
+ * @author Kay Kasemir
+ */
+@SuppressWarnings("nls")
+public class StringMultiSelectionComboDemo {
+    private static Text text;
+    private final static List<String> itemSet1 = Arrays.asList("Zero", "One",
+	    "Two", "Three", "Four", "Five", "Six");
+    private final static List<String> itemSet2 = Arrays.asList("1", "2", "3",
+	    "4", "5", "6");
+
+    public static void main(String[] args) {
+	Display display = new Display();
+	Shell shell = new Shell(display);
+
+	shell.setLayout(new GridLayout());
+
+	final MultipleSelectionCombo<String> list = new MultipleSelectionCombo<>(
+		shell, 0);
+	list.addPropertyChangeListener(new PropertyChangeListener() {
+
+	    @Override
+	    public void propertyChange(PropertyChangeEvent evt) {
+		if (evt.getPropertyName().equals("selection")) {
+		    StringBuilder sb = new StringBuilder();
+		    String loopDelim = "";
+		    for (String s : list.getSelection()) {
+			sb.append(loopDelim);
+			sb.append(s);
+			loopDelim = ", ";
+		    }
+		    text.setText(sb.toString());
+		}
+	    }
+	});
+	list.setLayoutData(new GridData(SWT.FILL, 0, true, false));
+	list.setForeground(display.getSystemColor(SWT.COLOR_DARK_BLUE));
+	list.setToolTipText("Enter items");
+
+	Button dump = new Button(shell, SWT.PUSH);
+	dump.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+	dump.setText("Dump selected items");
+	dump.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent e) {
+		System.out.println("Selected items: " + list.getSelection());
+	    }
+	});
+
+	shell.setSize(400, 300);
+
+	Button btnNewButton = new Button(shell, SWT.NONE);
+	btnNewButton.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent e) {
+		list.setItems(itemSet1);
+	    }
+	});
+	btnNewButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
+		false, 1, 1));
+	btnNewButton.setText("Items set1");
+
+	Button btnNewButton_1 = new Button(shell, SWT.NONE);
+	btnNewButton_1.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent e) {
+		list.setItems(itemSet2);
+	    }
+	});
+	btnNewButton_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
+		false, 1, 1));
+	btnNewButton_1.setText("Item set2");
+
+	Label lblSelectedValues = new Label(shell, SWT.NONE);
+	lblSelectedValues.setText("Selected Values:");
+
+	text = new Text(shell, SWT.BORDER);
+	text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+	// Set initial Data
+
+	list.setItems(itemSet1);
+	list.setSelection("One, Four  , Five");
+
+	shell.open();
+	while (!shell.isDisposed())
+	    if (!display.readAndDispatch())
+		display.sleep();
+	display.dispose();
+    }
+}
