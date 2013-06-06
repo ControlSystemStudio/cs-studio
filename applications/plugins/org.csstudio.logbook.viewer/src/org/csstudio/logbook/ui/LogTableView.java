64bc3319bed47d5e9b8f73f7fc2b354158e7da45
Kunal Shroff <shroffk@bnl.gov>
2013-06-06 12:12:02 -0400

    ..logbookViewer: adding search dialog.
    Change-Id: I7d66e4680ff97342383ce33111a4693bc2b6e4a5

--- applications/plugins/org.csstudio.logbook.viewer/src/org/csstudio/logbook/ui/LogTableView.java
+++ applications/plugins/org.csstudio.logbook.viewer/src/org/csstudio/logbook/ui/LogTableView.java
@@ -4,22 +4,29 @@
 package org.csstudio.logbook.ui;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Collection;
 
 import org.csstudio.autocomplete.ui.AutoCompleteWidget;
 import org.csstudio.logbook.LogEntry;
+import org.csstudio.logbook.Logbook;
 import org.csstudio.logbook.LogbookClient;
 import org.csstudio.logbook.LogbookClientManager;
+import org.csstudio.logbook.Tag;
+import org.csstudio.logbook.util.LogEntrySearchUtil;
 import org.csstudio.ui.util.PopupMenuUtil;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseMoveListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
@@ -40,6 +47,9 @@ public class LogTableView extends ViewPart {
     private LogbookClient logbookClient;
     private Label label;
 
+    private List<String> logbooks = Collections.emptyList();
+    private List<String> tags = Collections.emptyList();
+
     public LogTableView() {
     }
 
@@ -53,40 +63,58 @@ public class LogTableView extends ViewPart {
 	lblLogQuery.setLayoutData(fd_lblLogQuery);
 	lblLogQuery.setText("Log Query:");
 
+	Button btnNewButton = new Button(parent, SWT.NONE);
+	btnNewButton.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent e) {
+		try {
+		    if (logbooks.isEmpty() && initializeClient()) {
+			logbooks = new ArrayList<String>();
+			for (Logbook logbook : logbookClient.listLogbooks()) {
+			    logbooks.add(logbook.getName());
+			}
+		    }
+		    if (tags.isEmpty() && initializeClient()) {
+			tags = new ArrayList<String>();
+			for (Tag tag : logbookClient.listTags()) {
+			    tags.add(tag.getName());
+			}
+		    }
+		    LogEntrySearchDialog dialog = new LogEntrySearchDialog(
+			    parent.getShell(),
+			    logbooks,
+			    tags,
+			    LogEntrySearchUtil.parseSearchString(text.getText()));
+		    dialog.setBlockOnOpen(true);
+		    if (dialog.open() == IDialogConstants.OK_ID) {
+			text.setText(dialog.getSearchString());
+			search();
+		    }
+		} catch (Exception e2) {
+		    // TODO make this widget composite with exception
+		    e2.printStackTrace();
+		}
+	    }
+	});
+	FormData fd_btnNewButton = new FormData();
+	fd_btnNewButton.top = new FormAttachment(0, 3);
+	fd_btnNewButton.right = new FormAttachment(100, -5);
+	btnNewButton.setLayoutData(fd_btnNewButton);
+	btnNewButton.setText("Adv Search");
+
 	text = new Text(parent, SWT.BORDER);
 	text.addKeyListener(new KeyAdapter() {
 	    @Override
 	    public void keyReleased(KeyEvent e) {
 		if (e.keyCode == SWT.CR || e.keyCode == SWT.KEYPAD_CR) {
-		    if (logbookClient == null) {
-			try {
-			    logbookClient = LogbookClientManager
-				    .getLogbookClientFactory().getClient();
-			} catch (Exception e1) {
-			    e1.printStackTrace();
-			}
-		    } else {
-			try {
-			    final List<LogEntry> logEntries = new ArrayList<LogEntry>(
-				    logbookClient.findLogEntries(text.getText()));
-			    Display.getDefault().asyncExec(new Runnable() {
-
-				@Override
-				public void run() {
-				    logEntryTable.setLogs(logEntries);
-				}
-			    });
-			} catch (Exception e1) {
-			    e1.printStackTrace();
-			}
-		    }
+		    search();
 		}
 	    }
 	});
 	FormData fd_text = new FormData();
+	fd_text.right = new FormAttachment(btnNewButton, -5);
 	fd_text.left = new FormAttachment(lblLogQuery, 5);
 	fd_text.top = new FormAttachment(0, 5);
-	fd_text.right = new FormAttachment(100, -3);
 	text.setLayoutData(fd_text);
 
 	// Add AutoComplete support, use type logEntrySearch
@@ -124,6 +152,39 @@ public class LogTableView extends ViewPart {
 
 	PopupMenuUtil.installPopupForView(logEntryTable, getSite(),
 		logEntryTable);
+	initializeClient();
+    }
+
+    private boolean initializeClient() {
+	if (logbookClient == null) {
+	    try {
+		logbookClient = LogbookClientManager.getLogbookClientFactory()
+			.getClient();
+		return true;
+	    } catch (Exception e1) {
+		e1.printStackTrace();
+		return false;
+	    }
+	}
+	return true;
+    }
+
+    private void search() {
+	if (initializeClient()) {
+	    try {
+		final List<LogEntry> logEntries = new ArrayList<LogEntry>(
+			logbookClient.findLogEntries(text.getText()));
+		Display.getDefault().asyncExec(new Runnable() {
+
+		    @Override
+		    public void run() {
+			logEntryTable.setLogs(logEntries);
+		    }
+		});
+	    } catch (Exception e1) {
+		e1.printStackTrace();
+	    }
+	}
     }
 
     @Override
