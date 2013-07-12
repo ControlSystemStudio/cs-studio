be5f51af1fd67ce1d500f57e08bdd736dd9e2cdd
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-07-12 17:58:41 -0400

    o.c.u.pvmanager.widgets: adding dispose of pv and alarm toVTableWidget

--- core/plugins/org.csstudio.utility.pvmanager.widgets/src/org/csstudio/utility/pvmanager/widgets/VTableWidget.java
+++ core/plugins/org.csstudio.utility.pvmanager.widgets/src/org/csstudio/utility/pvmanager/widgets/VTableWidget.java
@@ -4,6 +4,8 @@ import org.csstudio.ui.util.composites.BeanComposite;
 import org.csstudio.ui.util.widgets.ErrorBar;
 import org.csstudio.utility.pvmanager.ui.SWTUtil;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -13,7 +15,11 @@ import org.epics.pvmanager.PVReaderEvent;
 import org.epics.pvmanager.PVReaderListener;
 import org.epics.pvmanager.formula.ExpressionLanguage;
 import org.epics.util.time.TimeDuration;
+import org.epics.vtype.Alarm;
+import org.epics.vtype.AlarmSeverity;
 import org.epics.vtype.VTable;
+import org.epics.vtype.ValueFactory;
+import org.epics.vtype.ValueUtil;
 
 /**
  * Widget that can display a formula that returns a VTable.
@@ -46,6 +52,16 @@ public class VTableWidget extends BeanComposite {
 		tableDisplay.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true,
 				1, 1));
 		forwardPropertyChange(tableDisplay, "vTable", "value");
+		addDisposeListener(new DisposeListener() {
+			
+			@Override
+			public void widgetDisposed(DisposeEvent e) {
+				if (pv != null) {
+					pv.close();
+					pv = null;
+				}
+			}
+		});
 	}
 
 	private VTableDisplay tableDisplay;
@@ -91,6 +107,13 @@ public class VTableWidget extends BeanComposite {
 	
 	public VTable getValue() {
 		return tableDisplay.getVTable();
+	}
+	
+	public Alarm getAlarm() {
+		if (pv == null) {
+			return ValueFactory.alarmNone();
+		}
+		return ValueUtil.alarmOf(getValue(), pv.isConnected());
 	}
 
 }
