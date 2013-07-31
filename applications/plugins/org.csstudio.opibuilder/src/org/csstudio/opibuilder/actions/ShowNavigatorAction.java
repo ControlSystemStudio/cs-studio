e8a9b22139d9d78ca5bff5e5d3ddd31e3627aac0
xihui <ztyaner11@gmail.com>
2013-07-31 14:00:13 -0400

    BOY: add OPI Editor/Runtime Perspective context menu. Top Files submenu

--- applications/plugins/org.csstudio.opibuilder/src/org/csstudio/opibuilder/actions/ShowNavigatorAction.java
+++ applications/plugins/org.csstudio.opibuilder/src/org/csstudio/opibuilder/actions/ShowNavigatorAction.java
@@ -9,51 +9,35 @@ package org.csstudio.opibuilder.actions;
 import java.util.logging.Level;
 
 import org.csstudio.opibuilder.OPIBuilderPlugin;
-import org.csstudio.opibuilder.editor.OPIEditorPerspective;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.WorkbenchException;
 
-/**The action that opens the OPI Editor perspective
+/**The action that opens the OPI Runtime perspective
  * @author Xihui Chen
  */
-public class OpenOPIPerspectiveAction implements IWorkbenchWindowActionDelegate {
+public class ShowNavigatorAction implements IObjectActionDelegate {
 
 	private IWorkbenchWindow window;
 
-	public void dispose() {
-	    // NOP
-	}
-
-	public void init(IWorkbenchWindow window) {
-		this.window = window;
-	}
-
+	@SuppressWarnings("deprecation")
 	public void run(IAction action) {
 		try {
 			if(window == null)
 				window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 			final IWorkbenchPage page = window.getActivePage();
-			final String current = page.getPerspective().getId();
-            if (current.equals(OPIEditorPerspective.ID))
-            {
-                if (MessageDialog.openQuestion(window.getShell(),
-                        "Reset OPI Editor Perspective?",
-                        "You are already in OPI Editor perspective.\n" +
-                        "Do you want to reset it to default arrangement?"))
-                    page.resetPerspective();
-            }else			
-            	PlatformUI.getWorkbench().showPerspective(
-			        OPIEditorPerspective.ID, window);
+			page.showView(IPageLayout.ID_RES_NAV);			
 		} catch (WorkbenchException e) {
 			final String message = NLS.bind(
-					"Failed to open OPI Editor perspective. \n{0}", e.getMessage());
+					"Failed to open navigator. \n{0}", e.getMessage());
 			MessageDialog.openError(null, "Error",
 						message);
 			OPIBuilderPlugin.getLogger().log(Level.WARNING, message, e);
@@ -61,6 +45,10 @@ public class OpenOPIPerspectiveAction implements IWorkbenchWindowActionDelegate 
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
-	    // NOP
+	}
+
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		window=targetPart.getSite().getWorkbenchWindow();
 	}
 }
