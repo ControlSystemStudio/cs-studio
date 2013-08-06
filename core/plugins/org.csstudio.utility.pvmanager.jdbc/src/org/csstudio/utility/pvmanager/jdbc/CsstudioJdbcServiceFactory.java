8a3f34f57e4c79f0cdcec0d92363a00a476c2fb4
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-08-06 17:00:02 -0400

    o.c.u.pvmanager.jdbc: adding drivers

--- core/plugins/org.csstudio.utility.pvmanager.jdbc/src/org/csstudio/utility/pvmanager/jdbc/CsstudioJdbcServiceFactory.java
+++ core/plugins/org.csstudio.utility.pvmanager.jdbc/src/org/csstudio/utility/pvmanager/jdbc/CsstudioJdbcServiceFactory.java
@@ -11,12 +11,24 @@ public class CsstudioJdbcServiceFactory extends JDBCXMLServiceFactory {
 
 	public CsstudioJdbcServiceFactory() {
 		super(getJdbcServiceDirectory());
+		loadDriver("com.mysql.jdbc.Driver");
+		loadDriver("org.postgresql.Driver");
+		loadDriver("oracle.jdbc.driver.OracleDriver");
 	}
 	
 	public static File getJdbcServiceDirectory() {
 		File productDirectory = new File(Platform.getInstallLocation().getURL().getFile() + "/configuration/services/jdbc");
 		Logger.getLogger(CsstudioJdbcServiceFactory.class.getName()).log(Level.CONFIG, "Reading JDBC Services configuration directory " + productDirectory);
 		return productDirectory;
+	}
+	
+	private static void loadDriver(String className) {
+		try {
+			Class.forName(className);
+			Logger.getLogger(CsstudioJdbcServiceFactory.class.getName()).log(Level.CONFIG, "Loaded driver " + className);
+		} catch (Exception ex) {
+			Logger.getLogger(CsstudioJdbcServiceFactory.class.getName()).log(Level.WARNING, "Couldn't load driver " + className);
+		}
 	}
 
 }
