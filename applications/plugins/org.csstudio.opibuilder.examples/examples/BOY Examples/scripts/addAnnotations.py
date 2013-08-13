7bce0ae7e07a5f9059df31bd41aa360ac4c0eca8
xihui <ztyaner11@gmail.com>
2013-08-13 11:15:14 -0400

    XYGraph, BOY: add Annotation Listener

---
+++ applications/plugins/org.csstudio.opibuilder.examples/examples/BOY Examples/scripts/addAnnotations.py
@@ -0,0 +1,55 @@
+from org.csstudio.opibuilder.scriptUtil import PVUtil
+from org.csstudio.swt.widgets.figures.IntensityGraphFigure import IROIListener, IROIInfoProvider
+from org.csstudio.simplepv import IPVListener
+from java.lang import Thread, Runnable
+from org.eclipse.swt.widgets import Display
+from org.csstudio.swt.xygraph.figures import Annotation, IAnnotationListener
+
+leftPV = pvs[1]
+rightPV = pvs[2]
+
+
+xyGraph = widget.getFigure().getXYGraph()
+
+leftAnnotation = Annotation("Left", xyGraph.primaryXAxis, xyGraph.primaryYAxis)
+leftAnnotation.setCursorLineStyle(Annotation.CursorLineStyle.UP_DOWN)
+xyGraph.addAnnotation(leftAnnotation);
+
+class LeftAnnotationListener(IAnnotationListener):
+	'''Listener on left annotation
+	'''
+	def annotationMoved(self, oldX, oldY, newX, newY):
+		leftPV.setValue(newX)
+		
+leftAnnotation.addAnnotationListener(LeftAnnotationListener())
+
+rightAnnotation = Annotation("Right", xyGraph.primaryXAxis, xyGraph.primaryYAxis)
+rightAnnotation.setCursorLineStyle(Annotation.CursorLineStyle.UP_DOWN)
+xyGraph.addAnnotation(rightAnnotation);
+
+class RightAnnotationListener(IAnnotationListener):
+	'''Listener on right annotation
+	'''
+	def annotationMoved(self, oldX, oldY, newX, newY):
+		rightPV.setValue(newX)
+
+rightAnnotation.addAnnotationListener(RightAnnotationListener())
+
+currentDisplay = Display.getCurrent()
+class UpdateAnnotationUITask(Runnable):
+	def run(self):
+		#this method must be called in UI thread
+		leftAnnotation.setValues(PVUtil.getDouble(leftPV), 5)
+		rightAnnotation.setValues(PVUtil.getDouble(rightPV), 5)
+				
+class UpdateAnnotationPVListener(IPVListener):
+	'''Update the ROI while ROI PV value updated'''
+	def valueChanged(self, pv):
+		currentDisplay.asyncExec(UpdateAnnotationUITask())
+
+
+leftPV.addListener(UpdateAnnotationPVListener())
+rightPV.addListener(UpdateAnnotationPVListener())
+
+
+
