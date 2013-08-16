1d7dbdbb0a48ad68ecbba566f516b57b80f594fc
Kay Kasemir <kasemirk@ornl.gov>
2013-08-16 17:00:10 -0400

    o.c.scan: Scan scripts outside of Eclipse use scan.client.jar
    .. located via env. var. SCAN_CLIENT.
    Removed the growing logic for trying to use partial binaries from CSS
    plugins when not running under Eclipse

--- products/SNS/plugins/org.csstudio.scan/jython/jython.sh
+++ products/SNS/plugins/org.csstudio.scan/jython/jython.sh
@@ -6,18 +6,23 @@
 
 # Configure this --------------------------------------------
 
-# Absolute path of jython.jar
-# JYTHON=/home/css/CSS3.1.1/plugins/org.python_*/jython.jar
+# Jython
+JYTHON=/home/controls/css/CSS/plugins/org.python_*/jython.jar
 
-# Directory that contains scan_client.py, scan*.py
-# export JYTHONPATH="/home/controls/css/Share/scan:../jython"
+# Scan client
+export SCAN_CLIENT=/home/controls/css/scan.client.jar
+
+# Directory that contains scan_client.py, scan_ui.py
+# from the org.csstudio.scan/jython directory
+export JYTHONPATH="/home/controls/share/scan"
+export JYTHONPATH="/SNS/users/ky9/my_bl7/applications/bl7-ScanSupport/scan"
+
 
 # -----------------------------------------------------------
-
+# During development, allow usage within the source tree,
+# overriding the above
 if [ -r ../../../../../applications/plugins/org.python/jython.jar ]
 then
-    # When run from within the source tree,
-    # org.python would be under "applications"
     JYTHON=../../../../../applications/plugins/org.python/jython.jar
 fi
 
@@ -27,9 +32,8 @@ then
 fi
 
 # When running jython as a JAR like this...
-#  java -jar ../../yabes.client/lib/jython.jar "$@"
-# one cannot add more to the classpath.
+#  java -jar ../../jython.jar "$@"
+# one cannot extend the classpath; only the jar is used.
 # So we put jython on the class path
 # and specify the main class to run.
-
 java -cp $JYTHON org.python.util.jython "$@"
