3fa03e3ef87c14d78f0a0b9c08a8832b20ffe76b
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-08-28 11:28:22 -0400

    CS-STUDIO: renaming product plugin and removing some nsls2 references

--- org.csstudio.product/src/org/csstudio/nsls2/product/Activator.java
+++ org.csstudio.product/src/org/csstudio/nsls2/product/Activator.java
@@ -10,7 +10,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.csstudio.nsls2.product";
+	public static final String PLUGIN_ID = "org.csstudio.product";
 
 	// The shared instance
 	private static Activator plugin;
