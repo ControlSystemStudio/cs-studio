3cb50b5887aa78cd63f5f97811b29c2c771430bc
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-08-30 09:45:09 -0400

    o.c.u.pvmanager.extra: update to SNAPSHOT Adding array functions.

---
+++ src/org/epics/pvmanager/formula/SubArrayFormulaFunction.java
@@ -0,0 +1,111 @@
+/**
+ * 
+ */
+package org.epics.pvmanager.formula;
+
+import static org.epics.vtype.ValueFactory.alarmNone;
+import static org.epics.vtype.ValueFactory.displayNone;
+import static org.epics.vtype.ValueFactory.newVNumberArray;
+import static org.epics.vtype.ValueFactory.timeNow;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.epics.util.array.ListMath;
+import org.epics.vtype.VNumber;
+import org.epics.vtype.VNumberArray;
+
+/**
+ * @author shroffk
+ * 
+ */
+public class SubArrayFormulaFunction implements FormulaFunction {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.epics.pvmanager.formula.FormulaFunction#isPure()
+     */
+    @Override
+    public boolean isPure() {
+	return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.epics.pvmanager.formula.FormulaFunction#isVarArgs()
+     */
+    @Override
+    public boolean isVarArgs() {
+	return false;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.epics.pvmanager.formula.FormulaFunction#getName()
+     */
+    @Override
+    public String getName() {
+	return "subArray";
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.epics.pvmanager.formula.FormulaFunction#getDescription()
+     */
+    @Override
+    public String getDescription() {
+	return "Return a subarray of the given array, between the specified fromIndex (inclusive) and toIndex(exclusive)";
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.epics.pvmanager.formula.FormulaFunction#getArgumentTypes()
+     */
+    @Override
+    public List<Class<?>> getArgumentTypes() {
+	return Arrays.<Class<?>> asList(VNumberArray.class, VNumber.class,
+		VNumber.class);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.epics.pvmanager.formula.FormulaFunction#getArgumentNames()
+     */
+    @Override
+    public List<String> getArgumentNames() {
+	return Arrays.asList("Array", "fromIndex", "toIndex");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.epics.pvmanager.formula.FormulaFunction#getReturnType()
+     */
+    @Override
+    public Class<?> getReturnType() {
+	return VNumberArray.class;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.epics.pvmanager.formula.FormulaFunction#calculate(java.util.List)
+     */
+    @Override
+    public Object calculate(List<Object> args) {
+	VNumberArray numberArray = (VNumberArray) args.get(0);
+	int fromIndex = ((VNumber) args.get(1)).getValue().intValue();
+	int toIndex = ((VNumber) args.get(2)).getValue().intValue();
+	return newVNumberArray(
+		ListMath.limit(numberArray.getData(), fromIndex, toIndex),
+		alarmNone(), timeNow(), displayNone());
+    }
+
+}
