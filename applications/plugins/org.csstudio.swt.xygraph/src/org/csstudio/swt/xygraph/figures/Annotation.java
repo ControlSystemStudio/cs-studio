b4202544f07ab0d7c7af2b6d60d9f340d7253d8a
xihui <ztyaner11@gmail.com>
2013-09-23 16:36:50 -0400

    swt.xygraph: fix an annotation bug that freezes xygraph

--- applications/plugins/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/figures/Annotation.java
+++ applications/plugins/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/figures/Annotation.java
@@ -679,7 +679,8 @@ public class Annotation extends Figure implements IAxisListener, IDataProviderLi
 		currentPosition = new Point(xAxis.getValuePosition(xValue, false),
 				yAxis.getValuePosition(yValue, false));
 		updateInfoLableText();
-		layout();
+		if(getParent()!=null)
+			layout();
 	}
 	
 	public void axisRangeChanged(Axis axis, Range old_range, Range new_range) {
