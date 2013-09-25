fc843a4c6e230ffd9743970e294f55c8dc1f9b89
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-09-25 10:01:33 -0400

    CS-STUDIO: adding prepare release script that modifies splash screen and about dialog

--- products/CS-STUDIO/prepare-release.sh
+++ products/CS-STUDIO/prepare-release.sh
@@ -10,6 +10,6 @@ exit -1
 fi
 
 echo ::: Prepare splash :::
-java -jar $BUILD_DIR/ImageLabeler-1.0.jar $VERSION 394 53 plugins/org.csstudio.nsls2.product/splash-template.bmp plugins/org.csstudio.nsls2.product/splash.bmp
+java -jar $BUILD_DIR/ImageLabeler-1.0.jar $VERSION 462 53 plugins/org.csstudio.product/splash-template.bmp plugins/org.csstudio.product/splash.bmp
 echo ::: Change about dialog version :::
-echo 0=$VERSION > plugins/org.csstudio.nsls2.product/about.mappings
+echo 0=$VERSION > plugins/org.csstudio.product/about.mappings
