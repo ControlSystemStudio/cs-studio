3bc9c54a2ed59cb75fa2b269524f0c641bc6c555
jhatje <jan.hatje@desy.de>
2013-11-27 13:23:21 +0100

    Desy projects: remove o.c.utility.documentservice.*, toolbox

--- applications/plugins/org.csstudio.utility.toolbox/src/org/csstudio/utility/toolbox/framework/ColumnCreator.java
+++
@@ -1,9 +0,0 @@
-package org.csstudio.utility.toolbox.framework;
-
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.TableViewerColumn;
-
-public interface ColumnCreator {
-
-	TableViewerColumn create(TableViewer tableViewer, int colNumber);
-}
--- applications/plugins/org.csstudio.utility.toolbox/src/org/csstudio/utility/toolbox/func/Func1Void.java
+++
@@ -1,5 +0,0 @@
-package org.csstudio.utility.toolbox.func;
-
-public interface Func1Void<A> {
-	void apply(A arg0);
-}
