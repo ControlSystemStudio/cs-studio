ecffbd017a211a529130c84014554b541a2f5f54
Frédéric ARNAUD <frederic.arnaud@iter.org>
2013-12-02 18:00:46 +0000

    ITER: Added missing ITER copyrights to .java files

--- products/ITER/plugins/org.csstudio.utility.dbdparser/src/org/csstudio/utility/dbdparser/antlr/DbdFileLexer.java
+++ products/ITER/plugins/org.csstudio.utility.dbdparser/src/org/csstudio/utility/dbdparser/antlr/DbdFileLexer.java
@@ -1,5 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2013 ITER Organization.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 // $ANTLR 3.4 cs-studio/products/ITER/plugins/org.csstudio.utility.dbdparser.test/src/org/csstudio/utility/dbdparser/antlr/DbdFile.g 2013-10-04 12:03:00
-
 package org.csstudio.utility.dbdparser.antlr;
 
 
