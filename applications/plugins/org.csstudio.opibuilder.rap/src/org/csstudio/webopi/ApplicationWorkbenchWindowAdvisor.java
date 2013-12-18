cca88fb8de2c4bc8547046d700eb05aeed721a9b
Frédéric ARNAUD <frederic.arnaud@iter.org>
2013-12-18 17:22:11 +0000

    o.c.opibuilder.rap:[WebOPI] Fixed bug when startup_opi preference is set with a .plt file (Issue #318)

--- applications/plugins/org.csstudio.opibuilder.rap/src/org/csstudio/webopi/ApplicationWorkbenchWindowAdvisor.java
+++ applications/plugins/org.csstudio.opibuilder.rap/src/org/csstudio/webopi/ApplicationWorkbenchWindowAdvisor.java
@@ -4,11 +4,16 @@ import org.csstudio.opibuilder.preferences.PreferencesHelper;
 import org.csstudio.opibuilder.runmode.RunModeService;
 import org.csstudio.opibuilder.runmode.RunnerInput;
 import org.csstudio.opibuilder.runmode.RunModeService.TargetWindow;
+import org.csstudio.opibuilder.util.SingleSourceHelper;
 import org.csstudio.webopi.util.RequestUtil;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.rwt.RWT;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
@@ -41,13 +46,33 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 				path = PreferencesHelper.getStartupOPI();
 			if(path == null)
 				return;
-			RunModeService.getInstance().runOPI(path, 
-    			TargetWindow.SAME_WINDOW, null);
-		}else	
-			RunModeService.getInstance().runOPI(
-					runnerInput.getPath(), TargetWindow.SAME_WINDOW, null, runnerInput.getMacrosInput());
-    	
-    }
+			if (path.getFileExtension().toLowerCase().equals("opi")) {
+				RunModeService.getInstance().runOPI(path, TargetWindow.SAME_WINDOW, null);
+			} else {
+				runOther(path);
+			}
+		} else {
+			IPath path = runnerInput.getPath();
+			if (path != null) {
+				if (path.getFileExtension().toLowerCase().equals("opi")) {
+					RunModeService.getInstance().runOPI(path, TargetWindow.SAME_WINDOW, null, runnerInput.getMacrosInput());
+				} else {
+					runOther(path);
+				}
+			}
+		}
+	}
+
+	private void runOther(final IPath path) {
+		IWorkbenchPage page = PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getActivePage();
+		try {
+			SingleSourceHelper.openEditor(page, path);
+		} catch (Exception e) {
+			String message = NLS.bind("Failed to open the editor. \n {0}", e);
+			MessageDialog.openError(null, "Error in opening OPI", message);
+		}
+	}
     
     public void preWindowOpen() {
     	
