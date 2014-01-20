4c793b56c9fdbee432d807c830fae19dd79a0f06
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-01-20 11:29:37 -0500

    org.epics.graphene: javadocs update

--- applications/plugins/org.epics.graphene/src/org/epics/graphene/Iterators.java
+++ applications/plugins/org.epics.graphene/src/org/epics/graphene/Iterators.java
@@ -4,7 +4,6 @@
  */
 package org.epics.graphene;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -16,36 +15,6 @@ import org.epics.util.array.IteratorNumber;
  * @author carcassi
  */
 public class Iterators {
-    /**
-     *Creates a pointer for the given data at the given starting offset.
-     * Can not point past the given end offset.
-     * @param data double[] 
-     * @param startOffset current index of the new pointer.
-     * @param endOffset last index that the pointer will be able to point to.
-     * @return IteratorDouble, pointer to the data at the index of startOffset.
-     */
-    public static IteratorDouble arrayIterator(final double[] data, final int startOffset, final int endOffset) {
-        return new IteratorDouble() {
-            
-            int index = startOffset;
-
-            @Override
-            public boolean hasNext() {
-                return index != endOffset;
-            }
-
-            @Override
-            public double nextDouble() {
-                if (!hasNext())
-                    throw new NoSuchElementException();
-                double value = data[index];
-                index++;
-                if (index == data.length)
-                    index = 0;
-                return value;
-            }
-        };
-    }
     
     /**
      *Creates a pointer for the given data at index 0.
