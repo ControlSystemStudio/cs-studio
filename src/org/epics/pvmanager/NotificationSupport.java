bd55d88a2a5bcf881562f214d30f67e29a8244d6
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-01-28 10:04:48 -0500

    o.c.u.pvmanager.*: update to 2.4

--- src/org/epics/pvmanager/NotificationSupport.java
+++ src/org/epics/pvmanager/NotificationSupport.java
@@ -1,6 +1,6 @@
 /**
- * Copyright (C) 2010-12 Brookhaven National Laboratory
- * All rights reserved. Use is subject to license terms.
+ * Copyright (C) 2010-14 pvmanager developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
  */
 package org.epics.pvmanager;
 
@@ -8,7 +8,7 @@ import org.epics.pvmanager.util.NullUtils;
 
 /**
  * Dedicated notification type support.
- * 
+ *
  * @param <T> type for which the notifications are prepared
  * @author bknerr
  * @since 17.01.2011
@@ -23,7 +23,7 @@ public abstract class NotificationSupport<T> extends TypeSupport<T> {
     public NotificationSupport(Class<T> clazz) {
         super(clazz, NotificationSupport.class);
     }
-    
+
     /**
      * Returns the final value by using the appropriate type support.
      *
@@ -33,17 +33,35 @@ public abstract class NotificationSupport<T> extends TypeSupport<T> {
      * @return the value to be notified
      */
     public static <T> Notification<T> notification(final T oldValue, final T newValue) {
+        NotificationSupport<T> support = findNotificationSupportFor(newValue);
+        return support.prepareNotification(oldValue, newValue);
+    }
+    
+    /**
+     * Retrieves the notification support based on the given value.
+     * <p>
+     * If no support is found, an exception is thrown.
+     * 
+     * @param <T> value type
+     * @param newValue the value; can't be null
+     * @return the notification support
+     */
+    public static <T> NotificationSupport<T> findNotificationSupportFor(final T newValue) {
         @SuppressWarnings("unchecked")
         Class<T> typeClass = (Class<T>) newValue.getClass();
-        NotificationSupport<T> support =
-            (NotificationSupport<T>) findTypeSupportFor(NotificationSupport.class,
-                                                              typeClass);
+        NotificationSupport<T> support
+                = (NotificationSupport<T>) findTypeSupportFor(NotificationSupport.class,
+                        typeClass);
         if (support == null) {
-            throw new RuntimeException("Values for type " + typeClass.getSimpleName() + " are not setup for notification.");
+            String name = typeClass.getSimpleName();
+            if (name == null || name.trim().isEmpty()) {
+                name = typeClass.getName();
+            }
+            throw new RuntimeException("Final value can't be of type " + name + ".");
         }
-        return support.prepareNotification(oldValue, newValue);
-    }    
-    
+        return support;
+    }
+
     /**
      * Given the old and new value, prepare the final value that will be notified.
      * This method is guaranteed to be called in the notification thread (the
@@ -55,7 +73,7 @@ public abstract class NotificationSupport<T> extends TypeSupport<T> {
      * @return the value to be notified
      */
     public abstract Notification<T> prepareNotification(final T oldValue, final T newValue);
-    
+
     /**
      * Support for notification of immutable types. Notification is enabled if
      * the value changed according to {@link Object#equals(java.lang.Object) }.
@@ -66,13 +84,13 @@ public abstract class NotificationSupport<T> extends TypeSupport<T> {
      */
     public static <T> NotificationSupport<T> immutableTypeSupport(final Class<T> clazz) {
         return new NotificationSupport<T>(clazz) {
-          @Override
-          public Notification<T> prepareNotification(final T oldValue, final T newValue) {
-              if (NullUtils.equalsOrBothNull(oldValue, newValue)) {
-                return new Notification<T>(false, null);
+            @Override
+            public Notification<T> prepareNotification(final T oldValue, final T newValue) {
+                if (NullUtils.equalsOrBothNull(oldValue, newValue)) {
+                    return new Notification<T>(false, null);
+                }
+                return new Notification<T>(true, newValue);
             }
-              return new Notification<T>(true, newValue);
-          }
         };
     }
 }
