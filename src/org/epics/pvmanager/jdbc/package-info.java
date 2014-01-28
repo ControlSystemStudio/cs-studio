2d7e06332f57209368b3d03f6e29c7a61b6ccfae
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-01-28 10:04:48 -0500

    o.c.u.pvmanager.*: update to 2.4

--- src/org/epics/pvmanager/jdbc/package-info.java
+++ src/org/epics/pvmanager/jdbc/package-info.java
@@ -1,9 +1,8 @@
 /**
- * Copyright (C) 2010-12 Brookhaven National Laboratory
- * All rights reserved. Use is subject to license terms.
+ * Copyright (C) 2010-14 pvmanager developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
  */
-
 /**
- * Support for database services based on JDBC.
+ * Support for database services based on JDBC (<a href="doc-files/jdbc-xml-syntax.html">jdbc xml syntax</a>).
  */
 package org.epics.pvmanager.jdbc;
