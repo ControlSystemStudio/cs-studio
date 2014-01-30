acaf593b37935abee4673f62a1af508d54be0634
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-01-30 16:23:16 -0500

    build: sync script should now commit changes for epics-util

--- build/sync-diirt.sh
+++ build/sync-diirt.sh
@@ -9,5 +9,7 @@ echo Synching epics-util
 rm -rf $HGDIR
 hg clone http://hg.code.sf.net/p/epics-util/code $HGDIR
 rsync -r --delete $HGDIR/src/main/java/org/epics/util ../core/plugins/org.epics.util/src/org/epics/
+git add ../core/plugins/org.epics.util/src/org/epics/util
 rm -rf $HGDIR
+git commit --author="Gabriele Carcassi <gabriele.carcassi@gmail.com>" -m "org.epics.util: update to current SNAPSHOT"
 echo Done epics-util
