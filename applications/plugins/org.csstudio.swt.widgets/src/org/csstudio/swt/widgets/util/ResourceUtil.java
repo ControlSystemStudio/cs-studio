60f3111a032ea20f9ab5abac34afc571d665add6
Kay Kasemir <kasemirk@ornl.gov>
2014-02-27 17:17:30 -0500

    o.c.swt.widgets: Last change missed the import for URI

--- applications/plugins/org.csstudio.swt.widgets/src/org/csstudio/swt/widgets/util/ResourceUtil.java
+++ applications/plugins/org.csstudio.swt.widgets/src/org/csstudio/swt/widgets/util/ResourceUtil.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 
