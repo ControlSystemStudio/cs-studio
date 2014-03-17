8070b252964e15da9c5c9bf0ed9678b2dafde405
Dylan Maxwell <maxwelld@frib.msu.edu>
2014-03-17 18:04:32 -0400

    Remove source code for org.epics.pvmanager.pva.* and update MANIFEST.MF

--- plugins/org.csstudio.utility.pvmanager.pva/src/org/epics/pvmanager/pva/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright (C) 2010-14 pvmanager developers. See COPYRIGHT.TXT
- * All rights reserved. Use is subject to license terms. See LICENSE.TXT
- */
-/**
- * Support for Epics 4 data source (<a href="doc-files/pva-datasource.html">channel syntax</a>).
- */
-package org.epics.pvmanager.pva;
