d036501a7622831d6166101a6d5ba19c03105871
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-04-10 14:18:58 -0400

    o.c.graphene.opiwidgets: histogram supports selection and configuration

--- applications/plugins/org.csstudio.graphene.opiwidgets/src/org/csstudio/graphene/opiwidgets/HistogramGraph2DWidgetEditPart.java
+++ applications/plugins/org.csstudio.graphene.opiwidgets/src/org/csstudio/graphene/opiwidgets/HistogramGraph2DWidgetEditPart.java
@@ -3,13 +3,15 @@
  */
 package org.csstudio.graphene.opiwidgets;
 
+import org.csstudio.graphene.BubbleGraph2DWidget;
+import org.csstudio.graphene.HistogramGraph2DWidget;
 import org.eclipse.draw2d.IFigure;
 
 /**
  * @author shroffk
  * 
  */
-public class HistogramGraph2DWidgetEditPart extends AbstractPointDatasetGraph2DWidgetEditpart<HistogramGraph2DWidgetFigure, HistogramGraph2DWidgetModel> {
+public class HistogramGraph2DWidgetEditPart extends AbstractGraph2DWidgetEditpart<HistogramGraph2DWidgetFigure, HistogramGraph2DWidgetModel> {
 
 	/*
 	 * (non-Javadoc)
@@ -22,6 +24,22 @@ public class HistogramGraph2DWidgetEditPart extends AbstractPointDatasetGraph2DW
 		HistogramGraph2DWidgetFigure figure = new HistogramGraph2DWidgetFigure(this);
 		configure(figure, getWidgetModel());
 		return figure;
+	}
+	
+	@Override
+	protected void configure(HistogramGraph2DWidgetFigure figure, HistogramGraph2DWidgetModel model) {
+		super.configure(figure, model);
+		HistogramGraph2DWidget widget = figure.getSWTWidget();
+		if (figure.isRunMode()) {
+			widget.setHighlightSelectionValue(model.isHighlightSelectionValue());
+		}
+	}
+
+	@Override
+	protected void registerPropertyChangeHandlers() {
+		super.registerPropertyChangeHandlers();
+		setPropertyChangeHandler(BubbleGraph2DWidgetModel.PROP_HIGHLIGHT_SELECTION_VALUE, getReconfigureWidgetPropertyChangeHandler());
+		setPropertyChangeHandler(BubbleGraph2DWidgetModel.PROP_SELECTION_VALUE_PV, getReconfigureWidgetPropertyChangeHandler());
 	}
 
 }
--- applications/plugins/org.csstudio.graphene.opiwidgets/src/org/csstudio/graphene/opiwidgets/HistogramGraph2DWidgetModel.java
+++ applications/plugins/org.csstudio.graphene.opiwidgets/src/org/csstudio/graphene/opiwidgets/HistogramGraph2DWidgetModel.java
@@ -4,6 +4,9 @@
 package org.csstudio.graphene.opiwidgets;
 
 import org.csstudio.graphene.HistogramGraph2DWidget;
+import org.csstudio.opibuilder.properties.BooleanProperty;
+import org.csstudio.opibuilder.properties.StringProperty;
+import org.csstudio.opibuilder.properties.WidgetPropertyCategory;
 import org.csstudio.opibuilder.widgets.extra.AbstractSelectionWidgetModelDescription;
 
 
@@ -23,6 +26,26 @@ public class HistogramGraph2DWidgetModel extends AbstractPointDatasetGraph2DWidg
 	@Override
 	public String getTypeID() {
 		return ID;
+	}
+	
+	public static final String PROP_HIGHLIGHT_SELECTION_VALUE = "highlight_selection_value"; //$NON-NLS-1$
+	public static final String PROP_SELECTION_VALUE_PV = "selection_value_pv"; //$NON-NLS-1$
+	
+	@Override
+	protected void configureProperties() {
+		super.configureProperties();
+		addProperty(new BooleanProperty(PROP_HIGHLIGHT_SELECTION_VALUE,
+				"Highlight Selection Value", WidgetPropertyCategory.Basic, false));
+		addProperty(new StringProperty(PROP_SELECTION_VALUE_PV,
+				"Selection Value PV (VTable)", WidgetPropertyCategory.Basic, ""));
+	}
+	
+	public String getSelectionValuePv() {
+		return (String) getCastedPropertyValue(PROP_SELECTION_VALUE_PV);
+	}
+
+	public boolean isHighlightSelectionValue() {
+		return (Boolean) getCastedPropertyValue(PROP_HIGHLIGHT_SELECTION_VALUE);
 	}
 	
 	@Override
