0e7d9f404065f90f18795dbf4092c1bc045ab5f2
Eric Berryman <eric.berryman@gmail.com>
2014-04-16 16:18:26 -0400

    unset DISPLAY for packaging

--- package.sh
+++ package.sh
@@ -14,5 +14,6 @@ unzip repository/target/products/*64.zip -d ${WIN_BUILD_DIR}
 cd ${WIN_BUILD_DIR}
 cp -R ../applicationIcons cs-studio/
 
+unset DISPLAY
 wine InnoSetup5/ISCC.exe css-windows-build/install_script_general.iss "\dCSSVersion=${VERSION}"
 echo ::: DONE :::
