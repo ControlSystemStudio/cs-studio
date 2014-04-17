4dd4b77feaca63377913e111bff7641d069f1f56
Eric Berryman <eric.berryman@gmail.com>
2014-04-17 09:26:01 -0400

    added prepare-release.sh

--- prepare-release.sh
+++ prepare-release.sh
@@ -2,10 +2,13 @@
 
 # Check parameters
 VERSION=$1
+COMPATLINK=$2
+MILESTONE=$3
+NOTES=$4
 BUILD_DIR="build"
-if [ -z "$VERSION" ]
+if [! $# == 4 ]
 then 
-  echo You must provide the product version \(e.g. \"prepare_release.sh 3.2.4\"\)
+  echo You must provide the product version, compat link, milestone, notes \(e.g. \"prepare_release.sh 3.3.0 \"https://github\" \"https://github\" \"Some notes\"\"\)
 exit -1
 fi
 
@@ -13,3 +16,20 @@ echo ::: Prepare splash :::
 java -jar $BUILD_DIR/ImageLabeler-1.0.jar $VERSION 462 53 plugins/org.csstudio.product/splash-template.bmp plugins/org.csstudio.product/splash.bmp
 echo ::: Change about dialog version :::
 echo 0=$VERSION > plugins/org.csstudio.product/about.mappings
+
+echo ::: Updating plugin versions ::
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$VERSION -Dartifacts=org.csstudio.product,org.csstudio.startup.intro
+
+HTML="<h2>Version ${VERSION} - $(date +"%Y-%m-%d")</h2>
+<ul>
+<li>See specific application changelogs</li>
+<li>${NOTES}</li>
+<li><a href="${COMPATLINK}" shape="rect">Compatibility Notes and Know Bugs</a></li>
+<li><a href="${MILESTONE}" shape="rect">Closed Issues</a></li>
+</ul>"
+
+sed -i "{N; s/\(<\/p>\)/\1\n\n${HTML}/}" plugins/org.csstudio.startup.intro/html/changelog.html
+
+echo ::: Committing and tagging version $VERSION :::
+git commit -m "Updating changelog, splash, manifests to version $VERSION"
+git tag CSS-$VERSION
