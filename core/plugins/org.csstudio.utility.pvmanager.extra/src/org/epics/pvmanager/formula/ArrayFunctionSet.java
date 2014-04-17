f18efafa5fc1739ab79453f38cf027e415c93f1c
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-04-16 20:28:46 -0400

    o.c.u.pvmanager.extra: update to current SNAPSHOT

--- core/plugins/org.csstudio.utility.pvmanager.extra/src/org/epics/pvmanager/formula/ArrayFunctionSet.java
+++ core/plugins/org.csstudio.utility.pvmanager.extra/src/org/epics/pvmanager/formula/ArrayFunctionSet.java
@@ -156,6 +156,7 @@ public class ArrayFunctionSet extends FormulaFunctionSet {
                             }
                         })
                 .addFormulaFunction(new DftFormulaFunction())
+                .addFormulaFunction(new ArrayRangeOfFormulaFunction())
         );
     }
 }
