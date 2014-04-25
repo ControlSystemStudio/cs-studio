e9545e5f3cfc4dac811e266f3aeccfee279efe78
Frédéric ARNAUD <frederic.arnaud@iter.org>
2014-04-25 09:18:26 +0000

    Alarm Notifier: Adapted new logbook automated action plugin to be generic (Issue #354)

--- applications/plugins/org.csstudio.alarm.beast.notifier.logbook/src/org/csstudio/alarm/beast/notifier/logbook/Activator.java
+++ applications/plugins/org.csstudio.alarm.beast.notifier.logbook/src/org/csstudio/alarm/beast/notifier/logbook/Activator.java
@@ -5,15 +5,20 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
-package org.csstudio.alarm.beast.notifier.olog;
+package org.csstudio.alarm.beast.notifier.logbook;
 
-/**
- * Constant definitions for plug-in preferences
- */
-public class PreferenceConstants {
+import java.util.logging.Logger;
 
-	public static final String Olog_URL = "olog_url";
-	public static final String Default_logbook = "Default.logbook";
-	public static final String Default_level = "Default.level";
+public class Activator {
+
+	/** Plugin ID defined in MANIFEST.MF */
+	final public static String PLUGIN_ID = "org.csstudio.alarm.beast.notifier.logbook";
+
+	final private static Logger logger = Logger.getLogger(PLUGIN_ID);
+
+	/** @return Logger for plugin ID */
+	public static Logger getLogger() {
+		return logger;
+	}
 
 }
--- products/ITER/plugins/org.csstudio.iter.notifier.ologauthentication/src/org/csstudio/iter/notifier/ologauthentication/PreferenceConstants.java
+++ products/ITER/plugins/org.csstudio.iter.notifier.ologauthentication/src/org/csstudio/iter/notifier/ologauthentication/PreferenceConstants.java
@@ -12,6 +12,7 @@ package org.csstudio.iter.notifier.ologauthentication;
  */
 public class PreferenceConstants {
 
+	public static final String Olog_URL = "olog_url";
 	public static final String anonymous_olog_user = "anonymous_olog_user";
 	public static final String anonymous_olog_password = "anonymous_olog_password";
 	public static final String connected_olog_user = "connected_olog_user";
