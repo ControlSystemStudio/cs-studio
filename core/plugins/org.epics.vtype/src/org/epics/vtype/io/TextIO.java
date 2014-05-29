61d0641fe914cc3f346c4b04d38023b4f120fcc4
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-05-29 10:07:29 -0400

    org.epics.vtype: update to current SNAPSHOT

--- core/plugins/org.epics.vtype/src/org/epics/vtype/io/TextIO.java
+++ core/plugins/org.epics.vtype/src/org/epics/vtype/io/TextIO.java
@@ -6,7 +6,10 @@
 package org.epics.vtype.io;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Reader;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -14,8 +17,11 @@ import java.util.regex.Pattern;
 import org.epics.util.array.ArrayDouble;
 import org.epics.util.array.ListDouble;
 import static org.epics.util.text.StringUtil.DOUBLE_REGEX_WITH_NAN;
+import org.epics.vtype.VNumberArray;
+import org.epics.vtype.VStringArray;
 import org.epics.vtype.VType;
 import org.epics.vtype.ValueFactory;
+import org.epics.vtype.ValueUtil;
 
 /**
  * Utility class to serialize/de-serialize VTypes to text files.
@@ -26,36 +32,60 @@ public class TextIO {
     private static final Pattern pDouble = Pattern.compile(DOUBLE_REGEX_WITH_NAN);
 
     /**
-     * Reader a file where each line represents a value.
+     * Reads a file where each line represents a value in an array.
      * If all lines are numbers the result is a VNumberArray, otherwise
      * is a VStringArray.
      * 
      * @param reader the stream to read
      * @return a type
+     * @throws IOException all exceptions go through
      */
-    public static VType readList(Reader reader) {
-        try {
-            Matcher doubleMatcher = pDouble.matcher("");
-            List<String> lines = new ArrayList<>();
-            BufferedReader br = new BufferedReader(reader);
-            String line;
-            boolean numbers = true;
-            while ((line = br.readLine()) != null) {
-                lines.add(line);
-                if (!doubleMatcher.reset(line).matches()) {
-                    numbers = false;
-                }
+    public static VType readList(Reader reader) throws IOException {
+        Matcher doubleMatcher = pDouble.matcher("");
+        List<String> lines = new ArrayList<>();
+        BufferedReader br = new BufferedReader(reader);
+        String line;
+        boolean numbers = true;
+        while ((line = br.readLine()) != null) {
+            lines.add(line);
+            if (!doubleMatcher.reset(line).matches()) {
+                numbers = false;
             }
-            
-            if (numbers) {
-                ListDouble numberList = convertToListDouble(lines);
-                return ValueFactory.toVType(numberList);
-            } else {
-                return ValueFactory.toVType(lines);
-            }
-        } catch (Exception exception) {
-            throw new RuntimeException("Can't read list", exception);
         }
+
+        if (numbers) {
+            ListDouble numberList = convertToListDouble(lines);
+            return ValueFactory.toVType(numberList);
+        } else {
+            return ValueFactory.toVType(lines);
+        }
+    }
+    
+    /**
+     * Writes a file where each line represents a value in an array.
+     * <p>
+     * No buffering, flushing or closing is performed by this function.
+     * 
+     * @param vType the type to be written
+     * @param writer the writer
+     * @throws IOException all exceptions go through
+     */
+    public static void writeList(VType vType, Writer writer) throws IOException {
+        PrintWriter out = new PrintWriter(writer);
+        if (vType instanceof VNumberArray) {
+            VNumberArray array = (VNumberArray) vType;
+            for (int i = 0; i < array.getData().size(); i++) {
+                out.println(array.getData().getDouble(i));
+            }
+        } else if (vType instanceof VStringArray) {
+            VStringArray array = (VStringArray) vType;
+            for (int i = 0; i < array.getData().size(); i++) {
+                out.println(array.getData().get(i));
+            }
+        } else {
+            throw new UnsupportedOperationException("Can't serialize " + ValueUtil.typeOf(vType).getSimpleName() + " to a list");
+        }
+        out.flush();
     }
     
     // TODO: copied from CsvParser
