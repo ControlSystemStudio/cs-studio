31869879b6a4ab77882a917e8570982d5b527b6a
Kunal Shroff <shroffk@bnl.gov>
2014-06-18 13:55:52 -0400

    #441: Log Viewer Application bug fixes and upgrades
    Setting default order of logEntry list

--- applications/plugins/org.csstudio.logbook.viewer/src/org/csstudio/logbook/ui/LogQueryJob.java
+++ applications/plugins/org.csstudio.logbook.viewer/src/org/csstudio/logbook/ui/LogQueryJob.java
@@ -4,6 +4,9 @@
 package org.csstudio.logbook.ui;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 import org.csstudio.logbook.LogEntry;
@@ -16,40 +19,50 @@ import org.eclipse.core.runtime.jobs.Job;
 
 /**
  * @author Kunal Shroff
- *
+ * 
  */
 public class LogQueryJob extends Job {
 
-	private final static String name = "LogQueryJob";
+    private final static String name = "LogQueryJob";
 
-	private final String query;
-	private final LogbookClient logbookClient;
+    private final String query;
+    private final LogbookClient logbookClient;
 
-	LogQueryJob(String query, LogbookClient logbookClient) {
-	    super(name);
-	    this.query = query;
-	    this.logbookClient = logbookClient;
-	}
+    LogQueryJob(String query, LogbookClient logbookClient) {
+	super(name);
+	this.query = query;
+	this.logbookClient = logbookClient;
+    }
 
-	void completedQuery(LogResult result) {
+    void completedQuery(LogResult result) {
 
-	}
+    }
 
-	@Override
-	protected IStatus run(IProgressMonitor monitor) {
-	    LogResult result = null;
-	    try {
-		List<LogEntry> logs = new ArrayList<LogEntry>(
-			logbookClient.findLogEntries(query));
-		result = new LogResult(logs, null);
-	    } catch (Exception e) {
-		result = new LogResult(null, e);
-	    } finally {
-		if (!monitor.isCanceled()) {
-		    completedQuery(result);
+    @Override
+    protected IStatus run(IProgressMonitor monitor) {
+	LogResult result = null;
+	try {
+	    List<LogEntry> logs = new ArrayList<LogEntry>(
+		    logbookClient.findLogEntries(query));
+	    Collections.sort(logs, new Comparator<LogEntry>() {
+
+		@Override
+		public int compare(LogEntry o1, LogEntry o2) {
+		    Date d1 = o1.getCreateDate();
+		    Date d2 = o2.getCreateDate();
+		    return d2.compareTo(d1);
 		}
+
+	    });
+	    result = new LogResult(logs, null);
+	} catch (Exception e) {
+	    result = new LogResult(null, e);
+	} finally {
+	    if (!monitor.isCanceled()) {
+		completedQuery(result);
 	    }
-	    return Status.OK_STATUS;
 	}
+	return Status.OK_STATUS;
+    }
 
 }
