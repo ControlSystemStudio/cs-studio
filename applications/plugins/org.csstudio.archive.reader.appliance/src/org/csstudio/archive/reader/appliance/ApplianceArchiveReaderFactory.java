ec31ea0a006fc68779aa3db3a2b55ac4e57799e7
Eric Berryman <eric.berryman@gmail.com>
2014-07-30 17:02:20 -0400

    o.c.a.reader.appliance: possible concurrency issue

--- applications/plugins/org.csstudio.archive.reader.appliance/src/org/csstudio/archive/reader/appliance/ApplianceArchiveReaderFactory.java
+++ applications/plugins/org.csstudio.archive.reader.appliance/src/org/csstudio/archive/reader/appliance/ApplianceArchiveReaderFactory.java
@@ -1,5 +1,6 @@
 package org.csstudio.archive.reader.appliance;
 
+import java.util.concurrent.ConcurrentHashMap;
 import org.csstudio.archive.reader.ArchiveReader;
 import org.csstudio.archive.reader.ArchiveReaderFactory;
 
@@ -13,8 +14,19 @@ public class ApplianceArchiveReaderFactory implements ArchiveReaderFactory{
 	/* (non-Javadoc)
 	 * @see org.csstudio.archive.reader.ArchiveReaderFactory#getArchiveReader(java.lang.String)
 	 */
+	final private static ConcurrentHashMap<String, ApplianceArchiveReader> cache = new ConcurrentHashMap<String, ApplianceArchiveReader>();
+
 	@Override
 	public ArchiveReader getArchiveReader(String url) throws Exception {
-		return new ApplianceArchiveReader(url, Activator.getDefault().isUseStatistics());
+
+		ApplianceArchiveReader result = cache.get(url);
+	    if( result == null ) {
+	    	ApplianceArchiveReader reader = cache.putIfAbsent(url,new ApplianceArchiveReader(url, Activator.getDefault().isUseStatistics()));
+	      if( reader != null ) {
+	        result = reader;
+	      }
+	    }
+	    return result;
 	}
+	
 }
