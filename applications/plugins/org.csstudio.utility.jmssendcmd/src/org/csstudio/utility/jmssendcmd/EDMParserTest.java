2a96a3ce91f2b531754e02b6ab9d24e1793ed338
Kay Kasemir <kasemirk@ornl.gov>
2014-07-30 11:24:09 -0400

    o.c.utility.jmssendcmd: Fix EDMPUTLOGGER 'ssh' format
    Allow values without "::ffff:" as well as with that prefix.

--- applications/plugins/org.csstudio.utility.jmssendcmd/src/org/csstudio/utility/jmssendcmd/EDMParserTest.java
+++ applications/plugins/org.csstudio.utility.jmssendcmd/src/org/csstudio/utility/jmssendcmd/EDMParserTest.java
@@ -11,26 +11,33 @@ import junit.framework.TestCase;
 @SuppressWarnings("nls")
 public class EDMParserTest extends TestCase
 {
-    @Test
-    /** Various tests to confirm the EDM input string was parsed correctly */
-    public void testEDMParser() throws Throwable
+    /** Original type of message when this software was developed */
+	@Test
+    public void testOriginalEDMMessage() throws Throwable
     {
-        //  EDMParser parser =new EDMParser("user=\"nypaver\" host=\"ics-srv02\"  dsp=\"localhost:14.0\" name=\"test\" old=\"11.000000\" new=\"12.000000\"");      
         final EDMParser parser = new EDMParser("user=\"nypaver\" host=\"ics-srv02\" ssh=\"::ffff:160.91.234.112 56165 ::ffff:160.91.230.38 22\" unknown_extra_stuff=\"ignored\" dsp=\"localhost:14.0\" name=\"test\" old=\"11.000000\" new=\"12.000000\"");      
 
-        /**
-         * Confirm the parser read the input string correctly.
-         */
+        // Confirm the parser read the input string correctly.
         assertEquals("nypaver", parser.getUser());
         // Should get the name of the 'real' host from which user logged in
-        //  assertEquals("ics-srv02", parser.getHost());
         assertEquals("160.91.234.112", parser.getHost());
         assertEquals("test", parser.getPVName());
         assertEquals("12.000000", parser.getValue());
     }
+	
+    /** New type of EDM message seen at SNS with different network/Linux setup */
+	@Test
+    public void testNewEDMMessage() throws Throwable
+    {
+        final EDMParser parser = new EDMParser("user=\"fred\" host=\"ics-srv-accl2\" ssh=\"172.31.96.16 53395 172.31.72.100 22\" dsp=\"localhost:12.0\" name=\"SCL_LLRF:FCM21b:cavAmpGoal\" old=\"11.000000\" new=\"10.000000\"");
+        assertEquals("fred", parser.getUser());
+        assertEquals("172.31.96.16", parser.getHost());
+        assertEquals("SCL_LLRF:FCM21b:cavAmpGoal", parser.getPVName());
+        assertEquals("10.000000", parser.getValue());
+    }
 
-    @Test
     /** Check error handling */
+	@Test
     public void testParseError()
     {
         try
