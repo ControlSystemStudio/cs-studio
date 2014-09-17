b0d71417f1dd83642c2feacac4c023198bfccdb7
Kunal Shroff <shroffk@bnl.gov>
2014-09-17 11:13:33 -0400

    #462: Adding check to ensure features don't have an empty provider and description feature clean up (owner of the feature/application) (Lead: Kunal)

--- build/org.csstudio.validation.scripts/check_feature.py
+++ build/org.csstudio.validation.scripts/check_feature.py
@@ -7,19 +7,28 @@ A simple script which scans all feature.xml files under the directory it was inv
 '''
 
 import os.path
-import re
 import sys
 import xml.etree.ElementTree as ET
+
+defaultDesc = "[Enter Feature Description here.]"
 
 incorrectFiles = []
 for dirpath, dirnames, filenames in os.walk("."):
     for completefilename in [ os.path.join(dirpath, f) for f in filenames if f == "feature.xml" ]:
         tree = ET.parse(completefilename)
         if tree.findall("requires"):
-            incorrectFiles.append(completefilename)
+            incorrectFiles.append(completefilename + " REASON: The feature file should not have the requires node, you can ensure this by removing all the dependencies defined in the feature")
+        root = tree.getroot()
+        if "provider-name" not in root.attrib:
+            incorrectFiles.append(completefilename + " REASON: The feature file should have a provider")
+        descElem = root.find("description")
+        if descElem is not None:
+            if defaultDesc in descElem.text and "url" not in descElem.attrib:
+                incorrectFiles.append(completefilename + " REASON: No feature description")
+        else:
+            incorrectFiles.append(completefilename + " REASON: No feature description")
 if len(incorrectFiles) != 0:
-    print 'Following incorrectly configured feature.xml files are committed to repository. \
-    The feature file should not have the requires node, you can ensure this by removing all the dependencies defined in the feature'
+    print 'Following incorrectly configured feature.xml files are committed to repository.'    
     for f in incorrectFiles:
         print f
     sys.exit(-1)
