426882ecf75b8f174367a5873b215820268e8b2d
bobnarj <jaka.bobnar@cosylab.com>
2014-10-28 12:24:07 +0100

    Added missing compile command.

--- products/ITER/thirdparty/de.desy.language.snl.ui/src/de/desy/language/snl/ui/editor/CompileCommandHandler.java
+++ products/ITER/thirdparty/de.desy.language.snl.ui/src/de/desy/language/snl/ui/editor/CompileCommandHandler.java
@@ -4,11 +4,28 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 public class CompileCommandHandler extends AbstractHandler implements IHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		System.out.println();
+
+		IEditorPart editor = HandlerUtil.getActiveEditor(event);
+		if (editor instanceof SNLEditor) {
+			if (editor.isDirty()) {
+				MessageBox box = new MessageBox(editor.getSite().getShell(),
+						SWT.ICON_INFORMATION);
+				box.setText("Unsaved changes");
+				box.setMessage("There are unsaved changes.\nPlease save before compilation.");
+				box.open();
+			} else {
+				((SNLEditor)editor).compileFile(new NullProgressMonitor());
+			}
+		}
 		return null;
 	}
 
