8db67b557be86e7409ef3e1bec6453a9bbb6db33
bobnarj <jaka.bobnar@cosylab.com>
2014-10-29 15:09:42 +0100

    Suppressed too many CONFIG messages ft startup rom one of the jersey classes.

--- applications/plugins/org.csstudio.alarm.beast.notifier/src/org/csstudio/alarm/beast/notifier/Application.java
+++ applications/plugins/org.csstudio.alarm.beast.notifier/src/org/csstudio/alarm/beast/notifier/Application.java
@@ -31,6 +31,7 @@ public class Application implements IApplication {
 
 	private static final String[] VERBOSE_PACKAGES = new String[] {
 			"com.sun.jersey.core.spi.component",
+			"com.sun.jersey.core.spi.component.ProviderServices",
 			"com.sun.jersey.spi.service.ServiceFinder" };
 
 	final public static String APPLICATION_NAME = "AlarmNotifier";
