ef0a98218f7a0833c0c67cfc659bbf18716ba850
bobnarj <jaka.bobnar@cosylab.com>
2014-10-30 20:32:51 +0100

    Proper handling of the verbose loggers. Do it before workbench is created.

--- products/ITER/products/org.csstudio.iter.css.product/src/main/java/org/csstudio/iter/css/product/EarlyStartup.java
+++ products/ITER/products/org.csstudio.iter.css.product/src/main/java/org/csstudio/iter/css/product/EarlyStartup.java
@@ -7,43 +7,21 @@
  ******************************************************************************/
 package org.csstudio.iter.css.product;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.csstudio.iter.css.product.preferences.Preferences;
 import org.csstudio.iter.css.product.util.WorkbenchUtil;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IStartup;
 
 public class EarlyStartup implements IStartup {
-
-	private static final String[] VERBOSE_PACKAGES = new String[] {
-			"com.sun.jersey.core.spi.component",
-			"com.sun.jersey.core.spi.component.ProviderServices",
-			"com.sun.jersey.spi.service.ServiceFinder" };
-	private final List<Logger> strongRefLoggers = new ArrayList<>();
-
 	@Override
 	public void earlyStartup() {
-		// Set upper log level on too verbose packages
-		Level verboseLogLevel;
-		try {
-			verboseLogLevel = Preferences.getVerboseLogLevel();
-		} catch (Exception e) {
-			verboseLogLevel = Level.SEVERE;
-		}
-		for (String verbosePackage : VERBOSE_PACKAGES) {
-			Logger logger = Logger.getLogger(verbosePackage);
-			logger.setLevel(verboseLogLevel);
-			for (Handler handler : logger.getHandlers()) {
-				handler.setLevel(verboseLogLevel);
+	
+		Display.getDefault().asyncExec(new Runnable(){
+			@Override
+			public void run() {
+				WorkbenchUtil.unbindDebugLast();
 			}
-			//keep strong references to all loggers. Otherwise the LogMaager will flush them out
-			strongRefLoggers.add(logger);
-		}
-
+		});
+		
 		// Remove Perspectives coming with XML Editor
 		WorkbenchUtil.removeUnWantedPerspectives();
 
