5969caa8d12e3d8004fa3ce6dd09166c772bebad
Frédéric ARNAUD <frederic.arnaud@iter.org>
2014-11-03 09:56:45 +0000

    ITER: Updated DBD & DB parser unit tests to check EPICS 3.15 new fields if defined

--- products/ITER/plugins/org.csstudio.utility.dbdparser.test/src/org/csstudio/utility/dbdparser/util/DbdUtilUnitTest.java
+++ products/ITER/plugins/org.csstudio.utility.dbdparser.test/src/org/csstudio/utility/dbdparser/util/DbdUtilUnitTest.java
@@ -8,7 +8,14 @@
 package org.csstudio.utility.dbdparser.util;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.csstudio.utility.dbdparser.data.Field;
+import org.csstudio.utility.dbdparser.data.RecordType;
 import org.csstudio.utility.dbdparser.data.Template;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,6 +25,7 @@ import org.junit.Test;
  * {@link String} with comments & C declaration replaced by an empty line.
  * generateTemplate: return a non-null {@link Template} initialized from
  * base.dbd file in the specified directory.
+ * Test compatibility with EPICS 3.15.
  * 
  * @author Fred Arnaud (Sopra Group) - ITER
  */
@@ -54,6 +62,95 @@ public class DbdUtilUnitTest {
 			Template t = DbdUtil.generateTemplate("resources/test/");
 			Assert.assertNotNull(t);
 			Assert.assertFalse(t.getRecordTypes().isEmpty());
+		} catch (Exception e) {
+			Assert.fail(e.getMessage());
+		}
+	}
+
+	/**
+	 * Test that the {@link DbdParser} is compatible with EPICS 3.15 Based on
+	 * EPICS Base Release 3.15.0.2
+	 * (http://aps.anl.gov/epics/base/R3-15/0-docs/RELEASE_NOTES.html)
+	 */
+	@Test
+	public void testEPICSFields() {
+		final String basePath = "/opt/codac/epics/base/dbd";
+		if (!UnitTestUtils.isEPICS315(basePath))
+			return;
+
+		try {
+			Template dbdTemplate = DbdUtil.generateTemplate(basePath);
+			Assert.assertNotNull(dbdTemplate);
+
+			// New record types
+			Map<String, List<Field>> fieldMap = new HashMap<String, List<Field>>();
+			for (RecordType recordType : dbdTemplate.getRecordTypes())
+				fieldMap.put(recordType.getName(), recordType.getFields());
+			Assert.assertTrue(fieldMap.keySet().contains("lso"));
+			Assert.assertTrue(fieldMap.keySet().contains("lsi"));
+			Assert.assertTrue(fieldMap.keySet().contains("printf"));
+			Assert.assertTrue(fieldMap.keySet().contains("histogram"));
+
+			Field biRecordZNAM = null;
+			for (Field f : fieldMap.get("bi"))
+				if (f.getName().equals("ZNAM"))
+					biRecordZNAM = f;
+			Assert.assertNotNull(biRecordZNAM);
+			Assert.assertTrue(biRecordZNAM.getRules().keySet().contains("prop"));
+
+			Set<String> fieldsNames = new HashSet<String>();
+			for (Field f : fieldMap.get("bi"))
+				fieldsNames.add(f.getName());
+			// New Undefined Severity field UDFS (common field)
+			Assert.assertTrue(fieldsNames.contains("UDFS"));
+
+			// Sequence record enhancements
+			fieldsNames.clear();
+			for (Field f : fieldMap.get("seq"))
+				fieldsNames.add(f.getName());
+			Assert.assertTrue(fieldsNames.contains("OFFS"));
+			Assert.assertTrue(fieldsNames.contains("SHFT"));
+			Assert.assertTrue(fieldsNames.contains("DOF"));
+			Assert.assertTrue(fieldsNames.contains("DOLF"));
+			Assert.assertTrue(fieldsNames.contains("DLYF"));
+
+			// Sequence record enhancements
+			fieldsNames.clear();
+			for (Field f : fieldMap.get("fanout"))
+				fieldsNames.add(f.getName());
+			Assert.assertTrue(fieldsNames.contains("OFFS"));
+			Assert.assertTrue(fieldsNames.contains("SHFT"));
+			Assert.assertTrue(fieldsNames.contains("LNK0"));
+
+			// Alarm filtering added to input record types
+			fieldsNames.clear();
+			for (Field f : fieldMap.get("ai"))
+				fieldsNames.add(f.getName());
+			Assert.assertTrue(fieldsNames.contains("AFTC"));
+			Assert.assertTrue(fieldsNames.contains("AFVL"));
+			Assert.assertTrue(fieldsNames.contains("UDFS"));
+
+			fieldsNames.clear();
+			for (Field f : fieldMap.get("calc"))
+				fieldsNames.add(f.getName());
+			Assert.assertTrue(fieldsNames.contains("AFTC"));
+			Assert.assertTrue(fieldsNames.contains("AFVL"));
+			Assert.assertTrue(fieldsNames.contains("UDFS"));
+
+			fieldsNames.clear();
+			for (Field f : fieldMap.get("longin"))
+				fieldsNames.add(f.getName());
+			Assert.assertTrue(fieldsNames.contains("AFTC"));
+			Assert.assertTrue(fieldsNames.contains("AFVL"));
+			Assert.assertTrue(fieldsNames.contains("UDFS"));
+
+			fieldsNames.clear();
+			for (Field f : fieldMap.get("mbbi"))
+				fieldsNames.add(f.getName());
+			Assert.assertTrue(fieldsNames.contains("AFTC"));
+			Assert.assertTrue(fieldsNames.contains("AFVL"));
+			Assert.assertTrue(fieldsNames.contains("UDFS"));
+
 		} catch (Exception e) {
 			Assert.fail(e.getMessage());
 		}
