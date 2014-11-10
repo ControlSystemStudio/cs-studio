3dbad601f186d23067ed7c64801619a9f42ef9c9
Matthew Furseman <matthew.furseman@diamond.ac.uk>
2014-11-10 10:33:05 +0000

    o.c.simplepv: Ensure NaN is printed, not U+FFFD

--- core/plugins/org.csstudio.simplepv/src/org/csstudio/simplepv/VTypeHelper.java
+++ core/plugins/org.csstudio.simplepv/src/org/csstudio/simplepv/VTypeHelper.java
@@ -481,6 +481,13 @@ public class VTypeHelper {
 				if (precision == -1)
 					return formatScalarNumber(FormatEnum.COMPACT, numValue, precision);
 				else {
+					// Sun's implementation of the JDK returns the Unicode replacement
+					// character, U+FFFD, when asked to parse a NaN. This is more
+					// consistent with the rest of CSS.
+					if(Double.isNaN(numValue.doubleValue())) {
+						return Double.toString(Double.NaN);
+					}
+
 					NumberFormat numberFormat = formatCacheMap.get(precision);
 					if (numberFormat == null) {
 						numberFormat = new DecimalFormat("0"); //$NON-NLS-1$
