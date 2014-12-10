b6c13f29cc5950451f1ec159c3fdce9b61ad32c3
kasemir <kasemirk@ornl.gov>
2014-12-10 10:20:32 -0500

    o.c.swt.rtplot: 'Add Annotation' dialog always shows trace color
    .. even if 'row' in table is selected

--- applications/plugins/org.csstudio.swt.rtplot/src/org/csstudio/swt/rtplot/internal/AddAnnotationDialog.java
+++ applications/plugins/org.csstudio.swt.rtplot/src/org/csstudio/swt/rtplot/internal/AddAnnotationDialog.java
@@ -16,6 +16,10 @@ import org.csstudio.swt.rtplot.SWTMediaPool;
 import org.csstudio.swt.rtplot.Trace;
 import org.csstudio.swt.rtplot.data.PlotDataItem;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.StyledCellLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
@@ -26,7 +30,6 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 
 /** Dialog for adding annotation to a trace
@@ -36,15 +39,37 @@ import org.eclipse.swt.widgets.Text;
 public class AddAnnotationDialog<XTYPE extends Comparable<XTYPE>> extends Dialog
 {
     final private RTPlot<XTYPE> plot;
-    final private List<Trace<XTYPE>> traces;
-    private Table trace_list;
+    // Thread-save snapshot of traces at time dialog was opened
+    final private List<Trace<XTYPE>> traces = new ArrayList<>();
+    private SWTMediaPool media;
+    private TableViewer trace_list;
     private Text text;
+
+    /** 'styled' provider that always shows the annotation's color,
+     *  even while table row is selected
+     */
+    class MyLabelProvider extends StyledCellLabelProvider
+    {
+        MyLabelProvider()
+        {
+            super(COLORS_ON_SELECTION);
+        }
+
+        @SuppressWarnings({ "unchecked", "rawtypes" })
+        @Override
+        public void update(final ViewerCell cell)
+        {
+            final Trace<XTYPE> trace = (Trace) cell.getElement();
+            cell.setText(trace.getName());
+            cell.setForeground(media.get(trace.getColor()));
+            super.update(cell);
+        }
+    }
 
     public AddAnnotationDialog(final Shell shell, final RTPlot<XTYPE> plot)
     {
         super(shell);
         this.plot = plot;
-        traces = new ArrayList<>();
         for (Trace<XTYPE> trace : plot.getTraces())
             traces.add(trace);
     }
@@ -66,26 +91,24 @@ public class AddAnnotationDialog<XTYPE extends Comparable<XTYPE>> extends Dialog
     protected Control createDialogArea(final Composite parent)
     {
         final Composite composite = (Composite) super.createDialogArea(parent);
-        final SWTMediaPool media = new SWTMediaPool(parent);
+        media = new SWTMediaPool(parent);
         composite.setLayout(new GridLayout(2, false));
 
         Label label = new Label(composite, SWT.NONE);
         label.setText(Messages.AddAnnotation_Trace);
         label.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false));
 
-        trace_list = new Table(composite, SWT.SINGLE | SWT.FULL_SELECTION | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
-        trace_list.setLinesVisible(true);
+        trace_list = new TableViewer(composite, SWT.SINGLE | SWT.FULL_SELECTION | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
+        final Table table = trace_list.getTable();
+        table.setLinesVisible(true);
         GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
         gd.minimumHeight = 100;
-        trace_list.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-        for (Trace<XTYPE> trace : traces)
-        {
-            final TableItem item = new TableItem(trace_list, SWT.NONE);
-            item.setText(trace.getName());
-            item.setForeground(media.get(trace.getColor()));
-        }
-        trace_list.setToolTipText(Messages.AddAnnotation_Trace_TT);
-        trace_list.addMouseListener(new MouseAdapter()
+        table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        trace_list.setContentProvider(new ArrayContentProvider());
+        trace_list.setLabelProvider(new MyLabelProvider());
+        trace_list.setInput(traces);
+        table.setToolTipText(Messages.AddAnnotation_Trace_TT);
+        table.addMouseListener(new MouseAdapter()
         {
             @Override
             public void mouseDoubleClick(MouseEvent e)
@@ -116,7 +139,7 @@ public class AddAnnotationDialog<XTYPE extends Comparable<XTYPE>> extends Dialog
     @Override
     protected void okPressed()
     {
-        plot.addAnnotation(traces.get(trace_list.getSelectionIndex()), text.getText());
+        plot.addAnnotation(traces.get(trace_list.getTable().getSelectionIndex()), text.getText());
         super.okPressed();
     }
 }
