c3656695be73fe534f900c859d96e550dd7e7bdf
kasemir <kasemirk@ornl.gov>
2015-01-08 15:15:00 -0500

    o.c.swt.xygraph: CircularBuffer fix from Nebula
    See #787

--- applications/plugins/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/dataprovider/CircularBuffer.java
+++ applications/plugins/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/dataprovider/CircularBuffer.java
@@ -12,10 +12,10 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-/**A particular circular buffer. New arrived data will be appended to the tail of the buffer. 
- * When buffer is full, the oldest data will be deleted when new data arrived. 
- * 
- * @author Xihui Chen 
+/**A particular circular buffer. New arrived data will be appended to the tail of the buffer.
+ * When buffer is full, the oldest data will be deleted when new data arrived.
+ *
+ * @author Xihui Chen
  */
 public class CircularBuffer<T> extends AbstractCollection<T> {
 	private int bufferSize =0;
@@ -23,68 +23,70 @@ public class CircularBuffer<T> extends AbstractCollection<T> {
 	private int head;
 	private int tail;
 	private int count;
-	
+
 	public CircularBuffer(int bufferSize) {
 		if(bufferSize <=0)
 			throw new IllegalArgumentException("Buffer size must be greater than zero.");
 		this.setBufferSize(bufferSize, true);
 	}
-	
+
 	/** Add an element.
 	 * @param element
 	 */
-	public synchronized boolean add(T element){		
+	@Override
+    public synchronized boolean add(T element){
 		if(tail == head && count == bufferSize) { //buffer is full
-			buffer[tail] = element;	
+			buffer[tail] = element;
 			head = (head + 1) % bufferSize;
 			tail = (tail + 1) % bufferSize;
 			return true;
 		}
 		else{//buffer is not full
-			buffer[tail] = element;	
-			tail = (tail + 1) % bufferSize;					
+			buffer[tail] = element;
+			tail = (tail + 1) % bufferSize;
 			count++;
 			return true;
 		}
 	}
-	
+
 	/**Get element
 	 * @param index the index of the element in the buffer.
 	 * @return the element. null if the data at the index doesn't exist.
 	 */
 	public synchronized T getElement(int index){
 		if(index < count)
-			return buffer[(head + index) % bufferSize];		
+			return buffer[(head + index) % bufferSize];
 		else
 			return null;
 	}
-	
+
 	/**Get head element
 	 * @return the head element. null if the buffer is empty.
 	 */
 	public synchronized T getHead(){
 		if(count > 0)
-			return buffer[head];		
+			return buffer[head];
 		else
 			return null;
 	}
-	
+
 	/**Get tail element
 	 * @return the tail element. null if the buffer is empty.
 	 */
 	public synchronized T getTail(){
 		if(count > 0)
-			return buffer[(head+count-1)%bufferSize];		
+			return buffer[(head+count-1)%bufferSize];
 		else
 			return null;
 	}
-	
-	
-	
+
+
+
 	/**
 	 * clear the buffer;
 	 */
-	public synchronized void clear(){
+	@Override
+    public synchronized void clear(){
 		// Configure buffer to appear empty
 		head = 0;
 		tail = 0;
@@ -96,28 +98,30 @@ public class CircularBuffer<T> extends AbstractCollection<T> {
 	/**Set the buffer size.
 	 * @param bufferSize the bufferSize to set
 	 * @param clear clear the buffer if true. Otherwise keep the exist data;
-	 * Extra data on the end would be omitted if the new bufferSize is less 
-	 * than the exist data count. 
+	 * Oldest data will be omitted if the new bufferSize is less
+	 * than the exist data count.
 	 */
 	@SuppressWarnings("unchecked")
 	public synchronized void setBufferSize(int bufferSize, boolean clear) {
-		assert bufferSize > 0;		
-		if(this.bufferSize != bufferSize){			
-			if(clear){//clear 
+		assert bufferSize > 0;
+		if(this.bufferSize != bufferSize){
+			if(clear){//clear
 				buffer = (T[]) new Object[bufferSize];
 				clear();
 			}else{// keep the exist data
 				T[] tempBuffer = (T[]) toArray();
 				buffer = (T[]) new Object[bufferSize];
-				for(int i=0; i<Math.min(bufferSize, count); i++){
-					buffer[i] = tempBuffer[i];
+				if (bufferSize < count) {
+				    System.arraycopy(tempBuffer, count-bufferSize, buffer, 0, bufferSize);
+				} else {
+                    System.arraycopy(tempBuffer, 0, buffer, 0, count);
 				}
 				count = Math.min(bufferSize, count);
 				head =0;
 				tail = count%bufferSize;
-			}	
+			}
 			this.bufferSize = bufferSize;
-		}		
+		}
 	}
 
 	/**
@@ -125,22 +129,26 @@ public class CircularBuffer<T> extends AbstractCollection<T> {
 	 */
 	public synchronized int getBufferSize() {
 		return bufferSize;
-	}	
+	}
 
 
-	public Iterator<T> iterator() {
+	@Override
+    public Iterator<T> iterator() {
 		return new Iterator<T>(){
 			private int index=0;
 
-			public boolean hasNext() {
+			@Override
+            public boolean hasNext() {
 				return index < count;
 			}
-			public T next() {
+			@Override
+            public T next() {
 				if(!hasNext())
 					throw new NoSuchElementException();
 				return buffer[(head+index++)%bufferSize];
 			}
-			public void remove() {}			
+			@Override
+            public void remove() {}
 		};
 	}
 
@@ -148,7 +156,7 @@ public class CircularBuffer<T> extends AbstractCollection<T> {
 	public int size() {
 		return count;
 	}
-	
-	
-	
+
+
+
 }
