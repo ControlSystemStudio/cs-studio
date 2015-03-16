7885fbf352af92c0a42aafdd329dfacfa3f972d3
Frédéric ARNAUD <frederic.arnaud@iter.org>
2015-03-16 12:43:55 +0000

    Batik: Added new SVG handler for animation

---
+++ core/utility/utility-plugins/org.csstudio.utility.batik/src/org/csstudio/utility/batik/SVGHandlerListener.java
@@ -0,0 +1,12 @@
+package org.csstudio.utility.batik;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * @author Fred Arnaud (Sopra Steria Group) - ITER
+ */
+public interface SVGHandlerListener {
+
+	public void newImage(BufferedImage awtImage);
+
+}
