1970d627a7abdb37fa1e94d1f885fc872a27ff24
bobnarj <jaka.bobnar@cosylab.com>
2015-07-17 09:54:15 +0200

    Added the missing method, which was removed by one of the previous commit and broke the build.

--- applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.rap/src/org/csstudio/webopi/ApplicationWorkbenchWindowAdvisor.java
+++ applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.rap/src/org/csstudio/webopi/ApplicationWorkbenchWindowAdvisor.java
@@ -64,6 +64,17 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
             }
         }
     }
+    
+	private void runOther(final IPath path) {
+		IWorkbenchPage page = PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getActivePage();
+		try {
+			SingleSourceHelper.openEditor(page, path);
+		} catch (Exception e) {
+			String message = NLS.bind("Failed to open the editor. \n {0}", e);
+			MessageDialog.openError(null, "Error in opening OPI", message);
+		}
+	}
 
     public void preWindowOpen() {
 
