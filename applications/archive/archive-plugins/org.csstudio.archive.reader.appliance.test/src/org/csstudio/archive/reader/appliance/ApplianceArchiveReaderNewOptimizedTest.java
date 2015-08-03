442e1ae7485bea9378804f3ef783172bec76460a
jbobnar <jaka.bobnar@cosylab.com>
2015-08-03 13:51:08 +0200

    Forgot to add new classes for the new optimized operator.

---
+++ applications/archive/archive-plugins/org.csstudio.archive.reader.appliance.test/src/org/csstudio/archive/reader/appliance/ApplianceArchiveReaderNewOptimizedTest.java
@@ -0,0 +1,104 @@
+package org.csstudio.archive.reader.appliance;
+
+import static org.junit.Assert.assertEquals;
+
+import org.csstudio.archive.reader.ArchiveReader;
+import org.csstudio.archive.reader.appliance.testClasses.TestApplianceArchiveReader;
+import org.csstudio.archive.reader.appliance.testClasses.TestGenMsgIteratorRaw;
+import org.csstudio.archive.reader.appliance.testClasses.TestGenMsgIteratorWaveform;
+import org.csstudio.archive.vtype.ArchiveVNumber;
+import org.csstudio.archive.vtype.ArchiveVStatistics;
+import org.csstudio.archive.vtype.ArchiveVType;
+import org.csstudio.archive.vtype.TimestampHelper;
+import org.epics.util.time.TimeDuration;
+import org.epics.util.time.Timestamp;
+import org.junit.Test;
+
+/**
+ *
+ * <code>ApplianceArchiveReaderNewOptimizedTest</code> tests the new optimized data retrieval using a dummy loopback
+ * appliance server. This class test the retrieval methods, if they decode the data correctly. It doesn't tests if data
+ * makes sense.
+ *
+ * @author <a href="mailto:jaka.bobnar@cosylab.com">Jaka Bobnar</a>
+ *
+ */
+public class ApplianceArchiveReaderNewOptimizedTest extends AbstractArchiverReaderTesting {
+
+    @Override
+    protected ArchiveReader getReader() {
+        return new TestApplianceArchiveReader(true, true);
+    }
+
+    /**
+     * Tests {@code ApplianceArchiveReader#getOptimizedValues(int, String, Timestamp, Timestamp)} method for a double
+     * type PV where the requested number of points is greater than total number of points.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testDataRetrievalWhenThereAreNotManyPoints() throws Exception {
+        Timestamp end = Timestamp.now();
+        Timestamp start = end.minus(TimeDuration.ofHours(24.0));
+        ArchiveVNumber[] vals = getValuesNumber("test_pv", true, 2000, start, end);
+        assertEquals("Number of values comparison", 100, vals.length);
+
+        ArchiveVNumber val = null;
+        for (int i = 0; i < vals.length; i++) {
+            val = (ArchiveVNumber) vals[i];
+            assertEquals(
+                    "Value comparison",
+                    Double.valueOf(TestGenMsgIteratorRaw.VALUES_DOUBLE[i % TestGenMsgIteratorRaw.VALUES_DOUBLE.length]),
+                    (Double) val.getValue(), 0.0001);
+            assertEquals("Severity", getSeverity(TestGenMsgIteratorRaw.SEVERITIES[i%TestGenMsgIteratorRaw.SEVERITIES.length]), val.getAlarmSeverity());
+            assertEquals("Status", String.valueOf(TestGenMsgIteratorRaw.STATUS[i%TestGenMsgIteratorRaw.SEVERITIES.length]), val.getAlarmName());
+        }
+    }
+
+    /**
+     * Tests {@code ApplianceArchiveReader#getOptimizedValues(int, String, Timestamp, Timestamp)} method for a double
+     * type PV.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testDataRetrievalDouble() throws Exception {
+        Timestamp end = Timestamp.now();
+        Timestamp start = end.minus(TimeDuration.ofHours(24.0));
+        ArchiveVType[] vals = getValuesStatistics("test_pv", 10, start, end);
+        assertEquals("Number of values comparison", 10, vals.length);
+
+        long startM = TimestampHelper.toMillisecs(start);
+
+        ArchiveVStatistics val = null;
+        for (int i = 0; i < vals.length; i++) {
+            val = (ArchiveVStatistics)vals[i];
+            assertEquals(
+                    "Mean Value comparison",
+                    TestGenMsgIteratorWaveform.VALUE_DOUBLE[i % TestGenMsgIteratorWaveform.VALUE_DOUBLE.length][0],
+                    val.getAverage().doubleValue(), 0.0001);
+            assertEquals(
+                    "STD Value comparison",
+                    TestGenMsgIteratorWaveform.VALUE_DOUBLE[i % TestGenMsgIteratorWaveform.VALUE_DOUBLE.length][1],
+                    val.getStdDev().doubleValue(), 0.0001);
+            assertEquals(
+                    "Min Value comparison",
+                    TestGenMsgIteratorWaveform.VALUE_DOUBLE[i % TestGenMsgIteratorWaveform.VALUE_DOUBLE.length][2],
+                    val.getMin().doubleValue(), 0.0001);
+            assertEquals(
+                    "Max Value comparison",
+                    TestGenMsgIteratorWaveform.VALUE_DOUBLE[i % TestGenMsgIteratorWaveform.VALUE_DOUBLE.length][3],
+                    val.getMax().doubleValue(), 0.0001);
+            assertEquals(
+                    "Count Value comparison",
+                    (int)TestGenMsgIteratorWaveform.VALUE_DOUBLE[i % TestGenMsgIteratorWaveform.VALUE_DOUBLE.length][4],
+                    val.getNSamples().intValue());
+            assertEquals("Timestamp comparison", startM + i, TimestampHelper.toMillisecs(val.getTimestamp()));
+            assertEquals("Severity", getSeverity(TestGenMsgIteratorWaveform.SEVERITIES[i
+                    % TestGenMsgIteratorWaveform.SEVERITIES.length]), val.getAlarmSeverity());
+            assertEquals("Status",
+                    String.valueOf(TestGenMsgIteratorWaveform.STATUS[i % TestGenMsgIteratorWaveform.STATUS.length]),
+                    val.getAlarmName());
+        }
+    }
+}
---
+++ applications/archive/archive-plugins/org.csstudio.archive.reader.appliance/src/org/csstudio/archive/reader/appliance/ApplianceOptimizedValueIterator.java
@@ -0,0 +1,169 @@
+package org.csstudio.archive.reader.appliance;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.csstudio.archive.vtype.ArchiveVNumber;
+import org.csstudio.archive.vtype.ArchiveVStatistics;
+import org.csstudio.archive.vtype.TimestampHelper;
+import org.epics.archiverappliance.retrieval.client.DataRetrieval;
+import org.epics.archiverappliance.retrieval.client.EpicsMessage;
+import org.epics.archiverappliance.retrieval.client.GenMsgIterator;
+import org.epics.util.text.NumberFormats;
+import org.epics.util.time.Timestamp;
+import org.epics.vtype.Display;
+import org.epics.vtype.VType;
+import org.epics.vtype.ValueFactory;
+
+import edu.stanford.slac.archiverappliance.PB.EPICSEvent.PayloadInfo;
+import edu.stanford.slac.archiverappliance.PB.EPICSEvent.PayloadType;
+
+/**
+ *
+ * <code>ApplianceOptimizedValueIterator</code> retrieves the data using the optimised iterator, which returns the
+ * samples based on the time window and requested number of points. If it has less samples than requested it returns raw
+ * data, if it has more samples it returns the returns data bins.
+ *
+ * @author <a href="mailto:jaka.bobnar@cosylab.com">Jaka Bobnar</a>
+ *
+ */
+public class ApplianceOptimizedValueIterator extends ApplianceValueIterator {
+
+    private final int requestedPoints;
+    private final boolean useStatistics;
+
+    /**
+     * Constructor that fetches data from appliance archive reader.
+     *
+     * @param reader instance of appliance archive reader
+     * @param name name of the PV
+     * @param start start of the time period
+     * @param end end of the time period
+     * @param points the number of requested points
+     * @param useStatistics true if the returned data should include statistics or false if only mean value should be
+     *            present
+     * @param listener the listener that is notified when the iterator is closed
+     *
+     * @throws IOException if there was an error during the data fetch process
+     * @throws ArchiverApplianceInvalidTypeException if the type of data cannot be returned in optimized format
+     * @throws ArchiverApplianceException if it is not possible to load optimised data for the selected PV
+     */
+    public ApplianceOptimizedValueIterator(ApplianceArchiveReader reader, String name, Timestamp start, Timestamp end,
+            int points, boolean useStatistics, IteratorListener listener) throws ArchiverApplianceException,
+            IOException {
+        super(reader, name, start, end, listener);
+        this.requestedPoints = points;
+        this.useStatistics = useStatistics;
+        this.display = determineDisplay(reader, name, end);
+        fetchData();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.csstudio.archive.reader.appliance.ApplianceValueIterator#fetchDataInternal(java.lang.String)
+     */
+    @Override
+    protected void fetchDataInternal(String pvName) throws ArchiverApplianceException {
+        String optimized = new StringBuilder().append(ApplianceArchiveReaderConstants.OP_OPTIMIZED)
+                .append(requestedPoints).append('(').append(pvName).append(')').toString();
+        super.fetchDataInternal(optimized);
+    }
+
+    /**
+     * Determine and return display values.
+     *
+     * @param reader instance of appliance archive reader
+     * @param name name of the PV
+     *
+     * @return the display
+     * @throws IOException if there was an error reading data
+     * @throws ArchiverApplianceInvalidTypeException if the data cannot be loaded with the optimized method
+     */
+    private Display determineDisplay(ApplianceArchiveReader reader, String name, Timestamp time)
+            throws ArchiverApplianceInvalidTypeException, IOException {
+        // to retrieve the display, request the raw data for the end timestamp
+        java.sql.Timestamp timestamp = TimestampHelper.toSQLTimestamp(time);
+        DataRetrieval dataRetrieval = reader.createDataRetriveal(reader.getDataRetrievalURL());
+        GenMsgIterator genMsgIterator = dataRetrieval.getDataForPV(name, timestamp, timestamp);
+        if (genMsgIterator != null) {
+            try {
+                PayloadInfo payloadInfo = null;
+                Iterator<EpicsMessage> it = genMsgIterator.iterator();
+                if (it.hasNext()) {
+                    it.next();
+                    payloadInfo = genMsgIterator.getPayLoadInfo();
+                    if (!isDataTypeOKForOptimized(payloadInfo.getType())) {
+                        throw new ArchiverApplianceInvalidTypeException("Cannot use optimized data on type "
+                                + payloadInfo.getType(), name, payloadInfo.getType());
+                    }
+                    return getDisplay(payloadInfo);
+                }
+            } finally {
+                genMsgIterator.close();
+            }
+        }
+
+        return ValueFactory.newDisplay(Double.NaN, Double.NaN, Double.NaN, "", NumberFormats.toStringFormat(),
+                Double.NaN, Double.NaN, Double.NaN, Double.NaN, Double.NaN);
+    }
+
+    /**
+     * Check if the type of data is OK to be loaded in mean mode. Mean mode is possible only with numeric scalars.
+     *
+     * @param type the type to check
+     * @return true if OK or false otherwise
+     */
+    private boolean isDataTypeOKForOptimized(PayloadType type) {
+        return type == PayloadType.SCALAR_BYTE || type == PayloadType.SCALAR_DOUBLE || type == PayloadType.SCALAR_FLOAT
+                || type == PayloadType.SCALAR_INT || type == PayloadType.SCALAR_SHORT;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.csstudio.archive.reader.appliance.ApplianceValueIterator#next()
+     */
+    @Override
+    public VType next() throws Exception {
+        EpicsMessage message;
+        synchronized (this) {
+            if (closed) {
+                return null;
+            }
+            message = mainIterator.next();
+        }
+        PayloadType type = mainStream.getPayLoadInfo().getType();
+        if (type == PayloadType.WAVEFORM_DOUBLE) {
+            if (closed) {
+                return null;
+            }
+            if (message.getElementCount() < 5) {
+                throw new ArchiverApplianceException(
+                        "The optimized post processor returned less than 5 values per sample.");
+            }
+            if (useStatistics) {
+                return new ArchiveVStatistics(
+                        TimestampHelper.fromSQLTimestamp(message.getTimestamp()),
+                        getSeverity(message.getSeverity()),
+                        String.valueOf(message.getStatus()),
+                        display,
+                        message.getNumberAt(0).doubleValue(),
+                        message.getNumberAt(2).doubleValue(),
+                        message.getNumberAt(3).doubleValue(),
+                        message.getNumberAt(1).doubleValue(),
+                        message.getNumberAt(4).intValue());
+            } else {
+                return new ArchiveVNumber(
+                        TimestampHelper.fromSQLTimestamp(message.getTimestamp()),
+                        getSeverity(message.getSeverity()),
+                        String.valueOf(message.getStatus()),
+                        display,
+                        message.getNumberAt(0).doubleValue());
+            }
+        } else {
+            // raw data
+            return super.extractData(message);
+        }
+    }
+}
