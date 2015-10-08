08b0d31b8159277a6d2ec1e6e289b024185c5d05
jbobnar <jaka.bobnar@cosylab.com>
2015-10-08 13:09:43 +0200

    Reintroduced the OPIRuntime as editor and created two fragments: one opens opiruntime in editor, the other one opens opiruntime in shell or view.
    Conflicts:
    	applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.rcp/fragment.xml

--- applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.rcp/src/org/csstudio/opibuilder/util/SingleSourceHelperImpl.java
+++ applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.rcp/src/org/csstudio/opibuilder/util/SingleSourceHelperImpl.java
@@ -11,7 +11,6 @@ import org.csstudio.opibuilder.actions.SendEMailAction;
 import org.csstudio.opibuilder.actions.SendToElogAction;
 import org.csstudio.opibuilder.editparts.AbstractBaseEditPart;
 import org.csstudio.opibuilder.runmode.IOPIRuntime;
-import org.csstudio.opibuilder.runmode.OPIShell;
 import org.csstudio.opibuilder.runmode.OPIView;
 import org.csstudio.opibuilder.widgetActions.OpenFileAction;
 import org.csstudio.ui.util.dialogs.ExceptionDetailsErrorDialog;
@@ -36,7 +35,6 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
@@ -228,15 +226,4 @@ public class SingleSourceHelperImpl extends SingleSourceHelper{
             throws Exception {
         SingleSourcePlugin.getUIHelper().openEditor(page, path);
     }
-
-    @Override
-    protected void iOpenOPIShell(IPath path, MacrosInput input) {
-        OPIShell.openOPIShell(path, input);
-    }
-
-    @Override
-    protected IOPIRuntime iGetOPIShellForShell(Shell shell) {
-        return OPIShell.getOPIShellForShell(shell);
-    }
-
 }
