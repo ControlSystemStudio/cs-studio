23a6f4e3d411d9de5d1136b0ca699703817c590d
Nick Battam <xzl80115@diamond.ac.uk>
2015-11-20 16:35:04 +0000

    Explicitly set CSS ByteWidget LED border width and colour from EDM

--- applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.converter/src/main/org/csstudio/opibuilder/converter/writer/Opi_ByteClass.java
+++ applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.converter/src/main/org/csstudio/opibuilder/converter/writer/Opi_ByteClass.java
@@ -37,9 +37,11 @@ public class Opi_ByteClass extends OpiWidget {
         new OpiBoolean(widgetContext, "effect_3d", false);
         new OpiBoolean(widgetContext, "square_led", true);
 
+        // EDM line width is returned as '0' if unchanged from default '1'
+        new OpiInt(widgetContext, "led_border", Math.max(1, r.getLineWidth()));
+        new OpiColor(widgetContext, "led_border_color", r.getLineColor(), r);
 
         new OpiColor(widgetContext, "on_color", r.getOnColor(), r);
-
         new OpiColor(widgetContext, "off_color", r.getOffColor(), r);
 
         if(r.getControlPv() !=null){
