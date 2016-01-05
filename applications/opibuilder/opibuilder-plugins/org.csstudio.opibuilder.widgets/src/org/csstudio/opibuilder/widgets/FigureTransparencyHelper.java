1a0037781168cdf9da2b8bbd5bdb819608cf3169
lamberm <matthieu.lambert@iter.org>
2016-01-05 09:23:20 +0000

    Correction on background from alarm
    no alarm display if transparency enabled
    
    Conflicts:
    	applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.widgets/src/org/csstudio/opibuilder/widgets/FigureTransparencyHelper.java
    	applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.widgets/src/org/csstudio/opibuilder/widgets/TransparencyPropertyChange.java

--- applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.widgets/src/org/csstudio/opibuilder/widgets/FigureTransparencyHelper.java
+++ applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.widgets/src/org/csstudio/opibuilder/widgets/FigureTransparencyHelper.java
@@ -8,44 +8,41 @@ import org.csstudio.opibuilder.properties.WidgetPropertyCategory;
 import org.csstudio.swt.widgets.symbol.SymbolImage;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.RGB;
 
 public class FigureTransparencyHelper {
-	/** property key */
-	public static final String PROP_TRANSPARENCY = "transparency";
+    /** property key */
+    public static final String PROP_TRANSPARENCY = "transparency";
 
-	public static void setBackground(SymbolImage symbolImage, Color backgroundColor,
-			AbstractWidgetModel model) {
-		boolean transparency = isTransparency(model);
-		RGB rgb = backgroundColor.getRGB();
-		if (transparency && model.getBackgroundColor().red == rgb.red && model.getBackgroundColor().blue == rgb.blue
-				&& model.getBackgroundColor().green == rgb.green) {
-			symbolImage.setBackgroundColor(null);
-		} else {
-			symbolImage.setBackgroundColor(backgroundColor);
-		}
-	}
+    public static void setBackground(SymbolImage symbolImage, Color backgroundColor,
+            AbstractWidgetModel model) {
+        boolean transparency = isTransparency(model);
+        if (transparency) {
+            symbolImage.setBackgroundColor(null);
+        } else {
+            symbolImage.setBackgroundColor(backgroundColor);
+        }
+    }
 
-	public static void addProperty(AbstractWidgetModel model) {
-		model.addProperty(new BooleanProperty(PROP_TRANSPARENCY, "Transparency",
-				WidgetPropertyCategory.Display, false));
-		model.getProperty(PROP_TRANSPARENCY).addPropertyChangeListener(new TransparencyPropertyChange(model));
-	}
-	
-	public static void addHandler(AbstractBaseEditPart editPart, IFigure figure) {
-		editPart.setPropertyChangeHandler(FigureTransparencyHelper.PROP_TRANSPARENCY, new IWidgetPropertyChangeHandler() {
+    public static void addProperty(AbstractWidgetModel model) {
+        model.addProperty(new BooleanProperty(PROP_TRANSPARENCY, "Transparent",
+                WidgetPropertyCategory.Display, false));
+    }
+
+    public static void addHandler(AbstractBaseEditPart editPart, IFigure figure) {
+        editPart.setPropertyChangeHandler(FigureTransparencyHelper.PROP_TRANSPARENCY, new IWidgetPropertyChangeHandler() {
+            @Override
             public boolean handleChange(final Object oldValue, final Object newValue,
                     final IFigure figure) {
                 figure.repaint();
                 return true;
             }
         });
-	}
-	
-	public static boolean isTransparency(AbstractWidgetModel model) {
-		if (model.getProperty(FigureTransparencyHelper.PROP_TRANSPARENCY) != null) {
-			return (Boolean) model.getProperty(FigureTransparencyHelper.PROP_TRANSPARENCY).getPropertyValue();
-		}
-		return false;
-	}
+    }
+
+    public static boolean isTransparency(AbstractWidgetModel model) {
+        if (model.getProperty(FigureTransparencyHelper.PROP_TRANSPARENCY) != null) {
+            return (Boolean) model.getProperty(FigureTransparencyHelper.PROP_TRANSPARENCY).getPropertyValue();
+        }
+        return false;
+    }
 }
