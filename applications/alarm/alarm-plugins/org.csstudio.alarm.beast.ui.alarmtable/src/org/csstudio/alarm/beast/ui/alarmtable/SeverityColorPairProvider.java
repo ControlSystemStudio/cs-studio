df17cabe3a85240dc19b50b2b30fb19a22f14736
boris-v <boris@whiletrue.com>
2016-01-12 12:23:24 +0100

    AlarmTable SeverityColorPair fix: set color for UNDEFINED_ACK to INVALID_ACK of not explicitly defined

--- applications/alarm/alarm-plugins/org.csstudio.alarm.beast.ui.alarmtable/src/org/csstudio/alarm/beast/ui/alarmtable/SeverityColorPairProvider.java
+++ applications/alarm/alarm-plugins/org.csstudio.alarm.beast.ui.alarmtable/src/org/csstudio/alarm/beast/ui/alarmtable/SeverityColorPairProvider.java
@@ -76,6 +76,8 @@ public class SeverityColorPairProvider implements DisposeListener
             colors[SeverityLevel.MAJOR_ACK.ordinal()] = new Color(display, colors[SeverityLevel.MAJOR.ordinal()].getRGB());
         if (colors[SeverityLevel.INVALID_ACK.ordinal()] == null && colors[SeverityLevel.INVALID.ordinal()] != null)
             colors[SeverityLevel.INVALID_ACK.ordinal()] = new Color(display, colors[SeverityLevel.INVALID.ordinal()].getRGB());
+        if (colors[SeverityLevel.UNDEFINED_ACK.ordinal()] == null && colors[SeverityLevel.INVALID_ACK.ordinal()] != null)
+            colors[SeverityLevel.UNDEFINED_ACK.ordinal()] = new Color(display, colors[SeverityLevel.INVALID_ACK.ordinal()].getRGB());
     }
 
     /** @see DisposeListener */
