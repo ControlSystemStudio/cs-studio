fcc70c2d3cfbf00b8b89a412d64d20f087454030
jbobnar <jaka.bobnar@cosylab.com>
2016-01-15 12:00:13 +0100

    Forgot to add new files.

---
+++ plugins/org.csstudio.ui.fx.util/src/org/csstudio/ui/fx/util/UnfocusableCheckBox.java
@@ -0,0 +1,37 @@
+package org.csstudio.ui.fx.util;
+
+import javafx.scene.control.CheckBox;
+
+/**
+ * <code>UnfocusableCheckBox</code> is an extension of the checkbox, which cannot receive focus.
+ *
+ * @author <a href="mailto:jaka.bobnar@cosylab.com">Jaka Bobnar</a>
+ *
+ */
+public class UnfocusableCheckBox extends CheckBox {
+
+    /**
+     * Constructs a new unfocusable checkbox without any text.
+     */
+    public UnfocusableCheckBox() {
+        super();
+    }
+
+    /**
+     * Constructs a new unfocusable checkbox.
+     *
+     * @param text the text to display next the checkbox
+     */
+    public UnfocusableCheckBox(String text) {
+        super(text);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see javafx.scene.Node#requestFocus()
+     */
+    @Override
+    public void requestFocus() {
+    }
+}
---
+++ plugins/org.csstudio.ui.fx.util/src/org/csstudio/ui/fx/util/UnfocusableToggleButton.java
@@ -0,0 +1,48 @@
+package org.csstudio.ui.fx.util;
+
+import javafx.scene.Node;
+import javafx.scene.control.ToggleButton;
+
+/**
+ * <code>UnfocusableToggleButton</code> is an extension of the toggle button, which cannot receive focus.
+ *
+ * @author <a href="mailto:jaka.bobnar@cosylab.com">Jaka Bobnar</a>
+ *
+ */
+public class UnfocusableToggleButton extends ToggleButton {
+
+    /**
+     * Constructs a new unfocusable toggle button.
+     */
+    public UnfocusableToggleButton() {
+        super();
+    }
+
+    /**
+     * Constructs a new unfocusable toggle button with the given text.
+     *
+     * @param text the button text
+     */
+    public UnfocusableToggleButton(String text) {
+        super(text);
+    }
+
+    /**
+     * Constructs a new unfocusable toggle button with the given text and graphics node.
+     *
+     * @param text the button text
+     * @param graphics the graphics for the button
+     */
+    public UnfocusableToggleButton(String text, Node graphics) {
+        super(text, graphics);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see javafx.scene.Node#requestFocus()
+     */
+    @Override
+    public void requestFocus() {
+    }
+}
