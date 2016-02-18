c82b0c36e9d65a6170bf0af7db0cf18536e71ed1
Will Rogers <wahrogers@gmail.com>
2016-02-18 14:53:26 +0000

    Move perspective saver initialisation into ApplicationWorkbenchAdvisor.

--- core/utility/utility-plugins/org.csstudio.utility.product/src/org/csstudio/utility/product/PerspectiveHandler.java
+++ core/utility/utility-plugins/org.csstudio.utility.product/src/org/csstudio/utility/product/PerspectiveHandler.java
@@ -5,56 +5,16 @@ import java.util.logging.Logger;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.commands.IHandlerListener;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.ui.IMemento;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.WorkbenchException;
 
 /**
- * Create a PerspectiveSaver on startup.
+ * Handler for loading perspectives.
  */
-public class PerspectiveHandler extends AbstractHandler implements IWorkbenchWindowAdvisorExtPoint {
+public class PerspectiveHandler extends AbstractHandler {
 
-    private Logger log = Logger.getLogger(PerspectiveHandler.ID);
-
-    @Override
-    public void preWindowOpen() {
-        log.config("PerspectiveHandler: preWindowOpen");
-        IEclipseContext context = PlatformUI.getWorkbench().getService(IEclipseContext.class);
-        PerspectiveSaver ps = ContextInjectionFactory.make(PerspectiveSaver.class, context);
-        ps.init();
-    }
-
-    @Override
-    public boolean preWindowShellClose() {
-        return true;
-    }
-
-    @Override
-    public void postWindowRestore() throws WorkbenchException {}
-
-    @Override
-    public void postWindowCreate() {}
-
-    @Override
-    public void postWindowOpen() {}
-
-    @Override
-    public void postWindowClose() {}
-
-    @Override
-    public IStatus saveState(IMemento memento) {
-        return null;
-    }
-
-    @Override
-    public IStatus restoreState(IMemento memento) {
-        return null;
-    }
+    private Logger log = Logger.getLogger(PerspectiveHandler.class.getName());
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
