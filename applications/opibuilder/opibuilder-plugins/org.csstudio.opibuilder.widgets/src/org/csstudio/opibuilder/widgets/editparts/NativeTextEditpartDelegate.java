acdc15295cfcc840cfcc0f7a695d05f32b674fa7
jbobnar <jaka.bobnar@cosylab.com>
2016-03-24 12:18:58 +0100

    Ignore all traversal events that are not triggered by tab.

--- applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.widgets/src/org/csstudio/opibuilder/widgets/editparts/NativeTextEditpartDelegate.java
+++ applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.widgets/src/org/csstudio/opibuilder/widgets/editparts/NativeTextEditpartDelegate.java
@@ -127,8 +127,8 @@ public class NativeTextEditpartDelegate implements ITextInputEditPartDelegate {
                     }
                 });
                 text.addTraverseListener(e -> {
-                    //if return or escape key triggered the event, ignore
-                    if (e.character == '\r' || e.keyCode == 27 || skipTraverse) return;
+                    //if key code is not tab, ignore
+                    if (e.character != '\t' || skipTraverse) return;
                     e.doit = false;
                     skipTraverse = true;
                     if (e.stateMask == 0) {
