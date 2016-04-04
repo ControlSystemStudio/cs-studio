606701d42ba6aaf92eacd309830e670f4363a054
Eric Berryman <berryman@frib.msu.edu>
2016-04-04 16:54:06 -0400

    remove redundant plugins

--- core/unorganized/unorganized-plugins/org.csstudio.dal/src/org/csstudio/dal/simple/IConnectionUpdateListener.java
+++
@@ -1,9 +0,0 @@
-package org.csstudio.dal.simple;
-
-import org.csstudio.dal.context.ConnectionState;
-
-public interface IConnectionUpdateListener {
-
-    public void connectionChanged(String channelName, ConnectionState state);
-
-}
