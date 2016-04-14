9c448a2f1b946e4b0264d877da3e05d8c514324c
Kunal Shroff <shroffk@bnl.gov>
2016-04-14 09:08:34 -0400

    #1750 Fixing search string parsers to support the apputil time representation

--- applications/logbook/logbook-plugins/org.csstudio.logbook.test/src/org/csstudio/logbook/util/SearchStringParserTest.java
+++ applications/logbook/logbook-plugins/org.csstudio.logbook.test/src/org/csstudio/logbook/util/SearchStringParserTest.java
@@ -65,46 +65,88 @@ public class SearchStringParserTest {
         expectedTextAndlogbookAndTags,
         searchParser(textAndlogbookAndTags, defaultKey));
 
-    String textAndlogbookAndTagsAndFrom = "test logbook:Operations tags:testTag from:5 days ago";
+    String textAndlogbookAndTagsAndFrom = "test logbook:Operations tags:testTag from:-5 days";
     Map<String, String> expectedTextAndlogbookAndTagsAndFrom = new HashMap<String, String>();
     expectedTextAndlogbookAndTagsAndFrom.put("text", "test");
     expectedTextAndlogbookAndTagsAndFrom.put("logbook", "Operations");
     expectedTextAndlogbookAndTagsAndFrom.put("tags", "testTag");
-    expectedTextAndlogbookAndTagsAndFrom.put("from", "5 days ago");
+    expectedTextAndlogbookAndTagsAndFrom.put("from", "-5 days");
     assertEquals("Failed to parse " + textAndlogbookAndTagsAndFrom,
         expectedTextAndlogbookAndTagsAndFrom,
         searchParser(textAndlogbookAndTagsAndFrom, defaultKey));
 
-    String textAndlogbookAndTagsAndFrom2 = "test logbook:Operations tags:testTag from:last 5 days";
+    String textAndlogbookAndTagsAndFrom2 = "test logbook:Operations tags:testTag from:-5 days";
     Map<String, String> expectedTextAndlogbookAndTagsAndFrom2 = new HashMap<String, String>();
     expectedTextAndlogbookAndTagsAndFrom2.put("text", "test");
     expectedTextAndlogbookAndTagsAndFrom2.put("logbook", "Operations");
     expectedTextAndlogbookAndTagsAndFrom2.put("tags", "testTag");
-    expectedTextAndlogbookAndTagsAndFrom2.put("from", "last 5 days");
+    expectedTextAndlogbookAndTagsAndFrom2.put("from", "-5 days");
     assertEquals("Failed to parse " + textAndlogbookAndTagsAndFrom2,
         expectedTextAndlogbookAndTagsAndFrom2,
         searchParser(textAndlogbookAndTagsAndFrom2, defaultKey));
 
-    String textAndlogbookAndTagsAndRange = "test logbook:Operations tags:testTag from:last 5 days to:now";
+    String textAndlogbookAndTagsAndRange = "test logbook:Operations tags:testTag from:-5 days to:now";
     Map<String, String> expectedTextAndlogbookAndTagsAndRange = new HashMap<String, String>();
     expectedTextAndlogbookAndTagsAndRange.put("text", "test");
     expectedTextAndlogbookAndTagsAndRange.put("logbook", "Operations");
     expectedTextAndlogbookAndTagsAndRange.put("tags", "testTag");
-    expectedTextAndlogbookAndTagsAndRange.put("from", "last 5 days");
+    expectedTextAndlogbookAndTagsAndRange.put("from", "-5 days");
     expectedTextAndlogbookAndTagsAndRange.put("to", "now");
     assertEquals("Failed to parse " + textAndlogbookAndTagsAndRange,
         expectedTextAndlogbookAndTagsAndRange,
         searchParser(textAndlogbookAndTagsAndRange, defaultKey));
 
-    String search = "search for some text logbook:Operations, LOTO,Commissioning tags: testTag, testTag2 from:last 5 days to: now";
+    String search = "search for some text logbook:Operations, LOTO,Commissioning tags: testTag, testTag2 from:-5 days to: now";
     Map<String, String> expectedSearch = new HashMap<String, String>();
     expectedSearch.put("text", "search for some text");
     expectedSearch.put("logbook", "Operations, LOTO,Commissioning");
     expectedSearch.put("tags", "testTag, testTag2");
-    expectedSearch.put("from", "last 5 days");
+    expectedSearch.put("from", "-5 days");
     expectedSearch.put("to", "now");
     assertEquals("Failed to parse " + search, expectedSearch,
         searchParser(search, defaultKey));
+    
+    // Test for the new absolute time format associated with the java time
+    String searchAbsoluteTime = "search for some text logbook:Operations, LOTO,Commissioning tags: testTag, testTag2 from:2016/04/12 14:11:00.000 to: now";
+    Map<String, String> expectedSearchAbsoluteTime = new HashMap<String, String>();
+    expectedSearchAbsoluteTime.put("text", "search for some text");
+    expectedSearchAbsoluteTime.put("logbook", "Operations, LOTO,Commissioning");
+    expectedSearchAbsoluteTime.put("tags", "testTag, testTag2");
+    expectedSearchAbsoluteTime.put("from", "2016/04/12 14:11:00.000");
+    expectedSearchAbsoluteTime.put("to", "now");
+    assertEquals("Failed to parse " + searchAbsoluteTime, expectedSearchAbsoluteTime,
+        searchParser(searchAbsoluteTime, defaultKey));
+    
+    searchAbsoluteTime = "search for some text logbook:Operations, LOTO,Commissioning tags: testTag, testTag2 from:2016/04/12 14:11:00.000 to: 2016/04/12 15:11";
+    expectedSearchAbsoluteTime = new HashMap<String, String>();
+    expectedSearchAbsoluteTime.put("text", "search for some text");
+    expectedSearchAbsoluteTime.put("logbook", "Operations, LOTO,Commissioning");
+    expectedSearchAbsoluteTime.put("tags", "testTag, testTag2");
+    expectedSearchAbsoluteTime.put("from", "2016/04/12 14:11:00.000");
+    expectedSearchAbsoluteTime.put("to", "2016/04/12 15:11");
+    assertEquals("Failed to parse " + searchAbsoluteTime, expectedSearchAbsoluteTime,
+        searchParser(searchAbsoluteTime, defaultKey));
+    
+    searchAbsoluteTime = "search for some text logbook:Operations, LOTO,Commissioning tags: testTag, testTag2 from:14:11 to:15:11";
+    expectedSearchAbsoluteTime = new HashMap<String, String>();
+    expectedSearchAbsoluteTime.put("text", "search for some text");
+    expectedSearchAbsoluteTime.put("logbook", "Operations, LOTO,Commissioning");
+    expectedSearchAbsoluteTime.put("tags", "testTag, testTag2");
+    expectedSearchAbsoluteTime.put("from", "14:11");
+    expectedSearchAbsoluteTime.put("to", "15:11");
+    assertEquals("Failed to parse " + searchAbsoluteTime, expectedSearchAbsoluteTime,
+        searchParser(searchAbsoluteTime, defaultKey));
+    
+    String searchRelativeTime = "search for some text logbook:Operations, LOTO,Commissioning tags: testTag, testTag2 from:-5 days 0.0 seconds to: now";
+    Map<String, String> expectedRelativeAbsoluteTime = new HashMap<String, String>();
+    expectedRelativeAbsoluteTime.put("text", "search for some text");
+    expectedRelativeAbsoluteTime.put("logbook", "Operations, LOTO,Commissioning");
+    expectedRelativeAbsoluteTime.put("tags", "testTag, testTag2");
+    expectedRelativeAbsoluteTime.put("from", "-5 days 0.0 seconds");
+    expectedRelativeAbsoluteTime.put("to", "now");
+    assertEquals("Failed to parse " + searchRelativeTime, expectedRelativeAbsoluteTime,
+        searchParser(searchRelativeTime, defaultKey));
+    
     }
 
 }
