e4401fd66818f6cf88d2202b3cf75854b8db5b10
kasemir <kasemirk@ornl.gov>
2016-05-04 17:14:42 -0400

    o.c.java: Add 'compact' format method

--- core/platform/platform-plugins/org.csstudio.java.test/src/org/csstudio/java/time/TimestampFormatsTest.java
+++ core/platform/platform-plugins/org.csstudio.java.test/src/org/csstudio/java/time/TimestampFormatsTest.java
@@ -11,6 +11,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 import org.junit.Test;
 
@@ -28,5 +29,29 @@ public class TimestampFormatsTest
         System.out.println(text);
         assertThat(text, equalTo("2015/02/25 08:42:00"));
         assertThat(TimestampFormats.FULL_FORMAT.format(time), equalTo("2015/02/25 08:42:00.000000000"));
+    }
+
+    @Test
+    public void testCompactFormatter()
+    {
+        Instant time = Instant.now();
+        assertThat(TimestampFormats.formatCompactDateTime(null), equalTo("?"));
+
+        // Timestamp on same day should be shown as HH:MM:SS
+        // Note that this test could fail if run just around midnight
+        // where 'now' turns into a new day.
+        String text = TimestampFormats.formatCompactDateTime(time);
+        System.out.println("Time, today: " + text);
+        assertThat(text.length(), equalTo(8));
+
+        time = time.minus(1, ChronoUnit.DAYS);
+        text = TimestampFormats.formatCompactDateTime(time);
+        System.out.println("Some time yesterday: " + text);
+        assertThat(text.length(), equalTo(11));
+
+        time = time.minus(365, ChronoUnit.DAYS);
+        text = TimestampFormats.formatCompactDateTime(time);
+        System.out.println("A day a year ago: " + text);
+        assertThat(text.length(), equalTo(10));
     }
 }
