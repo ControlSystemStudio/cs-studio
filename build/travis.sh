412b68ccf86cf6a7a53b4eb807a02c741ee74411
Eric Berryman <eric.berryman@gmail.com>
2017-01-23 07:57:49 -0500

    travisci test timeout (#2044)
    travis: allow arguments for core or applications
    
    * travis: allow arguments for core or applications
    
    * travis: separate core and apps
    
    * Update travis.sh
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update .travis.yml
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update .travis.yml

--- build/travis.sh
+++ build/travis.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-set -e
 
 function doCompile {
-  mvn clean verify -fcore/pom.xml -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Pcheckstyle
-  mvn clean verify -fapplications/pom.xml -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dcsstudio.composite.repo=core/p2repo -Pcheckstyle
+  if [ "$CORE" = true ] ; then
+    mvn clean verify -fcore/pom.xml -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Pcheckstyle
+  fi
+  if [ "$APPLICATIONS" = true ] ; then
+    mvn clean verify -fapplications/pom.xml -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dcsstudio.composite.repo=core/p2repo -Pcheckstyle
+  fi
 }
 
 function doCompileWithDeploy {
   echo "<settings><servers><server><id>s3.site</id><username>\${env.S3USER}</username><password>\${env.S3PASS}</password></server></servers></settings>" > ~/settings.xml
-  mvn clean verify -fcore/pom.xml --settings ~/settings.xml -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -PuploadRepo
-  mvn clean verify -fapplications/pom.xml --settings ~/settings.xml -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dcsstudio.composite.repo=core/p2repo -PuploadRepo
+  if [ "$CORE" = true ] ; then
+    mvn clean verify -fcore/pom.xml --settings ~/settings.xml -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -PuploadRepo
+  fi
+  if [ "$APPLICATIONS" = true ] ; then
+    mvn clean verify -fapplications/pom.xml --settings ~/settings.xml -Declipse.p2.mirrors=false -Dtycho.localArtifacts=ignore -Dcsstudio.composite.repo=core/p2repo -PuploadRepo
+  fi
 }
 
 function catTests {
@@ -26,6 +33,31 @@ echo $REPO
 echo $REPO_ORG
 echo $SHA
 
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -c|--core)
+    CORE=true
+    shift # past argument
+    ;;
+    -a|--applications)
+    APPLICATIONS=true
+    shift # past argument
+    ;;
+    --default)
+    CORE=true
+    APPLICATIONS=true
+    ;;
+    *)
+            # unknown option
+    ;;
+  esac
+  shift # past argument or value
+done
+
+echo "CORE=${CORE}"
+echo "APP=${APPLICATIONS}"
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$REPO_ORG" == "ControlSystemStudio" ] && ([[ "$TRAVIS_BRANCH" =~ ^[0-9]+\.[0-9]+\.x ]] || [ "$TRAVIS_BRANCH" == "master" ]); then
     echo "Deploying"
