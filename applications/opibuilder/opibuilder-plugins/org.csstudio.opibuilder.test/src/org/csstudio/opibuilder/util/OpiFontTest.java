291f0ccac5748b0ce9c90957c4848850d12a2d33
Will Rogers <wahrogers@gmail.com>
2017-02-03 12:13:25 +0000

    Opibuilder: another couple of tests for OPIFont.

--- applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.test/src/org/csstudio/opibuilder/util/OpiFontTest.java
+++ applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.test/src/org/csstudio/opibuilder/util/OpiFontTest.java
@@ -1,9 +1,12 @@
 package org.csstudio.opibuilder.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Point;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -13,36 +16,67 @@ public class OpiFontTest {
 
     private final int FONT_SIZE = 10;
 
+    private final int DUMMY_STYLE = 5;
+
     private final Point DUMMY_DPI = new Point(100, 100);
+
+    private OPIFont testOPIFont;
+
+    /**
+     * This mock uses protected methods in OPIFont to override static calls
+     * that would otherwise prevent testing.
+     * All relevant logic should still be the same.
+     */
+    private class TestableOPIFont extends OPIFont {
+        public TestableOPIFont(FontData fontData) {
+            super(fontData);
+        }
+        public TestableOPIFont(OPIFont font) {
+            super(font);
+        }
+        @Override
+        protected boolean getDefaultIsInPixels() {
+            return false;
+        }
+        protected Point getDPI() {
+            return DUMMY_DPI;
+        }
+    }
+
+    @Before
+    public void setUp() {
+        this.testOPIFont = new TestableOPIFont(new FontData("Arial", FONT_SIZE, DUMMY_STYLE));
+    }
+
+    @Test
+    public void testCopyConstructor() {
+        OPIFont copyFont = new TestableOPIFont(testOPIFont);
+        assertEquals(testOPIFont, copyFont);
+        assertTrue(testOPIFont != copyFont);
+        assertTrue(testOPIFont.hashCode() == copyFont.hashCode());
+    }
 
     @Test
     public void testSizeInPoints() {
-        OPIFont opiFont = new OPIFont(new FontData("Arial", FONT_SIZE, 0)) {
-            @Override  // Can't call this without the RCP running
-            protected boolean getDefaultIsInPixels() {
-                return false;
-            }
-        };
         // Returned size should be 10 points
-        assertEquals(FONT_SIZE, opiFont.getFontData().getHeight());
+        assertEquals(FONT_SIZE, testOPIFont.getFontData().getHeight());
     }
 
     @Test
     public void testSizeInPixels() throws Exception {
-        OPIFont opiFont = new OPIFont(new FontData("Arial", FONT_SIZE, 0)) {
-            @Override  // Can't call this without the RCP running
-            protected boolean getDefaultIsInPixels() {
-                return false;
-            }
-            protected Point getDPI() {
-                return DUMMY_DPI;
-            }
-        };
         // Set font size to be in pixels
-        opiFont.setSizeInPixels(true);
+        testOPIFont.setSizeInPixels(true);
         // Returned size should be 10 pixels converted to points based on the display DPI
         int expected = FONT_SIZE * OPIFont.POINTS_PER_INCH / DUMMY_DPI.y;
-        assertEquals(expected, opiFont.getFontData().getHeight());
+        assertEquals(expected, testOPIFont.getFontData().getHeight());
+    }
+
+    @Test
+    public void testGetRawFontData() throws Exception {
+        // Set font size to be in pixels
+        testOPIFont.setSizeInPixels(true);
+        // Returned size should still be 10 pixels
+        assertEquals(FONT_SIZE, testOPIFont.getRawFontData().getHeight());
     }
 
 }
