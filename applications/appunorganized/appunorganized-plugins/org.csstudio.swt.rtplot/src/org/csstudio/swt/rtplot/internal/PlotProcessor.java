afe7dfcf05dab91836c68b05009ba51d8b6a7d99
kasemir <kasemirk@ornl.gov>
2017-02-22 09:03:34 -0500

    Databrowser: Fix autoscale
    #2026 changed autoscale to only consider the 'visible' range, but
    omitted the first sample at-or-before the start time.
    
    This update performs a binary search for the actual start & end sample
    to consider, then autoscale using that range.
    
    Port of similar fix for databrowser3

--- applications/appunorganized/appunorganized-plugins/org.csstudio.swt.rtplot/src/org/csstudio/swt/rtplot/internal/PlotProcessor.java
+++ applications/appunorganized/appunorganized-plugins/org.csstudio.swt.rtplot/src/org/csstudio/swt/rtplot/internal/PlotProcessor.java
@@ -64,23 +64,28 @@ public class PlotProcessor<XTYPE extends Comparable<XTYPE>>
             {
                 double low = Double.MAX_VALUE;
                 double high = -Double.MAX_VALUE;
+                final PlotDataSearch<XTYPE> search = new PlotDataSearch<>();
                 data.getLock().lock();
                 try
-                {
-                    final int N = data.size();
-                    for (int i=0; i<N; ++i)
+                {   // Consider first sample at-or-before start
+                    int start = search.findSampleLessOrEqual(data, x_range.getLow());
+                    if (start < 0)
+                        start = 0;
+                    // Last sample is the one just inside end of range.
+                    int stop = search.findSampleLessOrEqual(data, x_range.getHigh());
+                    if (stop < 0)
+                        stop = 0;
+                    // Check [start .. stop], including stop
+                    for (int i=start; i<=stop; ++i)
                     {
                         final PlotDataItem<XTYPE> item = data.get(i);
-                        if (x_range.contains(item.getPosition()))
-                        {
-                            final double value = item.getValue();
-                            if (! Double.isFinite(value))
-                                continue;
-                            if (value < low)
-                                low = value;
-                            if (value > high)
-                                high = value;
-                        }
+                        final double value = item.getValue();
+                        if (! Double.isFinite(value))
+                            continue;
+                        if (value < low)
+                            low = value;
+                        if (value > high)
+                            high = value;
                     }
                 }
                 finally
