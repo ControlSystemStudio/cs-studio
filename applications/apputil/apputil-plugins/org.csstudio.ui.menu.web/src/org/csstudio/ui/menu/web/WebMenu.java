22317bb7298c2c898f0e80ced5c1125b55a20b1c
Claudio Rosati <Claudio.Rosati@esss.se>
2017-03-23 15:46:42 +0100

    o.c.ui.menu.web: * '|' can be used to add separators between web menu items.

--- applications/apputil/apputil-plugins/org.csstudio.ui.menu.web/src/org/csstudio/ui/menu/web/WebMenu.java
+++ applications/apputil/apputil-plugins/org.csstudio.ui.menu.web/src/org/csstudio/ui/menu/web/WebMenu.java
@@ -7,6 +7,7 @@
  ******************************************************************************/
 package org.csstudio.ui.menu.web;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -18,66 +19,92 @@ import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.actions.CompoundContributionItem;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
-/** Menu with entries for web links.
+
+/**
+ * Menu with entries for web links.
+ * <p>
+ * To be registered as 'dynamic' menu in plugin.xml.
+ * <p>
+ * Expects a preference "weblinks" that lists further prefs,
+ * separated by space. See preferences.ini for example.
  *
- *  <p>To be registered as 'dynamic' menu in plugin.xml.
- *
- *  <p>Expects a preference "weblinks" that lists further prefs,
- *  separated by space. See preferences.ini for example.
- *
- *  @author Kay Kasemir
- *
+ * @author Kay Kasemir
+ * @author Claudio Rosati
  */
-public class WebMenu extends CompoundContributionItem
-{
+public class WebMenu extends CompoundContributionItem {
+
     @Override
-    protected IContributionItem[] getContributionItems()
-    {
-        final ImageDescriptor icon =
-            AbstractUIPlugin.imageDescriptorFromPlugin(Activator.ID, "icons/weblink.png"); //$NON-NLS-1$
-        final IMenuManager items = new MenuManager(Messages.WebLinks, icon, null);
-        final IAction[] actions = createWeblinkActions();
-        for (IAction action : actions)
-            items.add(action);
-        return new IContributionItem[] { items };
+    protected IContributionItem[] getContributionItems ( ) {
+
+        final ImageDescriptor icon = AbstractUIPlugin.imageDescriptorFromPlugin(Activator.ID, "icons/weblink.png"); //$NON-NLS-1$
+        final IMenuManager itemsManager = new MenuManager(Messages.WebLinks, icon, null);
+        final Object[] items = createWeblinkItems();
+
+        for ( Object item : items ) {
+            if ( item instanceof IAction ) {
+                itemsManager.add((IAction) item);
+            } else if ( item instanceof IContributionItem ){
+                itemsManager.add((IContributionItem) item);
+            }
+        }
+
+        return new IContributionItem[] { itemsManager };
+
     }
 
-
-    /** Parse preferences, obtain web link actions
-     *  @return web link actions
+    /**
+     * Parse preferences, obtain web link actions
+     *
+     * @return web link actions
      */
-    private IAction[] createWeblinkActions()
-    {
-        final List<IAction> web_actions = new ArrayList<IAction>();
+    private Object[] createWeblinkItems ( ) {
 
+        final List<Object> web_items = new ArrayList<>();
         final IPreferencesService prefs = Platform.getPreferencesService();
         final String weblinks = prefs.getString(Activator.ID, "weblinks", null, null); //$NON-NLS-1$
-        if (weblinks == null)
-            return new IAction[0];
-        final String[] link_prefs = weblinks.split("[ \t]+"); //$NON-NLS-1$
-        for (String pref : link_prefs)
-        {
-            final String descriptor = prefs.getString(Activator.ID, pref, null, null);
-            if (descriptor == null)
-                continue;
-            final String[] link = descriptor.split("\\|"); //$NON-NLS-1$
-            if (link.length != 2)
-            {
-                Logger.getLogger(getClass().getName()).log(Level.WARNING,
-                    "Web link doesn't follow the LABEL|URL pattern: {0}", pref); //$NON-NLS-1$
-                continue;
-            }
-            final String label = link[0];
-            final String url = link[1];
-            Logger.getLogger(getClass().getName()).log(Level.FINE, "Web link {0} = {1} ({2})", //$NON-NLS-1$
-                    new Object[] { pref, label, url });
 
-            web_actions.add(new OpenWebBrowserAction(label, url));
+        if ( weblinks == null )
+            return new Object[0];
+
+        final String[] link_prefs = weblinks.split("[ \t]+"); //$NON-NLS-1$
+
+        for ( String pref : link_prefs ) {
+
+            if ( "|".equals(pref) ) {
+                web_items.add(new Separator());
+            } else {
+
+                final String descriptor = prefs.getString(Activator.ID, pref, null, null);
+
+                if ( descriptor == null )
+                    continue;
+
+                final String[] link = descriptor.split("\\|"); //$NON-NLS-1$
+
+                if ( link.length != 2 ) {
+                    Logger.getLogger(getClass().getName()).log(Level.WARNING, "Web link doesn't follow the LABEL|URL pattern: {0}", pref); //$NON-NLS-1$
+                    continue;
+                }
+
+                final String label = link[0];
+                final String url = link[1];
+
+                Logger.getLogger(getClass().getName()).log(Level.FINE, "Web link {0} = {1} ({2})", //$NON-NLS-1$
+                        new Object[] { pref, label, url });
+
+                web_items.add(new OpenWebBrowserAction(label, url));
+
+            }
+
         }
-        return web_actions.toArray(new IAction[web_actions.size()]);
+
+        return web_items.toArray(new Object[web_items.size()]);
+
     }
+
 }
