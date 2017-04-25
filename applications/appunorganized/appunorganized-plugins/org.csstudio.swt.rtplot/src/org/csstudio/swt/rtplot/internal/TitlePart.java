145d480498e7489f6f5c3dd17a0c0a7473fb1f90
Kunal Shroff <shroffk@bnl.gov>
2017-04-25 13:27:21 -0400

    fixing inherited doc and imports.

--- applications/appunorganized/appunorganized-plugins/org.csstudio.swt.rtplot/src/org/csstudio/swt/rtplot/internal/TitlePart.java
+++ applications/appunorganized/appunorganized-plugins/org.csstudio.swt.rtplot/src/org/csstudio/swt/rtplot/internal/TitlePart.java
@@ -47,7 +47,7 @@ public class TitlePart extends PlotPart
      * Invoked to paint the part.
      *  <p>Is invoked on background thread.
      *  <p>Derived part can override, should invoke super.
-     * @param gc {@link GC} for painting in background thread 
+     * @param gc {@link GC} for painting in background thread
      * @param media {@link SWTMediaPool}
      * @param font {@link Font}
      */
