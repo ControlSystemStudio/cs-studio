be9c2d68de266ac94afe9bf6664433221beccfaa
Amanda Carpenter <ajccarpenter@gmail.com>
2017-06-27 14:35:36 -0400

    PVStructureHelper: implement decoding of NTTable pv values and also arbitrary structures as VTable

--- core/base/base-plugins/org.csstudio.vtype.pv/src/org/csstudio/vtype/pv/pva/PVStructureHelper.java
+++ core/base/base-plugins/org.csstudio.vtype.pv/src/org/csstudio/vtype/pv/pva/PVStructureHelper.java
@@ -7,24 +7,36 @@
  ******************************************************************************/
 package org.csstudio.vtype.pv.pva;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
+import org.diirt.util.array.ArrayBoolean;
+import org.diirt.util.array.ArrayByte;
 import org.diirt.util.array.ArrayDouble;
 import org.diirt.util.array.ArrayFloat;
 import org.diirt.util.array.ArrayInt;
 import org.diirt.util.array.ArrayLong;
 import org.diirt.util.array.ArrayShort;
 import org.diirt.vtype.AlarmSeverity;
+import org.diirt.vtype.Array;
 import org.diirt.vtype.VImage;
+import org.diirt.vtype.VTable;
 import org.diirt.vtype.VType;
 import org.diirt.vtype.ValueFactory;
+import org.diirt.vtype.table.ListNumberProvider;
+import org.diirt.vtype.table.VTableFactory;
 import org.epics.pvdata.factory.ConvertFactory;
+import org.epics.pvdata.pv.BooleanArrayData;
 import org.epics.pvdata.pv.Convert;
 import org.epics.pvdata.pv.Field;
 import org.epics.pvdata.pv.PVBoolean;
+import org.epics.pvdata.pv.PVBooleanArray;
 import org.epics.pvdata.pv.PVByteArray;
 import org.epics.pvdata.pv.PVField;
+import org.epics.pvdata.pv.PVInt;
 import org.epics.pvdata.pv.PVScalar;
 import org.epics.pvdata.pv.PVScalarArray;
 import org.epics.pvdata.pv.PVStringArray;
@@ -34,7 +46,9 @@ import org.epics.pvdata.pv.PVUnion;
 import org.epics.pvdata.pv.Scalar;
 import org.epics.pvdata.pv.ScalarArray;
 import org.epics.pvdata.pv.ScalarType;
+import org.epics.pvdata.pv.Structure;
 import org.epics.pvdata.pv.StructureArrayData;
+import org.epics.pvdata.pv.Type;
 
 /** Helper for reading & writing PVStructure
  *
@@ -91,6 +105,17 @@ class PVStructureHelper
             return decodeNTArray(actual_struct);
         if (type.equals("epics:nt/NTNDArray:1.0"))
             return decodeNTNDArray(actual_struct);
+        if (type.equals("epics:nt/NTTable:1.0"))
+        	return decodeNTTable(actual_struct);
+        
+        //TODO: (?) Attempt to handle arbitrary structure as table value (i.e., the
+        //"value" field of an NTTable type). Need to verify number of rows is consistent[1],
+        //and, if not, fall back to the current behavior for non-NT structures.
+        //[1] rows: All terminal fields of a table should be of the same
+        //length, since this is the number of rows in the table. The decoding
+        //function might throw an exception if the rows don't
+        //match. This could be caught by a handler, which would fall back to
+        //creating an alarmed string.
 
         // Handle data that contains a "value", even though not marked as NT*
         final Field value_field = actual_struct.getStructure().getField("value");
@@ -98,6 +123,17 @@ class PVStructureHelper
             return decodeNTScalar(actual_struct);
         else if (value_field instanceof ScalarArray)
             return decodeNTArray(actual_struct);
+        else if (value_field instanceof Structure)
+        {
+        	try
+        	{
+        		return decodeAsTableValue(actual_struct.getStructureField("value"), new ArrayList<>());
+        	}
+        	catch (Exception e)
+        	{
+        		//fall through
+        	}
+        }
 
         // Create string that indicates name of unknown type
         return ValueFactory.newVString(actual_struct.getStructure().toString(),
@@ -342,6 +378,225 @@ class PVStructureHelper
             throw new Exception("Cannot extract byte[] from " + value);
 
         return ValueFactory.newVImage(height, width, data);
+    }
+    
+    /**
+     * Decode table from NTTable
+     * @param struct
+     * @return
+     * @throws Exception
+     */
+	private static VTable decodeNTTable(final PVStructure struct) throws Exception
+    {
+    	final PVScalarArray labels_array = struct.getScalarArrayField("labels", ScalarType.pvString);
+    	final int labels_length = labels_array.getLength();
+    	final String [] labels_strings = new String [labels_length];
+    	convert.toStringArray(labels_array, 0, labels_length, labels_strings, 0);
+    	//create a new list for column names (labels), because if any field is a structure,
+    		//labels need to be created for each element of the structure
+    	final List<String> names  = new ArrayList<>(Arrays.asList(labels_strings));
+    	
+    	final PVStructure value_struct = struct.getStructureField("value");
+    	
+		return decodeAsTableValue(value_struct, names);
+    }
+	
+	/**
+	 * Decode a PVStructure (like an NTTable "value" field) as a VTable.
+	 * 
+	 * <p>Sub-structures (fields which are structures) are handled recursively. Their fields are added
+	 * to the table in the order used by PVStructure.getSubField(int).
+	 * Labels of sub-structure fields are represented as "sub-structure-label/sub-structure-field-name".
+	 * This holds for any level of (sub-)(sub-)...(sub-)sub-structure.
+	 * @param value_struct Structure to decode
+	 * @param names Column names. If these are the top-level labels of the value structure, there should be
+	 * 			as many names as there are fields. Otherwise, names should be an empty list. For each field,
+	 * 			if the corresponding name is null or not in the list, the field name is used as its label. If
+	 * 			the column name ends with a "slash" character ('/'), the field name is appended to it.
+	 * @return VTable representing the values of the structure
+	 * @throw Exception If the rows are not all the same size; that is, the lengths of the terminal fields
+	 * 						(array length for arrays, or 1 for scalars) are not the same.
+	 */
+	private static VTable decodeAsTableValue(final PVStructure value, final List<String> names) throws Exception
+	{
+    	final List<Class<?>> types = new ArrayList<>();
+    	final List<Object> values  = new ArrayList<>();
+		int rowSize = -1;
+		Stack<PVField> stack = new Stack<>();
+		PVField fields [] = value.getPVFields();
+		for (int i = fields.length; i > 0; )
+			stack.push(fields[--i]);
+		while (!stack.empty())
+		{
+			PVField field = stack.pop();
+			int index = types.size();
+			if (index >= names.size())
+				names.add(field.getFieldName());
+			else
+			{
+				String name = names.get(index);
+				if (name == null)
+					names.set(index, field.getFieldName());
+				else if (name.endsWith("/"))
+					names.set(index, name + field.getFieldName());
+			}
+			if (field instanceof PVScalar)
+			{
+				if (rowSize != 1 && rowSize >= 0)
+					throw new Exception("Table must have consistent row size");
+				getTableTypeAndValue((PVScalar) field, types, values);
+			}
+			else if (field instanceof PVScalarArray)
+			{
+				if (getTableTypeAndValue((PVScalarArray) field, types, values) != rowSize && rowSize >= 0)
+					throw new Exception("Table must have consistent row size");
+			}
+			else if (field instanceof PVStructure)
+			{
+				PVField [] subfields = ((PVStructure) field).getPVFields();
+				String name = names.get(index) + "/";
+				names.set(index, name);
+				names.addAll(index, Collections.nCopies(subfields.length-1, name));
+				for (int i = subfields.length; i-- > 0; )
+					stack.push(subfields[i]);
+			}
+			//TODO: other kinds of Field
+		} //end while not empty
+    	return ValueFactory.newVTable(types, names, values);
+    }
+    
+    @SuppressWarnings("restriction")
+	private static void getTableTypeAndValue(PVScalar scalar, List<Class<?>> types, List<Object> values)
+    {
+		switch(scalar.getScalar().getScalarType())
+		{
+			case pvDouble:
+				types.add(Double.TYPE);
+				double double_value = convert.toDouble(scalar);
+				values.add(new ArrayDouble(double_value));
+				break;
+			case pvFloat:
+				types.add(Float.TYPE);
+				float float_value = convert.toFloat(scalar);
+				values.add(new ArrayFloat(float_value));
+				break;
+			case pvLong:
+			case pvUInt:
+			case pvULong:
+				types.add(Long.TYPE);
+				long long_value = convert.toLong(scalar);
+				values.add(new ArrayLong(long_value));
+				break;
+			case pvUShort:
+			case pvInt:
+				types.add(Integer.TYPE);
+				int int_value = convert.toInt(scalar);
+				values.add(new ArrayInt(int_value));
+				break;
+			case pvUByte:
+			case pvShort:
+				types.add(Short.TYPE);
+				short short_value = convert.toShort(scalar);
+				values.add(new ArrayShort(short_value));
+				break;
+			case pvByte:
+				types.add(Byte.TYPE);
+				byte byte_value = convert.toByte(scalar);
+				values.add(new org.diirt.util.array.ArrayByte(byte_value));
+				break;
+			case pvBoolean: //Table can't handle ArrayBoolean, so use List<Boolean> instead
+				types.add(Boolean.TYPE);
+				boolean bool_value = ((PVBoolean)scalar).get();
+				values.add(Arrays.asList(bool_value));
+				break;
+			case pvString:
+				types.add(String.class);
+				String str_value = convert.toString(scalar);
+				values.add(Arrays.asList(str_value));
+				break;
+			//default: //throw exception?
+		}
+	}
+    
+    @SuppressWarnings("restriction")
+	private static int getTableTypeAndValue(PVScalarArray array, List<Class<?>> types, List<Object> values)
+    {
+    	final int length = array.getLength();
+    	//int to<X>Array(PVScalarArray pv, int offset, int len, <X>[]to, int toOffset);
+		switch(array.getScalarArray().getElementType())
+		{
+			case pvDouble:
+				types.add(Double.TYPE);
+				double [] double_value = new double [length];
+				convert.toDoubleArray(array, 0, length, double_value, 0);
+				values.add(new ArrayDouble(double_value));
+				break;
+			case pvFloat:
+				types.add(Float.TYPE);
+				float [] float_value = new float [length];
+				convert.toFloatArray(array, 0, length, float_value, 0);
+				values.add(new ArrayFloat(float_value));
+				break;
+			case pvLong:
+			case pvUInt:
+			case pvULong:
+				types.add(Long.TYPE);
+				long [] long_value = new long[length];
+				convert.toLongArray(array, 0, length, long_value, 0);
+				values.add(new ArrayLong(long_value));
+				break;
+			case pvUShort:
+			case pvInt:
+				types.add(Integer.TYPE);
+				int [] int_value = new int [length];
+				convert.toIntArray(array, 0, length, int_value, 0);
+				values.add(new ArrayInt(int_value));
+				break;
+			case pvUByte:
+			case pvShort:
+				types.add(Short.TYPE);
+				short [] short_value = new short [length];
+				convert.toShortArray(array, 0, length, short_value, 0);
+				values.add(new ArrayShort(short_value));
+				break;
+			case pvByte:
+				types.add(Byte.TYPE);
+				byte [] byte_value = new byte [length];
+				convert.toByteArray(array, 0, length, byte_value, 0);
+				values.add(new org.diirt.util.array.ArrayByte(byte_value));
+				break;
+			case pvBoolean:
+				types.add(Boolean.TYPE);
+				//No Convert method for boolean. Have to do it the hard way.
+				boolean [] bool_value = getArray((PVBooleanArray)array, length);
+				List<Boolean> value = new ArrayList<Boolean>(length);
+				for (boolean bool : bool_value)
+					value.add(bool);
+				values.add(value);
+				break;
+			case pvString:
+				types.add(String.class);
+				String [] str_value = new String [length];
+				convert.toStringArray(array, 0, length, str_value, 0);
+				values.add(Arrays.asList(str_value));
+				break;
+			//default: //throw exception?
+		}
+		return length;
+	}
+    
+    //based off double [] getArray(PVDoubleArray) example from pvDataJava documentation
+    private static boolean[] getArray(PVBooleanArray pv, final int len)
+    {
+        boolean[] storage = new boolean[len];
+        BooleanArrayData data = new BooleanArrayData();
+        int offset = 0;
+        while(offset < len) {
+            int num = pv.get(offset,(len-offset),data);
+            System.arraycopy(data.data,data.offset,storage,offset,num);
+            offset += num;
+        }
+        return storage;
     }
 
     /** @param structure {@link PVStructure} from which to read
