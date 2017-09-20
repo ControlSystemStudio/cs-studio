046f53c0c89f7d23d98a034c334c8bcd39e075e5
Dominic Oram <dominic.oram@stfc.ac.uk>
2017-09-20 13:46:22 +0100

    Reverted accidental changes

--- applications/archive/archive-plugins/org.csstudio.archive.engine/src/org/csstudio/archive/engine/Application.java
+++ applications/archive/archive-plugins/org.csstudio.archive.engine/src/org/csstudio/archive/engine/Application.java
@@ -22,8 +22,11 @@ import org.csstudio.archive.engine.server.EngineServer;
 import org.csstudio.logging.LogConfigurator;
 import org.csstudio.security.PasswordInput;
 import org.csstudio.security.preferences.SecurePreferences;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.osgi.service.prefs.BackingStoreException;
 
 /** Eclipse Application for CSS archive engine
  *  @author Kay Kasemir
@@ -43,6 +46,22 @@ public class Application implements IApplication
     /** Option: if skip reading last sample time or not */
     private boolean skip_last = false;
 
+    String[] getPrefValue(final String option) {
+        if (option != null) { // Split "plugin/key=value"
+            String[] pref_val = new String[2];
+            final int sep = option.indexOf("=");
+            if (sep >= 0) {
+                pref_val[0] = option.substring(0, sep);
+                pref_val[1] = option.substring(sep + 1);
+            } else {
+                pref_val[0] = option;
+                pref_val[1] = null;
+            }
+            return pref_val;
+        }
+        return null;
+    }
+
     /** Obtain settings from preferences and command-line arguments
      *  @param args Command-line arguments
      *  @return <code>true</code> if continue, <code>false</code> to end application
@@ -51,7 +70,7 @@ public class Application implements IApplication
     private boolean getSettings(final String args[], final IApplicationContext context)
     {
         // Display configuration info
-        final String version = (String) context.getBrandingBundle().getHeaders().get("Bundle-Version");
+        final String version = context.getBrandingBundle().getHeaders().get("Bundle-Version");
         final String app_info = context.getBrandingName() + " " + version;
 
         // Create the parser and run it.
@@ -64,6 +83,8 @@ public class Application implements IApplication
                     "HTTP server port", 4812);
         final StringOption engine_name_opt = new StringOption(parser,
                     "-engine", "demo_engine", "Engine config name", null);
+        final StringOption preference_opt = new StringOption(parser, "-set_pref", "plugin.name/preference=value",
+                "Set a preference for a specific plugin", null);
         final StringOption set_password_opt = new StringOption(parser,
                 "-set_password", "plugin/key=value", "Set secure preferences", null);
         parser.addEclipseParameters();
@@ -89,24 +110,17 @@ public class Application implements IApplication
         }
 
         // Check arguments
-        String option = set_password_opt.get();
-        if (option != null)
-        {    // Split "plugin/key=value"
-            final String pref, value;
-            final int sep = option.indexOf("=");
-            if (sep >= 0)
+        String[] pref_val = getPrefValue(set_password_opt.get());
+
+        if (pref_val != null)
+        {
+            if (pref_val[1] == null)
             {
-                pref = option.substring(0, sep);
-                value = option.substring(sep + 1);
-            }
-            else
-            {
-                pref = option;
-                value = PasswordInput.readPassword("Value for " + pref + ":");
+                pref_val[1] = PasswordInput.readPassword("Value for " + pref_val[0] + ":");
             }
             try
             {
-                SecurePreferences.set(pref, value);
+                SecurePreferences.set(pref_val[0], pref_val[1]);
             }
             catch (Exception ex)
             {
@@ -115,16 +129,49 @@ public class Application implements IApplication
             return false;
         }
 
-        if (engine_name_opt.get() == null)
+        for (final String pref_opt : preference_opt.getMany())
         {
-            System.out.println("Missing option " + engine_name_opt.getOption());
+            pref_val = getPrefValue(pref_opt);
+            if (pref_val != null)
+            {
+                final String pref = pref_val[0];
+                final String value = pref_val[1];
+
+                if (value == null) {
+                    System.out.println("Malformed option " + preference_opt.getOption() + " " + pref_opt);
+                    return false;
+                }
+                final int sep = pref.indexOf('/');
+                if (sep < 0) {
+                    System.out.println(
+                            "Malformed plugin/preference for option " + preference_opt.getOption() + " " + pref_opt);
+                    return false;
+                }
+                final String plugin = pref.substring(0, sep);
+                final String preference = pref.substring(sep + 1);
+
+                final IEclipsePreferences pref_node = InstanceScope.INSTANCE.getNode(plugin);
+                pref_node.put(preference, value);
+                try {
+                    pref_node.flush();
+                } catch (BackingStoreException e) {
+                    Activator.getLogger().log(Level.SEVERE,
+                            "Could not set plugin preference " + plugin + "/" + preference, e);
+                }
+            }
+        }
+
+        engine_name = engine_name_opt.get();
+
+        if (engine_name == null)
+        {
+            System.out.println("Must specificy engine option: " + engine_name_opt.getOption());
             System.out.println(parser.getHelp());
             return false;
         }
 
         // Copy stuff from options into member vars.
         port = port_opt.get();
-        engine_name = engine_name_opt.get();
         skip_last = skip_last_opt.get();
         return true;
     }
@@ -145,7 +192,7 @@ public class Application implements IApplication
         // Setup groups, channels, writer
         // This is all single-threaded!
         final Logger logger = Activator.getLogger();
-        EngineModel.VERSION =  (String) context.getBrandingBundle().getHeaders().get("Bundle-Version");
+        EngineModel.VERSION =  context.getBrandingBundle().getHeaders().get("Bundle-Version");
         logger.info("Archive Engine " + EngineModel.VERSION);
         try
         {
