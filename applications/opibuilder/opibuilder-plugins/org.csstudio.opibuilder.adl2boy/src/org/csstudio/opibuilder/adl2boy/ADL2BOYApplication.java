47e34418b8d7859ca45d6ceb3ec94dbd48443ed7
Kay Kasemir <kay.kasemir@gmail.com>
2017-12-05 14:12:42 -0500

    MEDM converter: Handle files on command line

--- applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/ADL2BOYApplication.java
+++ applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.adl2boy/src/org/csstudio/opibuilder/adl2boy/ADL2BOYApplication.java
@@ -7,8 +7,14 @@
  ******************************************************************************/
 package org.csstudio.opibuilder.adl2boy;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.csstudio.opibuilder.adl2boy.translator.TranslatorUtils;
+import org.csstudio.opibuilder.model.DisplayModel;
+import org.csstudio.opibuilder.persistence.XMLUtil;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 
@@ -36,9 +42,80 @@ public class ADL2BOYApplication implements IApplication
         final String args[] =
                 (String []) context.getArguments().get("application.args");
 
-        System.out.println(Arrays.toString(args));
+        final List<String> files = new ArrayList<>();
+        for (int i=0; i<args.length; ++i)
+        {
+            if (args[i].startsWith("-"))
+            {
+                if (i+1 < args.length)
+                {
+                    System.out.println("Ignoring argument " + args[i] + " " + args[i+1]);
+                    ++i;
+                }
+                else
+                    System.out.println("Ignoring argument " + args[i]);
+            }
+            else
+                files.add(args[i]);
+        }
+
+        try
+        {
+            if (! checkThenConvert(files))
+                System.exit(-1);
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            System.exit(-1);
+        }
 
         return IApplication.EXIT_OK;
+    }
+
+    private boolean checkThenConvert(final List<String> filenames) throws Exception
+    {
+        final List<File> inputs = new ArrayList<>();
+        final List<File> outputs = new ArrayList<>();
+        for (String name : filenames)
+        {
+            final File input = new File(name);
+            final File output = name.endsWith(".adl")
+                              ? new File(name.substring(0, name.length()-4) + ".opi")
+                              : new File(name + ".opi");
+            if (! input.canRead())
+            {
+                System.out.println("Cannot read " + input);
+                return false;
+            }
+            if (output.exists())
+            {
+                System.out.println("Output file already exists: " + output);
+                return false;
+            }
+            inputs.add(input);
+            outputs.add(output);
+        }
+
+        for (int i=0; i<inputs.size(); ++i)
+            convert(inputs.get(i), outputs.get(i));
+        return true;
+    }
+
+    private void convert(final File input, final File output) throws Exception
+    {
+        System.out.println("Reading " + input);
+        final DisplayModel model = TranslatorUtils.convertAdlToModel(input.toString());
+        final String xml = XMLUtil.widgetToXMLString(model, true);
+
+        System.out.println("Writing " + output);
+        try
+        (
+            final FileOutputStream out = new FileOutputStream(output);
+        )
+        {
+            out.write(xml.getBytes());
+        }
     }
 
     @Override
