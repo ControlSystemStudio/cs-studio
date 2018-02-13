3d27d48ba0291fa4ea233daae072e74b7c352b92
Borut <borut.terpinc@cosylab.com>
2018-02-13 08:17:36 +0000

    Reverted max messages from msghistory back to max. properties

--- applications/alarm/alarm-plugins/org.csstudio.alarm.beast.msghist/src/org/csstudio/alarm/beast/msghist/rdb/MessageRDB.java
+++ applications/alarm/alarm-plugins/org.csstudio.alarm.beast.msghist/src/org/csstudio/alarm/beast/msghist/rdb/MessageRDB.java
@@ -17,9 +17,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
 
-import org.csstudio.alarm.beast.msghist.Activator;
 import org.csstudio.alarm.beast.msghist.Messages;
 import org.csstudio.alarm.beast.msghist.model.Message;
 import org.csstudio.alarm.beast.msghist.model.MessagePropertyFilter;
@@ -30,7 +28,6 @@ import org.eclipse.osgi.util.NLS;
 
 /** Helper for accessing the CSS message RDB.
  *  @author Kay Kasemir
- *  @author Borut Terpinc
  */
 @SuppressWarnings("nls")
 public class MessageRDB
@@ -69,14 +66,17 @@ public class MessageRDB
      *  @param start Start time
      *  @param end End time
      *  @param filters Filters to use (not <code>null</code>).
-     *  @param max_messages Limit on the number of messages retrieved.
+     *  @param max_properties Limit on the number of properties(!) retrieved.
+     *                        Unclear how many properties to expect per message,
+     *                        so this is a vague overload throttle.
      *  @return Array of Messages or <code>null</code>
      */
     public Message[] getMessages(
             final IProgressMonitor monitor,
             final Calendar start, final Calendar end,
             final MessagePropertyFilter filters[],
-            final int max_messages, final DateTimeFormatter date_format) throws Exception
+            final int max_properties,
+            final DateTimeFormatter date_format)  throws Exception
     {
         monitor.beginTask("Reading Messages", IProgressMonitor.UNKNOWN);
         final ArrayList<Message> messages = new ArrayList<Message>();
@@ -88,25 +88,17 @@ public class MessageRDB
                 connection.prepareStatement(sql_txt);
         try
         {
-            StringBuffer spLog= new StringBuffer();
-
             int parm = 1;
             // Set start/end
             statement.setTimestamp(parm++, new Timestamp(start.getTimeInMillis()));
-            spLog.append(System.lineSeparator()).append(parm).append(" - ").append(new Timestamp(start.getTimeInMillis()));
             statement.setTimestamp(parm++, new Timestamp(end.getTimeInMillis()));
-            spLog.append(System.lineSeparator()).append(parm).append(" - ").append(new Timestamp(end.getTimeInMillis()));
-
             // Set filter parameters
-            for (MessagePropertyFilter filter : filters){
+            for (MessagePropertyFilter filter : filters)
                 statement.setString(parm++, filter.getPattern());
-                spLog.append(System.lineSeparator()).append(parm).append(" - ").append(filter.getPattern());
-            }
-            // Set query limit a bit higher than max_messages.
-            // This still limits the number of messages on the RDB side,
+            // Set query limit a bit higher than max_properties.
+            // This still limits the number of properties on the RDB side,
             // but allows the following code to detect exhausting the limit.
-            statement.setInt(parm++, max_messages+1);
-            spLog.append(System.lineSeparator()).append(parm).append(" - ").append(max_messages+1);
+            statement.setInt(parm++, max_properties+10);
 
             // One benchmark example:
             // Query took <<1 second, but reading all the messages took ~30.
@@ -114,10 +106,6 @@ public class MessageRDB
             // i.e. commenting the 'next' loop body.
             // So the local lookup of properties and packing
             // into a HashMap adds almost nothing to the overall time.
-
-            Activator.getLogger().log(Level.FINEST, () -> "Message history sql parameters:" + spLog.toString());
-            Activator.getLogger().log(Level.FINEST, () -> "Message history sql query:" + sql_txt.toString());
-
             final ResultSet result = statement.executeQuery();
             int sequence = 0;
             // Initialize id and datum as "no current message"
@@ -126,7 +114,7 @@ public class MessageRDB
             Date last_datum = null;
             Message last_message = null;
             Map<String, String> props = null;
-            int msg_count = 0;
+            int prop_count = 0;
             while (!monitor.isCanceled()  &&  result.next())
             {
                 // Fixed ID and DATUM
@@ -148,7 +136,6 @@ public class MessageRDB
                         final int count = messages.size();
                         if (count % 50 == 0)
                             monitor.subTask(count + " messages...");
-                        ++msg_count;
                     }
                     // Construct new message and values
                     props = new HashMap<String, String>();
@@ -164,6 +151,7 @@ public class MessageRDB
                 final String prop = sql.getPropertyNameById(result.getInt(res_idx++));
                 final String value = result.getString(res_idx);
                 props.put(prop, value);
+                ++prop_count;
             }
             // No more results.
             // Was another (partial) message assembled?
@@ -176,14 +164,14 @@ public class MessageRDB
                     last_message.setDelta(last_datum, datum);
             }
 
-            // Was readout stopped because we reached max. number of messages?
-            if (msg_count >= max_messages)
+            // Was readout stopped because we reached max. number of properties?
+            if (prop_count >= max_properties)
             {
                 props = new HashMap<String, String>();
                 props.put(Message.TYPE, "internal");
                 props.put(Message.SEVERITY, "FATAL");
                 props.put("TEXT",
-                        NLS.bind(Messages.ReachedMaxMessagesFmt, max_messages));
+                        NLS.bind(Messages.ReachedMaxPropertiesFmt, max_properties));
                 // Add this message both as the first and last messages,
                 // so user is more likely to see it.
                 // A dialog box is even harder to miss,
