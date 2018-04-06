e1a89a55e7b74e66731c9b1c027580478bd15de2
Eric Berryman <eric.berryman@gmail.com>
2018-04-06 10:25:34 -0400

    update to Eclipse 4.7 Oxygen

--- build/clean-and-build-no-tests.sh
+++ build/clean-and-build-no-tests.sh
@@ -54,23 +54,11 @@ echo "==== BUILDING cs-studio/applications"
 echo "===="
 (time mvn $MVNOPT -f cs-studio/applications/pom.xml --settings maven/settings.xml clean verify) | tee 4_cs-studio-applications.log
 
-#echo ""
-#echo "===="
-#echo "==== BUILDING org.csstudio.display.builder"
-#echo "===="
-#(cd org.csstudio.display.builder; time mvn $MVNOPT --settings ../maven/settings.xml -Dcss_repo=file:/Users/claudiorosati/Projects/GitHub/ess-css-#extra/ess_css_comp_repo clean verify) | tee 5_org.csstudio.display.builder.log
-
 echo ""
 echo "===="
 echo "==== BUILDING org.csstudio.product"
 echo "===="
 (time mvn $MVNOPT -f org.csstudio.product/pom.xml --settings maven/settings.xml clean verify) | tee 6_org.csstudio.product.log
-
-#echo ""
-#echo "===="
-#echo "==== BUILDING org.csstudio.ess.product"
-#echo "===="
-#(cd org.csstudio.ess.product; time mvn $MVNOPT --settings ../maven/settings.xml clean verify) | tee 7_org.csstudio.ess.product.log
 
 echo ""
 tail ?_*.log
