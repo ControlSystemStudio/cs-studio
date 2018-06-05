643f78af557fb14b9a3e2e29a4a7501d433718cf
Kunal Shroff <kunalshroff9@gmail.com>
2018-06-05 16:32:09 -0400

    #2437 2 example commands for launching phoebus applications.

---
+++ applications/phoebus/phoebus-plugins/org.csstudio.phoebus.integration/src/org/csstudio/phoebus/integration/actions/Messages.java
@@ -0,0 +1,33 @@
+package org.csstudio.phoebus.integration.actions;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+
+    /**
+     * The bundle name of the localization messages ressources.
+     */
+    private static final String BUNDLE_NAME = "org.csstudio.phoebus.integration.actions.messages"; //$NON-NLS-1$
+
+    public static String MultipleInstancesFmt;
+    public static String MultipleInstancesTitle;
+
+    // Probe messages
+    public static String Probe;
+    public static String ProbeErrorOpenProbe;
+    public static String ProbeLabel;
+
+    // Pvtree messages
+    public static String PVTree;
+    public static String PVTreeErrorOpenPvtree;
+    public static String PVTreeLabel;
+
+
+    static {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages() {
+        /* prevent instantiation */ }
+}
