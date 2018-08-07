3dac96510543f48f3ef028fa9aa4fb67931d8f89
Sky French <sky.french@diamond.ac.uk>
2018-08-07 12:43:52 +0100

    WIP: replacing PV selection Combo with MultiSelectCombo to allow the selection of multiple PVs in waveform plot; beginnings of modifications to WaveformView to handle multiple PV traces

---
+++ applications/databrowser/databrowser-plugins/org.csstudio.trends.databrowser2/src/org/csstudio/trends/databrowser2/multiselectcombo/MultiSelectCombo.java
@@ -0,0 +1,155 @@
+package org.csstudio.trends.databrowser2.multiselectcombo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.events.ShellAdapter;
+import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+
+
+/** An implementation of a Combo box with multi-select capabilities
+ * 
+ */
+
+public class MultiSelectCombo extends Composite {
+
+    Shell selectionShell;
+    Text selectionSummary;
+    String[] allItems;
+    List<Integer> selectedIndices;
+    
+    private Button[] itemButtons;
+    private SelectionAdapter selectionAdapter;
+    
+    public MultiSelectCombo(Composite parent, String[] items, int style) {
+        super(parent, style);
+        
+        allItems = items;
+        
+        setLayout(new GridLayout());
+        
+        selectedIndices = new ArrayList<Integer>();
+        
+        selectionSummary = new Text(this, SWT.READ_ONLY);
+        selectionSummary.setText("Select Item(s) for Waveform inspection ...");
+        selectionSummary.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseDown(MouseEvent event) {
+                super.mouseDown(event);
+                showSelectionShell();
+            }
+            
+        });
+        
+    }
+    
+    public void setItems(String[] names) {
+        
+        allItems = names;
+        //showSelectionShell();
+        
+        /*
+        final int numItems = names.length;
+        itemButtons = new Button[numItems];
+        for (int i = 0; i < numItems; i++) {
+            Button button = new Button(selectionShell, SWT.CHECK);
+            button.setText(names[i]);
+            if (selectedIndices.contains(i))
+                button.setSelection(true);
+            button.pack();
+            itemButtons[i] = button;
+            
+        }
+        */
+        //selectionList = new List(selectionShell, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+        //for (String item : allItems)
+         //   selectionList.add(item);
+        //selectionList.setSelection(selectedIndices);
+       //.setLayoutData(new GridData(GridData.FILL_BOTH))
+
+    }
+    
+    private void showSelectionShell() {
+        
+        Point p = selectionSummary.getParent().toDisplay(selectionSummary.getLocation());
+        Point size = selectionSummary.getSize();
+        Rectangle shellRect = new Rectangle(p.x, p.y + size.y, size.x, 0);
+
+        final int numItems = allItems.length;
+
+        selectionShell = new Shell(Display.getDefault(), SWT.NO_TRIM);
+        selectionShell.setLayout(new GridLayout());
+        this.setLayoutData(new GridData());
+        selectionShell.setSize(shellRect.width, 30*numItems);
+        selectionShell.setLocation(shellRect.x, shellRect.y);
+        
+        itemButtons = new Button[numItems];
+        for (int i = 0; i < numItems; i++) {
+            Button button = new Button(selectionShell, SWT.CHECK);
+            button.setText(allItems[i]);
+            if (selectedIndices.contains(i))
+                button.setSelection(true);
+            button.pack();
+            itemButtons[i] = button;
+            itemButtons[i].addSelectionListener(selectionAdapter);            
+        }
+        //selectionList = new List(selectionShell, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+        //for (String item : allItems)
+         //   selectionList.add(item);
+        //selectionList.setSelection(selectedIndices);
+       //.setLayoutData(new GridData(GridData.FILL_BOTH))
+        
+        selectionShell.open();
+
+       
+        
+        selectionShell.addShellListener(new ShellAdapter() {
+            public void shellDeactivated(ShellEvent event) {
+                if (selectionShell != null && !selectionShell.isDisposed()) {
+                   selectedIndices.clear();
+                   for (int i = 0; i < numItems; i++) {
+                       if (itemButtons[i].getSelection())
+                           selectedIndices.add(i);
+                   }
+                   //selectedIndices = selectionList.getSelectionIndices();
+                   selectionShell.dispose();
+               }
+            }
+            
+        });
+        
+    }
+    
+    public List<Integer> getSelectedIndices() {
+        selectedIndices.clear();
+        for (int i = 0; i < itemButtons.length; i++) {
+            if (itemButtons[i].getSelection())
+                selectedIndices.add(i);
+        }
+        return selectedIndices;
+    }
+
+    public void addSelectionListener(SelectionAdapter selAdapter) {
+        selectionAdapter = selAdapter;        
+    }
+    
+
+}
