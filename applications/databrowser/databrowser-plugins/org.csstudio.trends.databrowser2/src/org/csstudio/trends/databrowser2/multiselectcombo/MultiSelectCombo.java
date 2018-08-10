da3c27e96c7797cfaa5d79b9b349813fdc7a6461
Sky French <sky.french@diamond.ac.uk>
2018-08-10 10:14:58 +0100

    style improvements for MultiSelectCombo

--- applications/databrowser/databrowser-plugins/org.csstudio.trends.databrowser2/src/org/csstudio/trends/databrowser2/multiselectcombo/MultiSelectCombo.java
+++ applications/databrowser/databrowser-plugins/org.csstudio.trends.databrowser2/src/org/csstudio/trends/databrowser2/multiselectcombo/MultiSelectCombo.java
@@ -11,13 +11,12 @@ import org.eclipse.swt.events.ShellAdapter;
 import org.eclipse.swt.events.ShellEvent;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 
 
 /** An implementation of a Combo box for the selection of multiple items
@@ -27,7 +26,8 @@ import org.eclipse.swt.widgets.Text;
 public class MultiSelectCombo extends Composite {
 
     Shell selectionShell;
-    Text selectionSummary;
+    Button selectionSummary;
+    Label selectionLabel;
     String[] allItems;
     List<Integer> selectedIndices;
 
@@ -39,9 +39,12 @@ public class MultiSelectCombo extends Composite {
 
         allItems = items;
         selectedIndices = new ArrayList<Integer>();
-        setLayout(new GridLayout());
-        selectionSummary = new Text(this, SWT.READ_ONLY);
-        selectionSummary.setText("Select Item(s) for Waveform inspection ...");
+        GridLayout grdLyt = new GridLayout();
+        setLayout(grdLyt);
+        grdLyt.numColumns = 2;
+        selectionLabel = new Label(this, SWT.READ_ONLY);
+        selectionLabel.setText("Select Item(s)");
+        selectionSummary = new Button(this, SWT.ARROW | SWT.DOWN);
         selectionSummary.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseDown(MouseEvent event) {
@@ -60,15 +63,13 @@ public class MultiSelectCombo extends Composite {
     private void showSelectionShell() {
 
         Point p = selectionSummary.getParent().toDisplay(selectionSummary.getLocation());
-        Point size = selectionSummary.getSize();
+        Point size = selectionLabel.getSize();
         Rectangle shellRect = new Rectangle(p.x, p.y + size.y, size.x, 0);
 
         final int numItems = allItems.length;
         selectionShell = new Shell(Display.getDefault(), SWT.NO_TRIM);
         selectionShell.setLayout(new GridLayout());
-        this.setLayoutData(new GridData());
-        selectionShell.setSize(shellRect.width, 30*numItems);
-        selectionShell.setLocation(shellRect.x, shellRect.y);
+        selectionShell.setLocation(shellRect.x, shellRect.y + 10);
         itemButtons = new Button[numItems];
         for (int i = 0; i < numItems; i++) {
             Button button = new Button(selectionShell, SWT.CHECK);
@@ -79,7 +80,8 @@ public class MultiSelectCombo extends Composite {
             itemButtons[i] = button;
             itemButtons[i].addSelectionListener(selectionAdapter);
         }
-
+        selectionShell.layout(true, true);
+        selectionShell.setSize(selectionShell.computeSize(SWT.DEFAULT, SWT.DEFAULT, true));
         selectionShell.addShellListener(new ShellAdapter() {
             @Override
             public void shellDeactivated(ShellEvent event) {
