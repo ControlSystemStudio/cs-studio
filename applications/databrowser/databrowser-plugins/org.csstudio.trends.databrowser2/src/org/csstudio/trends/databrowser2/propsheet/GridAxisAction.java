e780c305932db64d686f2a0efd6725c010f998f5
Sky French <sky.french@diamond.ac.uk>
2018-08-10 14:38:10 +0100

    code refactor; add grid lines option

--- applications/databrowser/databrowser-plugins/org.csstudio.trends.databrowser2/src/org/csstudio/trends/databrowser2/propsheet/GridAxisAction.java
+++ applications/databrowser/databrowser-plugins/org.csstudio.trends.databrowser2/src/org/csstudio/trends/databrowser2/propsheet/GridAxisAction.java
@@ -3,23 +3,23 @@ package org.csstudio.trends.databrowser2.propsheet;
 import org.csstudio.trends.databrowser2.Messages;
 import org.csstudio.trends.databrowser2.model.Model;
 
-public class AutoscaleAxisAction extends CheckableAxisAction
+public class GridAxisAction extends CheckableAxisAction
 {
 
-    public AutoscaleAxisAction(final Model model,
+    public GridAxisAction(final Model model,
             final Integer axis_index)
     {
-        super(Messages.AutoScale, model,axis_index);
+        super(Messages.GridTT, model,axis_index);
     }
 
     @Override
     protected Boolean getAxisState() {
-        return axis_config.isAutoScale();
+        return axis_config.isGridVisible();
     }
 
     @Override
     protected void setAxisState(final Boolean state) {
-        axis_config.setAutoScale(state);
+        axis_config.setGridVisible(state);
     }
 
 
