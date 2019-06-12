51f0ce95a5444df8e854ed224535b137cf86adfc
Will Rogers <will.rogers@diamond.ac.uk>
2019-06-12 09:23:42 +0100

    Attempt to fix tests that use JAXB after JDK-11 changes.
    These copy the changes in the rest of cs-studio, but the test still
    fails to find JAXB-API.

--- applications/logbook/logbook-plugins/org.csstudio.logbook.olog.property.fault.test/src/org/csstudio/logbook/olog/property/fault/FaultConfigurationParsingTest.java
+++ applications/logbook/logbook-plugins/org.csstudio.logbook.olog.property.fault.test/src/org/csstudio/logbook/olog/property/fault/FaultConfigurationParsingTest.java
@@ -14,23 +14,32 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
 
 import org.csstudio.logbook.olog.property.fault.FaultConfiguration.Group;
+import org.csstudio.logbook.olog.property.fault.FaultConfigurationFactory;
 import org.junit.Test;
 
 public class FaultConfigurationParsingTest {
 
     @Test
     public void parserTest() throws JAXBException {
+        final ClassLoader orig = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(JAXBContext.class.getClassLoader());
 
-        JAXBContext context = JAXBContext.newInstance(FaultConfiguration.class);
-        Unmarshaller um = context.createUnmarshaller();
-        JAXBElement<FaultConfiguration> fc2 = um.unmarshal(new StreamSource(new File("resources/default_fault_config.xml")),
-                FaultConfiguration.class);
-        FaultConfiguration fc = fc2.getValue();
-        assertEquals("areas not equal", fc.getAreas(), Arrays.asList("Global", "Linac", "BR", "SR"));
-        assertEquals("subsystems not equal", fc.getSubsystems(), Arrays.asList("Diagnostics", "EPS", "RF", "Controls"));
-        assertEquals("devices not equal", fc.getDevices(), Arrays.asList("M01", "M02", "M03"));
-        List<Group> testGroups = Arrays.asList(new Group("Controls", "Kunal Shroff", "shroffk@bnl.gov"),
-                new Group("Operations", "Reid Smith", "smithr@bnl.gov"));
-        assertTrue("groups not equal", fc.getGroups().containsAll(testGroups));
+        try {
+	        JAXBContext context = JAXBContext.newInstance(FaultConfiguration.class.getPackageName(),
+	                FaultConfigurationFactory.class.getClassLoader());
+	        Unmarshaller um = context.createUnmarshaller();
+	        JAXBElement<FaultConfiguration> fc2 = um.unmarshal(new StreamSource(new File("resources/default_fault_config.xml")),
+	                FaultConfiguration.class);
+	        FaultConfiguration fc = fc2.getValue();
+	        assertEquals("areas not equal", fc.getAreas(), Arrays.asList("Global", "Linac", "BR", "SR"));
+	        assertEquals("subsystems not equal", fc.getSubsystems(), Arrays.asList("Diagnostics", "EPS", "RF", "Controls"));
+	        assertEquals("devices not equal", fc.getDevices(), Arrays.asList("M01", "M02", "M03"));
+	        List<Group> testGroups = Arrays.asList(new Group("Controls", "Kunal Shroff", "shroffk@bnl.gov"),
+	                new Group("Operations", "Reid Smith", "smithr@bnl.gov"));
+	        assertTrue("groups not equal", fc.getGroups().containsAll(testGroups));
+        } finally {
+            Thread.currentThread().setContextClassLoader(orig);
+        }
     }
+
 }
