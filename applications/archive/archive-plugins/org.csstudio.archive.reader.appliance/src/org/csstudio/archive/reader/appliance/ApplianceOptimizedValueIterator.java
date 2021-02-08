928e006ed98adbf527a30a7da45eae60903a6fcb
Rebecca Williams <rjw@observatorysciences.co.uk>
2021-02-08 15:46:56 +0000

    Adding preference to use the optimized with last sample postprocessing option from the AA.

--- applications/archive/archive-plugins/org.csstudio.archive.reader.appliance/src/org/csstudio/archive/reader/appliance/ApplianceOptimizedValueIterator.java
+++ applications/archive/archive-plugins/org.csstudio.archive.reader.appliance/src/org/csstudio/archive/reader/appliance/ApplianceOptimizedValueIterator.java
@@ -65,8 +65,15 @@ public class ApplianceOptimizedValueIterator extends ApplianceValueIterator {
      */
     @Override
     protected void fetchDataInternal(String pvName) throws ArchiverApplianceException {
-        String optimized = new StringBuilder().append(ApplianceArchiveReaderConstants.OP_OPTIMIZED)
+        final IPreferencesService prefs = Platform.getPreferencesService();
+        Boolean ppOptimizedWithLastSample = prefs.getBoolean(Activator.PLUGIN_ID, "ppOptimizedWithLastSample", false, null);
+        String optimized;
+        if (ppOptimizedWithLastSample)
+            optimized = new StringBuilder().append(ApplianceArchiveReaderConstants.OP_OPTIMIZED_WITH_LAST_SAMPLE)
                 .append(requestedPoints).append('(').append(pvName).append(')').toString();
+        else
+            optimized = new StringBuilder().append(ApplianceArchiveReaderConstants.OP_OPTIMIZED)
+            .append(requestedPoints).append('(').append(pvName).append(')').toString();
         super.fetchDataInternal(optimized);
     }
 
