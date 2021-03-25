a0d6eeb3e030ece255572f7270a85c810266249e
Rebecca Williams <rjw@observatorysciences.co.uk>
2021-03-25 09:44:30 +0000

    Reverting previous change and instead removed the caching of ArchiveApplianceReaders.

--- applications/archive/archive-plugins/org.csstudio.archive.reader.appliance/src/org/csstudio/archive/reader/appliance/ApplianceArchiveReaderFactory.java
+++ applications/archive/archive-plugins/org.csstudio.archive.reader.appliance/src/org/csstudio/archive/reader/appliance/ApplianceArchiveReaderFactory.java
@@ -1,6 +1,5 @@
 package org.csstudio.archive.reader.appliance;
 
-import java.util.concurrent.ConcurrentHashMap;
 import org.csstudio.archive.reader.ArchiveReader;
 import org.csstudio.archive.reader.ArchiveReaderFactory;
 
@@ -14,21 +13,10 @@ public class ApplianceArchiveReaderFactory implements ArchiveReaderFactory{
     /* (non-Javadoc)
      * @see org.csstudio.archive.reader.ArchiveReaderFactory#getArchiveReader(java.lang.String)
      */
-    final private static ConcurrentHashMap<String, ApplianceArchiveReader> cache = new ConcurrentHashMap<String, ApplianceArchiveReader>();
-
     @Override
     public ArchiveReader getArchiveReader(String url) throws Exception {
-
-        ApplianceArchiveReader result = cache.get(url);
-        if( result == null ) {
-            final ApplianceArchiveReader reader = new ApplianceArchiveReader(url,
-                    Activator.getDefault().isUseStatistics(), Activator.getDefault().isUseNewOptimizedOperator());
-            result = cache.putIfAbsent(url, reader);
-          if( result == null ) {
-            result = reader;
-          }
-        }
-        return result;
+	    final ApplianceArchiveReader reader = new ApplianceArchiveReader(url, Activator.getDefault().isUseStatistics(),
+		    Activator.getDefault().isUseNewOptimizedOperator());
+	    return reader;
     }
-
 }
